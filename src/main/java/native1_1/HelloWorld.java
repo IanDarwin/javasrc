85915710f3b4cfd4ca23fe342977d2d54591e1e5
Ian Darwin <ian@darwinsys.com>
1997-07-06 21:37:26 +0000

    Initial revision

---
+++ src/main/java/native1_1/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * A trivial class to show Java Native Interface 1.1 usage from Java.
+ */
+public class HelloWorld {
+	int myNumber = 42;        // used to show argument passing
+
+	// declare native class
+	public native void displayHelloWorld();
+
+	// Application main, call its display method
+	public static void main(String args[]) {
+		System.out.println("HelloWorld starting...");
+		HelloWorld hw = new HelloWorld();
+		hw.displayHelloWorld();		// call the native function
+		System.out.println("Back in Java, \"myNumber\" now " + hw.myNumber);
+	}
+
+	// Static code blocks are executed once, when class file is loaded
+	static {
+		System.load("libhello.so");
+	}
+}
