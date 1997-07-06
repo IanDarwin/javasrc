22072e0ebe9bf97c2896b65b5b1cc6c1bec9a0e1
Ian Darwin <ian@darwinsys.com>
1997-07-06 18:51:03 +0000

    Initial revision

---
+++ src/main/java/native1_1/javafromc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <jni.h>
+	
+int
+main(int argc, char *argv[])
+{
+	JavaVM *jvm;       /* The Java VM we will use */
+	JNIEnv *myEnv;       /* pointer to native environment */
+	JDK1_1InitArgs jvmArgs; /* JNI initialization arguments */
+	jclass myClass;		/* pointer to the class type */
+	jmethodID myMethod;		/* pointer to the main() method */
+	
+	JNI_GetDefaultJavaVMInitArgs(&jvmArgs);	/* set up the argument pointer */
+	/* Could change values now, like: jvmArgs.classpath = ...; */
+	
+	/* initialize the JVM! */
+	if (JNI_CreateJavaVM(&jvm, &myEnv, &jvmArgs) < 0) {
+		fprintf(stderr, "CreateJVM failed\n");
+		exit(1);
+	}
+	
+	/* find the class named in argv[1] */
+	myClass = (*myEnv)->FindClass(myEnv, argv[1]);
+	/* find the static void main(String[])  method of that class */
+	myMethod = (*myEnv)->GetStaticMethodID(myEnv, myClass, "test", "(I)V");
+	/* finally, call the method */
+	(*myEnv)->CallStaticVoidMethod(myEnv, myClass, myMethod, 100);
+	
+	(*jvm)->DestroyJavaVM(jvm);	/* no error checking as we're done anyhow */
+}
