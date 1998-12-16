e37db570ae6a7c793e1f35660833972f3d70eb55
Ian Darwin <ian@darwinsys.com>
1998-12-16 17:07:23 +0000

    Massive set of changes made for JDK1.2, including Swing Renaming and change the UI so that Users is just 1 of 5 equal modules (with Cal, TODO, Mail & JP). New JDM module switch. Zillions of minor changes throughout.

--- src/main/java/email/MailReaderBean.java
+++ src/main/java/email/MailReaderBean.java
@@ -2,9 +2,9 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
-import com.sun.java.swing.*;
-import com.sun.java.swing.border.*;
-import com.sun.java.swing.tree.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.tree.*;
 
 /**
   * Display a mailbox, initially faked.
