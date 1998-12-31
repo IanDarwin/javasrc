b441b887cbc584b1cf5fc914df3c3fca198893d3
Ian Darwin <ian@darwinsys.com>
1998-12-31 22:14:49 +0000

    Remove dead files; add some new ones. Duh.

---
+++ src/main/java/lang/ConstrException.java
@@ -0,0 +1,9 @@
+// Just to show that constructors are allowed to throw exceptions
+
+public class ConstrException {
+	public ConstrException(int i) {
+		if (i < 0)
+			throw new IllegalArgumentException("value " + i +
+				"invalid, must be non-negative");
+	}
+}
---
+++ src/main/java/structure/LinkedListDemo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * Demo of 1.2 java.util.LinkedList class
+ * using same example as my older LinkList class.
+ * @author	Ian Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class LinkedListDemo {
+	public static void main(String argv[]) {
+		System.out.println("Here is a demo of Java 1.2's LinkedList class");
+		LinkedList l = new LinkedList();
+		l.add(new Object());
+		l.add("Hello");
+
+		System.out.println("Here is a list of all the elements");
+		ListIterator li = l.listIterator(0);
+		while (li.hasNext())
+			System.out.println(li.next());
+
+		if (l.indexOf("Hello") < 0)
+			System.err.println("Lookup does not work");
+		else
+			System.err.println("Lookup works");
+	}
+}
