0a5253ca569495b336d11d93edd6edd9aee3c979
Ian Darwin <ian@darwinsys.com>
1999-01-04 20:40:14 +0000

    Change imports for JDK1.2 javax.swing.*;

--- src/main/java/crssetup/CrsSetup.java
+++ src/main/java/crssetup/CrsSetup.java
@@ -1,6 +1,6 @@
-import com.sun.java.swing.*;
-import com.sun.java.swing.text.*;
-import com.sun.java.swing.border.*;
+import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
