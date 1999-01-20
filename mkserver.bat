b6cafb89069e1934e26124697d6c2e3d5de32b6a
Ian Darwin <ian@darwinsys.com>
1999-01-20 18:17:34 +0000

    Script to make all the .class files needed for the server directory.

---
+++ mkserver.bat
@@ -0,0 +1,14 @@
+rem
+rem Build all the classes needed for the "server" directory
+rem $Id$
+rem
+rem Clock doesn't get -deprecation as it has too many, sigh.
+javac Clock.java
+move Clock.class ..\server
+javac -deprecation TreeLayout.java
+move Tree*.class ..\server
+javac -deprecation URLButton.java
+move URLButton.class ..\server
+javac -deprecation WebServer.java
+move Handler.class ..\server
+move WebServer.class ..\server
