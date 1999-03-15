363c6e2f9b2d5796d08d801b0026e8442535ee9d
Ian Darwin <ian@darwinsys.com>
1999-03-15 19:01:16 +0000

    Simple demo of Observable-Observer pair.

---
+++ src/main/java/patterns/behavioral/ObserverDemo.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/**
+ * A simple demo of Observable->Observer
+ *
+ * @author Ian Darwin
+ * @version $Id$
+ */
+public class ObservDemo extends Object {
+	MyObserver observer;
+	MyObservable model;
+	public ObservDemo() {
+
+		observer = new MyObserver();
+
+		model = new MyObservable(); 
+		model.addObserver(observer);
+
+	}
+
+	public static void main(String av[]) {
+		ObservDemo me = new ObservDemo();
+		me.demo();
+	}
+
+	public void demo() {
+		model.changeSomething();
+	}
+}
+
+/** The Observer normally maintains a view on the data */
+class MyObserver implements Observer {
+	/** For now, we just print the fact that we got notified. */
+	public void update( Observable obs, Object x ) {
+		System.out.println("update(" + obs + "," + x + ");");
+	}
+}
+
+/** The Observable normally maintains the data */
+class MyObservable extends Observable {
+	public void changeSomething() {
+		// Notify observers of change
+		setChanged();
+		notifyObservers();
+	}
+}
