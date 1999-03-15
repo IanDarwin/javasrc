c21c50964345a8a9d73f200429d3578eafc3e677
Ian Darwin <ian@darwinsys.com>
1999-03-15 22:26:51 +0000

    Add files for reading mail/news headers.

---
+++ src/main/java/email/Header.java
@@ -0,0 +1,12 @@
+/** Represent a Mail or News Header */
+public class Header {
+	public String type;
+	public String text;
+	Header(String n, String v) {
+		type = n;
+		text = v;
+	}
+	public String toString() {
+		return new StringBuffer("Header[").append(type).append(",").append(text).append("]").toString();
+	}
+}
---
+++ src/main/java/email/ReadHeaders.java
@@ -0,0 +1,74 @@
+import java.io.*;
+
+/**
+ * Read a file return mail headers one at a time.
+ */
+public class ReadHeaders {
+	protected BufferedReader is;
+
+	/** Construct */
+	public ReadHeaders(BufferedReader f) {
+		is = f;
+	}
+
+	/** Main program showing how to use it */
+    public static void main(String av[]) {
+        switch(av.length) {
+            case 0: 
+				ReadHeaders r = new ReadHeaders(
+					new BufferedReader(
+                        new InputStreamReader(System.in))); 
+				printit(r);
+				break;
+            default:
+				for (int i=0; i<av.length; i++)
+                    try {
+						ReadHeaders r = new ReadHeaders(
+							new BufferedReader(new FileReader(av[i])));
+						printit(r);
+                    } catch (FileNotFoundException e) {
+                        System.err.println(e);
+                    }
+				break;
+        }
+    }
+
+	/** Simple demo showing how to use it */
+	public static void printit(ReadHeaders r) {
+		while (r.more())  {
+			Header h = r.next();
+			System.out.println(h.type + " = " + h.text);
+		}
+	}
+
+	String inputLine;
+
+    /** check to see if more headers available */
+    public boolean more() {
+		if (is == null)
+			return false;
+        try {
+
+            inputLine = is.readLine();
+			if (inputLine == null || inputLine.length() == 0) {
+				is.close();
+				is = null;
+			}
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+			is = null;
+			return false;
+        }
+		return true;
+    }
+
+	public Header next() {
+		int pos = inputLine.indexOf(":");
+		if (pos < 0)
+			return new Header("??", inputLine);
+		return new Header(
+			inputLine.substring(0, pos),
+			inputLine.substring(pos + 1));
+	}
+}
+
