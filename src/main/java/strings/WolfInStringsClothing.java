f996ac36596fa422b624006a6ac239dac95e8d7f
Ian Darwin <ian@darwinsys.com>
1999-03-30 00:08:03 +0000

    Demo of what String subclass could do, if allowed.

---
+++ src/main/java/strings/WolfInStringsClothing.java
@@ -0,0 +1,10 @@
+/**
+ * If this class could be compiled, Java security would be a myth.
+ */
+public class WolfInStringsClothing extends java.lang.String {
+	public void setCharAt(int index, char newChar) {
+		// The implementation of this method
+		// would be left as an exercise for the reader.
+		// Hint: compile this code exactly as-is before bothering!
+	}
+}
