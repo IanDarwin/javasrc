879ac0b7e34b6fd3e69169945760511d474bb30c
Ian Darwin <ian@darwinsys.com>
1999-04-01 03:52:06 +0000

    Simple demo of String.charAt().

---
+++ src/main/java/strings/StrCharAt.java
@@ -0,0 +1,12 @@
+/** StrCharAt - show String.charAt()
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+
+public class StrCharAt {
+    public static void main(String av[]) {
+        String a = "A quick bronze fox lept a lazy bovine";
+		for (int i=0; i < a.length(); i++)
+			System.out.println("Char " + i + " is " + a.charAt(i));
+	}
+}
