1b5093975a903182fda0cc424fde2123743a0bbc
Ian Darwin <ian@darwinsys.com>
1999-04-11 16:44:48 +0000

    Add filter code so it processes stdin to stdout if no files, but does files with renaming for each one if given.

--- src/main/java/email/DropReceivedLines.java
+++ src/main/java/email/DropReceivedLines.java
@@ -10,26 +10,46 @@ public class DropReceivedLines {
         DropReceivedLines d = new DropReceivedLines();
 		// For stdin or each file, build a IndentContLineReader
 		// to treat the Received: lines as one.
-        if (av.length == 0) 
-            d.process(new IndentContLineReader(
-					new InputStreamReader(System.in)));
-		else for (int i=0; i<av.length; i++)
-			try {
-				d.process(new IndentContLineReader(new FileReader(av[i])));
-			} catch (FileNotFoundException e) {
-				System.err.println(e);
-			}
+		try {
+			if (av.length == 0) 
+				d.process(new IndentContLineReader(
+						new InputStreamReader(System.in)), 
+						new PrintWriter(System.out));
+			else for (int i=0; i<av.length; i++)
+				d.process(av[i]);
+		} catch (FileNotFoundException e) {
+			System.err.println(e.getMessage());
+		} catch (IOException e) {
+			System.err.println("I/O error " + e);
+		}
     }
 
+	protected static File tempFile = new File("holding.tmp");
+
+	/** Process one file given only its name */
+	public void process(String fileName) throws IOException {
+		File old = new File(fileName);
+		String newFileName = fileName + ".TMP";
+		File newf = new File(newFileName);
+		IndentContLineReader is =
+				new IndentContLineReader(new FileReader(fileName));
+		PrintWriter p = new PrintWriter(new FileWriter(newFileName));
+		process(is, p);		// call other process(), below
+		p.close();
+		old.renameTo(tempFile);
+		newf.renameTo(old);
+		tempFile.delete();
+	}
+
     /** process one file, given an open LineReader */
-    public void process(LineNumberReader is) {
+    public void process(LineNumberReader is, PrintWriter out) throws IOException {
         try {
             String lin;
 
 			// If line begins with "Received:", ditch it, and its continuations
             while ((lin = is.readLine()) != null) {
 				if (!lin.startsWith("Received:"))
-					System.out.println(lin);
+					out.println(lin);
             }
             is.close();
         } catch (IOException e) {
