739e73cee00a4c4d74252ac4b33db2bfcd5d2de1
Ian Darwin <ian@darwinsys.com>
1999-04-26 17:24:43 +0000

    Initial revision

---
+++ src/main/java/PhoneSim/PhoneSystem.java
@@ -0,0 +1,14 @@
+public class PhoneSystem {
+	public static void main(String a[]) throws InterruptedException {
+		while (true) {
+			final PhoneCall c = new PhoneCall();
+			Thread t = new Thread(new Runnable() {
+				public void run() {
+					c.call();
+				}
+			});
+			t.start();
+			Thread.sleep(1000);
+		}
+	}
+}
