d5144ea877a70879d00639e37094cb1dced18750
Ian Darwin <ian@darwinsys.com>
1999-05-11 20:23:28 +0000

    New entries, for javacook.

---
+++ src/main/java/numbers/RealValues.java
@@ -0,0 +1,12 @@
+import java.text.*;
+/**
+ * Show real values.
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version #Id$
+ */
+public class RealValues {
+	public static void main(String argv[]) {
+		System.out.println("The real 0.3 is " + 0.3);
+		System.out.println("The real 0.3 times 3 is " + 0.3*3);
+	}
+}
---
+++ src/main/java/numbers/StringToDouble.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+public class StringToDouble {
+
+	public static void main(String argv[]) {
+		String aNumber = argv[0];	// not argv[1]
+		double result;
+		try {
+			result = Double.parseDouble(aNumber); 
+		} catch(NumberFormatException exc) {
+			System.out.println("Invalid number " + aNumber);
+			return;
+		}
+		System.out.println("Number is " + result);
+	}
+}
