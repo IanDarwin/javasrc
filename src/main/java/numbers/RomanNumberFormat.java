7d35091944751451aeba3c49f040ab07d6ac42bb
Ian Darwin <ian@darwinsys.com>
1999-05-13 01:28:14 +0000

    Now formats several text cases from number to String correctly.

--- src/main/java/numbers/RomanNumberFormat.java
+++ src/main/java/numbers/RomanNumberFormat.java
@@ -1,59 +1,21 @@
 import java.text.*;
+import java.util.*;
 
 /**
  * Roman Number class. Not localized, since Roman's a Dead Dead Language
  * and we don't display Roman Numbers differently in different Locales.
+ * Filled with quick-n-dirty algorithms.
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
 public class RomanNumberFormat extends Format {
 
-	Hashtable r2a;
-	Hashtable a2r;
-	final int NRN;
-
-	public RomanNumberFormat() {
-		r2a = new Hashtable();
-		r2a.put("M", new Integer(1000));
-		r2a.put("D", new Integer(500));
-		r2a.put("C", new Integer(100));
-		r2a.put("L", new Integer(50));
-		r2a.put("X", new Integer(10));
-		r2a.put("V", new Integer(5));
-		r2a.put("I", new Integer(1));
-		NRN = r2a.size;
-		a2r = new Hashtable();
-		a2r.put(new Integer(1000), "M");
-		a2r.put(new Integer(500), "D");
-		a2r.put(new Integer(100), "C");
-		a2r.put(new Integer(50), "L");
-		a2r.put(new Integer(10), "X");
-		a2r.put(new Integer(5), "V");
-		a2r.put(new Integer(1), "I");
-	}
-
-	public StringBuffer format(Object n, StringBuffer toAppTo, FieldPosition p) {
-		System.out.println("In my format()!");
-		toAppTo.append(n);
-		return toAppTo;
-	}
-
-	/** Format a given long as a Roman Numeral */
-	public String format(long n) {
-		if (n < 0 || n > 4000)
-			throw new IllegalArgumentException(n + " must be from 0 to 4000");
-		StringBuffer sb = new StringBuffer();
-		// FORMATTING
-		String dig = Long.toString(n);
-		for (int i=0; i<dig.length(); i++)
-			char ch = dig.charAt(i);
-			if (n > map[i].n) {
-				sb.append(map[i].c);
-				n /= map[i].n;
-			}
-		}
-		return sb.toString();
-	}
+	static char A2R[][] = {
+			{ 0, 'M' },
+			{ 0, 'C', 'D', 'M' },
+			{ 0, 'X', 'L', 'C' },
+			{ 0, 'I', 'V', 'X' },
+	};
 
 	/** Format a given double as a Roman Numeral; just truncate to a
 	 * long, and call format(long).
@@ -62,17 +24,84 @@ public class RomanNumberFormat extends Format {
 		return format((long)n);
 	}
 
+	/** Format a given long as a Roman Numeral. Just call the
+	 * three-argument form.
+	 */
+	public String format(long n) {
+		if (n < 0 || n >= 4000)
+			throw new IllegalArgumentException(n + " must be >= 0 && < 4000");
+		StringBuffer sb = new StringBuffer();
+		format(new Integer((int)n), sb, new FieldPosition(NumberFormat.INTEGER_FIELD));
+		return sb.toString();
+	}
+
+	/* Format the given Number as a Roman Numeral, returning the
+	 * Stringbuffer (updated), and updating the FieldPosition.
+	 * This method is the REAL FORMATTING ENGINE.
+	 */
+	public StringBuffer format(Object on, StringBuffer sb, FieldPosition fp) {
+		if (!on instanceof Number)
+			throw new IllegalArgumentException(on + " must be a Number object");
+		if (fp.getField() != NumberFormat.INTEGER_FIELD)
+			throw new IllegalArgumentException(fp + " must be FieldPosition(NumberFormat.INTEGER_FIELD");
+		int n = ((Number)on).intValue();
+
+		// First, put the digits on a tiny stack. Must be 4 digits.
+		for (int i=0; i<4; i++) {
+			int d=n%10;
+			push(d);
+			// System.out.println("Pushed " + d);
+			n=n/10;
+		}
+
+		// Now pop and convert.
+		for (int i=0; i<4; i++) {
+			int ch = pop();
+			// System.out.println("Popped " + ch);
+			if (ch==0)
+				continue;
+			else if (ch <= 3) {
+				for(int k=1; k<=ch; k++)
+					sb.append(A2R[i][1]); // I
+			}
+			else if (ch == 4) {
+				sb.append(A2R[i][1]);	// I
+				sb.append(A2R[i][2]);	// V
+			}
+			else if (ch == 5) {
+				sb.append(A2R[i][2]);	// V
+			}
+			else if (ch <= 8) {
+				sb.append(A2R[i][2]);	// V
+				for (int k=6; k<=ch; k++)
+					sb.append(A2R[i][1]);	// I
+			}
+			else { // 9
+				sb.append(A2R[i][1]);
+				sb.append(A2R[i][3]);
+			}
+		}
+		// fp.setBeginIndex(0);
+		// fp.setEndIndex(3);
+		return sb;
+	}
+
 	/** Parse a generic object, returning an Object */
 	public Object parseObject(String what, ParsePosition where) {
 		// TODO PARSING HERE
 		return new Long(0);
 	}
 
-	/** Simple test case */
-	public static void main(String argv[]) {
-		RomanNumberFormat nf = new RomanNumberFormat();
-		System.out.println("Test of " + nf);
-		System.out.println("42->" + nf.format(42));
-		System.out.println("XIV->" + nf.parseObject("XIV", null));
+	/* Implement a toy stack */
+	protected int stack[] = new int[10];
+	protected int depth = 0;
+
+	/* Implement a toy stack */
+	protected void push(int n) {
+		stack[depth++] = n;
+	}
+	/* Implement a toy stack */
+	protected int pop() {
+		return stack[--depth];
 	}
 }
