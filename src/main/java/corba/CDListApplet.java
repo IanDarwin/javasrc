a8356f4ab6ac9300620d090eac953dcdc170cdb9
IanDarwin <>
1999-05-14 15:40:30 +0000

    Import a bunch of shell scripts, and an Applet version of the client (which might not be finished yet).

---
+++ src/main/java/corba/CDListApplet.java
@@ -0,0 +1,43 @@
+import CDService.*;
+import org.omg.CosNaming.*;
+import org.omg.CORBA.*;
+
+/** This is a simple Applet that just lists the CDs and tracks. */
+public class CDListApplet extends java.applet.Applet {
+	public void init() {
+		try {
+			// create and initialize the ORB
+			Debug.println("corba", "Initializing the ORB");
+			ORB orb = ORB.init(this, null);
+
+			// get the root naming context
+			Debug.println("corba", "Getting Root Naming");
+			org.omg.CORBA.Object objRef =
+				orb.resolve_initial_references("NameService");
+			NamingContext ncRef = NamingContextHelper.narrow(objRef);
+
+			// resolve the Object Reference in Naming
+			Debug.println("corba", "Finding CDListIterator Object in Naming");
+			NameComponent nc = new NameComponent("CDList", "");
+			NameComponent path[] = {nc};
+			CDListIterator cdListRef = CDListIteratorHelper.narrow(ncRef.resolve(path));
+
+			// Now get CDs from the server and print them.
+			Debug.println("corba", "Got CDListIterator!");
+			CD aCD;
+			cdListRef.first();
+			while (cdListRef.hasNext()) {
+				Debug.println("corba", "Getting next CD Object.");
+				aCD = cdListRef.next();
+				System.out.println(aCD.title());
+				Track[] tracks = aCD.getTracks();
+				for (int t=0; t<tracks.length; t++) {
+					System.out.println(tracks[t].title);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e) ;
+			e.printStackTrace(System.err);
+		}
+	}
+}
---
+++ src/main/java/corba/orb.bat
@@ -0,0 +1,3 @@
+REM Make sure the name server is running:
+
+start tnameserv -ORBInitialPort 1066
