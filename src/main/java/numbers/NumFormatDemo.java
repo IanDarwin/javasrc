5304bbd2d9599a1b81da45d6ea27b49dccd400a5
Ian Darwin <ian@darwinsys.com>
1999-05-14 15:37:50 +0000

    Rounding demo. NumFormatTest: make more specific to javacook example.

--- src/main/java/numbers/NumFormatDemo.java
+++ src/main/java/numbers/NumFormatDemo.java
@@ -2,47 +2,34 @@ import java.text.*;
 import java.util.*;
 
 /*
- * Program to show examples of NumberFormat at work.
- * Adapted from the NumberFormat javadoc page.
+ * Format a number our way and the default way.
  */
 public class NumFormatTest {
-   /** The main (and only) method in this class. */
-   public static void main(String av[]) { 
-     /** The list of all Locales that NumberFormat has.
-      * Normally we would have a GUI with a menu for this
-      */
-     Locale[] locales = NumberFormat.getAvailableLocales();
-     /** The number we print in many formats */
-     double myNumber = -1234.56;
-     NumberFormat form;
-     // just for fun, we print out a number with the locale number, currency
-     // and percent format for each locale we can.
-     for (int j = 0; j < 3; ++j) {
-         System.out.println("FORMAT GROUP " + j);
-         for (int i = 0; i < locales.length; ++i) {
-             if (locales[i].getCountry().length() == 0) {
-                // skip language-only
-                continue;
-             }
-             System.out.print(locales[i].getDisplayName());
-             switch (j) {
-             default:
-                 form = NumberFormat.getInstance(locales[i]); break;
-             case 1:
-                 form = NumberFormat.getCurrencyInstance(locales[i]); break;
-             case 0:
-                 form = NumberFormat.getPercentInstance(locales[i]); break;
-             }
-             try {
-		 // toPattern() will reveal the combination of #0., etc
-		 // that this particular local uses to format with!
-                 System.out.print(": " + ((DecimalFormat)form).toPattern()
-                              + " -> " + form.format(myNumber));
-             } catch (IllegalArgumentException iae) { }
-             try {
-                 System.out.println(" -> " + form.parse(form.format(myNumber)));
-             } catch (ParseException pe) { }
-         }
-       }
-    }
+	//+
+	/** A number to format */
+	public static final double intlNumber = 1024.25;
+	/** Another number to format */
+	public static final double ourNumber = 100.23456;
+	//-
+
+	/** The main (and only) method in this class. */
+	public static void main(String av[]) { 
+
+		//+
+		NumberFormat defForm = NumberFormat.getInstance();
+		NumberFormat ourForm = new DecimalFormat("##.##");
+		// toPattern() will reveal the combination of #0., etc
+		// that this particular local uses to format with!
+		System.out.println("defForm's pattern is " +
+			((DecimalFormat)defForm).toPattern());
+		System.out.println(defForm.format(intlNumber));
+
+		// We also show using the default format to parse a number
+		String input = "392.01";
+		try {
+			System.out.println(input + " -> " + defForm.parse(input));
+		} catch (ParseException pe) {
+			System.err.println(input + "not parseable!");
+		}
+	}
 }
---
+++ src/main/java/numbers/Round.java
@@ -0,0 +1,24 @@
+/**
+ * Demonstrate our own version round().
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class Round {
+	//+
+	/** We round a number up if its fraction exceeds this threshold. */
+	public static final double THRESHOLD = 0.54;
+	/* Return the closest long to the argument.
+	 * ERROR CHECKING OMITTED.
+	 */
+	static long round(double d) {
+		long di = (long)Math.floor(d);	// integral value below (or ==) d
+		if ((d - di) > THRESHOLD)
+			return di + 1;
+		else return di;
+	}
+	//-
+	public static void main(String argv[]) {
+		for (double d = 0.1; d<=1.0; d+=0.01)
+			System.out.println(d + "-> " + round(d));
+	}
+}
