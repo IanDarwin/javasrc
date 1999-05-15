2af65169808f69a675b1a83a0560081a31b000e0
Ian Darwin <ian@darwinsys.com>
1999-05-15 02:01:41 +0000

    Use the LogBase program

---
+++ src/main/java/numbers/LogBaseUse.java
@@ -0,0 +1,13 @@
+/**
+ * Log to arbitrary base
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class LogBaseUse {
+	//+
+	public static void main(String argv[]) {
+		double d = LogBase.log_base(10, 10000);
+		System.out.println("log10(10000) = " + d);
+	}
+	//-
+}
