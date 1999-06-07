fca02fd6f256a2073d9193e506f44086d41eba42
Ian Darwin <ian@darwinsys.com>
1999-06-07 12:54:12 +0000

    omments

--- src/main/java/strings/StringBufferDemo.java
+++ src/main/java/strings/StringBufferDemo.java
@@ -7,16 +7,25 @@ public class StringBufferDemo {
 		String s1 = "Hello" + ", " + "World";
 		System.out.println(s1);
 
+		// Build a StringBuffer, and append some things to it.
 		StringBuffer sb2 = new StringBuffer();
 		sb2.append("Hello");
 		sb2.append(',');
 		sb2.append(' ');
 		sb2.append("World");
+
+		// Get the StringBuffer's value as a String, and print it.
 		String s2 = sb2.toString();
 		System.out.println(s2);
+
+		// Now do the above all over again, but in a more 
+		// concise (and typical "real-world" Java) fashion.
 
 		StringBuffer sb3 = new StringBuffer().append("Hello").
 			append(',').append(' ').append("World");
 		System.out.println(sb3.toString());
+
+		// Exercise for the reader: do it all AGAIN but without
+		// creating any temporary variables.
 	}
 }
