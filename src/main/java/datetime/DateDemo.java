1c4cb9482a21460fa0df5105af654caf8cb24d5a
Ian Darwin <ian@darwinsys.com>
1999-07-18 16:28:26 +0000

    Writing the date/time chapter.

--- src/main/java/datetime/DateDemo.java
+++ src/main/java/datetime/DateDemo.java
@@ -4,19 +4,14 @@ import java.util.*;
 /** Show some date uses */
 public class DateDemo {
     public static void main(String[] args) {
+		//+
 		Date dNow = new Date();
 
 		/* Simple, Java 1.0 date printing */
 		System.out.println("It is now " + dNow.toString());
 
-		/* More correct Java 1.1 GregorianCalendar date handling */
-		GregorianCalendar gcNow = new GregorianCalendar();
-		int year = gcNow.get(Calendar.YEAR);
-		System.out.println("This is year " + year + " Anno Domini");
 
-		/* Fancier "DateFormat" using "simple"(?) format.
-		 * See the javadoc for java.text.SimpleDateFormat.
-		 */
+		// Use a SimpleDateFormat to print the date our way.
 		SimpleDateFormat formatter
 			= new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
 		System.out.println("It is " + formatter.format(dNow));
--- src/main/java/datetime/DatePrint2.java
+++ src/main/java/datetime/DatePrint2.java
@@ -1,6 +1,7 @@
 import java.util.*;
 /**
- * Show dates before 1970.
+ * Show dates before 1970, in JDK1.0/1.1 when Date was broken
+ * DO NOT USE ANYMORE -- use DateFormat instead!!.
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
