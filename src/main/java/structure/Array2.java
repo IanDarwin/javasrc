f4199b6967df4321ee6733fae5da5176c94129a0
Ian Darwin <ian@darwinsys.com>
1999-07-24 17:52:19 +0000

    Err, finish it.

--- src/main/java/structure/Array2.java
+++ src/main/java/structure/Array2.java
@@ -1,14 +1,16 @@
+import java.util.*;
+
 /** Re-allocate an array, bigger...
  * @author Ian Darwin
  * @version $Id$
  */
-public class ArrayDemo  {
+public class Array2  {
 	public static void main(String argv[]) {
 		int nDates = 0;
 		final int MAX = 10;
 		Calendar dates[] = new Calendar[MAX];
 		Calendar c;
-		while ((c=getDate()) != null)
+		while ((c=getDate()) != null) {
 
 			// if (nDates >= dates.length) {
 			// 	System.err.println("Too Many Dates! Simplify your life!!");
@@ -18,10 +20,22 @@ public class ArrayDemo  {
 			// better: reallocate, making data structure dynamic
 			if (nDates >= dates.length) {
 				Calendar tmp[] = new Calendar[dates.length + 10];
-				System.arraycopy(dates, tmp)
+				System.arraycopy(dates, 0, tmp, 0, dates.length);
 				dates = tmp;    // copies the array reference
 				// old array will be garbage collected soon...
 			}
 			dates[nDates++] = c;
 		}
+		System.out.println("Array size = " + dates.length);
 	}
+
+	static int n;
+	/* Dummy method to return a sequence of 21 Calendar references,
+	 * so the array should be sized >= 21.
+	 */
+	public static Calendar getDate() {
+		if (n++ > 21)
+			return null;
+		return Calendar.getInstance();
+	}
+}
