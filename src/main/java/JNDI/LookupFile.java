e5e121ec296add9825c6766140b934821b76020d
Ian Darwin <ian@darwinsys.com>
1999-07-31 17:08:31 +0000

    Initial revision

---
+++ src/main/java/JNDI/LookupFile.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import javax.naming.*;
+
+/** Example of lookup up filenames in a JNDI "file system context".
+ * @version $Id$
+ */
+public class LookupFile {
+	Hashtable env = new Hashtable();
+
+	public static void main(String av[]) {
+		LookupFile lf = new LookupFile();
+		for (int i=0; i<av.length; i++)
+			System.out.println(av[i] + "-->" + lf.look(av[i]));
+	}
+
+	/** The service provider */
+	final static String SP = "com.sun.jndi.fscontext.RefFSContextFactory";
+
+	/** The naming context */
+	Context ctx = null;
+
+	/** Constructor */
+	public LookupFile() {
+		env.put(Context.INITIAL_CONTEXT_FACTORY, SP);
+
+		try {
+			// Create the initial context
+			ctx = new InitialContext(env);
+		} catch (NamingException e) {
+			System.err.println("Problem Creating Context with " + SP);
+		}
+	}
+
+	/** Look up an object in the filesystem. */
+	protected String look(String name) {
+		try {
+			Object obj = ctx.lookup(name);
+
+			/* Got something? Return it. */
+			return obj.toString();
+
+		} catch (NamingException e) {
+			System.err.println("Problem looking up " + name + ": " + e);
+			return null;
+		}
+	}
+}
---
+++ src/main/java/performance/TimeSort.java
@@ -0,0 +1,25 @@
+/**
+ * Time a bunch of sorting.
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class TimeSort {
+	//+
+	public static void main(String argv[]) {
+		final int N = 1024 * 1024;
+		java.util.Random r = new java.util.Random();
+		long t0 = System.currentTimeMillis();
+		int data[] = new int[N];
+		for  (int i=0; i<N; i++)
+			data[i] = r.nextInt();
+		long t1 = System.currentTimeMillis();
+		java.util.Arrays.sort(data);
+		long t2 = System.currentTimeMillis();
+		long randTime = t1 - t0;
+		long sortTime = t2 - t1;
+		System.out.println(
+			 "randTime="  + QuickTimeFormat.msToSecs(randTime) + 
+			", sortTime=" + QuickTimeFormat.msToSecs(sortTime));
+	}
+	//-
+}
