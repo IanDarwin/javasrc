0c3c17ded70380c8a8e5234bb7459921152ac7ba
Ian Darwin <ian@darwinsys.com>
1999-08-07 23:51:34 +0000

    New file.

---
+++ src/main/java/structure/SubstrCompDemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/** Demonstrate use of SubstringComparator.
+ * @version $id$
+ */
+public class SubstrCompDemo {
+	public static void main(String[] unused) {
+		String[] strings = {
+			"painful", 
+			"mainly",
+			"gaining",
+			"raindrops"
+		};
+		Arrays.sort(strings);
+		dump(strings, "Using Default Sort");
+		Arrays.sort(strings, new SubstringComparator());
+		dump(strings, "Using SubstringComparator");
+
+	}
+	static void dump(String[] args, String title) {
+		System.out.println(title);
+		for (int i=0; i<args.length; i++)
+			System.out.println(args[i]);
+	}
+}
