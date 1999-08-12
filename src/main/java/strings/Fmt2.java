268e6306de55e2808b1ba668e5ba584ef44ddf2d
Ian Darwin <ian@darwinsys.com>
1999-08-12 17:27:54 +0000

    Finish writing the sucker. Use an array of Command objects.

--- src/main/java/strings/Fmt2.java
+++ src/main/java/strings/Fmt2.java
@@ -7,6 +7,12 @@ import java.util.*;
 public class Fmt2 extends Fmt {
 	/** The maximum column width */
 	public static final int COLWIDTH=72;
+	/** The constant for formatted mode */
+	protected final int MODE_FI = 1;
+	/** The constant for UNformatted mode */
+	protected final int MODE_NF = 0;
+	/** The current formatting mode */
+	protected int mode = MODE_FI;
 
 	public static void main(String av[]) throws IOException {
 		if (av.length == 0)
@@ -20,26 +26,20 @@ public class Fmt2 extends Fmt {
 		super(fname);
 	}
 
-	class command {
-		String cmdName;
-		void  action();
-		command(String s) {
-			cmdName = s;
-		}
-	}
-
-	command[] commands = {
-		new command("br") { void action() { skipLine(); } },
-		new command("nf") { void action() { mode = MODE_NF; } },
-		new command("bp") { void action() { put("\f"); /*formfeed*/} },
+	/** The Array of commands */
+	Command[] commands = {
+		new Command("br") { void action() { skipLine(); } },
+		new Command("bp") { void action() { put("\f"); /*formfeed*/} },
+		new Command("fi") { void action() { mode = MODE_FI; } },
+		new Command("nf") { void action() { mode = MODE_NF; } },
+		new Command("sp") { void action() { skipLine(); } },
 	};
 
 	/** Format the File contained in a constructed Fmt object */
 	public void format() throws IOException {
-		if (in == null)
-			throw new IllegalArgumentException("FMT: FileReader not initialized");
 		String w, f;
 		int col = 0;
+outer:
 		while ((w = in.readLine()) != null) {
 			if (w.length() == 0) {	// null line
 				skipLine();		// end current line
@@ -50,12 +50,13 @@ public class Fmt2 extends Fmt {
 				continue;
 			}
 			if (w.startsWith(".")) {// troff command, handle it.
-				for (int i=0; i<commands.length; i++)
-					command v = commands[i];
-					if (v.cmdName.equals(subString(w,1,2))) {
+				for (int i=0; i<commands.length; i++) {
+					Command v = commands[i];
+					if (v.cmdName.equals(w.substring(1, 2))) {
 						v.action();
-						continue;
+						continue outer;
 					} 
+				}
 				// else an unrecognized troff command
 				if (col>0) skipLine();	// flush
 				putln(w);
@@ -78,11 +79,10 @@ public class Fmt2 extends Fmt {
 		}
 		if (col>0) skipLine();
 		in.close();
-		in = null;		/* prevent tragic accidents */
 	}
 
 	/** Put a string to the output, with a newline */
-	protected void put(String s) {
+	protected void putln(String s) {
 		System.out.print(s);
 		System.out.print('\n');
 	}
@@ -95,5 +95,14 @@ public class Fmt2 extends Fmt {
 	/** Output a newline */
 	protected void skipLine() {
 		System.out.println();
+	}
+}
+
+/** A Command is a formatter command: it has a name and an action. */
+abstract class Command {
+	String cmdName;
+	abstract void  action();
+	Command(String s) {
+		cmdName = s;
 	}
 }
