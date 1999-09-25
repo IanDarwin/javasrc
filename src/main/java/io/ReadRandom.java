22e7ab35964e8bea2403456476f42b4a6ce68a5c
Ian Darwin <ian@darwinsys.com>
1999-09-25 02:08:57 +0000

    Better comments.

--- src/main/java/io/ReadRandom.java
+++ src/main/java/io/ReadRandom.java
@@ -5,34 +5,33 @@ import java.io.*;
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
-public class RandomRead {
+public class ReadRandom {
 	final static String FILENAME = "random.dat";
 	protected String fileName;
 	protected RandomAccessFile seeker;
 
 	public static void main(String argv[]) throws IOException {
-		//+
-		RandomRead r = new RandomRead(FILENAME);
+		ReadRandom r = new ReadRandom(FILENAME);
 
 		System.out.println("Offset is " + r.readOffset());
 		System.out.println("Message is \"" + r.readMessage() + "\".");
-		//-
 	}
 
 	/** Constructor: save filename, construct RandomAccessFile */
-	public RandomRead(String fname) throws IOException {
+	public ReadRandom(String fname) throws IOException {
 		fileName = fname;
 		seeker = new RandomAccessFile(fname, "r");
 	}
 
 	/** Read the Offset field, defined to be at location 0 in the file. */
 	public int readOffset() throws IOException {
-		seeker.seek(0);
-		return seeker.readInt();
+		seeker.seek(0);				// move to very beginning
+		return seeker.readInt();	// and read the offset
 	}
 
+	/** Read the message at the given offset */
 	public String readMessage() throws IOException {
-		seeker.seek(readOffset());
-		return seeker.readLine();
+		seeker.seek(readOffset());	// move to where
+		return seeker.readLine();	// and read the String
 	}
 }
