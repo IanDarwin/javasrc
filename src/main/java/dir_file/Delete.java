c015d325edeb36e5b7093e6cf99cc230ba9d9557
Ian Darwin <ian@darwinsys.com>
1999-10-08 20:40:03 +0000

    Fix class name.

--- src/main/java/dir_file/Delete.java
+++ src/main/java/dir_file/Delete.java
@@ -5,13 +5,13 @@ import java.io.*;
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
-public class Rename {
+public class Delete {
 	public static void main(String argv[]) throws IOException {
 
 		// Construct a File object for the backup created by editing
 		// this source file. The file probably already exists.
 		// My editor creates backups by putting ~ at the end of the name.
-		File bkup = new File("Rename.java~");
+		File bkup = new File("Delete.java~");
 		// Quick, now, delete it immediately:
 		bkup.delete();
 	}
