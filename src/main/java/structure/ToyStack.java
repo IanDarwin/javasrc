ae7defeb4799ee95c17e152e3429ef3321288d9b
Ian Darwin <ian@darwinsys.com>
1999-10-15 20:46:03 +0000

    A simple (dangerous) stack.

---
+++ src/main/java/structure/ToyStack.java
@@ -0,0 +1,23 @@
+/** Toy Stack. */
+public class ToyStack {
+
+	/** The maximum stack depth */
+	protected int MAX_DEPTH = 10;
+	/** The current stack depth */
+	protected int depth = 0;
+	/* The actual stack */
+	protected int stack[] = new int[MAX_DEPTH];
+
+	/* Implement a toy stack version of push */
+	protected void push(int n) {
+		stack[depth++] = n;
+	}
+	/* Implement a toy stack version of pop */
+	protected int pop() {
+		return stack[--depth];
+	}
+	/* Implement a toy stack version of peek */
+	protected int peek() {
+		return stack[depth];
+	}
+}
