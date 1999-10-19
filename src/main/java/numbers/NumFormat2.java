863557fa50f6c789143c04f9111c89ed2cef69e9
Ian Darwin <ian@darwinsys.com>
1999-10-19 19:52:50 +0000

    More.

---
+++ src/main/java/numbers/NumFormat2.java
@@ -0,0 +1,28 @@
+import java.text.*;
+import java.util.*;
+
+/*
+ * Format a number our way and the default way.
+ */
+public class NumFormat2 {
+	/** A number to format */
+	public static final double data[] = {
+		0, 1, 22d/7, 100.2345678
+	};
+
+	/** The main (and only) method in this class. */
+	public static void main(String av[]) { 
+		// Get a format instance
+		NumberFormat form = NumberFormat.getInstance();
+
+		// Set it to look like 999.99[99]
+		form.setMinimumIntegerDigits(3);
+		form.setMinimumFractionDigits(2);
+		form.setMaximumFractionDigits(4);
+
+		// Now print using it.
+		for (int i=0; i<data.length; i++)
+			System.out.println(data[i] + "\tformats as " +
+				form.format(data[i]));
+	}
+}
