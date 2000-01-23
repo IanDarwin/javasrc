faadbb5ed67eca7e83f37ae558b5d9c156015b84
Ian Darwin <ian@darwinsys.com>
2000-01-23 22:48:58 +0000

    More, more, more.

---
+++ src/main/java/network/DaytimeObject.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * DaytimeObject - connect to the Daytime (ascii) service.
+ * @author Ian F. Darwin
+ * @version $Id$
+ */
+public class DaytimeObject {
+	/** The TCP port for the object time service. */
+	public static final short TIME_PORT = 1951;
+
+	public static void main(String argv[]) {
+		String hostName;
+		if (argv.length == 0)
+			hostName = "localhost";
+		else
+			hostName = argv[0];
+
+		try {
+			Socket sock = new Socket(hostName, TIME_PORT);
+			ObjectInputStream is = new ObjectInputStream(new 
+				BufferedInputStream(sock.getInputStream()));
+
+			// Read and validate the Object
+			Object o = is.readObject();
+			if (!(o instanceof Date))
+				throw new IllegalArgumentException("Wanted Date, got " + o);
+
+			// Valid, so cast to Date, and print
+			Date d = (Date) o;
+			System.out.println("Time on " + hostName + " is " + d.toString());
+		} catch (ClassNotFoundException e) {
+			System.err.println("Wanted date, got INVALID CLASS (" + e + ")");
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}
---
+++ src/main/java/network/DaytimeObjectServer.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * DaytimeObject - connect to the Daytime (ascii) service.
+ * @author Ian F. Darwin
+ * @version $Id$
+ */
+public class DaytimeObjectServer {
+	/** The TCP port for the object time service. */
+	public static final short TIME_PORT = 1951;
+
+	public static void main(String argv[]) {
+		ServerSocket sock;
+		Socket  clientSock;
+		try {
+			sock = new ServerSocket(TIME_PORT);
+			while ((clientSock = sock.accept()) != null) {
+				System.out.println("Accept from " + clientSock.getInetAddress());
+				ObjectOutputStream os = new ObjectOutputStream(new
+					BufferedOutputStream(clientSock.getOutputStream()));
+
+				// Construct and write the Object
+				os.writeObject(new Date());
+
+				os.close();
+			}
+
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}
