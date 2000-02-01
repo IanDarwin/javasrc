b280ab3ded02a56b4f5db1ab33f98799c26937ac
Ian Darwin <ian@darwinsys.com>
2000-02-01 04:24:23 +0000

    Initial revision

---
+++ src/main/java/starting/IfDef.java
@@ -0,0 +1,9 @@
+/** Simple test to show if compilers exclude code that can't be reached. */
+public class IfDef {
+	public static void main(String argv[]) {
+		final boolean DEBUG = false;
+		System.out.println("Hello, World");
+		if (DEBUG)
+			System.out.println("Life is a voyage, not a destination.");
+	}
+}
