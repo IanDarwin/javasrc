071d972c225b1129f0e7c332a0474403fc98cb5a
Ian Darwin <ian@darwinsys.com>
2000-02-28 04:21:25 +0000

    Yet more book examples.

---
+++ src/main/java/javacomm/CommPortLister.java
@@ -0,0 +1,35 @@
+import javax.comm.*;
+import java.util.*;
+
+/**
+ * List the ports.
+ * @author	Ian F. Darwin, ian@darwinsys.com
+ * @version	$Id$
+ */
+public class CommPortLister {
+
+	/** Simple test program. */
+	public static void main(String ap[]) {
+		new CommPortLister().list();
+	}
+
+	/** Ask the Java Communications API * what ports it thinks it has. */
+	protected void list() {
+		// get list of ports available on this particular computer,
+		// by calling static method in CommPortIdentifier.
+		Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+
+		// Process the list, putting serial and parallel into ComboBoxes
+		while (pList.hasMoreElements()) {
+			CommPortIdentifier cpi = (CommPortIdentifier)pList.nextElement();
+			System.out.print("Port " + cpi.getName() + " ");
+			if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				System.out.println("is a Serial Port: " + cpi);
+			} else if (cpi.getPortType() == CommPortIdentifier.PORT_PARALLEL) {
+				System.out.println("is a Parallel Port: " + cpi);
+			} else {
+				System.out.println("is an Unknown Port: " + cpi);
+			}
+		}
+	}
+}
---
+++ src/main/java/javacomm/SerialLogger.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import javax.comm.*;
+import java.util.*;
+
+/**
+ * Read from multiple Serial ports, notifying when data arrives on any.
+ * @version $Id$
+ * @author	Ian F. Darwin, ian@darwinsys.com
+ */
+public class SerialLogger {
+
+	public static void main(String argv[])
+		throws IOException, NoSuchPortException, PortInUseException,
+			UnsupportedCommOperationException {
+
+		new SerialLogger();
+	}
+
+	/* Constructor */
+	public SerialLogger()
+		throws IOException, NoSuchPortException, PortInUseException,
+			UnsupportedCommOperationException {
+		
+		// get list of ports available on this particular computer,
+		// by calling static method in CommPortIdentifier.
+		Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+
+		// Process the list, putting serial and parallel into ComboBoxes
+		while (pList.hasMoreElements()) {
+			CommPortIdentifier cpi = (CommPortIdentifier)pList.nextElement();
+			String name = cpi.getName();
+			System.out.print("Port " + name + " ");
+			if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+				System.out.println("is a Serial Port: " + cpi);
+
+				SerialPort thePort;
+				try {
+					thePort = (SerialPort)cpi.open("Logger", 1000);
+				} catch (PortInUseException ev) {
+					System.err.println("Port in use: " + name);
+					continue;
+				}
+
+				// Tell the Comm API that we want serial events.
+				thePort.notifyOnDataAvailable(true);
+				try {
+					thePort.addEventListener(new Logger(cpi.getName(), thePort));
+				} catch (TooManyListenersException ev) {
+					// "CantHappen" error
+					System.err.println("Too many listeners(!) " + ev);
+					System.exit(0);
+				}
+			}
+		}
+	}
+
+	/** Handle one port. */
+	public class Logger implements SerialPortEventListener { 
+		String portName;
+		SerialPort thePort;
+		BufferedReader ifile;
+		public Logger(String name, SerialPort port) throws IOException {
+			portName = name;
+			thePort = port;
+			// Make a reader for the input file.
+			ifile = new BufferedReader(
+				new InputStreamReader(thePort.getInputStream()));
+		}
+		public void serialEvent(SerialPortEvent ev) {
+			String line;
+			try {
+				line = ifile.readLine();
+				if (line == null) {
+					System.out.println("EOF on serial port.");
+					System.exit(0);
+				}
+				System.out.println(portName + ": " + line);
+			} catch (IOException ex) {
+				System.err.println("IO Error " + ex);
+			}
+		}
+	}
+}
