aa624abb5365355b6c5edbe061510a8d4665f013
Ian Darwin <ian@darwinsys.com>
2000-02-28 23:14:17 +0000

    Hey, it works, for small domains of work! Text display a bit flakey. Flow control seems flakey, or maybe the overhead of creating a new String for each read is killing it. No file transfer yet.

--- src/main/java/javacomm/JModem.java
+++ src/main/java/javacomm/JModem.java
@@ -1,6 +1,8 @@
 import javax.comm.*;
 import java.io.*;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
@@ -10,10 +12,26 @@ import javax.swing.*;
  */
 public class JModem extends javax.swing.JFrame {
 
+  /** The javax.com.CommPort object in use */
+  private SerialPort thePort;
+
+  /** The input and output streams */
+  private InputStream serialInput;
+  private OutputStream serialOutput;
+
+  /** The size of the static read buffer. */
+  protected static final int BUFSIZE = 1024;
+  /** A buffer for the read listener; preallocated once. */
+  static byte[] buf = new byte[BUFSIZE];
+  /** A Thread for reading from the remote. */
+  Thread serialReadThread;
+  /** The TextArea */
+  JTextArea theTextArea;
+
   /** Initializes the Form */
   public JModem() {
     initComponents();
-  finishConstructor();
+    finishConstructor();
     pack();
   }
 
@@ -70,9 +88,9 @@ public class JModem extends javax.swing.JFrame {
       connectPanelLabel.setForeground (java.awt.Color.red);
       connectPanel.add (connectPanelLabel);
 
-      portLabel = new javax.swing.JLabel ();
-      portLabel.setText ("Port:");
-      connectPanel.add (portLabel);
+      portsLabel = new javax.swing.JLabel ();
+      portsLabel.setText ("Port:");
+      connectPanel.add (portsLabel);
 
       portsComboBox = new javax.swing.JComboBox ();
       portsComboBox.addActionListener (new java.awt.event.ActionListener () {
@@ -105,9 +123,9 @@ public class JModem extends javax.swing.JFrame {
         d7RadioButton.setText ("7");
         databitsPanel.add (d7RadioButton);
 
-        d8radioButton = new javax.swing.JRadioButton ();
-        d8radioButton.setText ("8");
-        databitsPanel.add (d8radioButton);
+        d8RadioButton = new javax.swing.JRadioButton ();
+        d8RadioButton.setText ("8");
+        databitsPanel.add (d8RadioButton);
 
       connectPanel.add (databitsPanel);
 
@@ -238,12 +256,21 @@ public class JModem extends javax.swing.JFrame {
 
     getContentPane ().add (xferPanel, "South");
 
-    theTextArea = new javax.swing.JTextArea ();
-    theTextArea.setColumns (80);
-    theTextArea.setRows (15);
-    getContentPane ().add (theTextArea, "Center");
-
   }//GEN-END:initComponents
+
+  private void theTextAreaKeyTyped (java.awt.event.KeyEvent evt) {//GEN-FIRST:event_theTextAreaKeyTyped
+    if (state != S_CONNECTED) {
+      getToolkit().beep();
+      return;
+    }
+    char ch = evt.getKeyChar();
+  if (ch == '\n') {    // XX if systemtype == dos
+    sendChar('\r');
+    // sendChar('\n');
+    return;
+  }
+    sendChar(ch);
+  }//GEN-LAST:event_theTextAreaKeyTyped
 
   private void helpAboutMenuItemActionPerformed (java.awt.event.ActionEvent evt) {//GEN-FIRST:event_helpAboutMenuItemActionPerformed
     JOptionPane.showMessageDialog(this,
@@ -251,9 +278,9 @@ public class JModem extends javax.swing.JFrame {
       "About JModem", JOptionPane.INFORMATION_MESSAGE);
   }//GEN-LAST:event_helpAboutMenuItemActionPerformed
 
-  private void baudComboBoxActionPerformed (java.awt.event.ActionEvent evt) {//GEN-FIRST:event_portsComboBoxActionPerformed
+  private void baudComboBoxActionPerformed (java.awt.event.ActionEvent evt) {
     // Add your handling code here:
-  }//GEN-LAST:event_portsComboBoxActionPerformed
+  }
 
   private void portsComboBoxActionPerformed (java.awt.event.ActionEvent evt) {//GEN-FIRST:event_portsComboBoxActionPerformed
     // Add your handling code here:
@@ -267,49 +294,72 @@ public class JModem extends javax.swing.JFrame {
   /** Finish the initializations. */
   private void finishConstructor() {
 
-    // get list of ports available on this particular computer,
-    // by calling static method in CommPortIdentifier.
-    Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+	// A TextArea subclass without keypress forwarding
+	class MyTextArea extends JTextArea {
+		MyTextArea(int r, int c) {
+			super(r, c);
+		}
+		protected void processComponentKeyEvent(java.awt.event.KeyEvent ke) {
+			// do nothing, to avoid user keystrokes appearing twice!
+		}
+	}
 
-    // Process the list of ports, putting serial ports into ComboBox
-    while (pList.hasMoreElements()) {
-      CommPortIdentifier cpi = (CommPortIdentifier)pList.nextElement();
-      if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-        portsComboBox.addItem(cpi.getName());
-		portsIDmap.put(cpi.getName(), cpi);
+    // Create the textarea with a JScrollpane wrapping it.
+	// Install it in Centre of the TextArea.
+	theTextArea = new MyTextArea(24,80);
+    getContentPane().add(new JScrollPane(theTextArea), BorderLayout.CENTER);
+	theTextArea.addKeyListener(new KeyAdapter() {
+		public void keyTyped(KeyEvent ke) {
+			theTextAreaKeyTyped(ke);
+		}
+	});
+
+      // get list of ports available on this particular computer,
+      // by calling static method in CommPortIdentifier.
+      Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+
+      // Process the list of ports, putting serial ports into ComboBox
+      while (pList.hasMoreElements()) {
+        CommPortIdentifier cpi = (CommPortIdentifier)pList.nextElement();
+        if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+          portsComboBox.addItem(cpi.getName());
+      portsIDmap.put(cpi.getName(), cpi);
+        }
       }
+    portsComboBox.setSelectedIndex(0);
+
+    // Load up the baud-rate combo box
+    for (int i=0; i<baudot.length; i++) {
+      baudComboBox.addItem(Integer.toString(baudot[i]));
     }
-	portsComboBox.setSelectedIndex(0);
+    baudComboBox.setSelectedIndex(0);
 
-	// Load up the baud-rate combo box
-	for (int i=0; i<baudot.length; i++) {
-		baudComboBox.addItem(Integer.toString(baudot[i]));
-	}
-	baudComboBox.setSelectedIndex(0);
+    // Load up the System Type combo box
+    for (int i=0; i<sysTypes.length; i++) {
+      sysTypeComboBox.addItem(sysTypes[i]);
+    }
+    sysTypeComboBox.setSelectedIndex(0);
 
-	// Load up the System Type combo box
-	for (int i=0; i<sysTypes.length; i++) {
-		sysTypeComboBox.addItem(sysTypes[i]);
-	}
-	sysTypeComboBox.setSelectedIndex(0);
+    // put radio buttons into groups to enforce single-selection
+    ButtonGroup b1 = new ButtonGroup();
+    b1.add(d7RadioButton);
+    b1.add(d8RadioButton);
+    d8RadioButton.setSelected(true);
 
-	// put radio buttons into groups to enforce single-selection
-	ButtonGroup b1 = new ButtonGroup();
-	b1.add(d7RadioButton);
-	b1.add(d8radioButton);
+    ButtonGroup b2 = new ButtonGroup();
+    b2.add(pNoneRadioButton);
+    b2.add(pEvenRadioButton);
+    b2.add(pOddRadioButton);
+    pNoneRadioButton.setSelected(true);
 
-	ButtonGroup b2 = new ButtonGroup();
-	b2.add(pNoneRadioButton);
-	b2.add(pEvenRadioButton);
-	b2.add(pOddRadioButton);
+    ButtonGroup b3 = new ButtonGroup();
+    b3.add(sendRadioButton);
+    b3.add(recvRadioButton);
 
-	ButtonGroup b3 = new ButtonGroup();
-	b3.add(sendRadioButton);
-	b3.add(recvRadioButton);
-
-	ButtonGroup b4 = new ButtonGroup();
-	b4.add(xferModeTextRadioButton);
-	b4.add(xferModeBinRadioButton);
+    ButtonGroup b4 = new ButtonGroup();
+    b4.add(xferModeTextRadioButton);
+    b4.add(xferModeBinRadioButton);
+    xferModeBinRadioButton.setEnabled(true);
   }
 
   private int M_RECEIVE = -1, M_SEND = +1;
@@ -353,89 +403,135 @@ public class JModem extends javax.swing.JFrame {
   private int submode = S_INTERACT;
 
   private void connectButtonActionPerformed (java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connectButtonActionPerformed
-	if (state == S_CONNECTED) {
-		disconnect();
-		connectButton.setText("Connect");
-	} else {
-		try {
-			connect();
-		} catch (PortInUseException pue) {
-			err("Port in use: close other app, or use different port.");
-			return;
-		} catch (UnsupportedCommOperationException uoe) {
-			err("Unsupported options error: try different settings");
-			return;
-		}
-		connectButton.setText("Disconnect");
-	}
+  if (state == S_CONNECTED) {
+    disconnect();
+    connectButton.setText("Connect");
+  theTextArea.setEditable(false);
+  } else {
+    try {
+      connect();
+    } catch (PortInUseException pue) {
+      err("Port in use: close other app, or use different port.");
+      return;
+    } catch (UnsupportedCommOperationException uoe) {
+      err("Unsupported options error: try different settings");
+      return;
+    }
+    connectButton.setText("Disconnect");
+  theTextArea.setEditable(true);
+  theTextArea.requestFocus();
+  }
   }
 
   protected void err(String s) {
-	JOptionPane.showMessageDialog(this,
-	  "Port in use: close other app, or use different port.",
-	  "JModem Error", JOptionPane.ERROR_MESSAGE);
-	return;
+  JOptionPane.showMessageDialog(this,
+    "Port in use: close other app, or use different port.",
+    "JModem Error", JOptionPane.ERROR_MESSAGE);
+  return;
   }
 
-  private SerialPort thePort;
-
-  private InputStream serialInput;
-  private PrintWriter serialOutput;
-
-  private void connect() throws PortInUseException, 
-  		UnsupportedCommOperationException {
+  private void connect() throws PortInUseException,
+      UnsupportedCommOperationException {
 
     // Open the specified serial port
-	CommPortIdentifier cpi = (CommPortIdentifier)portsIDmap.get(
-		portsComboBox.getSelectedItem());
-	thePort = (SerialPort)cpi.open("JModem", 15*1000);
+  CommPortIdentifier cpi = (CommPortIdentifier)portsIDmap.get(
+    portsComboBox.getSelectedItem());
+  thePort = (SerialPort)cpi.open("JModem", 15*1000);
 
-	// Set the serial port parameters.
-	int parity = 0;
-	if (pNoneRadioButton.isSelected()) parity = SerialPort.PARITY_NONE;
-	if (pEvenRadioButton.isSelected()) parity = SerialPort.PARITY_EVEN;
-	if (pOddRadioButton.isSelected())  parity = SerialPort.PARITY_ODD;
-	thePort.setSerialPortParams(
-		baudot[portsComboBox.getSelectedIndex()],		// baud
-		d7RadioButton.isSelected() ?					// data bits
-			SerialPort.DATABITS_7 : SerialPort.DATABITS_8,
-		SerialPort.STOPBITS_1,							// stop bits
-		parity);										// parity
+  // Set the serial port parameters.
+  int parity = 0;
+  if (pNoneRadioButton.isSelected()) parity = SerialPort.PARITY_NONE;
+  if (pEvenRadioButton.isSelected()) parity = SerialPort.PARITY_EVEN;
+  if (pOddRadioButton.isSelected())  parity = SerialPort.PARITY_ODD;
+  thePort.setSerialPortParams(
+    baudot[portsComboBox.getSelectedIndex()],    // baud
+    d7RadioButton.isSelected() ?          // data bits
+      SerialPort.DATABITS_7 : SerialPort.DATABITS_8,
+    SerialPort.STOPBITS_1,              // stop bits
+    parity);                    // parity
 
-	// Get the streams
-	try {
-		serialInput = thePort.getInputStream();
-	} catch (IOException e) {
-		err("Error getting input stream:\n" + e.toString());
-		return;
-	}
-	try {
-		serialOutput = new PrintWriter(thePort.getOutputStream());
-	} catch (IOException e) {
-		err("Error getting output stream:\n" + e.toString());
-		return;
-	}
+  thePort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN &
+		SerialPort.FLOWCONTROL_RTSCTS_OUT);
 
-	state = S_CONNECTED;
+  // Similar to "raw" mode: return when 1 or more chars available.
+  try {
+    thePort.enableReceiveThreshold(1);
+    if (!thePort.isReceiveThresholdEnabled()) {
+      err("Could not set receive threshold");
+    }
+    thePort.setInputBufferSize(buf.length);
+  } catch (UnsupportedCommOperationException ev) {
+    err("Unable to set receive threshold in Comm API; port unusable.");
+    disconnect();
+    return;
+  }
+
+  // Get the streams
+  try {
+    serialInput = thePort.getInputStream();
+  } catch (IOException e) {
+    err("Error getting input stream:\n" + e.toString());
+    return;
+  }
+  try {
+    serialOutput = thePort.getOutputStream();
+  } catch (IOException e) {
+    err("Error getting output stream:\n" + e.toString());
+    return;
+  }
+
+    // Now that we're all set, create a Thread to read data from the remote
+    serialReadThread = new Thread(new Runnable() {
+    int nbytes = buf.length;
+      public void run() {
+        do {
+        try {
+          nbytes = serialInput.read(buf, 0, buf.length);
+        } catch (IOException ev) {
+        err("Error reading from remote:\n" + ev.toString());
+        return;
+        }
+        theTextArea.append(new String(buf, 0, nbytes));
+        } while (serialInput != null);
+      }
+    });
+    serialReadThread.start();
+
+    state = S_CONNECTED;
 
   }//GEN-LAST:event_connectButtonActionPerformed
 
   private void disconnect() {
-  	System.out.println("Disconnecting!");
-	// Tell java.io we are done with the input and output
-	try {
-		serialInput.close();
-		serialOutput.close();
-	} catch (IOException e) {
-		err("IO Exception closing port:\n" + e.toString());
-	}
-	// Tell javax.comm we are done with the port.
-	thePort.close();
-	// Tell rest of program we are no longer online.
-	state = S_DISCONNECTED;
+    // Tell java.io we are done with the input and output
+    try {
+    serialReadThread.stop();  // IGNORE DEPRECATION WARNINGS; the Java
+    // API still does not give a reliable termination method for Threads
+    // that are blocked on e.g., local disk reads.
+      serialInput.close();
+      serialOutput.close();
+    serialOutput = null;
+    } catch (IOException e) {
+      err("IO Exception closing port:\n" + e.toString());
+    }
+    // Tell javax.comm we are done with the port.
+    thePort.removeEventListener();
+    thePort.close();
+    // Tell rest of program we are no longer online.
+    state = S_DISCONNECTED;
   }
 
-  private void sendChar() {
+  private void sendChar(char ch) {
+    if (state != S_CONNECTED)
+      return;
+    // theTextArea.append(ch);
+  // System.err.println("--> " + ch);
+  try {
+    serialOutput.write(ch);
+  } catch (IOException e) {
+    err("Output error on remote:\n" + e.toString() +
+      "\nClosing connection.");
+    disconnect();
+  }
   }
 
   /** Exit the Application */
@@ -447,16 +543,18 @@ public class JModem extends javax.swing.JFrame {
 // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JPanel connectPanel;
   private javax.swing.JPanel xferPanel;
-  private javax.swing.JTextArea theTextArea;
   private javax.swing.JLabel connectPanelLabel;
+  private javax.swing.JLabel portsLabel;
+  private javax.swing.JComboBox portsComboBox;
   private javax.swing.JLabel buadLabel;
   private javax.swing.JComboBox baudComboBox;
   private javax.swing.JPanel databitsPanel;
   private javax.swing.JPanel parityPanel;
+  private javax.swing.JLabel sysTypeLabel;
   private javax.swing.JComboBox sysTypeComboBox;
   private javax.swing.JButton connectButton;
   private javax.swing.JRadioButton d7RadioButton;
-  private javax.swing.JRadioButton d8radioButton;
+  private javax.swing.JRadioButton d8RadioButton;
   private javax.swing.JRadioButton pEvenRadioButton;
   private javax.swing.JRadioButton pOddRadioButton;
   private javax.swing.JRadioButton pNoneRadioButton;
@@ -472,12 +570,9 @@ public class JModem extends javax.swing.JFrame {
   private javax.swing.JRadioButton xferModeBinRadioButton;
   private javax.swing.JMenuBar jMenuBar1;
   private javax.swing.JMenu fileMenu;
-  private javax.swing.JMenuItem exitMenuItem;
-  private javax.swing.JComboBox portsComboBox;
-  private javax.swing.JLabel portLabel;
   private javax.swing.JMenu helpMenu;
+  private javax.swing.JMenuItem exitMenuItem;
   private javax.swing.JMenuItem helpAboutMenuItem;
-  private javax.swing.JLabel sysTypeLabel;
 // End of variables declaration//GEN-END:variables
 
 
