71ba4e59c0f7a63eaa37e1c90dbf960afb1a7b70
IanDarwin <>
2000-03-17 15:08:25 +0000

    Prints five random integers.

---
+++ src/main/java/jsp/FiveInts.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class FiveInts {
+	public static void main(String[] argv) {
+		Random r = new Random();
+		for (int i=0; i<5; i++)
+			System.out.println(r.nextInt());
+	}
+}
