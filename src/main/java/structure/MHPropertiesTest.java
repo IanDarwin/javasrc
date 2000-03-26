6d1fd04bd82bafdbee2eb1d3aede26d6a7b7f6b3
Ian Darwin <ian@darwinsys.com>
2000-03-26 23:56:34 +0000

    Add code to read '.mh_profile' into a Properties file.

---
+++ src/main/java/structure/MHPropertiesTest.java
@@ -0,0 +1,12 @@
+public class MHPropertiesTest {
+	public static void main(String[] args) throws java.io.IOException {
+		MHProperties mp = new MHProperties();
+		if (mp.size() == 0) {
+			System.out.println("# No properties loaded from " +
+				MHProperties.PROFILE_NAME);
+		} else {
+			mp.store(System.out, "Properties loaded from " +
+				MHProperties.PROFILE_NAME);
+		}
+	}
+}
