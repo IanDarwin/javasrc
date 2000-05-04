497fc2b4c3e13977d55ce4112d27da815712b494
Ian Darwin <ian@darwinsys.com>
2000-05-04 18:37:57 +0000

    New.

---
+++ src/main/java/netweb/GetResource.java
@@ -0,0 +1,7 @@
+public class GetResource {
+	public static void main(String[] argv) {
+		Class c = GetResource.class;
+		java.net.URL u = c.getResource("GetResource.java");
+		System.out.println(u);
+	}
+}
