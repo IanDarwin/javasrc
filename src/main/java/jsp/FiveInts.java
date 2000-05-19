1c21644dd52509691302252ec918cd1ccb40cc27
IanDarwin <>
2000-05-19 01:18:39 +0000

    Add PDF demo, and minor tweaks.

--- src/main/java/jsp/FiveInts.java
+++ src/main/java/jsp/FiveInts.java
@@ -2,8 +2,10 @@ import java.util.Random;
 
 public class FiveInts {
 	public static void main(String[] argv) {
+		//+
 		Random r = new Random();
 		for (int i=0; i<5; i++)
 			System.out.println(r.nextInt());
+		//-
 	}
 }
