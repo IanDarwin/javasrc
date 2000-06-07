c50f77307be37513fd5bb0978deaead753896558
Ian Darwin <ian@darwinsys.com>
2000-06-07 16:22:58 +0000

    Rewrite to use nextInt(), and comment better.

--- src/main/java/numbers/RandomInt.java
+++ src/main/java/numbers/RandomInt.java
@@ -1,12 +1,16 @@
-/** Generate random ints by scaling from Math.random().
- * Prints a series of random integers from 1 to 10, inclusive.
+import java.util.*;
+
+/** Generate random ints by asking Random() for
+ * a series of random integers from 1 to 10, inclusive.
  *
  * @author Ian Darwin, ian@darwinsys.com
  * @version $Id$
  */
 public class RandomInt {
 	public static void main(String[] a) {
-		for (int i=0; i<100; i++)
-			System.out.println(1+(int)(Math.random() * 10));
+		Random r = new Random();
+		for (int i=0; i<1000; i++)
+			// nextInt(10) goes from 0-9; add 1 for 1-10;
+			System.out.println(1+r.nextInt(10));
 	}
 }
