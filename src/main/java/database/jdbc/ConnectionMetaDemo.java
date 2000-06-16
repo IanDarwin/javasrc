9162f35598cb84cd5283b7f7331a8f534aef982b
Ian Darwin <ian@darwinsys.com>
2000-06-16 20:28:52 +0000

    Getting into the Database chapter.

---
+++ src/main/java/database/jdbc/ConnectionMetaDemo.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.sql.*;
+
+/** Load a driver and connect to a database.
+ */
+public class Connect {
+
+	public static void main(String av[]) {
+		String dbURL = "jdbc:odbc:Companies";
+	    try {
+			// Load the jdbc-odbc bridge driver
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+
+			// Enable logging
+			DriverManager.setLogStream(System.err);
+
+			System.out.println("Getting Connection");
+			Connection conn = 
+				DriverManager.getConnection(dbURL, "ian", "");	// user, passwd
+
+			// If a SQLWarning object is available, print its
+			// warning(s).  There may be multiple warnings chained.
+
+			SQLWarning warn = conn.getWarnings();
+			while (warn != null) {
+				System.out.println("SQLState: " + warn.getSQLState());
+				System.out.println("Message:  " + warn.getMessage());
+				System.out.println("Vendor:   " + warn.getErrorCode());
+				System.out.println("");
+				warn = warn.getNextWarning();
+		    }
+
+			// Process the connection here...
+
+			conn.close();	// All done with that DB connection
+
+	    } catch (ClassNotFoundException e) {
+			System.out.println("Can't load driver " + e);
+	    } catch (SQLException e) {
+			System.out.println("Database access failed " + e);
+	    }
+	}
+}
---
+++ src/main/java/database/jdbc/LoadDriver.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.sql.*;
+
+/** Load some drivers.  */
+public class LoadDriver {
+
+	public static void main(String av[]) {
+	    try {
+
+			// Try to load the jdbc-odbc bridge driver
+			// Should be present on Sun JDK implementations.
+			Class c = Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			System.out.println("Loaded " + c);
+
+			// Try to load an Oracle driver.
+			Class d = Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Loaded " + d);
+		} catch (ClassNotFoundException ex) {
+			System.err.println(ex);
+		}
+	}
+}
