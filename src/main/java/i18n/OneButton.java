5a1e74eb384ef033871109ce0cadd50174f8ae44
Ian Darwin <ian@darwinsys.com>
2000-06-25 01:15:00 +0000

    New Internationalization section.

---
+++ src/main/java/i18n/OneButton.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Create one button, internationalizedly.
+ */
+public class OneButton extends JFrame {
+
+	public static void main(String[] args) {
+		new OneButton().setVisible(true);
+	}
+
+	public OneButton() {
+		ResourceBundle rb = ResourceBundle.getBundle("Widgets");
+		try {
+			label = rb.getString("exit.label");
+		} catch (MissingResourceException e) {
+			label="Exit"; // fallback
+		}
+		this.add(new JButton(label));
+	}
+}
