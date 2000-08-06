7210c73efd20fa44d3bab92627294c568194ddcd
Ian Darwin <ian@darwinsys.com>
2000-08-06 01:52:27 +0000

    To become a small mailing list manager (mail sender).

---
+++ src/main/java/email/CustMailer.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/** CustMailer -- to be a small Mailing List sender. */
+public class CustMailer extends JFrame {
+	public static void main(String[] argv) {
+		new CustMailer().setVisible(true);
+	}
+	public CustMailer() {
+		JTabbedPane jtp = new JTabbedPane();
+		setContentPane(jtp);
+		jtp.addTab("Sending", new JPanel());
+		jtp.addTab("Editing", new JPanel()); 
+		
+	}
+
+}
