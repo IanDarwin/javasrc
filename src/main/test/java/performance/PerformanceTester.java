f7b9b693ef13dc59a18924c6a913d46d2dd66ca5
Ian Darwin <ian@darwinsys.com>
2000-08-11 15:00:27 +0000

    Perf Tester, from Linux Journal August 2000.

---
+++ src/main/test/java/performance/PerformanceTester.java
@@ -0,0 +1,49 @@
+public class PerformanceTester {
+	private int amount;
+	private static int NOBJECTS = 500000;
+
+	public void addAmount(int val) {
+		amount += val;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	/**
+	 * Benchmark driver. Creates half a million of simple account
+	 * objects, adds an amount to each one, and then computers the
+	 * sum of all amounts in all objects.
+	 * Reports elapsed time in milliseconds and number of objects
+	 * processed per millisecond.
+	 */
+	public static void main(String[] args) {
+		long starttime = 0;
+		long endtime = 0;
+		int sum = 0;
+
+		starttime = System.currentTimeMillis();
+
+		PerformanceTester[] ptarr = new PerformanceTester[NOBJECTS];
+
+		for (int i=0; i<NOBJECTS; i++) {
+			ptarr[i] = new PerformanceTester();
+		}
+
+		for (int i=0; i<NOBJECTS; i++) {
+			ptarr[i].addAmount(i);
+		}
+
+		for (int i=0; i<NOBJECTS; i++) {
+			sum += ptarr[i].getAmount();
+		}
+
+		endtime = System.currentTimeMillis();
+
+		System.out.println("Elapsed time in milliseconds for " +
+			NOBJECTS + " objects = " + (endtime - starttime));
+
+		System.out.println("Objects per milliseconds: " +
+			(NOBJECTS / (endtime - starttime)));
+	}
+}
