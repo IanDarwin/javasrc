b87aa3b83bec7b6bf53ac00c271e3b1d964869e4
Ian Darwin <ian@darwinsys.com>
2000-09-01 17:24:33 +0000

    new

---
+++ src/main/java/rmi/DateClient.java
@@ -0,0 +1,22 @@
+package darwinsys.distdate;
+
+import java.rmi.*;
+import java.util.*;
+
+/* A very simple client for the RemoteDate service. */
+public class DateClient {
+
+	/** The local proxy for the service. */
+	protected static RemoteDate netConn = null;
+
+	public static void main(String[] args) {
+		try {
+			netConn = (RemoteDate)Naming.lookup(RemoteDate.LOOKUPNAME);
+			Date today = netConn.getRemoteDate();
+			System.out.println(today.toString()); // XX use a DateFormat...
+		} catch (Exception e) {
+			System.err.println("RemoteDate exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
