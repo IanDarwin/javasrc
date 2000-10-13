d27afbe8f4df0993d8c2f65849d78c3760983684
Ian Darwin <ian@darwinsys.com>
2000-10-13 13:38:40 +0000

    Finish BatchMailer; expand error checking in Mailer.

--- src/main/java/email/BatchMailer.java
+++ src/main/java/email/BatchMailer.java
@@ -1,5 +1,6 @@
-import javax.mail.*;
+import java.io.*;
 import java.util.*;
+import javax.mail.*;
 
 /** Command-line batch mailer */
 public class BatchMailer {
@@ -10,7 +11,7 @@ public class BatchMailer {
 		b.sendMails();
 	}
 
-	String messageBody;
+	protected String messageBody;
 
 	/** Read the template file.
 	  * For now, hard-code name.
@@ -20,24 +21,23 @@ public class BatchMailer {
 		BufferedReader is = new BufferedReader(new FileReader("template.txt"));
 		String line;
 		StringBuffer bs = new StringBuffer();
-		while ((line = is.readLine() != null) {
+		while ((line = is.readLine()) != null) {
 			bs.append(line).append("\n");
 		}
+		messageBody = bs.toString();
 		is.close();
 	}
 
-	ArrayList custList = new ArrayList();
+	protected ArrayList custList = new ArrayList();
 
 	/** Read the customer list. 
 	  * For now, hard-code name.
 	  * For now, one customer email per line.
 	  */
 	public void readCustList() throws IOException {
-		messageBody = null;
-		BufferedReader is = new BufferedReader(new FileReader("obsd.custlist"));
+		BufferedReader is = new BufferedReader(new FileReader("custlist.txt"));
 		String line;
-		StringBuffer bs = new StringBuffer();
-		while ((line = is.readLine() != null) {
+		while ((line = is.readLine()) != null) {
 			custList.add(line);
 		}
 		is.close();
@@ -46,11 +46,12 @@ public class BatchMailer {
 	public void sendMails() {
 		Iterator it = custList.iterator();
 		while (it.hasNext()) {
-			string customer = (String)it.next();
+			String customer = (String)it.next();
 			try {
 				Mailer.send("mailhost", 
-					customer, "ian@darwinsys.com", "OpenBSD")
-			} catch (MailException e) {
+					customer, "ian@darwinsys.com", "OpenBSD", messageBody);
+				System.out.println(customer + " HANDOFF OK");
+			} catch (MessagingException e) {
 				System.out.println(customer + " barfed: " + e.toString());
 			}
 		}
