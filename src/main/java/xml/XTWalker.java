f1524dc1db237925609d702f69c88d18fe09968f
Ian Darwin <ian@darwinsys.com>
2000-10-14 19:42:44 +0000

    XTW using TreeWalker

---
+++ src/main/java/xml/XTWalker.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import org.w3c.dom.*;
+import com.sun.xml.tree.*;
+
+/** XML Tree Walker, using the provided TreeWalker
+ * @author Ian Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class XTWalker extends XTW {
+
+	public static void main(String av[]) {
+		if (av.length == 0) {
+			System.err.println("Usage: XTWalker file [...]");
+			return;
+		}
+		for (int i=0; i<av.length; i++) {
+			String name = av[i];
+			new XTWalker().convert(name, true);
+		}
+	}
+
+	/* Process all the nodes, recursively. */
+	protected void doRecursive(Node p) {
+		TreeWalker tw = new TreeWalker(p);
+		Node n;
+		while ((n = (Node)tw.getNext()) != null) {
+
+			doNode(n);
+
+		}
+	}
+}
