b41cf1615e6448d1ad91a4daa64eb85ea6bc0828
Ian Darwin <ian@darwinsys.com>
2001-02-23 17:23:05 +0000

    Zeroing in on Chapter One.

---
+++ src/main/java/starting/Fetch.java
@@ -0,0 +1,14 @@
+public class Fetch {
+	public static void main(String[] args) { 
+		String name = "poem", value;
+		Fetch f = new Fetch();
+		if (System.getProperty("debug.fetch") != null) {
+			System.err.println("Fetching " + name);
+		}
+		value = f.fetch(name);
+	}
+	public String fetch(String name) {
+		// in real life this would look up "name" in some table or database
+		return "jabberwocky";
+	}
+}
---
+++ src/main/java/starting/FetchDebug.java
@@ -0,0 +1,14 @@
+import com.darwinsys.util.Debug;
+
+public class FetchDebug {
+	public static void main(String[] args) { 
+		String name = "poem", value;
+		Fetch f = new Fetch();
+		Debug.println("fetch", "Fetching " + name);
+		value = f.fetch(name);
+	}
+	public String fetch(String name) {
+		// in real life this would look up "name" in some table or database
+		return "jabberwocky";
+	}
+}
