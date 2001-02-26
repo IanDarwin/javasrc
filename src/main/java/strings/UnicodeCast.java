b89828bd99612186121c6516447396b36ac96e66
Ian Darwin <ian@darwinsys.com>
2001-02-26 18:34:54 +0000

    New

---
+++ src/main/java/strings/UnicodeCast.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+/** Convert Unicode, ASCII and byte/int. */
+public class UnicodeCast {
+	public static void main(String[] args) {
+
+		// Simple arithmetic on chars - VERY BAD for internationalization!
+		System.out.println("'a' + 1 = " + (char)('a' + 1));
+
+		// Truncate characters by casting to byte (16-bit to 8-bit casting)
+		char yen = '\u00a5';		// Japanese Yen
+		char aeAcute = '\u01FC';	// Roman AE with acute accent
+		System.out.println("Yen as byte: " + (byte)yen);
+		System.out.println("AE' as byte: " + (byte)aeAcute);
+		System.out.println("Yen as byte to char: " + (char)(byte)yen);
+		System.out.println("AE' as byte to char: " + (char)(byte)aeAcute);
+
+		// Convert ints to chars
+		int iYen = 0xa5;
+		int iaeAcute = 0x01fc;
+		System.out.println("Yen from int = " + (char)iYen);
+		System.out.println("AE' from int = " + (char)iaeAcute);
+
+	}
+}
