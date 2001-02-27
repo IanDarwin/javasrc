fa1b221a55b1192a7d53d6d38d752b661a0b6e3b
Ian Darwin <ian@darwinsys.com>
2001-02-27 20:57:19 +0000

    New: simple example

---
+++ src/main/java/strings/SoundexSimple.java
@@ -0,0 +1,15 @@
+/** Simple demonstration of Soundex.  */
+public class SoundexSimple {
+
+	/** main */
+	public static void main(String[] args) { 
+		String[] names = {
+			"Darwin, Ian",
+			"Davidson, Greg",
+			"Darwent, William",
+			"Derwin, Daemon"
+		};
+		for (int i = 0; i< names.length; i++)
+			System.out.println(Soundex.soundex(names[i]) + ' ' + names[i]);
+	}
+}
