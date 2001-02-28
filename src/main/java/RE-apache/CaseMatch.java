8b32fb0f059563459e06dea78c39c95bb48a8e13
Ian Darwin <ian@darwinsys.com>
2001-02-28 23:56:02 +0000

    Get it working.

--- src/main/java/RE-apache/CaseMatch.java
+++ src/main/java/RE-apache/CaseMatch.java
@@ -5,19 +5,20 @@ import org.apache.regexp.*;
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
-public class RESimple {
+public class CaseMatch {
 	public static void main(String[] argv) throws RESyntaxException {
 		String pattern = "^q[^u]\\d+\\.";
 		String input = "QA777. is the next flight. It is on time.";
 
 		RE r = new RE(pattern, RE.MATCH_CASEINDEPENDENT);
 
+		boolean found;
 		found = r.match(input);			// will match any case
-		System.out.println("MATCH_CASEINDEPENDENT matched? " + found);
+		System.out.println("MATCH_CASEINDEPENDENT match " + found);
 
 		r.setMatchFlags(RE.MATCH_NORMAL);
 		found = r.match(input);		// will match case-sensitively
-		System.out.println("MATCH_NORMAL matched? " + found);
+		System.out.println("MATCH_NORMAL match was " + found);
 
 	}
 }
