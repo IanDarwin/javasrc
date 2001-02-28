06f2c14442773cfbc72e2d94eaa79c7f056d03d0
Ian Darwin <ian@darwinsys.com>
2001-02-28 17:37:01 +0000

    New: show case control.

---
+++ src/main/java/RE-apache/CaseMatch.java
@@ -0,0 +1,23 @@
+import org.apache.regexp.*;
+
+/**
+ * Show case control using RE class.
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class RESimple {
+	public static void main(String[] argv) throws RESyntaxException {
+		String pattern = "^q[^u]\\d+\\.";
+		String input = "QA777. is the next flight. It is on time.";
+
+		RE r = new RE(pattern, RE.MATCH_CASEINDEPENDENT);
+
+		found = r.match(input);			// will match any case
+		System.out.println("MATCH_CASEINDEPENDENT matched? " + found);
+
+		r.setMatchFlags(RE.MATCH_NORMAL);
+		found = r.match(input);		// will match case-sensitively
+		System.out.println("MATCH_NORMAL matched? " + found);
+
+	}
+}
