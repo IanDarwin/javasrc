e1df7cea17fa4ce85eba44506bd1a84ea104e285
Ian Darwin <ian@darwinsys.com>
2001-03-01 15:42:07 +0000

    New: simple

---
+++ src/main/java/numbers/RomanNumberSimple.java
@@ -0,0 +1,7 @@
+public class RomanNumberSimple {
+public static void main(String[] args) {
+	NumberFormat nf = new RomanNumberFormat();
+	int year = Calendar.getInstance().getYear();
+	System.out.println("2001->" + nf.format(year);
+}
+}
