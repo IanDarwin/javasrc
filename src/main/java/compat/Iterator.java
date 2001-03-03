db177529237d669b6c788f64ed7c149cea493b53
Ian Darwin <ian@darwinsys.com>
2001-03-03 20:18:26 +0000

    Updates for Data Structuring chapter.

--- src/main/java/compat/Iterator.java
+++ src/main/java/compat/Iterator.java
@@ -1,7 +1,20 @@
 package java.util;
 
+/** Degenerate version of Iterator interface, to allow
+ * JDK1.1-class compilers to compile Java 2 code.
+ * It won't WORK, but it should compile.
+ */
+
 public interface Iterator {
 	public boolean hasNext();
 	public Object next();
 	public void remove();
+}
+/** Degenerate decl of UnsupportedOperationException to let
+ * Iterators compile.
+ */
+class UnsupportedOperationException extends RuntimeException {
+	UnsupportedOperationException (String msg) {
+		super(msg);
+	}
 }
