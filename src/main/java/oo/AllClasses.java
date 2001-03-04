59ad5ddd52ab0300d3135d296fc1ef3104285b4f
Ian Darwin <ian@darwinsys.com>
2001-03-04 18:07:27 +0000

    Show all inner/nested class possibilities.

---
+++ src/main/java/oo/AllClasses.java
@@ -0,0 +1,13 @@
+public class  AllClasses {
+	/** Inner class can be used anywhere in this file */
+	public class Data {
+		int x;
+		int y;
+	}
+	public void getResults() {
+		addActionListener...
+}
+
+/** Class contained in same file as AllClasses, but can be used
+ * (with a warning) in other contexts.
+ */
