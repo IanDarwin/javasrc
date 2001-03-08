3e39afb7466dd20f66b1ebb309cd2c525266dfb1
Ian Darwin <ian@darwinsys.com>
2001-03-08 18:57:32 +0000

    New: Better way to convert filename to URL

---
+++ src/main/java/netweb/FileToURL.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+/** Convert a filename to a URL using the Java 2 java.io.File.toURL()
+ */
+public class FileToURL
+{
+	public static void main(String[] argv)
+	throws java.net.MalformedURLException {
+		java.net.URL u = new File("GetResource.java").toURL();
+		System.out.println(u);
+	}
+}
