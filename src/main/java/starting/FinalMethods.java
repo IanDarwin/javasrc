600a9b13f15c3d581c41f420a8e43d1e232bc4f5
Ian Darwin <ian@darwinsys.com>
2001-03-11 16:40:31 +0000

    Check out final methods and inlining

---
+++ src/main/java/starting/FinalMethods.java
@@ -0,0 +1,27 @@
+/** Investigate inlining of final methods.
+ * You need to investigate the resulting class file
+ * when generated from various compilers and options.
+ */
+public final class FinalMethods {
+	public static void main(String[] args) {
+		new FinalMethods().work();
+	}
+
+	public void work() {
+		methodA();
+		methodB();
+		methodC();
+	}
+
+	public void methodA() {
+		System.out.println("In MethodA");
+	}
+
+	private final void methodB() {
+		System.out.println("In MethodB");
+	}
+
+	public void methodC() {
+		System.out.println("In MethodC");
+	}
+}
