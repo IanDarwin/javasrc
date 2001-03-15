6833dc6e9c39171484695be7fb9857b4e50e8d01
Ian Darwin <ian@darwinsys.com>
2001-03-15 02:30:36 +0000

    new

---
+++ src/main/java/unfinished/JapanWriter.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class JapanWriter {
+	public static void main(String[] args) {
+		PrintWriter out = new PrintWriter(
+			new OutputStreamWriter(System.out, "Cp939"));
+
+		// Truncate characters by casting to byte (16-bit to 8-bit casting)
+		char yen = '\u00a5';		// Japanese Yen
+		char aeAcute = '\u01FC';	// Roman AE with acute accent
+		out.println("Yen : " + yen);
+		out.println("AE' : " + aeAcute);
+
+		out.close();
+	}
+}
+
