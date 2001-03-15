e8be40f6f71b7a6e2cc8f5213fd668dfc671aff2
Ian Darwin <ian@darwinsys.com>
2001-03-15 17:53:20 +0000

    New: read int from stdin

---
+++ src/main/java/io/ReadStdinInt.java
@@ -0,0 +1,23 @@
+import java.io.*;
+/**
+ * Read an int from Standard Input
+ * @author	Ian F. Darwin, ian@darwinsys.com
+ * @version 	$Id$
+ */
+public class ReadStdinInt {
+	public static void main(String[] ap) {
+		String line = null;
+		int val = 0;
+		try {
+			BufferedReader is = new BufferedReader(
+				new InputStreamReader(System.in));
+			line = is.readLine();
+			val = Integer.parseInt(line);
+		} catch (NumberFormatException ex) {
+			System.err.println("Not a valid number: " + line);
+		} catch (IOException e) {
+			System.err.println("Unexpected IO ERROR: " + e);
+		}
+		System.out.println("I read this number: " + val);
+	}
+}
