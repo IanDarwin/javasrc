70b31f43240adefc9af2599a7fc68a3160ce3adc
Ian Darwin <ian@darwinsys.com>
2001-04-09 13:21:37 +0000

    Cool: a Quiz Show buzz-in servlet.

---
+++ src/main/java/threads/buzzin/BuzzInServlet.java
@@ -0,0 +1,61 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/** A quiz-show "buzzer" servlet: the first respondant wins the chance
+ * to answer the skill-testing question. Correct operation depends on
+ * running in a Servlet container that CORRECTLY implements the Servlet
+ * spec, that is, a SINGLE INSTANCE of this servlet class exists, and it
+ * is run in a thread pool. This class does not implement "SingleThreadModel"
+ * so a correct Servlet implementation will use a single instance.
+ * <p>
+ * If you needed to work differently, you could synchronized on an object
+ * stored in the Servlet Application Context, at a slight increased cost
+ * in terms of system overhead.
+ */
+public class BuzzInServlet {
+
+	/** This controls the access */
+	protected static boolean buzzed = false;
+	/** who got the buzz? */
+	protected static String winner; 
+
+	/** doGet is called from the contestants web page.
+	 * Uses a synchronized code block to ensure that
+	 * only one contestant can change the state of "buzzed".
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		boolean igotit = false;
+		synchronized(this) {
+			if (!buzzed) {
+				igotit = buzzed = true;
+				winner = request.getRemoteUser();
+			}
+	 	}
+
+		PrintWriter out = response.getWriter();
+
+		if (igotit) {
+			out.println("YOU WIN");
+			// TODO - output HTML to play a sound file
+		} else {
+				out.println("Thanks for playing, " + request.getRemoteUser);
+				out.println(", but " + winner + " buzzed in first");
+		}
+	}
+
+	/** The Post method is used from an Administrator page (only in the
+	 * instructor/host's localweb directory). It is used to reset the
+	 * buzzer for the next question.
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		PrintWriter out = response.getWriter();
+		if (request.getParameter("hosthost").equals("syzzy") {
+			synchonized(this) {
+				buzzed = false;
+			}
+			out.println("RESET");
+		} else {
+			out.println("Your paltry attempts to breach security are rebuffed!");
+		}
+	}
+}
