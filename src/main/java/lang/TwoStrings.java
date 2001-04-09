35d64ad63dd35db7e8462879f07021322860d442
Ian Darwin <ian@darwinsys.com>
2001-04-09 12:25:13 +0000

    Expand to compare "a" and "a" as well as "a" and "new String("a")".

--- src/main/java/lang/TwoStrings.java
+++ src/main/java/lang/TwoStrings.java
@@ -1,10 +1,18 @@
 /**
- * See if Strings are shared on this implementation.
+ * See if Strings are shared in Java.
+ * The thing is, it depends on whether you say 
+ * "string" or new String("mystring");
  */
 public class TwoStrings {
 	public static void main(String[] argv) {
 		String one = "A String";
-		String two = new String(one);
+		String two = "A String";
+		String three = new String(one);
+		compare(one, two);
+		compare(two, three);
+	}
+	public static void compare(String one, String two) {
+		System.out.println("Comparing...");
 		if (one == two) {
 			System.out.println("Strings are shared: " +
 				one.hashCode() + ", " + two.hashCode());
@@ -14,5 +22,6 @@ public class TwoStrings {
 			System.out.println((Object)one);
 			System.out.println(two);
 		} else System.out.println("This is rather distressing, sir.");
+		System.out.println();
 	}
 }
