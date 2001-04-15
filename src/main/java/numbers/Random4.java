50f01a152e67f2d49321e8121bb0e6bf130885f6
Ian Darwin <ian@darwinsys.com>
2001-04-15 19:20:01 +0000

    Print nextDouble() and nextGaussian().

---
+++ src/main/java/numbers/Random4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+/** Print "n" calls to nextDouble() and nextGaussian() in raw form
+ * using java.util.Random.next*(); results can be plotted.
+ */
+public class Random4 {
+	public static void main(String[] argv) throws IOException {
+		// java.util.Random methods are non-static, do need to construct Math
+		Random r = new Random();
+		PrintWriter file1 = new PrintWriter(new FileWriter("file1"));
+		PrintWriter file2 = new PrintWriter(new FileWriter("file2"));
+		for (int i=0; i<10000; i++) {
+			file1.println(r.nextDouble());
+			file2.println(r.nextGaussian());
+		}
+		file1.close();
+		file2.close();
+	}
+}
