295b26e71963ada2bfb46342fdba9b00771dc616
Ian Darwin <ian@darwinsys.com>
2001-05-18 17:06:09 +0000

    new

---
+++ src/main/java/lang/InnerMethods.java
@@ -0,0 +1,14 @@
+/** Just to show that there is no such thing as inner methods in Java */
+public class InnerMethods {
+	public static void main(String[] args) {
+		new InnerMethods().work();
+	}
+
+	public void work() {	// EXPECT COMPILE ERROR
+		void inner() {
+			System.out.println("Hey, there is such a thing as Inner Methods");
+			System.out.println("in Java. Better tell James Gosling and Sun!");
+		}
+		inner();
+	}
+}
