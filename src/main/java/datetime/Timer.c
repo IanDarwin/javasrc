710e353851aef954195b9fd6fe29f12704a8fa46
Ian Darwin <ian@darwinsys.com>
2001-09-05 18:41:29 +0000

    Correct name of language(!).

--- src/main/java/datetime/Timer.c
+++ src/main/java/datetime/Timer.c
@@ -6,12 +6,12 @@
 int main(int argc, char* argv[]) {
 		time_t t0, t1;
 		t0=time(NULL);
-		printf("Java Starts at %ld\n", t0);
+		printf("C Starts at %ld\n", t0);
 		double k;
 		for (int i=0; i<1000000; i++)
 			k = 2.1 * sqrt((double)i);
 		t1=time(NULL);
-		printf("Java Ends at %ld\n", t0);
+		printf("C Ends at %ld\n", t0);
 		printf("Time=%ld\n", t1-t0);
 	}
 }
