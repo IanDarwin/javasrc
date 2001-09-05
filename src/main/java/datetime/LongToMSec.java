021463c401cbd3f1b9fb85702881a7724c7e8d04
Ian Darwin <ian@darwinsys.com>
2001-09-05 18:47:18 +0000

    New: implements code in Recipe 6.12 to convert time_t to seconds.

---
+++ src/main/java/datetime/LongToMSec.java
@@ -0,0 +1,18 @@
+/** Convert longs (time_t in UNIX terminology) to seconds.
+ */
+public class LongToMSec {
+	public static void main(String[] args) {
+		System.out.println(msToSecs(1000));
+		System.out.println(msToSecs(1100));
+		System.out.println(msToSecs(1024));
+	}
+
+	/** Convert a long ("time_t") to seconds and milliseconds */
+	public static String msToSecs(long t) {
+		// The first attempt fails for the case "1024"
+		//return t/1000 + "." + t%1000;
+
+		return Double.toString(t/1000D);
+	}
+}
+
