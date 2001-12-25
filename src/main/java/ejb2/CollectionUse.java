5c3197a3953650213d452462ecf9ee70921b9a57
IanDarwin <>
2001-12-25 21:01:47 +0000

    CartBean is a SessionBean. More demos.

---
+++ src/main/java/ejb2/CollectionUse.java
@@ -0,0 +1,37 @@
+import javax.naming.*;
+import javax.rmi.*;
+import java.util.*;
+
+/**
+ * Partial code to show using an Entity Bean's Collection-returning methods
+ *
+ * This program can <b>not</b> be run; it just shows the code for one part
+ * of the EJB-use puzzle.
+ *
+ * @author Ian Darwin, ian@darwinsys.com
+ * @version $Id$
+ */
+public class CollectionUse {
+	public static void main(String[] args) throws NamingException {
+		Context ctx = new InitialContext();
+		MyBeanHome h = (MyBeanHome) ctx.lookup("MyBeanHome");
+		Collection c = h.findAllByCategory("jazz");
+
+		Iterator it = c.iterator();
+		while (it.hasNext()) {
+			MyBean b = (MyBean)PortableRemoteObject.narrow(
+				it.next(), MyBean.class);
+			//
+			// can now call methods in this MyBean
+		}
+	}
+
+	// This is junk, just to make the main class compile
+	interface MyBeanHome {
+		public Collection findAllByCategory(String cat);
+	}
+
+	// This is junk, just to make the main class compile
+	class MyBean {
+	}
+}
--- src/main/java/ejb2/cart/cart/CartBean.java
+++ src/main/java/ejb2/cart/cart/CartBean.java
@@ -1,15 +1,15 @@
 import javax.ejb.*
 
-public class CartBean extends EntityBean {
+public class CartBean implements SessionBean {
 
-	private Vector cartItems();
+	private ArrayList cartItems;
 
 	public void ejbCreate() {
 		cartItems = new Vector();
 	}
 
 	public void add(Object o) {
-		cartItems.addElement(o);
+		cartItems.add(o);
 	}
 
 	public void checkOut() {
@@ -22,10 +22,9 @@ public class CartBean extends EntityBean {
 		Billing b = bHome.create(custID);
 
 		b.setTotal(totalPrice(cartItems));
-		
 	}
 
-	private int totalPrice(Vector v) {
+	private int totalPrice(List v) {
 		// not implemented yet
 		return 10000;	// i.e., 100.00
 	}
