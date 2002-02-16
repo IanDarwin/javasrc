9977264f9f2121daa01defbe65f54b2e41eb6720
Ian Darwin <ian@darwinsys.com>
2002-02-16 23:22:48 +0000

    WinClose2: EXIT_ON_CLOSE moved from WindowConstants to JFrame.

--- src/main/java/gui/WinClose2.java
+++ src/main/java/gui/WinClose2.java
@@ -10,7 +10,7 @@ public class WinClose2 {
 	public static void main(String[] argv) {
 		JFrame f = new JFrame("WinClose");
 		f.setSize(200, 100);
-		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 	}
 }
