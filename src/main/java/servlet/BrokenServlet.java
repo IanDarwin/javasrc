9f29d8287d6b756c900bd9033fd11a7ed3771d3d
IanDarwin <>
2002-03-15 18:14:24 +0000

    Intr: convert to Runnable, wasn't really used as Thread subclass. Mover: convert to Swing. Mover3: add stop and start capability. ProdCons: give a useful prompt. BrokenServlet: puzzle for the reader. UUID: generation of unique identifiers.

---
+++ src/main/java/servlet/BrokenServlet.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/*
+ * BrokenServlet -- so where is it broken, anyway?
+ * 
+ * @version $Id$
+ */
+public class BrokenServlet extends HttpServlet
+{
+	protected PrintWriter out;
+
+	/** Called in response to a GET request (data encoded in the URL) */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		out = response.getWriter(); 
+
+		// Simulate a database lookup
+		int result = 42;
+
+		print(result);
+
+	}
+
+	protected void print(int result);
+	{
+		out.println("<!DOCTYPE html PUBLIC " +
+			"\"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+			"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"\n" +
+			">");
+		out.println("<html>");
+		out.println("<head><title>Servlet Output</title></head>");
+		out.println("<body>");
+
+		out.println("<h1>Results</h1>");
+		out.println("<p>The result is " + result + "</p>");
+
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+}
