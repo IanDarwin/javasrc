bacaf4821b4502fcf77a4b647bfa807acfe66c5f
Ian Darwin <ian@darwinsys.com>
2002-06-11 13:41:57 +0000

    Ghost: regularize attributes as per Ant Guidelines. Use Execute class to do the exec. Add dummy ghost.exe for testing.

--- src/main/java/ant/GhostTask.java
+++ src/main/java/ant/GhostTask.java
@@ -1,31 +1,36 @@
 package com.darwinsys.ant;
 
 import java.io.*;
+import java.util.*;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+
+import org.apache.tools.ant.taskdefs.Execute;
 
 /**
  * An Ant Task to run Symantec Ghost in disk-to-image mode
  */
 public class GhostTask extends Task {
 	/** The source drive/directory */
-	private String source;
+	private String srcDir;
 	/** The target drive/directory */
-	private String target;
+	private String destFile;
 	/** Ghost home directory */
 	private String ghostHome = "C:\\program files\\symantec\\ghost";
 	/** Ghost program, version, extension, ... */
 	private String ghostProg = "ghost.exe";
+	/** whether to print+return or throw BuildException */
+	boolean failOnError = true;
 
 	/** Set the drive or directory name */
-	public void setSource(String source) {
-		this.source = source;
+	public void setSrcDir(String srcDir) {
+		this.srcDir = srcDir;
 	}
 
-	/** Set the target file name */
-	public void settarget(String target) {
-		this.target = target;
+	/** Set the destFile file name */
+	public void setDestFile(String destFile) {
+		this.destFile = destFile;
 	}
 
 	/** Set the Ghost home directory */
@@ -38,43 +43,78 @@ public class GhostTask extends Task {
 		this.ghostProg = GhostProg;
 	}
 
+	/** whether to print+return or throw BuildException */
+	public void setFailOnError(boolean fail) {
+		this.failOnError = fail;
+	}
+
+	/** Handle an error, either by throwing a BuildException or by
+	 * printing the message and the Throwable
+	 * @see setFailOnError(boolean);
+	 */
+	protected void die(String msg, Throwable t) {
+		if (failOnError) {
+			throw new BuildException(msg, t);
+		}
+		System.err.println(msg);
+		t.printStackTrace(System.err);
+	}
+
+	/** Handle an error, either by throwing a BuildException or by
+	 * printing the message.
+	 * @see setFailOnError(boolean);
+	 */
+	protected void die(String msg) {
+		if (failOnError) {
+				throw new BuildException(msg);
+		}
+		System.err.println(msg);
+	}
+
 	/** We should be all set now; run the task */
 	public void execute() throws BuildException {
+		if (srcDir == null)
+			die("src parameter is not set");
+		if (destFile == null)
+			die("dstFile parameter is not set");
 		System.out.println("Ghost is: " + ghostHome + "\\" + "ghost.exe");
 		File d = new File(ghostHome);
 		if (!d.isDirectory()) {
-			throw new BuildException("Ghost home " + ghostHome + " does not exist.");
+			die("Ghost home " + ghostHome + " does not exist.");
 		}
 		if (!d.isDirectory()) {
-			throw new BuildException("Ghost home " + ghostHome + " does not contain " + ghostProg);
+			die("Ghost home " + ghostHome + " does not contain " + ghostProg);
 		}
 		File prog = new File(d, "ghost.exe");
-		String command = null;
-		Process proc = null;
+		ArrayList command = new ArrayList();
+		Execute proc = null;
 		try { 
-			command = prog.getCanonicalPath() + "/d " + source + " " + target;
+			command.add(prog.getCanonicalPath() );
+			command.add("/d");
+			command.add(srcDir);
+			command.add(destFile);
 			System.out.println("Command is: " + command);
 
+			proc = new Execute();
+			proc.setAntRun(getProject());
+			proc.setVMLauncher(true); // OK to run directly, no wildcards &c.
+
+			String[] args = (String[])command.toArray(new String[0]);
+			proc.setCommandline(args);
+
 			// START IT RUNNING
-			proc = Runtime.getRuntime().exec(command);
+			proc.execute();
 
-			// ...
-			// XXX Get the stdout and stderr, copy to our output.
-			// See other Ant tasks to see best way...
-			// ...
-
-			// Wait for it to end...
-			proc.waitFor();
+			// Don't need to wait for it to end...
+			// proc.waitFor();
 
 		} catch (IOException ex) {
-			throw new BuildException("Failure in getCanonicalPath()", ex);
-		} catch (InterruptedException ex) {
-			throw new BuildException("Unexpected InterruptedException", ex);
+			die("Input or Output Failure", ex);
 		}
 		// assert(proc != null);
-		int ret = proc.exitValue();
+		int ret = proc.getExitValue();
 		if (ret != 0) {
-			throw new BuildException("Ghost program returned exitStatus " + ret);
+			die("Ghost program returned exitStatus " + ret);
 		}
 		// No news is good news; ant will say "BUILD SUCCESSFUL" if no failure.
 	}
