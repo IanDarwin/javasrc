41f1803fe42676d5c8328d141ab02ff55ff67778
Ian Darwin <ian@darwinsys.com>
2002-06-18 17:08:18 +0000

    Updated: create output file if filename given and doesn't exist yet.

--- src/main/java/spdf/Demo.java
+++ src/main/java/spdf/Demo.java
@@ -4,16 +4,25 @@ import com.darwinsys.spdf.Text;
 import com.darwinsys.spdf.MoveTo;
 import java.io.*;
 
-/** trivial text test of SPDF package
+/** A simple text test of SPDF package
  */
 public class Demo {
-	public static void main(String[] argv) {
-		PrintWriter pout = new PrintWriter(System.out);
+	public static void main(String[] argv) throws IOException {
+		PrintWriter pout;
+		if (argv.length == 0) {
+			pout = new PrintWriter(System.out);
+		} else {
+			if (new File(argv[0]).exists()) {
+				throw new IOException(
+				"Output file " + argv[0] + " already exists");
+			}
+			pout = new PrintWriter(new FileWriter(argv[0]));
+		}
 		PDF p = new PDF(pout);
 		Page p1 = new Page(p);
 		p1.add(new MoveTo(p, 100, 600));
 		p1.add(new Text(p, "Hello world, live on the web."));
-		p1.add(new Text(p, "Hello world, live on the web."));
+		p1.add(new Text(p, "Hello world, line 2 on the web."));
 		p.add(p1);
 		p.setAuthor("Ian F. Darwin");
 		p.writePDF();
