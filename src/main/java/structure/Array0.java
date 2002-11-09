5193261d43c42c77b2a2af6a004e292979d38c94
Ian Darwin <ian@darwinsys.com>
2002-11-09 16:27:49 +0000

    new

---
+++ src/main/java/structure/Array0.java
@@ -0,0 +1,19 @@
+/** Simple example
+ * @author Ian Darwin
+ * @version $Id$
+ */
+public class Array0  {
+	// Use this initializer form when the data are fixed:
+	static int monthLen[] = {
+			31, 28, 31, 30,
+			31, 30, 31, 31,
+			30, 31, 30, 31,
+	};
+		
+	public static void main(String[] argv) {
+		for (int i=0; i<monthLen.length; i++) {
+			System.out.println("Month " + (i+1) + " has " +
+				monthLen[i] + " days.");
+		}
+	}
+}
---
+++ src/main/test/java/structure/LinkListTest.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+/**
+ * Linked list class, written out in full using Java.
+ * @deprecated	Supplanted by java.util.LinkedList
+ * @author	Ian Darwin, ian@darwinsys.com
+ */
+public class LinkListTest {
+	public static void main(String[] argv) {
+		System.out.println("Here is a demo of implementing a List in Java");
+		System.out.println("(you know it don't come easy....)");
+		LinkList l = new LinkList();
+		l.add(new Object());
+		l.add("Hello");
+		l.add("End of list");
+		System.out.println("Here is a list of all the elements, from get");
+		System.out.println(l.get(0));
+		System.out.println(l.get(1));
+		System.out.println(l.get(2));
+
+		if (l.lookup("Hello"))
+			System.err.println("Lookup works");
+		else
+			System.err.println("Lookup does not work");
+
+		ListIterator li = l.listIterator();
+		if (li == null) {
+			throw new IllegalStateException("listIterator() returned null");
+		}
+
+		// Run the list frontwards
+		while (li.hasNext())
+			System.out.println("Next to: " + li.next());
+
+		// Now, for added fun, let's walk the linked list backwards.
+		while (li.hasPrevious()) {
+			System.out.println("Back to: " + li.previous());
+		}
+		
+	}
+}
