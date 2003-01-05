657c4de1a524370e609ae14e8b83ea3d73cf5061
Ian Darwin <ian@darwinsys.com>
2003-01-05 23:00:06 +0000

    Last javasrc commit from i386 instance of daroad. NETWatch/NetWatch.properties: update for new exercis. datetime/GregCalDemo.java environ/SysPropDemo.java - use Properties().store() instead of deprec. list(). servlet/chat/WEB-INF/web.xml - register new ChatListener. tools/build.xml - try running jr after compiling it. tools/jr.java  - big rewrite: mostly works, doesn't subclass sun.tools.javac.

--- src/main/java/environ/SysPropDemo.java
+++ src/main/java/environ/SysPropDemo.java
@@ -1,12 +1,13 @@
+import java.io.IOException;
 import java.util.*;
+
 /**
  * List one or more item(s) from System Properties
  */
 public class SysPropDemo {
-	public static void main(String[] argv) {
-		System.out.println("System Properties:");
+	public static void main(String[] argv) throws IOException {
 		if (argv.length == 0)
-			System.getProperties().list(System.out);
+			System.getProperties().store(System.out, "System Properties:");
 		else for (int i=0; i<argv.length; i++) {
 			String s = argv[i];
 		 	System.out.println(s + " = " + 
