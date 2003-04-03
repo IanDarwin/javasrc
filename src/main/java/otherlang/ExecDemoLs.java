4b0659c4ea245071cb0a9192e6eb02bee2b02ce7
Ian Darwin <ian@darwinsys.com>
2003-04-03 14:22:17 +0000

    Use a Thread to wait for the other process

--- src/main/java/otherlang/ExecDemoLs.java
+++ src/main/java/otherlang/ExecDemoLs.java
@@ -20,12 +20,30 @@ public class ExecDemoLs {
 
 		Debug.println("exec", "In Main after exec");
 
+		boolean done = false;
+
+		// Optional: start a thread to wait for the process to terminate.
+		// Don't just wait in main line, but here set a "done" flag and
+		// use that to control the main reading loop below.
+		Thread waiter = new Thread() {
+			public void run() {
+				try {
+					p.waitFor();
+				} catch (InterruptedException ex) {
+					// OK, just quit.
+					return;
+				}
+				System.out.println("Program terminated!");
+				done = true;
+			}
+		}.start();
+
 		// getInputStream gives an Input stream connected to 
 		// the process p's standard output (and vice versa). We use
 		// that to construct a BufferedReader so we can readLine() it.
 		is = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-		while ((line = is.readLine()) != null)
+		while (!done && ((line = is.readLine()) != null))
 			System.out.println(line);
 		
 		Debug.println("exec", "In Main after EOF");
