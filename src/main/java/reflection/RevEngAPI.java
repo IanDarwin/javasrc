b33965e6d4b6fe7bef860291a5e550d7e6281756
Ian Darwin <ian@darwinsys.com>
2003-04-08 20:01:10 +0000

    Bash into shape: printing of classes is starting to come together

--- src/main/java/reflection/RevEngAPI.java
+++ src/main/java/reflection/RevEngAPI.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.*;
+
 /** Make up a compilable version of the API, for use outside Sun license.
  * All public API info is public on Sun's web site, so this does not disclose
  * anything that is Sun Confidential.
@@ -5,11 +7,66 @@
  */
 public class MakeAPI extends APIFormatter {
 
-	public void printClass(Class C) {
+	public static void main(String[] argv) throws Exception {
+		new MakeAPI().doArgs(argv);
+	}
+
+	private String[] argNames = { "a", "b", "c", "d", "e", "f", "g", "h" };
+
+	public void doClass(Class c) {
+		String className = c.getName();
+
+		// Inner class
+		if (className.indexOf('$') != -1)
+			return;
+
 		// get name, as String, with . --> /
+		String fileName = className.replace('.','/')+".class";
+
+		System.out.println(className + " --> " + fileName);
+
 		// get directory part, mkdirs it
 		// create the file.
-		// print methods and fields to it.
-		// close it.
+
+		// print class header
+		System.out.println(c.toString() + '{');
+
+		// print constructors, methods and fields to it.
+		Constructor[] ctors = c.getDeclaredConstructors();
+		for (int i=0; i< ctors.length; i++) {
+			System.out.print('\t');
+			System.out.println(ctors[i] + "{}");
+		}
+		Method[] mems = c.getDeclaredMethods();
+		for (int i=0; i< mems.length; i++) {
+			Method m = mems[i];
+			int mods = m.getModifiers();
+			if (isPrivate(mods))
+				continue;
+			System.out.print('\t');
+			//System.out.print(modNames[mods]);
+			System.out.print(mods);
+			System.out.print(m.getReturnType());
+			System.out.print(m.getName() + "(");
+			Class[] classes = m.getParameterTypes();
+			for (int j=0; j<classes.length; j++) {
+				if (j>0) System.out.println(", ");
+				println(classes[j].getName() + ' ' + argNames[j]);
+			}
+			System.out.println(") + {");
+			System.out.println("\treturn;");
+			System.out.println("\t}");
+		}
+		Field[] flds = c.getDeclaredFields();
+		for (int i=0; i< flds.length; i++) {
+			System.out.print('\t');
+			System.out.println(flds[i]);
+		}
+		System.out.println("}");
+		// close output file.
+	}
+
+	private final boolean isPrivate(int i) {
+		return false;
 	}
 }
