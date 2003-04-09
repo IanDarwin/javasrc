7c52f5adc013b9659f326b796e6f318bb0cb652a
Ian Darwin <ian@darwinsys.com>
2003-04-09 12:25:49 +0000

    Sketch out creation of directory and file. Sketch out getting value for final variables. Add hook for building trivial build.xml file at end.

--- src/main/java/reflection/RevEngAPI.java
+++ src/main/java/reflection/RevEngAPI.java
@@ -21,12 +21,18 @@ public class MakeAPI extends APIFormatter {
 			return;
 
 		// get name, as String, with . --> /
-		String fileName = className.replace('.','/')+".class";
+		String slashName = className.replace('.','/');
+		String fileName = slashName + ".class";
 
 		System.out.println(className + " --> " + fileName);
 
-		// get directory part, mkdirs it
+		// String dirName = slashName.substring(0,
+		//		slashName.lastIndexOf("/"));
+		// new File(dirName).mkdirs();
+
 		// create the file.
+		// PrintWriter out = new PrintWriter(new FileWriter(fileName));
+
 		PrintStream out = System.out;	// fake for now
 
 		// print class header
@@ -60,8 +66,12 @@ public class MakeAPI extends APIFormatter {
 		}
 		Field[] flds = c.getDeclaredFields();
 		for (int i=0; i< flds.length; i++) {
+			Field f = flds[i];
 			out.print('\t');
-			out.println(flds[i]);
+			out.print(f);
+			// if isFinal(f) && isStatic(f)
+			//	get its value
+			out.println(';');
 		}
 		out.println("}");
 		// close output file.
@@ -69,5 +79,13 @@ public class MakeAPI extends APIFormatter {
 
 	private final boolean isPrivate(int i) {
 		return i & Modifiers.PRIVATE;
+	}
+
+	public void startFile() {
+		// save filename as project name
+	}
+
+	public void endFile() {
+		// generate a trivial "build.xml" for Ant to create the jar file.
 	}
 }
