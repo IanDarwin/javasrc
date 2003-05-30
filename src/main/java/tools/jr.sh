6b350172a979178d380b846741dee4720108ccc8
Ian Darwin <ian@darwinsys.com>
2003-05-30 18:10:55 +0000

    New/old: sh version of jr

---
+++ src/main/java/tools/jr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+darwinsys_jar="${HOME}/classes/ext/com-darwinsys-util.jar"
+tools_jar="/usr/local/jdk1.3.1-linux/lib/tools.jar"
+
+java -classpath ".:${tools_jar}:${darwinsys_jar}" com.darwinsys.tools.jr $*
