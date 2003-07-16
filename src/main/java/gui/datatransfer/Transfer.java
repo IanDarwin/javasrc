c52906c355f759d5541d855bec2d460b79e8ab34
Ian Darwin <ian@darwinsys.com>
2003-07-16 01:28:36 +0000

    Make three JComponents fields; add MouseListener; add routine to set TransferHandlers

--- src/main/java/gui/datatransfer/Transfer.java
+++ src/main/java/gui/datatransfer/Transfer.java
@@ -1,27 +1,40 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.datatransfer.*;
 import javax.swing.*;
 
+/**
+ * Demonstrate various aspects of Swing "data transfer".
+ * @author Ian Darwin, http://www.darwinsys.com
+ * @author Jonathan Fuerth, http://www.SQLPower.ca
+ */
 public class Transfer extends JFrame {
 	public static void main(String[] args) {
 		new Transfer().setVisible(true);
 	}
 
+	private JTextField tf;
+	private JLabel l;
+	private JComboBox propertyNameCombo;
+
 	public Transfer() {
+
+		// Establish the GUI
 		Container cp = new Box(BoxLayout.X_AXIS);
 		setContentPane(cp);
 		JPanel firstPanel = new JPanel();
-		JComboBox propertyNameCombo = new JComboBox();
+		propertyNameCombo = new JComboBox();
 		propertyNameCombo.addItem("font");
 		propertyNameCombo.addItem("text");
 		propertyNameCombo.addItem("background");
 		propertyNameCombo.addItem("foreground");
 		firstPanel.add(propertyNameCombo);
 		cp.add(firstPanel);
-		JTextField tf = new JTextField(10);
+		tf = new JTextField(10);
+		tf.setDragEnabled(true);
 		cp.add(tf);
 		cp.add(Box.createGlue());
-		JLabel l = new JLabel("Hello");
+		l = new JLabel("Hello");
 		cp.add(l);
 		cp.add(Box.createGlue());
 		JSlider stryder = new JSlider(SwingConstants.VERTICAL);
@@ -30,5 +43,24 @@ public class Transfer extends JFrame {
 		cp.add(stryder);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 300);
+
+		// Add Listeners and Converters
+		mySetTransferHandlers();
+
+		MouseListener myDragListener = new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				JComponent c = (JComponent)e.getSource();
+				TransferHandler handler = c.getTransferHandler();
+				handler.exportAsDrag(c, e, TransferHandler.COPY);
+			}
+		};
+		l.addMouseListener(myDragListener);
+	}
+
+	private void mySetTransferHandlers() {
+		TransferHandler th = 
+			new TransferHandler((String)propertyNameCombo.getSelectedItem());
+		tf.setTransferHandler(th);
+		l.setTransferHandler(th);
 	}
 }
