3e27929eb5fd59148bc8f450820182e3201b1d21
Ian Darwin <ian@darwinsys.com>
2003-07-16 00:55:24 +0000

    Mostly re-create the GUI from this afternoon's session :-)

--- src/main/java/gui/datatransfer/Transfer.java
+++ src/main/java/gui/datatransfer/Transfer.java
@@ -1,2 +1,34 @@
-public class Transfer {
+import java.awt.*;
+import java.awt.datatransfer.*;
+import javax.swing.*;
+
+public class Transfer extends JFrame {
+	public static void main(String[] args) {
+		new Transfer().setVisible(true);
+	}
+
+	public Transfer() {
+		Container cp = new Box(BoxLayout.X_AXIS);
+		setContentPane(cp);
+		JPanel firstPanel = new JPanel();
+		JComboBox propertyNameCombo = new JComboBox();
+		propertyNameCombo.addItem("font");
+		propertyNameCombo.addItem("text");
+		propertyNameCombo.addItem("background");
+		propertyNameCombo.addItem("foreground");
+		firstPanel.add(propertyNameCombo);
+		cp.add(firstPanel);
+		JTextField tf = new JTextField(10);
+		cp.add(tf);
+		cp.add(Box.createGlue());
+		JLabel l = new JLabel("Hello");
+		cp.add(l);
+		cp.add(Box.createGlue());
+		JSlider stryder = new JSlider(SwingConstants.VERTICAL);
+		stryder.setMinimum(10);
+		stryder.setMaximum(72);
+		cp.add(stryder);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 300);
+	}
 }
