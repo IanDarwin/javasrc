bf986b37432139eab61a3adc92c67b2b841d6c17
IanDarwin <>
2003-08-24 12:33:22 +0000

    New.

---
+++ src/main/java/servlet/HelloServletMinimal.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServletMinimal extends HttpServlet{
+
+	public void doGet(HttpServletRequest request,
+		HttpServletResponse response) throws IOException {
+
+		PrintWriter out = response.getWriter();
+
+		response.setContentType("text/html");
+
+		out.println("<h1>Hello from a Servlet</h1>");
+		out.println("<p>Server time is now ");
+		out.println(new Date().toString());
+	}
+}
