b67351838b584b17f1bd6ad900834a896c8d75e6
Ian Darwin <ian@darwinsys.com>
2003-08-24 12:33:22 +0000

    New.

---
+++ src/main/java/lang/Array2D.java
@@ -0,0 +1,11 @@
+public class Array2D {
+	public static void main(String[] args) {
+		int[][] data;
+		data = new int[10][];
+		data[0] = new int[5];
+		data[1] = new int[20];
+		System.out.println("data.length = " + data.length);
+		System.out.println("data[0].length = " + data[0].length);
+		System.out.println("data[1].length = " + data[1].length);
+	}
+}
---
+++ src/main/java/lang/MultiSuperInterfaces.java
@@ -0,0 +1,26 @@
+public class MultiSuperInterfaces {
+	public interface Marker 
+		extends java.io.Serializable, java.rmi.Remote, java.lang.Runnable {
+	}
+	public class Marked implements Marker {
+		public void run() {
+			// needed for Runnable
+		}
+	}
+	public static void main(String[] args) {
+		new MultiSuperInterfaces().print();
+	}
+
+	void print() {
+		Object o = new Marked();
+		if (o instanceof java.io.Serializable) {
+			System.out.println("Is Serializable");
+		}
+		if (o instanceof java.rmi.Remote) {
+			System.out.println("Is Remote");
+		}
+		if (o instanceof java.lang.Runnable) {
+			System.out.println("Is Runnable");
+		}
+	}
+}
