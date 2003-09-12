f5c97ebcfae4c9eff065744fee7ed2e7598f71a9
Ian Darwin <ian@darwinsys.com>
2003-09-12 14:18:17 +0000

    Write some text into the HTML file. Put try/catch in the Applet to show something in the status bar if there's an exception.

--- src/main/java/applet/AppletJavaScript.java
+++ src/main/java/applet/AppletJavaScript.java
@@ -4,9 +4,9 @@ import java.awt.event.*;
 import netscape.javascript.*;
 
 /** Simple demonstration of an Applet invoking JavaScript.
- * SERIOUSLY NETSCAPE DEPENDANT; requires a Netscape "javaxx.jar"
- * file in classpath to compile and a browser that makes the JSObject
- * and related classes available at runtime.
+ * SERIOUSLY BROWSER DEPENDANT; requires a Netscape "javaxx.jar" or
+ * "netscape.jar" on CLASSPATH to compile, AND a browser that makes the 
+ * JSObject and related classes available at runtime.
  */
 public class AppletJavaScript extends Applet {
 	/** Called to set up the Applet */
@@ -15,18 +15,23 @@ public class AppletJavaScript extends Applet {
 		Button button = new Button("Launch");
 		ActionListener listener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String htmlText = ta.getText();
-				JSObject topWindow = 
-				JSObject.getWindow(AppletJavaScript.this);
-				Object args[] = new Object[3];
-				args[2] = "width=300,height=300," +
-					"location=0,menubar=0,status=0,toolbar=0";
-				JSObject popupWindow = 
-					(JSObject)topWindow.call("open", args);
-				JSObject document = (JSObject)
-					popupWindow.getMember("document");
-				args = new Object[] {htmlText};
-				document.call("write", args);
+				try {
+					String htmlText = ta.getText();
+					JSObject topWindow = 
+						JSObject.getWindow(AppletJavaScript.this);
+					Object args[] = new Object[3];
+					// XXX what goes in 0 and 1 of this array?
+					args[2] = "width=300,height=300," +
+						"location=0,menubar=0,status=0,toolbar=0";
+					JSObject popupWindow = 
+						(JSObject)topWindow.call("open", args);
+					JSObject document = (JSObject)
+						popupWindow.getMember("document");
+					args = new Object[] {htmlText};
+					document.call("write", args);
+				} catch (Exception ex) {
+					showStatus(ex.getClass().getName() + " " + ex.getMessage());
+				}
 			}
 		};
 		button.addActionListener(listener);
