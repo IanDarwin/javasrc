f8e9781230d99e460770649b9b2584cf20d85af4
Ian Darwin <ian@darwinsys.com>
2003-11-24 20:36:23 +0000

    Remove dead import

--- src/main/java/numbers/RealValues.java
+++ src/main/java/numbers/RealValues.java
@@ -1,13 +1,10 @@
-import java.text.*;
 /**
  * Show real values.
  * @author Ian F. Darwin, ian@darwinsys.com
  * @version $Id$
  */
 public class RealValues {
-	// Whether strictfp is commented out or not, it seems to
-	// give the same answers, 0.3 and 0.8999999...
-	public static /*strictfp*/ void main(String[] argv) {
+	public static void main(String[] argv) {
 		System.out.println("The real 0.3 is " + 0.3);
 		System.out.println("The real 0.3 times 3 is " + 0.3*3);
 	}
