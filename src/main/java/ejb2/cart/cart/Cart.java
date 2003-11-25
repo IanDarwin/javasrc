1999085250ce109704efde0c8fa7882bc910d538
IanDarwin <>
2003-11-25 17:28:42 +0000

    Document; add throws; add methods

--- src/main/java/ejb2/cart/cart/Cart.java
+++ src/main/java/ejb2/cart/cart/Cart.java
@@ -1,5 +1,18 @@
-import javax.ejb.*
+import javax.ejb.*;
+import java.rmi.RemoteException;
 
+/** The Remote Interface for a Shopping Cart.
+ * The whole notion is not very realistic; who'd keep a Cart
+ * remotely, instead of locally or in an HttpSession in a web app?
+ */
 public interface Cart extends EJBObject {
-	public void add(Object o);
+	public void add(Product o) throws RemoteException;
+	public void remove(Product o) throws RemoteException;
+	/** The size of the cart, in items, like List.size() */
+	public int size() throws RemoteException;
+	public void checkOut() throws RemoteException;
+	/** Get the order number created by checkOut */
+	public int getOrderNumber() throws RemoteException;
+	/** Get the billing invoice number created by checkOut */
+	public int getBillingNumber() throws RemoteException;
 }
