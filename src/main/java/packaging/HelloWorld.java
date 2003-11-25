d0eb73ac6ce55250b21dd2871303853b8c909996
Ian Darwin <ian@darwinsys.com>
2003-11-25 17:29:31 +0000

    Now Swing-based example; add Created ident in manifest

--- src/main/java/packaging/HelloWorld.java
+++ src/main/java/packaging/HelloWorld.java
@@ -1,9 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Your basic, minimal, Hello World type program in Java.
  * $Id$
  */
 public class HelloWorld {
 	public static void main(String[] argv) {
-		System.out.println("Hello, World of Java");
+		String message = "Hello, World of Java";
+		JFrame jf = new JFrame(message);
+		jf.getContentPane().add(new JLabel(message));
+		jf.pack();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
 	}
 }
