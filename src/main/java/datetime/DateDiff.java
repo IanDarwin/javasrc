ae3ee71fc18054d9fd5437d9f1a82aa6791019f7
Ian Darwin <ian@darwinsys.com>
2003-11-25 14:23:00 +0000

    Remove Y2K refs; modernize

--- src/main/java/datetime/DateDiff.java
+++ src/main/java/datetime/DateDiff.java
@@ -4,20 +4,16 @@ import java.util.*;
  */
 public class DateDiff {
 	public static void main(String[] av) {
-		/** The ending date. This value
-		 * doubles as a Y2K countdown time.
-		 */
-		Date d1 = new GregorianCalendar(1999,11,31,23,59).getTime();
+		/** The date at the end of the last century */
+		Date d1 = new GregorianCalendar(2000,11,31,23,59).getTime();
 
 		/** Today's date */
-		Date d2 = new Date();
+		Date today = new Date();
 
 		// Get msec from each, and subtract.
-		long diff = d2.getTime() - d1.getTime();
+		long diff = today.getTime() - d1.getTime();
 
-		System.out.println("Difference between " + d2 + "\n" +
-			"\tand Y2K is " +
-			(diff / (1000*60*60*24)) +
-			" days.");
+		System.out.println("The 21st century (up to " + today + 
+			") is " + (diff / (1000*60*60*24)) + " days old.");
 	}
 }
