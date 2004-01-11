bb3dcc6eafc7a0eaebe0d8f3ba8f2fd2915d7763
Ian Darwin <ian@darwinsys.com>
2004-01-11 23:35:22 +0000

    AutoBoxing with HashMap

---
+++ src/main/java/structure/HashDemoGeneric.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class HashDemoGeneric {
+	public static void main(String[] args) {
+		HashMap<Integer,String> map = new HashMap<Integer,String>();
+
+		map.put(1, "Ian");
+		map.put(42, "Scott");
+		map.put(123, "Somebody else");
+
+		String name = map.get(42);
+		System.out.println(name);
+	}
+}
