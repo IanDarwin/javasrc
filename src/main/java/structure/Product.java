5cbfdc138d1ff2ff5495877015ddc85e1a74c5cd
Ian Darwin <ian@darwinsys.com>
2004-01-11 22:59:43 +0000

    Case labels must be un-qualified enum cases

--- src/main/java/structure/Product.java
+++ src/main/java/structure/Product.java
@@ -9,23 +9,23 @@ public class Product {
 		this.title = title;
 		this.artist = artist;
 		switch (media) {
-		case Media.book:
-			Debug.print("media", title + " is a book");
+		case book:
+			Debug.println("media", title + " is a book");
 			break;
-		case Media.music_cd:
-			Debug.print("media", title + " is a CD");
+		case music_cd:
+			Debug.println("media", title + " is a CD");
 			break;
 		case Media.music_vinyl:
-			Debug.print("media", title + " is a relic");
+			Debug.println("media", title + " is a relic");
 			break;
 		case Media.movie_vhs:
-			Debug.print("media", title + " is on tape");
+			Debug.println("media", title + " is on tape");
 			break;
 		case Media.movie_dvd:
-			Debug.print("media", title + " is on DVD");
+			Debug.println("media", title + " is on DVD");
 			break;
 		default:
-			Debug.print("media", "Warning: " + title + 
+			Debug.println("media", "Warning: " + title + 
 				": Unknown media " + media);
 			break;
 		}
