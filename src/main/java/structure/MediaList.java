10ee6a81f03a77b73918229b050122acaccbb49d
Ian Darwin <ian@darwinsys.com>
2004-01-11 17:43:31 +0000

    Simple demo to list an enum

---
+++ src/main/java/structure/MediaList.java
@@ -0,0 +1,8 @@
+/** Simple demo to print all the types of media. */
+public class MediaList {
+	public static void main(String[] args) {
+		for (Media m : Media.VALUES) {
+			System.out.println(m);
+		}
+	}
+}
