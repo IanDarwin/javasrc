f5b6d957a4804ea0c3ede38d0840277320f791d4
Ian Darwin <ian@darwinsys.com>
2004-01-11 23:00:10 +0000

    Add main that runs it

--- src/main/java/structure/ForeachDemo.java
+++ src/main/java/structure/ForeachDemo.java
@@ -1,7 +1,16 @@
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
 public class ForeachDemo {
-	void iterate(Collection c) {
+	static void iterate(Collection c) {
 	    for (Object o : c)
  		 System.out.println(o);
+	}
+	public static void main(String args[]) {
+		List<String> l = new ArrayList<String>();
+		l.add("Toronto");
+		l.add("Stockholm");
+		iterate(l);
 	}
 }
