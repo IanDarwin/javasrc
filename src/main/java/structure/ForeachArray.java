6d6ede13ec5f26b40bbf6dca2aa820c999470efb
Ian Darwin <ian@darwinsys.com>
2004-01-12 01:10:30 +0000

    rewrite, get rid of garbage imple from other class

--- src/main/java/structure/ForeachArray.java
+++ src/main/java/structure/ForeachArray.java
@@ -1,12 +1,8 @@
 public class ForeachArray {
-	}
 	public static void main(String args[]) {
-	    String 
-	    for (Object o : data)
- 		 System.out.println(o);
-		List<String> l = new ArrayList<String>();
-		l.add("Toronto");
-		l.add("Stockholm");
-		iterate(l);
+	    String[] data = { "Toronto", "Stockholm" };
+	    for (String s : data) {
+			System.out.println(s);
+		}
 	}
 }
