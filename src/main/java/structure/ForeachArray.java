a2e8b5aa20c7c6504f8dd8fb80e790a56cdae135
Ian Darwin <ian@darwinsys.com>
2004-01-12 01:07:33 +0000

    Use foreach on an array

---
+++ src/main/java/structure/ForeachArray.java
@@ -0,0 +1,12 @@
+public class ForeachArray {
+	}
+	public static void main(String args[]) {
+	    String 
+	    for (Object o : data)
+ 		 System.out.println(o);
+		List<String> l = new ArrayList<String>();
+		l.add("Toronto");
+		l.add("Stockholm");
+		iterate(l);
+	}
+}
