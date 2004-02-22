cbae99e5d3dadb4ee753ef77f27eb085364d20e6
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:03:42 +0000

    The detab() method now takes a PrintWriter as 2nd arginstead of hardcoding System.out.

--- src/main/java/strings/DeTab.java
+++ src/main/java/strings/DeTab.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /** detab- replace blanks by tabs and blanks.
  * @author Ian F. Darwin, http://www.darwinsys.com/
@@ -10,14 +11,15 @@ public class DeTab extends Tabs {
 
 	public static void main(String[] argv) throws IOException {
 		DeTab dt = new DeTab(8);
-		dt.detab(new BufferedReader(new InputStreamReader(System.in)));
+		dt.detab(new BufferedReader(new InputStreamReader(System.in)),
+				new PrintWriter(System.out));
 	}
 
 	public DeTab(int n) {
 		super(n);
 	}
 
-	public void detab(BufferedReader is) throws IOException {
+	public void detab(BufferedReader is, PrintWriter out) throws IOException {
 		String line;
 		char c;
 		int col;
@@ -31,7 +33,7 @@ public class DeTab extends Tabs {
 					continue;
 				}
 				do { // Tab, expand it, must put >=1 space
-					System.out.print(' ');
+					out.print(' ');
 				} while (!isTabStop(++col));
 			}
 			System.out.println();
