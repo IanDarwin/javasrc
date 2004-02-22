31d0207d5a23276185e18e95319f00e741dc4614
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:01:24 +0000

    Renamed Detab class to DeTab to be consisten with EnTab.

---
+++ src/main/java/strings/DeTab.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/** detab- replace blanks by tabs and blanks.
+ * @author Ian F. Darwin, http://www.darwinsys.com/
+ * @version $Id$
+ */
+public class DeTab extends Tabs {
+
+	public static void main(String[] argv) throws IOException {
+		DeTab dt = new DeTab(8);
+		dt.detab(new BufferedReader(new InputStreamReader(System.in)));
+	}
+
+	public DeTab(int n) {
+		super(n);
+	}
+
+	public void detab(BufferedReader is) throws IOException {
+		String line;
+		char c;
+		int col;
+		while ((line = is.readLine()) != null) {
+			col = 0;
+			for (int i=0; i<line.length(); i++) {
+				// Either ordinary character or tab.
+				if ((c=line.charAt(i)) != '\t') {
+					System.out.print(c); // Ordinary
+					++col;
+					continue;
+				}
+				do { // Tab, expand it, must put >=1 space
+					System.out.print(' ');
+				} while (!isTabStop(++col));
+			}
+			System.out.println();
+		}
+	}
+}
