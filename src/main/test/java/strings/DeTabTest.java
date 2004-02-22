d70a15a8ddb7ab4b747712fea552310a6e4b1009
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:15:08 +0000

    New: test for DeTab class.

---
+++ src/main/test/java/strings/DeTabTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+
+/**
+ * Simple tests for DeTab.
+ */
+public class DeTabTest extends TestCase {
+
+	DeTab dt = new DeTab(8);
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(DeTabTest.class);
+	}
+
+	public void testIdempotency() {
+		assertEquals("", dt.detabLine(""));
+		assertEquals(" a ", dt.detabLine(" a "));
+	}
+	
+	public void testDetabLine(){
+		assertEquals("       A", dt.detabLine("\tA"));
+	}
+}
