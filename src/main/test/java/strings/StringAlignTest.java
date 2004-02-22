dcb084a642c8697b8d228274e35034594735bd4b
Ian Darwin <ian@darwinsys.com>
2004-02-22 15:13:23 +0000

    New: half-baked regression for StringAlign

---
+++ src/main/test/java/strings/StringAlignTest.java
@@ -0,0 +1,52 @@
+package regress;
+
+import junit.framework.*;
+import StringAlign;
+
+/** StringAlign Test program.  */
+public class StringAlignTest extends TestCase {
+
+	String[] mesg = {"JavaFun", "JavaFun!" };
+
+	/** The object being tested */
+	protected StringAlign sa;
+
+	/** JUnit test classes require(?) this constructor */
+	public StringAlignTest(String name) {
+		super(name);
+	}
+
+	public void testLeft() {
+		for (int i=0; i<mesg.length; i++) {
+			System.out.println("Input String \"" + mesg[i] + "\"");
+			dump(StringAlign.JUST_LEFT, 5,
+				new StringAlign(5, StringAlign.JUST_LEFT).format(mesg[i]));
+			dump(StringAlign.JUST_LEFT, 10,
+				new StringAlign(10, StringAlign.JUST_LEFT).format(mesg[i]));
+		}
+	}
+
+	public void testCentre() {
+		for (int i=0; i<mesg.length; i++) {
+			System.out.println("Input String \"" + mesg[i] + "\"");
+			dump(StringAlign.JUST_CENTER, 5,
+				new StringAlign(5, StringAlign.JUST_CENTER).format(mesg[i]));
+			dump(StringAlign.JUST_CENTER, 10,
+				new StringAlign(10, StringAlign.JUST_CENTER).format(mesg[i]));
+		}
+	}
+
+	public void testRight() {
+		for (int i=0; i<mesg.length; i++) {
+			System.out.println("Input String \"" + mesg[i] + "\"");
+			dump(StringAlign.JUST_RIGHT, 5,
+				new StringAlign(5, StringAlign.JUST_RIGHT).format(mesg[i]));
+			dump(StringAlign.JUST_RIGHT, 10,
+				new StringAlign(10, StringAlign.JUST_RIGHT).format(mesg[i]));
+		}
+	}
+
+	void dump(int x, int len, String mesg) {
+		System.out.println(mesg);
+	}
+}
