8ef7facb658c00f68d9a70671daf653ab93bc655
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:21:44 +0000

    Organize imports.

--- src/main/java/strings/EnTab.java
+++ src/main/java/strings/EnTab.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 import com.darwinsys.util.Debug;
 
