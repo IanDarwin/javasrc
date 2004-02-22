61e4d91b68db0197b8f2dc97d585ab69f7a38491
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:16:39 +0000

    Break it out into detab(files) and detabLine(String),mainly for testing but also for reuse.

--- src/main/java/strings/DeTab.java
+++ src/main/java/strings/DeTab.java
@@ -7,8 +7,9 @@ import java.io.PrintWriter;
  * @author Ian F. Darwin, http://www.darwinsys.com/
  * @version $Id$
  */
-public class DeTab extends Tabs {
-
+public class DeTab {
+	Tabs ts;
+	
 	public static void main(String[] argv) throws IOException {
 		DeTab dt = new DeTab(8);
 		dt.detab(new BufferedReader(new InputStreamReader(System.in)),
@@ -16,27 +17,46 @@ public class DeTab extends Tabs {
 	}
 
 	public DeTab(int n) {
-		super(n);
+		ts = new Tabs(n);
+	}
+	public DeTab() {
+		ts = new Tabs();
 	}
 
+	/** detab one file (replace tabs with spaces)
+	 * @param is - the file to be processed
+	 * @param out - the updated file
+	 */
 	public void detab(BufferedReader is, PrintWriter out) throws IOException {
 		String line;
 		char c;
 		int col;
 		while ((line = is.readLine()) != null) {
-			col = 0;
-			for (int i=0; i<line.length(); i++) {
-				// Either ordinary character or tab.
-				if ((c=line.charAt(i)) != '\t') {
-					System.out.print(c); // Ordinary
-					++col;
-					continue;
-				}
-				do { // Tab, expand it, must put >=1 space
-					out.print(' ');
-				} while (!isTabStop(++col));
-			}
-			System.out.println();
+			out.println(detabLine(line));
 		}
 	}
+	
+	/** detab one line (replace tabs with spaces)
+	 * @param line - the line to be processed
+	 * @return the updated line
+	 */
+	public String detabLine(String line) {
+		char c;
+		int col;
+		StringBuffer sb = new StringBuffer();
+		col = 0;
+		for (int i = 0; i < line.length(); i++) {
+			// Either ordinary character or tab.
+			if ((c = line.charAt(i)) != '\t') {
+				sb.append(c); // Ordinary
+				++col;
+				continue;
+			}
+			do { // Tab, expand it, must put >=1 space
+				sb.append(' ');
+			} while (!ts.isTabStop(++col));
+		}
+		return sb.toString();
+	}
 }
+
