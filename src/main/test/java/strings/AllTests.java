760a22b8b7a73fdefe399ca401bac3fbe853b2a2
Ian Darwin <ian@darwinsys.com>
2004-02-22 21:48:40 +0000

    Add TestSuite.

---
+++ src/main/test/java/strings/AllTests.java
@@ -0,0 +1,24 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * JUnit tests for some of my String demos.
+ */
+public class AllTests {
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(AllTests.class);
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for default package");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(EnTabTest.class);
+		suite.addTestSuite(SoundexTest.class);
+		suite.addTestSuite(StringAlignTest.class);
+		suite.addTestSuite(TabsTest.class);
+		suite.addTestSuite(DeTabTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
