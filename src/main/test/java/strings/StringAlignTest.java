291ba8516faeeec20bdd3a0aa6efad0744aba634
Ian Darwin <ian@darwinsys.com>
2004-02-22 15:43:18 +0000

    More regressions leave chatter to StringAlignDemo, or Chatter.java.

--- src/main/test/java/strings/StringAlignTest.java
+++ src/main/test/java/strings/StringAlignTest.java
@@ -15,34 +15,40 @@ public class StringAlignTest extends TestCase {
 
 	public void testLeft() {
 		for (int i=0; i<mesg.length; i++) {
-			System.out.println("Input String \"" + mesg[i] + "\"");
 			assertEquals(mesg[i].substring(0, 5),
 				new StringAlign(5, StringAlign.JUST_LEFT).format(mesg[i]));
-			assertEquals(mesg[i].trim(),
-				new StringAlign(10, StringAlign.JUST_LEFT).format(mesg[i]));
+			String result = new StringAlign(10, StringAlign.JUST_LEFT).
+					format(mesg[i]);
+			assertEquals(mesg[i], result.trim());
+			assertEquals(result.length(), 10);
 		}
 	}
 
 	public void testCentre() {
 		for (int i=0; i<mesg.length; i++) {
-			System.out.println("Input String \"" + mesg[i] + "\"");
-			dump(StringAlign.JUST_CENTER, 5,
-				new StringAlign(5, StringAlign.JUST_CENTER).format(mesg[i]));
-			dump(StringAlign.JUST_CENTER, 10,
-				new StringAlign(10, StringAlign.JUST_CENTER).format(mesg[i]));
+			assertEquals(mesg[i].substring(0, 5),
+				new StringAlign(5, StringAlign.JUST_CENTER).format(mesg[i]).trim());
+			assertEquals(" " + mesg[i],
+				new StringAlign(10, StringAlign.JUST_CENTER).format(mesg[i]).substring(0, mesg[i].length()+1));
 		}
 	}
 
 	public void testRight() {
 		for (int i=0; i<mesg.length; i++) {
 			System.out.println("Input String \"" + mesg[i] + "\"");
-			dump(StringAlign.JUST_RIGHT, 5,
-				new StringAlign(5, StringAlign.JUST_RIGHT).format(mesg[i]));
+			assertEquals(mesg[i].substring(0, 5),
+				new StringAlign(5, StringAlign.JUST_RIGHT).format(mesg[i]).trim());
+			// XXX need to test this more.
 			dump(StringAlign.JUST_RIGHT, 10,
 				new StringAlign(10, StringAlign.JUST_RIGHT).format(mesg[i]));
 		}
 	}
 
+	public void testNullString() {
+		assertEquals("     ", new StringAlign(5, StringAlign.JUST_RIGHT).format(""));
+	}
+
+	// Consider phasing out this method.
 	void dump(int x, int len, String mesg) {
 		System.out.println(mesg);
 	}
