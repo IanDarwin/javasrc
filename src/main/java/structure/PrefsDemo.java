df28506b4416dd59bc12e8b6113acb50f4d0f29f
Ian Darwin <ian@darwinsys.com>
2004-02-29 15:38:58 +0000

    Comment.

--- src/main/java/structure/PrefsDemo.java
+++ src/main/java/structure/PrefsDemo.java
@@ -1,12 +1,23 @@
 import java.util.prefs.Preferences;
 
+/**
+ * Simple demonstration of using Preferences.
+ * @version $Id$
+ */
 public class PrefsDemo {
 	public static void main(String[] args) {
+
+		// Setup the Preferences for this application, by class.
 		Preferences prefs = Preferences.userNodeForPackage(PrefsDemo.class);
+
+		// Retrieve some preferences previously stored, with defaults in case
+		// this is the first run.
 		String text    = prefs.get("textFontName", "lucida-bright");
 		String display = prefs.get("displayFontName", "lucida-blackletter");
 		System.out.println(text);
 		System.out.println(display);
+
+		// Assume the user chose new preference values: Store them back.
 		prefs.put("textFontName", "times-roman");
 		prefs.put("displayFontName", "helvetica");
 	}
