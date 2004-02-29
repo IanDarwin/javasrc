945a54e6ba7e8d7fda1ddd108a3582fe7b44bbcd
Ian Darwin <ian@darwinsys.com>
2004-02-29 17:59:38 +0000

    Compilable?

--- src/main/java/structure/CallTrack.java
+++ src/main/java/structure/CallTrack.java
@@ -3,19 +3,21 @@
  * of the ArrayList to find the object (or end of list) before which
  * to insert the new user. 
  */
+public class CallTrack {
 
-/** The list of User objects. */
-ArrayList usrList = new Vector();
-/** The scrolling list */
-java.awt.List visList = new List();
-/** Add one (new) Candidate to the lists */
-protected void add(Candidate c) {
-	String n = c.lastname;
-	int i;
-	for (i=0; i<usrList.size(); i++)
-		if (n.compareTo(((User)(usrList.elementAt(i))).lastname) <= 0)
-			break;
-	visList.add(c.getName(), i);
-	usrList.add(i, c);
-	visList.select(i);      // ensure current
+	/** The list of User objects. */
+	ArrayList usrList = new Vector();
+	/** The scrolling list */
+	java.awt.List visList = new List();
+	/** Add one (new) Candidate to the lists */
+	protected void add(Candidate c) {
+		String n = c.lastname;
+		int i;
+		for (i=0; i<usrList.size(); i++)
+			if (n.compareTo(((User)(usrList.elementAt(i))).lastname) <= 0)
+				break;
+		visList.add(c.getName(), i);
+		usrList.add(i, c);
+		visList.select(i);      // ensure current
+	}
 }
