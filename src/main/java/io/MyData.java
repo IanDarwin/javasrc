1f3e207a3bed8d7d2b075068da220fcd0fd8bf21
Ian Darwin <ian@darwinsys.com>
2004-02-29 04:01:35 +0000

    Simplify: remove inner class.

--- src/main/java/io/MyData.java
+++ src/main/java/io/MyData.java
@@ -1,3 +1,5 @@
+package io;
+
 import java.io.Serializable;
 
 /** Simple data class used in Serialization demos. */
@@ -15,24 +17,10 @@ public class MyData implements Serializable {
 		// Save the clear text p/w in the object, it won't get serialized
 		passwordClear = clear;
 		// So we must save the encryption! Encryption not shown here.
-		passwordCypher = new DES().encrypt(passwordClear);
+		passwordCypher = passwordClear;
 	}
 
 	public String toString() {
 		return "MyData[" + userName + ",XXXXX]";
-	}
-
-	/** This just generates a String; Strings are serializable.
-	 * This class does not need to be Serializable (or, presumably,
-	 * JDO enhanced?)
-	 */
-	class DES {
-		public DES() {
-		}
-
-		// Obviously just a placeholder.
-		public String encrypt(String s) {
-			return s;
-		}
 	}
 }
