90b2a8adf28d6d64a47bb8f7e1b461a308d2d426
Ian Darwin <ian@darwinsys.com>
2004-02-29 18:51:00 +0000

    Move methods and fields from CallTrack fragment's fake Person to here

--- src/main/java/structure/Person.java
+++ src/main/java/structure/Person.java
@@ -1,11 +1,27 @@
 /** A sample class used in some of the notes accompanying this code set.
- * N.B. This is NOT "best practices": should be private fields,
- * constructor, and get methods.
- * (Done this way so examples are shorter).
+ * N.B. This is NOT "best practices": should all be private fields,
+ * and get/set methods.  (Done this way so examples are shorter).
  */
 public class Person {
-	public String name;
+	private String firstName, lastName;
 	public String address;
 	public String phone;
 	public String Country;
+	
+	public Person(String f, String l) {
+		firstName = f;
+		lastName = l;
+	}
+	public String getFirstName() {
+		return lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getName() {
+		return getFirstName() + ' ' + getLastName();
+	}
+	public String toString() {
+		return getName();
+	}
 }
