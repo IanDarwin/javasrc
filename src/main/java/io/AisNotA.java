45026c43e96f5f9ac0f97629a743b7029cada0ec
Ian Darwin <ian@darwinsys.com>
2004-03-01 01:41:56 +0000

    New.

---
+++ src/main/java/io/AisNotA.java
@@ -0,0 +1,8 @@
+/** Show that 'A' is not always (printed as) 'A'.
+ */
+public class AisNotA {
+	public static void main(String[] String) {
+		int a = 'A';
+		System.out.println(a);
+	}
+}
---
+++ src/main/test/java/io/AllTests.java
@@ -0,0 +1,19 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author ian
+ */
+public class AllTests {
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(AllTests.suite());
+	}
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for default package");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(FortranLineReaderTest.class);
+		suite.addTestSuite(IndentContLineReaderTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
