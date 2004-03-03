f3363098e22f3177591478c78a5d1bc755408a8c
Ian Darwin <ian@darwinsys.com>
2004-03-03 17:22:14 +0000

    Date Format I18N

---
+++ src/main/java/datetime/DateFormatBest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.text.*;
+
+/** The "best" way to format a date/time is to use
+ * the DateFormat.getInstance() method, which will
+ * be localized automatically.
+ * @version $Id$
+ */
+public class DateFormatBest {
+	public static void main(String[] args) {
+		Date today = new Date();
+
+		DateFormat df = DateFormat.getInstance();
+		System.out.println(df.format(today));
+
+		DateFormat df_fr = 
+			DateFormat.getDateInstance(DateFormat.FULL, Locale.FRENCH);
+		System.out.println(df_fr.format(today));
+	}
+}
