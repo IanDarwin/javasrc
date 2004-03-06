a2317ab428b45cbf46615eb1de9d7b9a2d4251c5
Ian Darwin <ian@darwinsys.com>
2004-03-06 20:59:12 +0000

    Driver for 1.5 java.util.Scanner

---
+++ src/main/java/io/ScannerTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import junit.framework.TestCase;
+
+/** A series of JUnit tests for java.util.Scanner.
+ * Not trying to help Sun debug the thing; writing JUnit tests
+ * is a good way to "test" one's understanding of a new API.
+ */
+public class ScannerTest extends TestCase {
+
+	public void testDateString() {
+		String sampleDate = "25 Dec 1988";	// K&R
+
+		Scanner sDate = Scanner.create(sampleDate);
+		int dom = sDate.nextInt();
+		String mon = sDate.nextString();
+		int year = sDate.nextYear();
+
+		assertEquals(dom, 25);
+		assertEquals(mon, "Dec");
+		assertEquals(year, 1988);
+	}
+}
