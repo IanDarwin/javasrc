195eafc57459a7d12efad5cb609e0c9e2e36ccdd
Ian Darwin <ian@darwinsys.com>
2004-03-06 22:52:32 +0000

    Blooper: ChoiceFormat was not being used, spotted by Gisbert

--- src/main/java/numbers/FormatPluralsChoice.java
+++ src/main/java/numbers/FormatPluralsChoice.java
@@ -10,8 +10,13 @@ public class FormatPluralsChoice extends FormatPlurals {
 	static String[] formats = { "items", "item", "items"};
 	static ChoiceFormat myFormat = new ChoiceFormat(limits, formats);
 
+	/** report -- using conditional operator */
+	public static void report(int n) {
+		System.out.println("We used " + n + " " + myFormat.format(n));
+	}
+
 	public static void main(String[] argv) {
-		report(0);	// inherited method
+		report(0);
 		report(1);
 		report(2);
 	}
