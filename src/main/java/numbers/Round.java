935c06df76ec9b2ae994cc85b8eb15c46f9b24ec
Ian Darwin <ian@darwinsys.com>
2004-03-06 22:17:32 +0000

    Handle negatives (thx Gisbert); several style changes; show Math.round too

--- src/main/java/numbers/Round.java
+++ src/main/java/numbers/Round.java
@@ -4,21 +4,29 @@
  * @version $Id$
  */
 public class Round {
-	//+
 	/** We round a number up if its fraction exceeds this threshold. */
 	public static final double THRESHOLD = 0.54;
-	/* Return the closest long to the argument.
-	 * ERROR CHECKING OMITTED.
+	/* 
+	 * Round floating values to integers.
+	 * @Return the closest int to the argument.
+	 * @param d A non-negative values to be rounded.
 	 */
-	static long round(double d) {
-		long di = (long)Math.floor(d);	// integral value below (or ==) d
-		if ((d - di) > THRESHOLD)
+	static int round(double d) {
+		if (d < 0) {
+			throw new IllegalArgumentException("Value must be non-negative");
+		}
+		int di = (int)Math.floor(d);	// integral value below (or ==) d
+		if ((d - di) > THRESHOLD) {
 			return di + 1;
-		else return di;
+		} else {
+			return di;
+		}
 	}
-	//-
+	
 	public static void main(String[] argv) {
-		for (double d = 0.1; d<=1.0; d+=0.01)
-			System.out.println(d + "-> " + round(d));
+		for (double d = 0.1; d<=1.0; d+=0.01) {
+			System.out.println("My way:  " + d + "-> " + round(d));
+			System.out.println("Math way:" + d + "-> " + Math.round(d));
+		}
 	}
 }
