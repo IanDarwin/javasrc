d00b6d024314f754a01cca6fa2781e8da0137b0d
Ian Darwin <ian@darwinsys.com>
2004-03-07 23:59:47 +0000

    New.

---
+++ src/main/java/structure/IteratorDemo.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/** Iterator used to walk through a List.
+ * @version $Id$
+ */
+public class IteratorDemo {
+
+	public static void main(String[] argv) {
+
+		List l = new ArrayList();
+		StructureDemo source = new StructureDemo(15);
+
+		// Add lots of elements to the list...
+		l.addElement(source.getDate());
+		l.addElement(source.getDate());
+		l.addElement(source.getDate());
+
+		int i = 0;
+
+		Iterator it = l.iterator();
+
+		// Process the data structure using an iterator.
+		// This part of the code does not know or care
+		// if the data is an an array, a List, a Vector, or whatever.
+		while (it.hasNext()) {
+			Object o = it.next();
+			System.out.println("Element " + i++ + " = " + o);
+		}
+	}
+}
