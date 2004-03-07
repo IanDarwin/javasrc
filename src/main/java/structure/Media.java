ed674ceacb2906883859710c83e2ab7b45ba3d3c
Ian Darwin <ian@darwinsys.com>
2004-03-07 23:33:18 +0000

    Comment out hard-coded value and constructor using this.value; not sure when or if I compiled it, but 1.5 Beta 1 won't accept it.

--- src/main/java/structure/Media.java
+++ src/main/java/structure/Media.java
@@ -1,7 +1,8 @@
 public enum Media {
-	book(1), music_cd(2), music_vinyl(3), movie_vhs(4), movie_dvd(5);
 
-	public Media(int value) {
-		this.value = value;
-	}
+	book, music_cd, music_vinyl, movie_vhs, movie_dvd;
+	//book(1), music_cd(2), music_vinyl(3), movie_vhs(4), movie_dvd(5);
+	//public Media(int value) {
+	//	this.value = value;
+	//}
 }
