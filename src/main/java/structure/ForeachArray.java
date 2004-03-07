03bd3230b6f7ea69a6a413ea5625ba151ad5f008
Ian Darwin <ian@darwinsys.com>
2004-03-07 22:17:33 +0000

    Indentation

--- src/main/java/structure/ForeachArray.java
+++ src/main/java/structure/ForeachArray.java
@@ -1,7 +1,7 @@
 public class ForeachArray {
 	public static void main(String args[]) {
-	    String[] data = { "Toronto", "Stockholm" };
-	    for (String s : data) {
+		String[] data = { "Toronto", "Stockholm" };
+		for (String s : data) {
 			System.out.println(s);
 		}
 	}
