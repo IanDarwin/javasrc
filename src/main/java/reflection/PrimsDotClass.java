df8fa83950d38ff2b95aca58d9e7cb9cbf16d179
Ian Darwin <ian@darwinsys.com>
2004-03-07 18:57:12 +0000

    New.

---
+++ src/main/java/reflection/PrimsDotClass.java
@@ -0,0 +1,9 @@
+/**
+ * Show that you can, in fact, take the .class of a primitive.
+ */
+public class PrimsDotClass {
+
+	public static void main(String[] args) {
+		System.out.println(int.class);
+	}
+}
