a030045337439cbc2275764baa3c62496bd61a72
Ian Darwin <ian@darwinsys.com>
2004-03-07 19:00:08 +0000

    Show that it's a real Class object.

--- src/main/java/reflection/PrimsDotClass.java
+++ src/main/java/reflection/PrimsDotClass.java
@@ -1,9 +1,14 @@
+import java.lang.reflect.Method;
+
 /**
  * Show that you can, in fact, take the .class of a primitive.
  */
 public class PrimsDotClass {
 
 	public static void main(String[] args) {
-		System.out.println(int.class);
+		Class c = int.class;
+		System.out.println(c.getName());
+		Method[] methods = c.getMethods();
+		System.out.println(c.getName() + " has " + methods.length + " methods");
 	}
 }
