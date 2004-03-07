a67bc5b2b82f9c7db6ae9040c2b97dff25065bcb
Ian Darwin <ian@darwinsys.com>
2004-03-07 16:31:58 +0000

    Repair imports; remove dead Catch for IOException

--- src/main/java/io/ReadStdinInt15.java
+++ src/main/java/io/ReadStdinInt15.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.util.Scanner;
 /**
  * Read an int from Standard Input, using 1.5
  * @author	Ian F. Darwin, http://www.darwinsys.com/
@@ -11,10 +11,7 @@ public class ReadStdinInt15 {
 			Scanner sc = Scanner.create(System.in);      // Requires J2SE 1.5
 			val = sc.nextInt();
 		} catch (NumberFormatException ex) {
-			System.err.println("Not a valid number: " + line);
-			return;
-		} catch (IOException e) {
-			System.err.println("Unexpected IO ERROR: " + e);
+			System.err.println("Not a valid number: " + ex);
 			return;
 		}
 		System.out.println("I read this number: " + val);
