12d272d007eb145309de2087ecccd1935737c561
Ian Darwin <ian@darwinsys.com>
2004-03-08 04:35:36 +0000

    Wahoo! An even better way of catching GUI thread events.

---
+++ src/main/java/gui/ThreadBasedCatcher.java
@@ -0,0 +1,39 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+/**
+ * name - purpose
+ * @author ian
+ * @verion $Id$
+ */
+public class ThreadBasedCatcher extends JFrame{
+
+	public static void main(String[] args) {
+		new ThreadBasedCatcher().setVisible(true);
+	}
+	public ThreadBasedCatcher(){
+		Container cp = getContentPane();
+		JButton crasher = new JButton("Crash");
+		cp.add(crasher);
+		crasher.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				throw new RuntimeException("You asked for it");
+			}
+		});
+		Thread.setDefaultUncaughtExceptionHandler(
+				new Thread.UncaughtExceptionHandler(){
+					public void uncaughtException(Thread t, Throwable ex){
+						System.out.println(
+							"You crashed thread " + t.getName());
+						System.out.println(
+							"Exception was: " + ex.toString());
+					}
+				});
+		pack();
+	}
+}
