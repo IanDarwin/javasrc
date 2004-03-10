5ff5106b2112b6674b929a50158f2095adbf7b85
Ian Darwin <ian@darwinsys.com>
2004-03-10 17:31:45 +0000

    Regress

---
+++ src/main/test/java/io/SimpleCalcTest.java
@@ -0,0 +1,30 @@
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import junit.framework.TestCase;
+
+public class SimpleCalcTest extends TestCase {
+
+	final String TEST = "// This file is a test of the SimpleCalc class\n" +
+		"2 4 + = 3 / =	// should print 6, 2\n" +
+		"22 7 / =		// should print 3.141592857...\n";
+
+	final String EXPECT = "6.0\n" +
+		"2.0\n" +
+		"3.142857142857143\n";
+
+	char[] outputChars = new char[100];
+
+
+	public void testIt() throws Exception {
+		BufferedReader is = new BufferedReader(
+			new StringReader(TEST));
+		StringWriter fluffy = new StringWriter();
+		PrintWriter ps = new PrintWriter(fluffy);
+
+		SimpleCalc sc = new SimpleCalc(is, ps);
+		assertEquals(EXPECT, fluffy.getBuffer().toString());
+	}
+}
