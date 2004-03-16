e0c567a5fb7022cb41ec1ca034934e45c8848b73
IanDarwin <>
2004-03-16 00:05:18 +0000

    Remove space in name; some implementations throw MFUE on it

--- src/main/java/rmi/callback/TickerServer.java
+++ src/main/java/rmi/callback/TickerServer.java
@@ -6,6 +6,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface TickerServer extends Remote {
-	public static final String LOOKUP_NAME = "Ticker Service";
+	public static final String LOOKUP_NAME = "Ticker_Service";
 	public void connect(Client d) throws RemoteException;
 }
