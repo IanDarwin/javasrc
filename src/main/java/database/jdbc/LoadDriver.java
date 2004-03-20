b9d636aca0f43c4815e1bbf6ef59a300ef6e97d3
Ian Darwin <ian@darwinsys.com>
2004-03-20 03:14:24 +0000

    Comment and formatting

--- src/main/java/database/jdbc/LoadDriver.java
+++ src/main/java/database/jdbc/LoadDriver.java
@@ -1,14 +1,13 @@
-/** Load some drivers.  */
+/** 
+ * Load some drivers.
+ */
 public class LoadDriver {
-
 	public static void main(String[] av) {
-	    try {
-
+		try {
 			// Try to load the jdbc-odbc bridge driver
 			// Should be present on Sun JDK implementations.
 			Class c = Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 			System.out.println("Loaded " + c);
-
 			// Try to load an Oracle driver.
 			Class d = Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("Loaded " + d);
