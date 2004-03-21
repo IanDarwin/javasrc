e106fd0629e2ed79e1cd20aba8c14480d8353317
Ian Darwin <ian@darwinsys.com>
2004-03-21 20:06:20 +0000

    Undo broken Eclipse formatting. Add more cases; now 2 work, 3 don't, which is probably right.

--- src/main/java/regex/CanonEqDemo.java
+++ src/main/java/regex/CanonEqDemo.java
@@ -1,15 +1,21 @@
 import java.util.regex.*;
+
 /**
- * CanonEqDemo - show use of Pattern.CANON_EQ, by comparing two ways of
- * entering the Spanish word for "equal" and see if they are equal.
+ * CanonEqDemo - show use of Pattern.CANON_EQ, by comparing varous ways of
+ * entering the Spanish word for "equal" and see if they are considered equal
+ * by the RE-matching engine.
  * 
  * @version $Id$
  */
 public class CanonEqDemo {
 	public static void main(String[] args) {
 		String pattStr = "\u00e9gal"; // Žgal
-		String[] input = {"e\u00b4gal", // e + Latin-1 "acute"
-				"e'gal" // e + single quote
+		String[] input = {
+				"\u00e9gal", // Žgal - this one had better match :-)
+				"e\u0301gal", // e + "Combining acute accent"
+				"e\u02cagal", // e + "modifier letter acute accent"
+				"e'gal", // e + single quote
+				"e\u00b4gal", // e + Latin-1 "acute"
 		};
 		Pattern pattern = Pattern.compile(pattStr, Pattern.CANON_EQ);
 		for (int i = 0; i < input.length; i++) {
