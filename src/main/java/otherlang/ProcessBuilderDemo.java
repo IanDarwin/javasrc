c63362af2df546377ad6e3146c9f19215cdef6c7
Ian Darwin <ian@darwinsys.com>
2004-03-21 18:04:23 +0000

    New: Demo of 1.5 ProcessBuilder.

---
+++ src/main/java/otherlang/ProcessBuilderDemo.java
@@ -0,0 +1,34 @@
+import com.darwinsys.util.Debug;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * ProcessBuilderDemo shows how to execute an external 
+ * program (in this case the MS-Windows notepad program).
+ */
+public class ProcessBuilderDemo {
+
+	public static void main(String argv[]) 
+	throws InterruptedException, IOException {
+		
+		List<String> command = new ArrayList<String>();
+		command.add("notepad");
+		command.add("foo.txt");
+		ProcessBuilder builder = new ProcessBuilder(command);
+		Map<String, String> environ = builder.environment();
+		environ.put("PATH", "/windows;/windows/system32;/winnt");
+		builder.directory(new File(System.getProperty("user.home")));
+
+		final Process godot = builder.start();
+		
+		Debug.println("exec", "Waiting for Godot");
+		godot.waitFor();
+
+		System.out.println("Program terminated!");
+		return;
+	}
+}
