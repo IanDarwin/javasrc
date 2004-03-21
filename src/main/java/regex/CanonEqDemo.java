60f57412a8fc3a08b58d2d3b5b4067412a9bc007
Ian Darwin <ian@darwinsys.com>
2004-03-21 19:37:29 +0000

    Multiple input forms, with a loop.

--- src/main/java/regex/CanonEqDemo.java
+++ src/main/java/regex/CanonEqDemo.java
@@ -1,20 +1,23 @@
 import java.util.regex.*;
-
 /**
  * CanonEqDemo - show use of Pattern.CANON_EQ, by comparing two ways of
  * entering the Spanish word for "equal" and see if they are equal.
+ * 
  * @version $Id$
  */
 public class CanonEqDemo {
 	public static void main(String[] args) {
-		String pattStr = "\u00e9gal";		// Žgal
-		String input   = "e\u00b4gal";	// e«gal
+		String pattStr = "\u00e9gal"; // Žgal
+		String[] input = {"e\u00b4gal", // e + Latin-1 "acute"
+				"e'gal" // e + single quote
+		};
 		Pattern pattern = Pattern.compile(pattStr, Pattern.CANON_EQ);
-		if (pattern.matcher(input).matches()) {
-			System.out.println(pattStr + " matches input " + input);
-		} else {
-			System.out.println(pattStr + " does not match input " + input);
+		for (int i = 0; i < input.length; i++) {
+			if (pattern.matcher(input[i]).matches()) {
+				System.out.println(pattStr + " matches input " + input[i]);
+			} else {
+				System.out.println(pattStr + " does not match input " + input[i]);
+			}
 		}
-		
 	}
 }
