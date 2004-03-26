41420d141cc282a5cc4eb5752254bd0f1e5dc61c
Ian Darwin <ian@darwinsys.com>
2004-03-26 02:51:12 +0000

    New.

---
+++ src/main/java/strings/StringBuilderDemo.java
@@ -0,0 +1,29 @@
+/**
+ * StringBuilderDemo: construct the same String three different ways.
+ */
+public class StringBuilderDemo {
+
+	public static void main(String[] argv) {
+
+		String s1 = "Hello" + ", " + "World";
+		System.out.println(s1);
+
+		// Build a StringBuilder, and append some things to it.
+		StringBuilder sb2 = new StringBuilder();
+		sb2.append("Hello");
+		sb2.append(',');
+		sb2.append(' ');
+		sb2.append("World");
+
+		// Get the StringBuilder's value as a String, and print it.
+		String s2 = sb2.toString();
+		System.out.println(s2);
+
+		// Now do the above all over again, but in a more 
+		// concise (and typical "real-world" Java) fashion.
+
+		StringBuilder sb3 = new StringBuilder().append("Hello").
+			append(',').append(' ').append("World");
+		System.out.println(sb3.toString());
+	}
+}
