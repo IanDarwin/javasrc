e6767af32e98a2d7a750f99086d9572231a2760a
Ian Darwin <ian@darwinsys.com>
2004-03-26 02:50:21 +0000

    Vector -> List

--- src/main/java/strings/StrTokDemo4b.java
+++ src/main/java/strings/StrTokDemo4b.java
@@ -5,18 +5,18 @@ public class StrTokDemo4b {
 	/** The delimiter used between fields on input. */
 	public final static String DELIM = "|";
 
-	/** Processes one String, returns it as an array of fields */
+	/** Processes one String, returns it as an array of Strings */
 	public static String[] process(String line) {
-		// A Vector is an array-like container that grows dynamically;
+		// ArrayList is an array-like container that grows dynamically;
 		// see the chapter on Java Collections.
-		Vector results = new Vector();
+		List results = new ArrayList();
 
 		// Unless you ask StringTokenizer to give you the tokens,
 		// it silently discards multiple null tokens.
 		StringTokenizer st = new StringTokenizer(line, DELIM, true);
 
 		int i = 0;
-		// Stuff each token into the Vector
+		// Stuff each token into the ArrayList
 		while (st.hasMoreTokens()) {
 			String s = st.nextToken();
 			if (s.equals(DELIM)) {
@@ -26,10 +26,11 @@ public class StrTokDemo4b {
 					results.add(null);
 				continue;
 			}
-			results.addElement(s);
+			results.add(s);
 		}
+
 		// Make an Array just the right size, ask the
-		// Vector to copy its data into it, and return it.
+		// ArrayList to copy its data into it, and return it.
 		String[] sResults = new String[results.size()];
 		results.toArray(sResults);
 		return sResults;
