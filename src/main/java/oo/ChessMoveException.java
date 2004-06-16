9283cfe2ea71e7e54858572f8bee64bac144e162
Ian Darwin <ian@darwinsys.com>
2004-06-16 22:26:27 +0000

    New.

---
+++ src/main/java/oo/ChessMoveException.java
@@ -0,0 +1,9 @@
+/** A ChessMoveException is thrown  when the user makes an illegal move. */
+public class ChessMoveException extends Exception {
+	public ChessMoveException () {
+		super();
+	}
+	public ChessMoveException (String msg) {
+		super(msg);
+	}
+}
