c4131350c155bcf5c04cfda39b148e7d84b6a6b4
Ian Darwin <ian@darwinsys.com>
2004-06-16 20:31:45 +0000

    Comma-separated list example

---
+++ src/main/java/strings/StringBufferCommaList.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+
+public class StringBufferCommaList {
+	public static void main(String[] args) {
+		StringTokenizer st = new StringTokenizer("Alpha Bravo Charlie");
+		StringBuffer sb = new StringBuffer();
+		while (st.hasMoreElements()) {
+			sb.append(st.nextToken());
+			if (st.hasMoreElements()) {
+				sb.append(", ");
+			}
+		}
+		System.out.println(sb);
+	}
+}
