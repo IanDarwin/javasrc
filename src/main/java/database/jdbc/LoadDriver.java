72c4633ecd0c3284fb8de3c3ba0e0fe614122223
Ian Darwin <ian@darwinsys.com>
2004-06-17 00:39:04 +0000

    Use class.getName() to display success

--- src/main/java/database/jdbc/LoadDriver.java
+++ src/main/java/database/jdbc/LoadDriver.java
@@ -7,10 +7,10 @@ public class LoadDriver {
 			// Try to load the jdbc-odbc bridge driver
 			// Should be present on Sun JDK implementations.
 			Class c = Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-			System.out.println("Loaded " + c);
+			System.out.println("Loaded " + c.getName());
 			// Try to load an Oracle driver.
 			Class d = Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("Loaded " + d);
+			System.out.println("Loaded " + d.getName());
 		} catch (ClassNotFoundException ex) {
 			System.err.println(ex);
 		}
