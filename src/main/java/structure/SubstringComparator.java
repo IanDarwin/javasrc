f27a767d3e8fc8e87c48eece8a1607675f1d64d1
Ian Darwin <ian@darwinsys.com>
2004-06-21 02:09:07 +0000

    Import Comparator, don't use full name of it

--- src/main/java/structure/SubstringComparator.java
+++ src/main/java/structure/SubstringComparator.java
@@ -1,7 +1,9 @@
+import java.util.Comparator;
+
 /** Comparator for comparing strings ignoring first character.
  * @version $id$
  */
-public class SubstringComparator implements java.util.Comparator {
+public class SubstringComparator implements Comparator {
 	public int compare(Object o1, Object o2) {
 		String s1 = o1.toString().substring(1);
 		String s2 = o2.toString().substring(1);
