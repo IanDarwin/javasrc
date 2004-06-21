d5014c4e253231cd2d61ccbcf968518bc53668a4
Ian Darwin <ian@darwinsys.com>
2004-06-21 03:01:07 +0000

    Repair breakage.

--- src/main/java/database/jdbc/StoredProcedureDemo.java
+++ src/main/java/database/jdbc/StoredProcedureDemo.java
@@ -3,9 +3,11 @@ import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.darwinsys.sql.ConnectionUtil;
+
 /** FRAGMENT ONLY showing syntax of prepared statement.
  */
-public class CallableExample {
+public class StoredProcedureDemo {
 	public static void main(String[] args) throws SQLException {
 
 		Connection conn = ConnectionUtil.getConnection("jabadot");
