c207c116089d9c6038032aed309ee2bd4b2ff2be
Ian Darwin <ian@darwinsys.com>
2004-06-21 02:08:24 +0000

    Was stored under wrong name?

---
+++ src/main/java/structure/PropsCompanies.java
@@ -0,0 +1,28 @@
+import java.util.Properties;
+
+/**
+ * Demonstrate Properties reading/writing.
+ * @see HashTableDemo, for the older Hashtable.
+ */
+public class PropsComanies {
+
+	public static void main(String[] argv) throws java.io.IOException {
+
+		Properties props = new Properties();
+
+		// Get my data
+		props.put("Adobe", "Mountain View, CA");
+		props.put("IBM", "White Plains, NY");
+		props.put("Learning Tree", "Los Angeles, CA");
+		props.put("Microsoft", "Redmond, WA");
+		props.put("Netscape", "Mountain View, CA");
+		props.put("O'Reilly", "Sebastopol, CA");
+		props.put("Sun", "Mountain View, CA");
+
+		// Now load additional properties
+		props.load(System.in);
+
+		// List merged properties, using System.out
+		props.list(System.out);
+	}
+}
