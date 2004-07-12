f97e1291e619afbcebd564bce67eb73d5adb5306
Ian Darwin <ian@darwinsys.com>
2004-07-12 22:36:43 +0000

    New: abstract base class for typesafe enumerations, similar to what's in J2SE 1.5. Several examples coming.

---
+++ src/main/java/lang/TypesafeEnum.java
@@ -0,0 +1,48 @@
+package ca.tcp.utils;
+
+/**
+ * Top-level class for Enumerations implementing Bloch's Typesafe Enum pattern,
+ * similar to how he extended it for Java 1.5 (with valueOf() method).
+ * When we move to 1.5, change all subclasses of this to J2SE 1.5 enums.
+ * See Java Cookbook, 2nd Edition, Chapter 8.
+ */
+public abstract class Enum {
+	/** The name of this class, must be set in a static initializer. */
+	protected static String klass;
+	/** The value of this instance */
+	private String value;
+	/** The maximum number of values an enum can have */
+	public static final int MAX_VALUES = 10;
+	
+	/** Although this is public, the implementing subclass' constructor must be 
+	 * private to ensure typesafe enumeration pattern.
+	 */
+	public Enum(String val) {
+		value = val;
+		all[allIndex++] = this;
+	}
+	
+	/** Returns the value of this value as a String */
+	public String toString() {
+		return value;
+	}
+	
+	private final static Enum[] all = new Enum[MAX_VALUES];
+	private static int allIndex;
+	
+	/** Returns the given Enum instance for the given String.
+	 * @throws IllegalArgumentException if the input is not one of the valid values.
+	 */
+	public static Enum getValueOf(String s) {
+		for (int i = 0; i < allIndex; i++) {
+			if (all[i].value.equals(s))	{
+				return all[i];
+			}
+		}
+		throw new IllegalArgumentException("Value '" + s + "' is not a valid " + klass + " enumeration value.");
+	}
+
+	public static Enum[] values() {
+		return (Enum[])all.clone();
+	}
+}
