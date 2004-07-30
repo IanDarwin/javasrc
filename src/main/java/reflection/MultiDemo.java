9877bb45245dbedabcdd8afea0842f3a0ab1c0c2
Ian Darwin <ian@darwinsys.com>
2004-07-30 00:13:51 +0000

    Utterly trivial class used by ClassLoaderMultiple as a demo.

---
+++ src/main/java/reflection/MultiDemo.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Utterly trivial class used by "ClassLoaderMultiple"
+ * @version $Id$
+ */
+public class MultiDemo {
+	static {
+		System.out.println("Demo loaded");
+	}
+	public MultiDemo() {
+	}
+	public static void test() {
+		System.out.println("Demo.test invoked");
+	}
+}
