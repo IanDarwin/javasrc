ea75bfa7e416f7b3b9a23f6b2f5d6782f3da2330
Ian Darwin <ian@darwinsys.com>
2004-08-08 01:03:17 +0000

    Rename this class -- too many HelloWorld programs running around!

--- src/main/java/packaging/HelloWorldForPackaging.java
+++ src/main/java/packaging/HelloWorldForPackaging.java
@@ -5,7 +5,7 @@ import javax.swing.JLabel;
  * Your basic, minimal, Hello World type program in Java.
  * $Id$
  */
-public class HelloWorld {
+public class HelloWorldForPackaging {
 	public static void main(String[] argv) {
 		String message = "Hello, World of Java";
 		JFrame jf = new JFrame(message);
