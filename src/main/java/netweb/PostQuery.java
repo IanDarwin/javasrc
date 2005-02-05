408986ea8ab2d48cf5b3edfdcbc2730f069f82c1
Ian Darwin <ian@darwinsys.com>
2005-02-05 17:07:29 +0000

    Repair the sending code: put & not \n between parameters; move doInputto before first sending; make more variables locals that are not needed asfields.

--- src/main/java/netweb/PostQuery.java
+++ src/main/java/netweb/PostQuery.java
@@ -13,10 +13,9 @@ import java.net.*;
  * @see			http://www.learningtree.com/us/courses/471.htm
  */
 public class PostQuery extends Applet implements ActionListener{
+
+	private static final long serialVersionUID = 3258128046665315634L;
 	protected Button goButton;
-	protected URLConnection conn;
-	protected PrintWriter ps;
-	protected BufferedReader is;
 
 	public PostQuery() {
 		add(goButton = new Button("Go for it!"));
@@ -35,24 +34,23 @@ public class PostQuery extends Applet implements ActionListener{
 			showStatus("Connecting to " + myNewURL);
 
 			// Try to open the connection...
-			conn = myNewURL.openConnection();
+			URLConnection conn = myNewURL.openConnection();
 			showStatus("Connected! to " + myNewURL);
 			conn.setDoOutput(true);
+			conn.setDoInput(true);
 			conn.setUseCaches(false);	// ensure response always from server
 
-			ps = new PrintWriter(
+			PrintWriter pw = new PrintWriter(
 				new OutputStreamWriter(conn.getOutputStream()));
 
 			showStatus("Sending...");
 
-			ps.println(URLEncoder.encode("key") + "=" +
-					URLEncoder.encode("some value"));
-			ps.println(URLEncoder.encode("key 2") + "=" +
-					URLEncoder.encode("another value"));
-			ps.close();
-
-			conn.setDoInput(true);
-			is = new BufferedReader(
+			send(pw, "key", "some value");
+			send(pw, "key 2", "another value");
+			pw.println();
+			pw.close();
+			
+			BufferedReader is = new BufferedReader(
 				new InputStreamReader(conn.getInputStream()));
 
 			showStatus("Receiving...");
@@ -67,5 +65,23 @@ public class PostQuery extends Applet implements ActionListener{
 			showStatus("Error, look in Java Console for details!");
 			System.err.println("Error!\n" + err);
 		}
+	}
+	
+	private static boolean first = true;
+	/** Send one parameter, given its name and value; put 
+	 * '&' before second and subsequent parameters.
+	 * @param pw
+	 * @param name
+	 * @param value
+	 * @throws UnsupportedEncodingException
+	 */
+	private static void send(PrintWriter pw, String name, String value ) throws UnsupportedEncodingException {
+		if (first) {
+			first = false;
+		} else {
+			pw.print("&");
+		}
+		pw.print(URLEncoder.encode(name, "UTF-8") + "=" +
+				URLEncoder.encode(value, "UTF-8"));
 	}
 }
