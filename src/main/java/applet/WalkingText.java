28cd1be5b3ae6897dad34bdc837d368bcde2be35
Ian Darwin <ian@darwinsys.com>
2005-02-28 02:07:48 +0000

    Add parameters, code cleanup. Still has deprecation warnings.

--- src/main/java/applet/WalkingText.java
+++ src/main/java/applet/WalkingText.java
@@ -4,12 +4,27 @@ import java.applet.*;
 import java.awt.*;
 
 public class WalkingText extends Applet implements Runnable {
+
+	private static final long serialVersionUID = 3257567317243082032L;
 	protected String mesg = null;
 	protected int  xloc, yloc, width, height, textWidth, textHeight;
-	protected Thread t;
+	protected Thread runnerThread;
 	protected boolean done = false;
 	/** How long to nap for each move */
-	protected int napTime = 150;
+	protected int napTime = 200;
+	private Font theFont;
+	private int xIncrement = 5;
+
+	/**  Usage information */
+	public String[][] getParameterInfo() {
+		return new String[][]  {
+			{ "text", "text", "Text to walk across the screen" },
+			{ "fontName", "text", "Name of font to display in" },
+			{ "fontsize", "int", "How big to make the text" },
+			{ "xincrement", "int", "How far to move the text each tick" },
+			{ "ticktime", "int", "How long (in mSec) to sleep after each move" },
+		};
+	}
 
 	/** Applet Initializer */
 	public void init() {
@@ -28,61 +43,60 @@ public class WalkingText extends Applet implements Runnable {
 		if (fontName == null)
 			fontName = "Helvetica";
 
-		// System.out.println("Font is " + pSize + " point " + fontName);
-		Font f = new Font(fontName, Font.PLAIN, Integer.parseInt(pSize));
-		setFont(f);
+		theFont = new Font(fontName, Font.PLAIN, Integer.parseInt(pSize));
+		setFont(theFont);
 
-		FontMetrics fm = getToolkit().getFontMetrics(f);
+		// XXX deprecated, replace with theFont.getLineMetrics(mesg), then need
+		// a way to compute the width; this is easier for now.
+		FontMetrics fm = getToolkit().getFontMetrics(theFont);
 		textWidth = fm.stringWidth(mesg);
 		textHeight = fm.getHeight();
 		// System.out.println("TextWidth " + textWidth + ", ht " + textHeight);
 
 		// use textHeight in y coordinate calculation
 		yloc = height - ((height-textHeight) / 2);
-	}
-
-	/** This is important: we create a thread, so we must kill it */
-	public void stop() {
-		done = true;
-		t = null;
+		
+		String xIncrString = getParameter("xincrement");
+		if (xIncrString != null) {
+			xIncrement = Integer.parseInt(xIncrString);
+		}
+		String sleepTimeString = getParameter("sleeptime");
+		if (sleepTimeString != null) {
+			napTime = Integer.parseInt(sleepTimeString);
+		}
 	}
 
 	/** create the thread and start it. */
 	public void start() {
-		if (t == null)
-			t = new Thread(this);
+		if (runnerThread == null)
+			runnerThread = new Thread(this);
 		done = false;
-		t.start();
+		runnerThread.start();
 	}
 
-	// Usage:
-	public String[][] getParameterInfo() {
-		String[][] params = {
-			{ "text", "text", "Text to walk across the screen" },
-			{ "fontName", "text", "Name of font to display in" },
-			{ "fontsize", "int", "How big to make the text" },
-		};
-
-		return params;
+	/** Important: we create a thread, so we must kill it */
+	public void stop() {
+		done = true;
+		runnerThread = null;
 	}
 
-
-	/** Run is called by the Thread class when there is work to do */
+	/** Run is called by the Thread class  */
 	public void run() {
 		while (!done) {
-			if ((xloc+=5) > getSize().width)
+			if ((xloc+=xIncrement ) > getSize().width)
 				xloc = 0;
 			repaint();
 			try {
 				Thread.sleep(napTime);
 			} catch (Exception e) {
-				System.out.println("Who dares to interrupt my Sleep()? " + e);
+				System.out.println("Who dares to interrupt my sleep()? " + e);
 			};
 		}
 	}
 
 	/** Paint is called by Applet to redraw the screen */
 	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
 		g.drawString(mesg, xloc, yloc);
 		// if ((xloc + textWidth) > getSize().width) {
 		// 	int negLoc = textWidth-(width-xloc);
