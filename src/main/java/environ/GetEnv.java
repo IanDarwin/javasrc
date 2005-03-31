b48570fdc3c2742ee2658aff7c86f5541213995c
Ian Darwin <ian@darwinsys.com>
2005-03-31 17:32:16 +0000

    Add explanatory note about the history and re-ification of getenv.

--- src/main/java/environ/GetEnv.java
+++ src/main/java/environ/GetEnv.java
@@ -1,7 +1,13 @@
 package environ;
 
+/**
+ * Demo of System.getenv().
+ * This very basic OS-dependent method worked in 1.1 (and 1.2?), was 
+ * deprecated in 1.3, throws an exception in 1.4, and WORKS AGAIN in 1.5.
+ * @version $Id$
+ */
 public class GetEnv {
-	public static void main(String[] argv) {
+	public static void main(String[] argv) {		
 		System.out.println("System.getenv(\"PATH\") = " + System.getenv("PATH"));
 	}
 }
