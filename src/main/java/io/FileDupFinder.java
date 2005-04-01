f7f93eb97dd236a793d514045ffc80040cf1ed83
Ian Darwin <ian@darwinsys.com>
2005-04-01 17:39:59 +0000

    Oops, remove a file descriptor leak.

--- src/main/java/io/FileDupFinder.java
+++ src/main/java/io/FileDupFinder.java
@@ -24,10 +24,15 @@ public class FileDupFinder implements FileHandler {
 	private static final int BUFSIZE = 65536;
 	byte[] data = new byte[BUFSIZE];
 	
+	/** Called to digest a file, report if the digest is in the Map, else add it to the Map.
+	 * Note that it leaves the first file it sees with a given digest in the Map.
+	 */
 	public void visit(File f) throws IOException {
 		String path = f.getAbsolutePath();
 
 		String hash = getHash(f);
+		
+		// System.out.println(path + "-->" + hash); // left in so you can run this, compare with md5/md5sum...
 
 		String hashedPath = null;
 		if ((hashedPath = (String)seenFiles.get(hash)) != null) {
@@ -42,18 +47,23 @@ public class FileDupFinder implements FileHandler {
 	 * @return
 	 */
 	private String getHash(File f) throws IOException {
-		InputStream in = new FileInputStream(f);
+		InputStream in = null;
+
 		MessageDigest md;
 		try {
+			in = new FileInputStream(f);
 			md = (MessageDigest) MessageDigest.getInstance("MD5");
+			int n = 0;
+			while ((n = in.read(data)) > 0) {
+				md.update(data, 0, n);		// digest it
+			}
 		} catch (NoSuchAlgorithmException e) {
 			throw new IOException(e.toString());
-		}
+		} finally {
+			if (in != null)
+				in.close();
+		}		
 		
-		int n = 0;
-		while ((n = in.read(data)) > 0) {
-			md.update(data, 0, n);		// digest it
-		}
 		// Digest the credentials.
 		byte[] digest = md.digest();
 
@@ -62,5 +72,4 @@ public class FileDupFinder implements FileHandler {
 
 		return result;
 	}
-
 }
