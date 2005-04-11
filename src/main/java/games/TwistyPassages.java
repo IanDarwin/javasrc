5840594375d0e62d048f23669256d1032ffa7650
Ian Darwin <ian@darwinsys.com>
2005-04-11 22:09:11 +0000

    Silly fragment from an Adventure-style game.

---
+++ src/main/java/games/TwistyPassages.java
@@ -0,0 +1,55 @@
+package games;
+
+import java.io.*;
+
+/** Fake up the twisty passages part of Adenture(6) */
+public class TwistyPassages {
+
+	private static String[] list = {
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all alike.",
+		"You are in a maze of twisty little passages, all different.",
+		"You are in a little maze of twisty passages, all different.",
+		"You are in a twisty little maze of passages, all different.",
+		"You are in a twisty maze of little passages, all different.",
+		"You are in a little twisty maze of passages, all different.",
+		"You are in a maze of little twisty passages, all different.",
+	};
+	
+	private static String[] directions = {
+		"east",
+		"north",
+		"south",
+		"west",
+		"up",
+		"down"
+	};
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader is = new BufferedReader(
+				new InputStreamReader(System.in));
+		String inputLine;
+
+		while ((inputLine = is.readLine()) != null) {
+			if (!isVerb(inputLine)) {
+				System.out.println("I don't understand that word");
+			}
+			System.out.println(list[(int)(Math.random() * list.length)]);
+		}
+		is.close();
+	}
+
+	private static boolean isVerb(String inputLine) {
+		for (int i = 0; i < directions.length; i++) {
+			if (directions[i].startsWith(inputLine)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
