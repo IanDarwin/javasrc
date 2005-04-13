9679edcd93a0c3a56b0c57941a3270356897d6c0
Ian Darwin <ian@darwinsys.com>
2005-04-13 19:51:50 +0000

    Major cleanup (but doesn't work).

--- src/main/java/xml/JDOMDemo.java
+++ src/main/java/xml/JDOMDemo.java
@@ -9,7 +9,6 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.ProcessingInstruction;
-import org.jdom.input.DOMBuilder;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.DOMOutputter;
 
@@ -18,86 +17,92 @@ import org.jdom.output.DOMOutputter;
  */
 public class JDOMDemo {
 
-    public static void main(String[] args) {
-  
+	public static void main(String[] args) {
+
 		// Must be at least one file or URL argument
-        if (args.length == 0) {
-            System.out.println("Usage: java JDOMDemo URL [...]"); 
-        } 
-        
-        SAXBuilder saxBuilder = new SAXBuilder();
-        DOMBuilder domBuilder = new DOMBuilder();
-                
-        for (int i = 0; i < args.length; i++) {
-      
-            try {
-                Document jdomDocument = saxBuilder.build(args[i]);
+		String fileName = null;
+		try {
+			if (args.length == 0) {
 
-                DOMOutputter domOutputter = new DOMOutputter();
+				fileName = "build.xml";
+				System.out.println("JDOMDemo.main(): using " + fileName);
+	
+				process(fileName);
+			} else {
+				for (int i = 0; i < args.length; i++) {
+					System.out.println("JDOMDemo.main(): using " + (fileName = args[i]));
 
-                /*
-                 * Test getting DOM Document from JDOM Document
-                org.w3c.dom.Document domDocument = domOutputter.output(doc);
-                 */
+					process(fileName);
+				}
+			}
+		} catch (JDOMException e) { //  well-formedness or other error
+			System.out.println(fileName + " is not a well formed XML document.");
+			System.out.println(e.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Input or Output error:" + fileName + ": " + ex);
+		}
 
-                /*
-                 * Test getting DOM Element from JDOM Element
-                 */
-                org.w3c.dom.Element domElement = 
-                	domOutputter.output(jdomDocument.getRootElement());
+	}
 
-                /*
-                 * Test getting JDOM Element from DOM Element
-                 */
-                org.jdom.Element jdomElement = domBuilder.build(domElement);
-                demo(jdomElement);
+	/** Show writing out DOM document.
+	 * @param jdomDocument
+	 * @param domOutputter
+	 * @throws JDOMException
+	 * @throws IOException 
+	 */
+	private static void process(String fileName) throws JDOMException, IOException {
+		System.out.println("JDOMDemo.myWriteOut()");
+		
+		SAXBuilder saxBuilder = new SAXBuilder();
+		
+		Document jdomDocument = saxBuilder.build(fileName);
+		
+		DOMOutputter domOutputter = new DOMOutputter();
+		
+		org.w3c.dom.Document domElement = domOutputter.output(jdomDocument);
+	}
 
-            } catch (JDOMException e) { // indicates a well-formedness or other error
-                System.out.println(args[i] + " is not a well formed XML document.");
-                System.out.println(e.getMessage());
-            } catch (IOException ex) {
-				System.out.println("Input or Output error:" + 
-					args[i] + ": " + ex);
-			}     
-        }
-    }
+	/**
+	 * Show iterating over a Document and printing some elements.
+	 * @param doc
+	 */
+	public static void demo(Document doc) {
 
-    public static void demo(Document doc) {
-
-        List children = doc.getContent();
-        Iterator iterator = children.iterator();
-        while (iterator.hasNext()) {
-            Object o = iterator.next();
-            if (o instanceof Element) {
-                demo((Element) o);
-            }
-            else if (o instanceof Comment)
+		List children = doc.getContent();
+		Iterator iterator = children.iterator();
+		while (iterator.hasNext()) {
+			Object o = iterator.next();
+			if (o instanceof Element) {
+				demo((Element) o);
+			} else if (o instanceof Comment)
 				doComment((Comment) o);
-            else if (o instanceof ProcessingInstruction) 
+			else if (o instanceof ProcessingInstruction)
 				doPI((ProcessingInstruction) o);
-        }
-    }     
+		}
+	}
 
-    public static void demo(Element element) {
+	/** Show iterating over one Element and its kids.
+	 * @param element
+	 */
+	public static void demo(Element element) {
 		System.out.println("Element " + element);
 
-        List attributes = element.getAttributes();
-        List children = element.getContent();
-        Iterator iterator = children.iterator();
-        while (iterator.hasNext()) {
-            Object o = iterator.next();
-            if (o instanceof Element) {
-                demo((Element) o);
-            }
-            else if (o instanceof Comment) 
-				doComment((Comment)o);
-            else if (o instanceof ProcessingInstruction) 
-				doPI((ProcessingInstruction)o);
-            else if (o instanceof String) {
-                System.out.println("String: " + o);
-            }   
-        }
-    }  
+		List attributes = element.getAttributes();
+		List children = element.getContent();
+		Iterator iterator = children.iterator();
+		while (iterator.hasNext()) {
+			Object o = iterator.next();
+			if (o instanceof Element) {
+				demo((Element) o);
+			} else if (o instanceof Comment)
+				doComment((Comment) o);
+			else if (o instanceof ProcessingInstruction)
+				doPI((ProcessingInstruction) o);
+			else if (o instanceof String) {
+				System.out.println("String: " + o);
+			}
+		}
+	}
 
 	public static void doComment(Comment c) {
 		System.out.println("Comment: " + c);
