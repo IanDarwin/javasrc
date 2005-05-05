e4a3e0148c2aced7145cffd3942758e1ee9548f7
Ian Darwin <ian@darwinsys.com>
2005-05-05 03:21:14 +0000

    Modernize from a declared Vector to declared as List<T>.

--- src/main/java/sched/CalEventMgr.java
+++ src/main/java/sched/CalEventMgr.java
@@ -1,23 +1,24 @@
 package sched;
 
 /** Generic(?) Event Handling for the DateSelectedListeners */
+import java.util.List;
 import java.util.Vector;
 public class CalEventMgr {
-	public Vector v = new Vector();
+	public List<DateSelectedListener> v = new Vector<DateSelectedListener>();
 
 	public void addDateSelectedListener(DateSelectedListener dl) {
-		v.addElement(dl);
+		v.add(dl);
 	}
 
 	public void removeDateSelectedListener(DateSelectedListener dl) {
-		v.removeElement(dl);
+		v.remove(dl);
 	}
 
 	public void fireEvent(DateSelectedEvent dse) {
 		Object s = dse.getSource();
 		// System.out.println(s + " says fireEvent( " + dse + ")");
 		for (int i=0; i<v.size(); i++) {
-			DateSelectedListener l = ((DateSelectedListener)v.elementAt(i));
+			DateSelectedListener l = ((DateSelectedListener)v.get(i));
 			if (l == s)
 				continue;
 			System.out.println("Telling " + l + " about " + dse);
