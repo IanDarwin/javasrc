5786820d2901ee09f681218e8a4b00f56fa58e76
Ian Darwin <ian@darwinsys.com>
2005-06-26 01:46:54 +0000

    New: FillDisk by writing infinite slop. Good for security (pseudo-disk-erase)but can also be for local DoS attack in absence of disk quotas.

---
+++ src/main/java/io/FillDisk.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.*;
+
+/** FillDisk writes until the disk is full.
+ * Like any tool,can be used for evil (DOS) or good (writing
+ * over blocks after you remove an important file).
+ */
+public class FillDisk {
+	public static void main(String[] args) throws IOException {
+		File f = File.createTempFile("fill", "disk");
+		OutputStream os = new FileOutputStream(f);
+		System.out.println("Filename is " + f.getAbsolutePath());
+		byte[] data = new byte[65535];
+		while (os != null) {
+			try {
+				os.write(data);
+			} catch (IOException e) {
+				System.out.println(e);
+				f.delete();
+				os = null;
+			}
+		}
+	}
+}
