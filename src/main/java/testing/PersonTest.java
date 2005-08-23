16feb1265dd84f31bc60447d3e885cabf9feec73
Ian Darwin <ian@darwinsys.com>
2005-08-23 22:51:07 +0000

    Shorten message.

--- src/main/java/testing/PersonTest.java
+++ src/main/java/testing/PersonTest.java
@@ -13,7 +13,7 @@ public class PersonTest extends TestCase {
 	public void testNameConcat() {
 		Person p = new Person("Ian", "Darwin");
 		String f = p.getFullName();
-		assertEquals("Name concatenation broken", "Ian Darwin", f);
+		assertEquals("Name concatenation", "Ian Darwin", f);
 	}
 
 }
