e3f793820243752b9ac280ac671d5dab5fbf5586
Ian Darwin <ian@darwinsys.com>
2005-09-23 17:38:26 +0000

    Send table name in UPPER CASE.

--- src/main/java/database/jdbc/ShowTable.java
+++ src/main/java/database/jdbc/ShowTable.java
@@ -14,7 +14,8 @@ import com.darwinsys.sql.ConnectionUtil;
 public class ShowTable {
 	public static void main(String[] args) throws SQLException {
 		String config = args[0];
-		String table = args[1];
+		// Some DatabaseMetaData impls require table names upper case
+		String table = args[1].toUpperCase();
 		Connection c = ConnectionUtil.getConnection(config);
 		DatabaseMetaData md = c.getMetaData();
 		ResultSet rs = md.getColumns(null, null, table, "%");
