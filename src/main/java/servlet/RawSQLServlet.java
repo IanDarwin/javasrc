07f46829c9e2f83cb92d5d3cd3b53ee2f7b15a77
IanDarwin <>
2005-10-29 00:09:01 +0000

    Organize Imports.

--- src/main/java/servlet/RawSQLServlet.java
+++ src/main/java/servlet/RawSQLServlet.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
