387533eaf2da2cf55a291097dfe77c126aff2b5f
Ian Darwin <ian@darwinsys.com>
2006-01-19 17:53:33 +0000

    Go further; if File claims we can write in a dir, try to do so, and report on that.

--- src/main/test/java/io/FileCanTestDirWritable.java
+++ src/main/test/java/io/FileCanTestDirWritable.java
@@ -1,6 +1,7 @@
 package io;
 
 import java.io.File;
+import java.io.IOException;
 
 /** Let's see if the File object can test for writeable directories on platforms
  * (directory write implies the ability to create/remove/rename files, at least
@@ -23,9 +24,18 @@ public class FileCanTestDirWritable {
 	 */
 	private static void process(final String path) {
 		File f = new File(path);
-		System.out.printf("File object is %s%n", f);
-		System.out.printf("File %s a DIRECTORY%n", f.isDirectory() ? "is" : "is not");
-		System.out.printf("File %s be written%n", f.canWrite() ? "can" : "can not");
+		System.out.printf("File asserts %s %s a DIRECTORY%n", path, f.isDirectory() ? "is" : "is not");
+		System.out.printf("File claims  %s %s be written%n" , path, f.canWrite() ? "can" : "can not");
+		if (f.isDirectory() && f.canWrite()) {
+			try {
+				File g = File.createTempFile("FileCanTestDirWritable", null, f);
+				System.out.printf("... And it can; we created %s%n", g);
+				g.delete();
+				System.out.printf("... (We removed it...)");
+			} catch (IOException e) {
+				System.out.printf("... But in fact we cannot create a file there: %s%n", e);
+			}
+		}
 		System.out.println();
 	}
 
