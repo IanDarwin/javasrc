a417c03ecf460babb783fa2029ad7dd19f97c29c
Ian Darwin <ian@darwinsys.com>
2006-01-27 04:57:20 +0000

    Switch to using find(), put trash at front to prove that it works.

--- src/main/java/regex/GetParen1.java
+++ src/main/java/regex/GetParen1.java
@@ -5,8 +5,8 @@ import java.util.regex.*;
 public class GetParen1 {
 	public static void main(String[] args) {
 		Pattern patt = Pattern.compile("(\\w+)\\s(\\d+)"); 
-		Matcher matcher = patt.matcher("Bananas 123"); 
-		matcher.lookingAt();
+		Matcher matcher = patt.matcher("No Bananas 123"); 
+		matcher.find();
 		System.out.println("Name: " + matcher.group(1)); 
 		System.out.println("Number: " + matcher.group(2)); 
 	}
