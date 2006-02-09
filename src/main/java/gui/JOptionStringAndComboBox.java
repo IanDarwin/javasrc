6a96f22b1767b65efb98066e1ee5cb7afb252c92
Ian Darwin <ian@darwinsys.com>
2006-02-09 17:25:24 +0000

    Demo of JOptionPane with both a String and a JComboBox.

---
+++ src/main/java/gui/JOptionStringAndComboBox.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+
+public class JOptionStringAndComboBox {
+
+	public static void main(String[] args) {
+		
+		String[] strings = { "OK" };
+		JComboBox combo = new JComboBox();
+		Object[] data = { "Choose One", combo };
+		combo.addItem("One");
+		combo.addItem("Two");
+		int n = JOptionPane.showOptionDialog(null, data, "Title", JOptionPane.DEFAULT_OPTION, 
+				JOptionPane.QUESTION_MESSAGE, null, strings, strings[0]);
+		if (n == -1) {
+			JOptionPane.showMessageDialog(null, "Farewell");
+			System.exit(0);
+		}
+		String yourChoice = (String)combo.getSelectedItem();
+		JOptionPane.showMessageDialog(null, "You chose " + yourChoice, "Thanks", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
