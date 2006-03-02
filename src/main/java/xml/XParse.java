df0340b7f5db33450fe97d258a19affcd69a7fe8
Ian Darwin <ian@darwinsys.com>
2006-03-02 20:00:45 +0000

    Trying to validate using Schema, but seems to throw exception, may be document related, or not.

--- src/main/java/xml/XParse.java
+++ src/main/java/xml/XParse.java
@@ -1,11 +1,16 @@
 package xml;
 
 import java.io.File;
+import java.io.IOException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
 
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -15,23 +20,66 @@ import org.xml.sax.SAXParseException;
  */
 public class XParse {
 
-	/** Parse the file */
-	public static void parse(String fileName, boolean validate) {
-		try {
+	/** Parse the file 
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException */
+	public static void parse(String fileName, Schema schema, boolean validate) throws ParserConfigurationException, SAXException, IOException {
+
 			System.err.println("Parsing " + fileName + "...");
 
 			// Make the document a URL so relative DTD works.
 			String uri = "file:" + new File(fileName).getAbsolutePath();
 
-			DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-			if (validate)
-				f.setValidating(true);
-			DocumentBuilder p = f.newDocumentBuilder();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			if (validate || schema != null)
+				factory.setValidating(true);
+			if (schema != null) {
+				factory.setSchema(schema);
+			}
+			DocumentBuilder p = factory.newDocumentBuilder();
 			// Get local copies of DTDs...
 			p.setEntityResolver(new MyDTDResolver());
-			Document doc = p.parse(uri);
-			System.out.println("Parsed OK");
+			p.parse(uri);
+			System.out.println("Parsed/Validated OK");
+	}
 
+	/**
+	 * Parse one or more XML documents with optional validation.
+	 * <b>Note:</b> It is an unpleasant limitation of javax.xml.validation that parsing of DTD-based documents can
+	 * extract the DTD name/location and use it, whereas Schema(etc)-based validation requires the user to do
+	 * this manually before invoking the parser.
+	 * @param av Command args, may include -v for validation, and -a schema.xsd, before the filename(s);
+	 * all must use the same Schema.
+	 * @throws SAXException
+	 */
+	public static void main(String[] av) throws SAXException {
+		if (av.length == 0) {
+			System.err.println("Usage: XParse file");
+			return;
+		}
+		boolean validate = false;
+		Schema schema = null;
+		try {
+		for (int i=0; i<av.length; i++) {
+			if (av[i].equals("-v"))
+				validate = true;
+			else if (av[i].equals("-a")) {
+			    // create a SchemaFactory capable of understanding WXS schemas - from the Javadoc page
+			    SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+			    // load the W3c XML schema, represented by a Schema instance
+			    String schemaLocation = av[++i];
+				File file = new File(schemaLocation);
+				if (!file.exists()) {
+					throw new IOException("Schema location = " + schemaLocation + " does not exist");
+				}
+				Source schemaFile = new StreamSource(file);
+			    schema = factory.newSchema(schemaFile);
+			    
+			} else
+				parse(av[i], schema, validate);
+		}
 		} catch (SAXParseException ex) {
 			System.err.println("+================================+");
 			System.err.println("|       *SAX Parse Error*        |");
@@ -49,21 +97,8 @@ public class XParse {
 			System.err.println("+================================+");
 			System.err.println("|           *XML Error*          |");
 			System.err.println("+================================+");
-			System.err.println(ex.toString()); 
-		}
-	}
-
-	public static void main(String[] av) {
-		if (av.length == 0) {
-			System.err.println("Usage: XParse file");
-			return;
-		}
-		boolean validate = false;
-		for (int i=0; i<av.length; i++) {
-			if (av[i].equals("-v"))
-				validate = true;
-			else
-				parse(av[i], validate);
+			ex.printStackTrace();
+			
 		}
 	}
 }
