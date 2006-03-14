39dad49ebf47a57a9059228b7eb696a402916742
Ian Darwin <ian@darwinsys.com>
2006-03-14 16:37:45 +0000

    Comment that valueof is case sensitive; use toUpperCase on line before calline valueof.

--- src/main/java/structure/EnumDemo.java
+++ src/main/java/structure/EnumDemo.java
@@ -3,14 +3,17 @@ package structure50;
 public class EnumDemo {
 
 	enum Color {
-		RED, BLUE, GREEN
+		RED, BLUE, GREEN;
 	};
 
 	public static void main(String[] args) {
 		Color color = Color.RED;
 		String line = getLine();
-		if (line != null)
-			color = Color.valueOf(line);
+		if (line != null) {
+			// valueOf is case-sensitive, so translate "user input" to upper case
+			// since enum values are (by 30 years of convention) in UPPER CASE.
+			color = Color.valueOf(line.toUpperCase());
+		}
 		switch (color) {
 		case RED:
 			System.out.println("You like dark apples");
@@ -24,8 +27,11 @@ public class EnumDemo {
 		}
 	}
 
+	static String[] lines = { "Green", "blue", "RED" };
+	static int i = 0;
+	
 	private static String getLine() {
-		return "GREEN";
+		return lines[i++%3];
 	}
 
 }
