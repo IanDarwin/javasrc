b6d5b713e356dede8e8a5468fb96f538e3bf4c46
Ian Darwin <ian@darwinsys.com>
2006-03-17 04:27:46 +0000

    Sample bean using day, mon, year.

---
+++ src/main/java/testing/MyDate.java
@@ -0,0 +1,34 @@
+package testing;
+
+public class MyDate {
+	public Integer year, month, day;
+
+	public String toString() {
+		return year + "/" + month + "/" + day;
+	}
+
+	public Integer getDay() {
+		return day;
+	}
+
+	public void setDay(Integer day) {
+		this.day = day;
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public void setMonth(Integer month) {
+		this.month = month;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+}
