c5a8f11f131e7c71657f882600146e582937fb1a
Ian Darwin <ian@darwinsys.com>
2006-04-02 01:13:45 +0000

    Modernize, convert to Swing, use a file chooser, re-create missing properties file, &c.

--- src/main/java/diary/Diary.java
+++ src/main/java/diary/Diary.java
@@ -1,25 +1,45 @@
 package diary;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.io.*;
-import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+import com.darwinsys.swingui.I18N;
 
 /** This just maintains a text file as a Diary, with TimeStamps.
  */
-public class Diary extends Frame {
+public class Diary extends JFrame {
 	/** The width of the text */
 	public static final int COLS = 70;
 	/** The TextArea which displays the text, and does most of the work. */
-	TextArea tf;
+	JTextArea tf;
 	/** The Version name and number */
 	public final static String VERSION = "Java Diary 0.0";
 	/** The default filename, if none given on the command line */
 	public final static String DEFAULT_FILE_NAME = "Diary.txt";
 	/** The actual filename */
 	protected String fileName = null;
-
 	/** "main program" method - construct and show */
 	public static void main(String[] argv) {
 
@@ -35,43 +55,50 @@ public class Diary extends Frame {
 	/** Construct the object including its GUI */
 	public Diary() {
 		super("My Diary (" + VERSION + ")");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		MenuItem mi;
 
-		add(BorderLayout.NORTH, new Label(VERSION, Label.CENTER));
+		add(BorderLayout.NORTH, new JLabel(VERSION, JLabel.CENTER));
 
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				setVisible(false);
-				dispose();
-				System.exit(0);
-			}
-		});
-		MenuBar mb = new MenuBar();
-		setMenuBar(mb);
+		JMenuBar mb = new JMenuBar();
+		setJMenuBar(mb);
 
-		ResourceBundle b = ResourceBundle.getBundle("DiaryMenus");
+		ResourceBundle b = ResourceBundle.getBundle("diary.DiaryMenus");
 		String menuLabel;
 		try { menuLabel = b.getString("file"+".label"); }
 		catch (MissingResourceException e) { menuLabel="file"; }
 
-		Menu fm = new Menu(menuLabel);
-		fm.add(mi = mkMenuItem(b, "file", "open"));
-		fm.add(mi = mkMenuItem(b, "file", "new"));
+		JMenu fm = new JMenu(menuLabel);
+		JMenuItem mi;
+		fm.add(mi = I18N.mkMenuItem(b, "file", "open"));
+		mi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = getFileChooser();
+				int returnVal = chooser.showOpenDialog(Diary.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = chooser.getSelectedFile();
+					setFile(file.getAbsolutePath());
+				} else {
+					System.out.println("You did not choose a filesystem object.");
+				}
+			}
+		});
+		fm.add(mi = I18N.mkMenuItem(b, "file", "new"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				fileName = null;
 				tf.setText("");
 			}
 		});
-		fm.add(mi = mkMenuItem(b, "file", "save"));
+		fm.add(mi = I18N.mkMenuItem(b, "file", "save"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				saveFile(null);
 			}
 		});
 		fm.addSeparator();
-		fm.add(mi = mkMenuItem(b, "file", "exit"));
+		fm.add(mi = I18N.mkMenuItem(b, "file", "exit"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Diary.this.setVisible(false);
@@ -81,14 +108,14 @@ public class Diary extends Frame {
 		});
 		mb.add(fm);
 
-		Menu vm = mkMenu(b,  "edit");
-		vm.add(mi = mkMenuItem(b, "edit", "format"));
+		JMenu vm = I18N.mkMenu(b,  "edit");
+		vm.add(mi = I18N.mkMenuItem(b, "edit", "format"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				format();
 			}
 		});
-		vm.add(mi = mkMenuItem(b, "edit", "timestamp"));
+		vm.add(mi = I18N.mkMenuItem(b, "edit", "timestamp"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tf.append("\n" + new Date().toString() + "\n");
@@ -96,20 +123,20 @@ public class Diary extends Frame {
 		});
 		mb.add(vm);
 
-		Menu hm = mkMenu(b,  "help");
-		hm.add(mi = mkMenuItem(b, "help", "about"));
+		JMenu hm = I18N.mkMenu(b,  "help");
+		hm.add(mi = I18N.mkMenuItem(b, "help", "about"));
 		mi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(Diary.this, 
 				VERSION + "\n" +
-				"Copyright (c) 1997 Ian F. Darwin\n" +
+				"Copyright (c) 1997-2006 Ian F. Darwin\n" +
 				"Free download from http://javacook.darwinsys.com/",
-				VERSION, JOptionPane.ERROR_MESSAGE);
+				VERSION, JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
-		mb.setHelpMenu(hm);		// needed for portability (Motif, etc.).
+		mb.add(hm);
 
-		add(BorderLayout.CENTER, tf = new TextArea(24,COLS));
+		add(BorderLayout.CENTER, tf = new JTextArea(24,COLS));
 
 		pack();
 
@@ -120,6 +147,20 @@ public class Diary extends Frame {
 		int newY = (them.height- us.height)/ 2;
 		setLocation(newX, newY);
 	}
+	
+	private JFileChooser chooser = null;
+	protected JFileChooser getFileChooser() {
+		if (chooser == null) {
+			chooser = new JFileChooser();
+//			JFileFilter filter = new JFileFilter();
+//			filter.addType("java");
+//			filter.addType("class");
+//			filter.addType("jar");
+//			filter.setDescription("Java-related files");
+//			chooser.addChoosableFileFilter(filter);
+		}
+		return chooser;
+	}
 
 	/** Format the selection (like unix fmt) */
 	void format() {
@@ -127,7 +168,12 @@ public class Diary extends Frame {
 		int col = 0;
 		int start = tf.getSelectionStart();
 		int end   = tf.getSelectionEnd();
-		StringTokenizer st = new StringTokenizer(tf.getSelectedText());
+		final String selectedText = tf.getSelectedText();
+		if (selectedText == null) {
+			JOptionPane.showMessageDialog(this, "Please select text before formatting");
+			return;
+		}
+		StringTokenizer st = new StringTokenizer(selectedText);
 		StringBuffer s = new StringBuffer();
 		while (st.hasMoreTokens()) {
 			String sval = st.nextToken();
@@ -140,6 +186,10 @@ public class Diary extends Frame {
 		}
 		tf.replaceRange(s.toString(), start, end);
 	}
+	
+	/**
+	 * Specify the filename.
+	 */
 	void setFile(String fn) {
 		BufferedReader is = null;
 		String s;
@@ -155,6 +205,9 @@ public class Diary extends Frame {
 		}
 	}
 
+	/**
+	 * Save the Model into the given file.
+	 */
 	void saveFile(String fn) {
 		if (fn == null)
 			fn = fileName;
@@ -172,29 +225,6 @@ public class Diary extends Frame {
 			if (os != null)
 				os.close();
 		}
-	}
-
-	/** Convenience routine to make a Menu */
-	public Menu mkMenu(ResourceBundle b, String name) {
-		String menuLabel;
-		try { menuLabel = b.getString(name+".label"); }
-		catch (MissingResourceException e) { menuLabel=name; }
-		return new Menu(menuLabel);
-	}
-
-	/** Convenience routine to make a MenuItem */
-	public MenuItem mkMenuItem(ResourceBundle b, String menu, String name) {
-		String miLabel;
-		try { miLabel = b.getString(menu + "." + name + ".label"); }
-		catch (MissingResourceException e) { miLabel=name; }
-		String key = null;
-		try { key = b.getString(menu + "." + name + ".key"); }
-		catch (MissingResourceException e) { key=null; }
-
-		if (key == null)
-			return new MenuItem(miLabel);
-		else
-			return new MenuItem(miLabel, new MenuShortcut(key.charAt(0)));
 	}
 
 }
