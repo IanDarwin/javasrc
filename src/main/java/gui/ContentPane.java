474a5e82f47191a774549400e933d78e9b86446c
Ian Darwin <ian@darwinsys.com>
2006-04-03 02:08:09 +0000

    Warning removal.

--- src/main/java/gui/ContentPane.java
+++ src/main/java/gui/ContentPane.java
@@ -1,11 +1,14 @@
 package gui;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ContentPane extends JFrame {
 	public ContentPane() {
 		Container cp = getContentPane();
 		// now add Components to "cp"...
+		cp.add(new JLabel("A Really Simple Demo", JLabel.CENTER));
 	}
 }
