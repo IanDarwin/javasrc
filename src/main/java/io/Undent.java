e35e8cbb41e091150a35cf335895c5b8dab8bfd6
Ian Darwin <ian@darwinsys.com>
2006-04-30 17:46:14 +0000

    Due to an error in my CVS repository, there were two different versions of  Undent in the tree, one in "io" and one in "Strings". Since it is covered in the Strings chapter of the Java Cookbook I have chosen to keep the copy under Strings.

--- src/main/java/io/Undent.java
+++ src/main/java/io/Undent.java
@@ -1,55 +1,12 @@
 package io;
 
-import java.io.*;
-
-/** Undent - remove leading spaces
- * @author Ian F. Darwin, http://www.darwinsys.com/
- * @version $Id$
+/** 
+ * Due to an error in my CVS repository, there were two different versions of
+ * Undent in the tree, one in "io" and one in "Strings".
+ * Since it is covered in the Strings chapter of the Java Cookbook
+ * I have chosen to keep the copy under Strings.
  */
 
 public class Undent {
-	//+
-	/** the default number of spaces to remove. */
-	int nSpaces = 2;
-	//-
-
-    public static void main(String[] av) {
-        Undent c = new Undent();
-        if (av.length == 0)
-            c.process(new BufferedReader(
-				new InputStreamReader(System.in)));
-		else for (int i=0; i<av.length; i++) {
-			try {
-				c.process(new BufferedReader(new FileReader(av[i])));
-			} catch (FileNotFoundException e) {
-				System.err.println(e);
-			}
-        }
-    }
-
-
-    /** undent one file, given an open BufferedReader.
-	 * Undent by removing UP TO "nSpaces" leading spaces.
-	 */
-    public void process(BufferedReader is) {
-		//+
-		// GRRR THIS DOES NOT QUITE WORK - FIX -- Ian
-        try {
-            String inputLine;
-
-            while ((inputLine = is.readLine()) != null) {
-				int i;
-				for (i=0; i<nSpaces; i++) {
-					if (!Character.isWhitespace(inputLine.charAt(i)))
-						break;
-				}
-                System.out.println(inputLine.substring(i));
-            }
-            is.close();
-		//-
-        } catch (IOException e) {
-            System.out.println("IOException: " + e);
-        }
-    }
-	//-
+	// Nothing here, see copy under the Strings directory
 }
