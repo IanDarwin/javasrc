b2c43822ad933cda2341416fbabb15213b48cd81
Ian Darwin <ian@darwinsys.com>
2006-05-02 22:04:22 +0000

    Add doc comments explaining what's going on!

--- src/main/java/lang/VarArgsDemo.java
+++ src/main/java/lang/VarArgsDemo.java
@@ -14,18 +14,46 @@ public class VarArgsDemo {
 		process(System.out, "Hello", "Goodbye");
 		process(System.out, 42, 1066, 1776);
 		process(System.out, "Foo", new Date(), new Object());
+		passThrough(System.out, "%s %s %s%n", "Foo", new Date(), new Object());
 	}
+	
+	/** Show passing the entire varargs list on to another method
+	 * @param out
+	 * @param fmt
+	 * @param args
+	 */
+	static void passThrough(PrintStream out, String fmt, Object ... args) {
+		line();
+		out.printf(fmt, args);
+	}
+	
+	/** Show iterating through the varargs list.
+	 * @param out
+	 * @param args
+	 */
 	static void process(PrintStream out, Object ... args) {
 		line();
-		for (int i = 0; i < args.length; i++){
-			out.println("Argument " + i + " is " + args[i]);
+		int i = 0;
+		for (Object o : args){
+			out.print("Argument " + ++i + " is " + o + "; ");
 		}
+		System.out.println();
 	}
+	
+	/** Overload using int varargs.
+	 * @param out
+	 * @param args
+	 */
 	static void process(PrintStream out, int ... args) {
+		line();
+		System.out.println("int varargs");
 		for (int i = 0; i < args.length; i++){
-			out.println("Argument " + i + " is " + args[i]);
+			out.print("Argument " + i + " is " + args[i] + "; ");
 		}
+		System.out.println();
 	}
+	
+	/** toy, draw a line... */
 	private static void line() {
 		System.out.println("--------------------------");
 	}
