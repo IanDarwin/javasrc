a90a6bbc158d564a865751797401fab014f55e62
Ian Darwin <ian@darwinsys.com>
2006-06-11 15:20:20 +0000

    Show bypassing "private" using Member.setAccessible(true).

---
+++ src/main/java/reflection/DefeatPrivacy.java
@@ -0,0 +1,38 @@
+package introspection;
+
+import java.lang.reflect.Field;
+
+/**
+ * Demonstrate that it is, in fact, all too easy to access private members
+ * of an object using Reflection, using the default SecurityManager (so this
+ * will probably not work in an Applet, for example...).
+ */
+public class DefeatPrivacy {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		new DefeatPrivacy().process();
+
+	}
+	
+	class X {
+		private int p = 42;
+		int q = 3;
+		public int r = 0x123;
+	}
+
+	private void process() throws Exception {
+		X x = new X();
+		System.out.println(x);
+		Class<? extends X> class1 = x.getClass();
+		Field[] flds = class1.getDeclaredFields();
+		for (Field f : flds) {
+			f.setAccessible(true);	// bye-bye "private"
+			System.out.println(f + "==" + f.get(x));
+		}
+	}
+
+}
