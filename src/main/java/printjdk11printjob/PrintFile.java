abc1c9b12eb09d58eed77bd2ac8e9a67d610f794
Ian Darwin <ian@darwinsys.com>
2006-06-13 16:02:02 +0000

    Modernize & code cleanup.

--- src/main/java/printjdk11printjob/PrintFile.java
+++ src/main/java/printjdk11printjob/PrintFile.java
@@ -1,14 +1,30 @@
 package graphics;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.PrintJob;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Properties;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
- * PrintFile -- Print a file named onthe command line
+ * PrintFile -- Print a file named on the command line
  */
-public class PrintFile extends Frame {
+public class PrintFile extends JFrame {
+	/**  */
+	private static final long serialVersionUID = -668723004626260238L;
 	/** The number of pages to print */
 	protected static final int NPAGES = 3;
 	/** The actual number of pages */
@@ -16,8 +32,9 @@ public class PrintFile extends Frame {
 	/** The PrintJob object */
 	PrintJob pjob = null;	// refers to whole print job
 
-	/** main program: instantiate and show. */
-	public static void main(String[] av) {
+	/** main program: instantiate and show. 
+	 * @throws IOException */
+	public static void main(String[] av) throws IOException {
 		PrintFile p = new PrintFile();
 		p.setVisible(true);
 		if (av.length==0)
@@ -30,11 +47,11 @@ public class PrintFile extends Frame {
 		System.exit(0);
 	}
 
-	/** Construct a PrintDemo1 with given Filename */
+	/** Constructor */
 	PrintFile() {
 		setLayout(new FlowLayout());
-		Button b;
-		add(b = new Button("Cancel"));
+		JButton b;
+		add(b = new JButton("Cancel"));
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (pjob != null)	// if quit while printing!
@@ -45,8 +62,10 @@ public class PrintFile extends Frame {
 		pack();
 	}
 
-	/** Print a file by name */
-	public void print(String fn) {
+	/** Print a file by name 
+	 * @throws IOException
+	 */
+	public void print(String fn) throws IOException {
 		// open it, call the other guy 
 		FileReader ifile = null;
 		try {
@@ -57,10 +76,10 @@ public class PrintFile extends Frame {
 		print(ifile);
 	}
 
-	/** Print a file by File */
-	public void print(Reader ifile) {
+	/** Print a file by File 
+	 * @throws IOException */
+	public void print(Reader ifile) throws IOException {
 		BufferedReader is = new BufferedReader(ifile);
-		Graphics g = null;	// refers to current page
 		System.out.println("Doing print");
 		pjob = getToolkit().getPrintJob(this,
 			"Printing Test", (Properties)null);
@@ -69,19 +88,17 @@ public class PrintFile extends Frame {
 		Dimension pDim = pjob.getPageDimension();
 		int pRes = pjob.getPageResolution();
 		System.out.println("Page size " + pDim + "; Res " + pRes);
-		g = pjob.getGraphics();
+		Graphics g = pjob.getGraphics();
+		if (g == null) {
+			System.err.println("Err, Graphics object from PrintJob is null!");
+			return;
+		}
 		g.setColor(Color.black);
 		g.setFont(new Font("SansSerif", Font.PLAIN, 12));
-		if (g == null) 	// ??
-			return;
 		int y = 100;
 		String line;
-		try {
-			while ((line = is.readLine()) != null) {
-				g.drawString(line, 10, y+=18);
-			}
-		} catch (IOException e) {
-			System.err.println(e);
+		while ((line = is.readLine()) != null) {
+			g.drawString(line, 10, y+=18);
 		}
 		// g.drawString("Page " + pgNum, 300, 300);
 		g.dispose(); // flush page
