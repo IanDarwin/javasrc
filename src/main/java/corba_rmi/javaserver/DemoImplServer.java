a17a85007acf6e2bbe98a297975d036e6ca7a287
IanDarwin <>
2006-07-13 18:27:19 +0000

    Make it compile - missing import, super() constructor.

--- src/main/java/corba_rmi/javaserver/DemoImplServer.java
+++ src/main/java/corba_rmi/javaserver/DemoImplServer.java
@@ -1,9 +1,17 @@
 package corba_rmi.javaserver;
 
+import java.rmi.RemoteException;
+
+import javax.rmi.PortableRemoteObject;
+
 import corba_rmi.Demo;
-import javax.naming.*;
 
 public class DemoImplServer extends PortableRemoteObject implements Demo {
+	
+	public DemoImplServer() throws RemoteException {
+		super();
+	}
+
 	public static void main(String[] args) { 
 		System.out.println("This program is not finished yet");
 	}
@@ -11,6 +19,6 @@ public class DemoImplServer extends PortableRemoteObject implements Demo {
 	private static int clientNum = 42;
 
 	public String getNext() {
-		return "You are " + ++clientNum;
+		return "You are client number " + ++clientNum;
 	}
 }
