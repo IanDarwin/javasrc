4ef46c1a814e927e11ed4386515b7380a4b7f5ec
Ian Darwin <ian@darwinsys.com>
2006-07-27 22:40:06 +0000

    Organize imports, remove warnings.

--- src/main/java/gui/JFileChooserDemo.java
+++ src/main/java/gui/JFileChooserDemo.java
@@ -1,11 +1,18 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.event.*;
-import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /** A simple demo of a JFileChooser in action. */
 public class JFileChooserDemo extends JPanel {
+
+	private static final long serialVersionUID = 2615629432967419176L;
 
 	/** Constructor */
 	public JFileChooserDemo(JFrame f) {
