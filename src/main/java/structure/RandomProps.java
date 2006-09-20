9a056edf02fe38a792aaed16473711405e1751d1
Ian Darwin <ian@darwinsys.com>
2006-09-20 14:09:08 +0000

    Better document the efficiency issues and other bits.

--- src/main/java/structure/RandomProps.java
+++ src/main/java/structure/RandomProps.java
@@ -6,9 +6,6 @@ import java.util.Random;
 
 /**
  * Show how to select a random value from a properties file.
- * This is more efficient than using Collections.toArray()
- * because this way only access the first 'n' references
- * while the toArray() copies all the references at least once.
  */
 public class RandomProps {
 	public static final String EMPTY_PROPERTIES_MESSAGE = "(empty properties)";
@@ -20,7 +17,16 @@ public class RandomProps {
 	}
 
 	/**
-	 * Select a randomly-chosen Value from the Properties
+	 * Select a randomly-chosen Value from the Properties.
+	 * This is more efficient - for the expected typical use of getting
+	 * one randome string from a Properties - than using Collections.toArray()
+	 * (or constructing a List from the Properties' KeySet)
+	 * because this way only access the first 'n' references once,
+	 * while the toArray() copies all the references at least once.
+	 * However, if you needed to retrieve many randomly chosen properties
+	 * from the same Properties object, then obviously
+	 * changing this class to pass a Properties into its constructor and
+	 * converting to a List or Array there would be more efficient
 	 * @param p
 	 */
 	public static String getRandomString(Properties p) {
--- src/main/test/java/structure/RandomPropsTest.java
+++ src/main/test/java/structure/RandomPropsTest.java
@@ -11,20 +11,21 @@ public class RandomPropsTest extends TestCase {
 		String ret = RandomProps.getRandomString(new Properties());
 		assertEquals(RandomProps.EMPTY_PROPERTIES_MESSAGE, ret);
 	}
-	
+
 	public void testOne() {
 		Properties p = new Properties();
 		p.put("foo", "bar");
 		assertEquals("bar", p.getProperty("foo"));
 	}
-	
+
 	public void testSystemProps() {
 		Properties p = System.getProperties();
 		Collection values = p.values();
 		for (int i = 0; i < 10; i++) {
 			String ret = RandomProps.getRandomString(p);
+			assertNotNull(ret);
 			assertTrue(values.contains(ret));
-			System.out.printf("Value %d = %s%n", i, ret);
+			System.out.printf("Random Value %d = %s%n", i, ret);
 		}
 	}
 }
