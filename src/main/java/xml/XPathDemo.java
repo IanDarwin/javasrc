7c98d6218f5c26de95ab254e7f85955c3e6166ff
Ian Darwin <ian@darwinsys.com>
2006-09-22 15:34:25 +0000

    Simple demo of XPath.

---
+++ src/main/java/xml/XPathDemo.java
@@ -0,0 +1,33 @@
+package xml;
+
+import java.io.ByteArrayInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+
+/**
+ * Simple demo of XPath.
+ */
+public class XPathDemo {
+
+	public static void main(String[] args) throws Exception {
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		String doc = "<?xml version='1.0'?>" +
+		"<section><sectiontitle>A Discourse of Numbers</sectiontitle>" +
+		"<sectionnumber>1.2</sectionnumber>" +
+		"<SC>Introduction</SC><p></p></section>";
+		Document document = builder.parse(new ByteArrayInputStream(doc.getBytes()));
+
+		// evaluate the XPath expression against the Document
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		String expression = "/section/sectionnumber";
+		Double chapNum = (Double) xpath.evaluate(expression, document, XPathConstants.NUMBER);
+		System.out.println("Section number is " + chapNum);
+	}
+
+}
