e13693abbb0f3031716dd9b4f65a042a5ed8800b
Ian Darwin <ian@darwinsys.com>
2006-09-23 22:57:45 +0000

    Make ConverterPrograms at least compilable.

---
+++ src/main/java/unfinished/pnmconv/ConverterPrograms.java
@@ -0,0 +1,97 @@
+package pnmconv;
+
+public class tmp {
+	class conv {
+		String name;
+		String conv;
+	}
+	public static final conv[] FT = {
+//		anytopnm
+//		atktopbm
+//		brushtopbm
+//		cmuwmtopbm
+//		fitstopgm
+//		fstopgm
+//		g3topbm
+//		gemtopbm
+//		giftog3
+//		giftoppm
+//		gouldtoppm
+//		hipstopgm
+//		icontopbm
+//		ilbmtoppm
+//		imgtoppm
+//		lispmtopgm
+//		macptopbm
+//		mgrtopbm
+//		mtvtoppm
+//		pcxtoppm
+//		pi1toppm
+//		pi3topbm
+//		picttoppm
+//		pjtoppm
+//		psidtopgm
+//		qrttoppm
+//		rasttopnm
+//		rawtopgm
+//		rawtoppm
+//		rgb3toppm
+//		spctoppm
+//		sputoppm
+//		tgatoppm
+//		tifftopnm
+//		xbmtopbm
+//		ximtoppm
+//		xpmtoppm
+//		xwdtopnm
+//		ybmtopbm
+//		yuvtoppm
+	};
+
+	public static final String[] TF = {
+//		pbmto10x
+//		pbmtoascii
+//		pbmtoatk
+//		pbmtobbnbg
+//		pbmtocmuwm
+//		pbmtoepson
+//		pbmtog3
+//		pbmtogem
+//		pbmtogo
+//		pbmtoicon
+//		pbmtolj
+//		pbmtomacp
+//		pbmtomgr
+//		pbmtopi3
+//		pbmtoplot
+//		pbmtoptx
+//		pbmtox10bm
+//		pbmtoxbm
+//		pbmtoybm
+//		pbmtozinc
+//		pgmtofits
+//		pgmtofs
+//		pgmtolispm
+//		pgmtopbm
+//		pgmtoppm
+//		pnmtops
+//		pnmtorast
+//		pnmtotiff
+//		pnmtoxwd
+//		ppmtogif
+//		ppmtoicr
+//		ppmtoilbm
+//		ppmtopcx
+//		ppmtopgm
+//		ppmtopi1
+//		ppmtopict
+//		ppmtopj
+//		ppmtopuzz
+//		ppmtorgb3
+//		ppmtosixel
+//		ppmtotga
+//		ppmtouil
+//		ppmtoxpm
+//		ppmtoyuv
+	};
+}
