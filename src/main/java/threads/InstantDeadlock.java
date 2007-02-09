2bf52f802b10f82d1268e8d86bf41bb4aa6345a3
Ian Darwin <ian@darwinsys.com>
2007-02-09 18:09:50 +0000

    Doc comment

--- src/main/java/threads/InstantDeadlock.java
+++ src/main/java/threads/InstantDeadlock.java
@@ -1,5 +1,8 @@
 package threads;
 
+/**
+ * The fastest way to a Threading deadlock
+ */
 public class InstantDeadlock {
 	public static void main(String[] args) throws Exception {
 		Thread.currentThread().join();
