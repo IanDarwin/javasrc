2d82ff33f4cd782c2bae5db5138257592ccbec00
Ian Darwin <ian@darwinsys.com>
2007-07-12 20:48:18 +0000

    Demo of Action. Backported from my 471-add slides :-)

---
+++ src/main/java/gui/ActionsDemo.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+/** An Action encapsulates:
+ * textual description for button/menuitem
+ * icon for display in menu/button
+ * enabled/disabled state
+ * an ActionListener
+ */
+@SuppressWarnings("serial")
+public class ActionsDemo extends JPanel {
+	final Action saver;
+
+	public ActionsDemo() {
+		saver = new AbstractAction("Save") {
+		     public void actionPerformed(ActionEvent e) {
+		       save();
+		    }
+		};
+		this.add(new JButton(saver));
+		JMenu saveMenu = new JMenu("File");
+		saveMenu.add(new JMenuItem(saver));
+		// ...
+
+	}
+
+	protected void save() {
+		System.out.println("This would save the file");
+		saver.setEnabled(false); // grays out both button and menu item
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame jf = new JFrame("ActionsDemo");
+		jf.add(new ActionsDemo());
+		jf.pack();
+		jf.setVisible(true);
+	}
+
+}
