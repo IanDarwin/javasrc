741de0347508fd31ee4218c10665eba6dd51dadf
Ian Darwin <ian@darwinsys.com>
2007-07-22 21:20:26 +0000

    JUnit test for APIFormatter.

---
+++ src/main/test/java/reflection/APIFormatterTest.java
@@ -0,0 +1,55 @@
+package reflection;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import junit.framework.TestCase;
+
+public class APIFormatterTest extends TestCase {
+	String fileName;
+
+	/**
+	 * To test it for real, create zip file with one class
+	 * from the same package, loaded from classpath.
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		File file = File.createTempFile("test", ".zip");
+		fileName = file.getAbsolutePath();
+		ZipOutputStream zf = new ZipOutputStream(new FileOutputStream(file));
+		zf.putNextEntry(new ZipEntry("foo.bar"));
+		zf.write("Hello".getBytes());
+		zf.putNextEntry(new ZipEntry("reflection/GetMethod.class"));
+		InputStream is =
+			GetMethod.class.getResourceAsStream("GetMethod.class");
+		int b = 0;
+		while((b = is.read()) != -1) {
+			zf.write((byte)b);
+		}
+		is.close();
+		zf.closeEntry();
+		zf.closeEntry();
+		zf.close();
+	}
+
+	Class x = null;
+	int nClasses = 0;
+	public final void testProcessOneZip() throws Exception {
+		APIFormatter target = new APIFormatter() {
+			@Override
+			protected void doClass(Class c) throws IOException {
+				++nClasses;
+				x = c;
+			}
+		};
+		target.processOneZip(fileName);
+		assertEquals(x, GetMethod.class);
+		assertEquals(1, nClasses);
+		System.out.println("Pass!");
+	}
+
+}
