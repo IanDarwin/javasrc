0d9e9c24f8685107d4ed2631a78c4d3a06582965
Ian Darwin <ian@darwinsys.com>
2007-08-01 20:11:25 +0000

    Better printing.

--- src/main/java/reflection/dynamicproxy/Quote.java
+++ src/main/java/reflection/dynamicproxy/Quote.java
@@ -82,6 +82,7 @@ public class DynamicProxyDemo {
             new Class[] { Quote.class }, handler);
 
 		System.out.println("Quote Proxy object is " + generatedProxy.getClass().getName());
+		objectBeingProxied.addQuote("Only the educated are free -- Epictetus");
 		System.out.println("Quote Proxy returned: " + generatedProxy.getQuote());
 	}
 }
