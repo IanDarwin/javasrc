20d7b1897d86353ef68c883bdad386a0dc1b3de8
IanDarwin <>
2007-11-13 01:13:51 +0000

    Add test for failure mode in division.

--- src/test/java/jaxwsservice/CalcUnitTest.java
+++ src/test/java/jaxwsservice/CalcUnitTest.java
@@ -1,16 +1,31 @@
 package client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import javax.xml.ws.soap.SOAPFaultException;
 
 import org.junit.Test;
 
 public class CalcTest {
 
+	Calc client = new CalcService().getCalcPort();
+
 	@Test
 	public void testAdd() {
-		Calc client = new CalcService().getCalcPort();
 		int result = client.add(34, 66);
 		assertEquals(100, result);
+		
+	}
+	
+	@Test
+	public void testDivide() {
+		try {
+			client.divide(99, 0);
+			fail("Did not throw exception for / 0");
+		} catch (SOAPFaultException e) {
+			System.out.println("testDivide: Caught expected exception");
+		}
 	}
 
 }
