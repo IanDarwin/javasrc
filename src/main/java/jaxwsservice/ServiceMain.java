fb4fa172ab794daedc1341ca7084dc98f5455754
IanDarwin <>
2007-11-13 01:10:02 +0000

    Just add comments.

--- src/main/java/jaxwsservice/ServiceMain.java
+++ src/main/java/jaxwsservice/ServiceMain.java
@@ -2,7 +2,11 @@ package service.toy;
 
 import javax.xml.ws.Endpoint;
 
+/** Java 6 and later allow simple services to be
+ * deployed using just the Java SE. So just run this!
+ */
 public class ServiceMain {
+	
 	public static void main(String[] args) {
 		// Create what used to be called the "service stub"
 		Calc impl = new Calc();
