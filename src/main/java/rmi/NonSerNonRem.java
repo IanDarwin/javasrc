d0f6d6f3f7588a64e00a5c4dc1d1ce3abbe16f05
Ian Darwin <ian@darwinsys.com>
2007-11-14 22:21:54 +0000

    RMI stuff moved to javaee project.

--- src/main/java/rmi/NonSerNonRem.java
+++
@@ -1,9 +0,0 @@
-package rmi;
-
-package darwinsys.distdate;
-
-/** This is an example of a class that is neither Serializable nor Remote. */
-public class NonSerNonRem {
-	public NonSerNonRem() {
-	}
-}
--- src/main/java/rmi/agent/agents/Agent.java
+++
@@ -1,8 +0,0 @@
-package agents;
-
-import java.rmi.*;
-
-public interface Agent {
-	public void setThreshold(double d) throws RemoteException;
-	public double getThreshold() throws RemoteException;
-}
