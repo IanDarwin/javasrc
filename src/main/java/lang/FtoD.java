9b61962300a1d58b804827c66d64304220447ad4
Ian Darwin <ian@darwinsys.com>
2008-01-08 20:30:29 +0000

    Format and comment.

--- src/main/java/lang/FtoD.java
+++ src/main/java/lang/FtoD.java
@@ -1,11 +1,15 @@
 package lang;
 
+/** Silly little example that shows promotion of
+ * float to double on method call.
+ */
 public class FtoD {
-public static void main(String[] args) { 
-	fmt(2.4f);
-}
+	
+	public static void main(String[] args) {
+		fmt(2.4f);
+	}
 
-public static void fmt(double d) {
-	System.out.println(d);
-}
+	public static void fmt(double d) {
+		System.out.println(d);
+	}
 }
