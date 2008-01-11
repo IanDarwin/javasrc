85e23266eef004ddb99df6eac79427c92b1f37a5
IanDarwin <>
2008-01-11 18:00:42 +0000

    Reformat and organize imports

--- src/main/java/webserver/MyResponse.java
+++ src/main/java/webserver/MyResponse.java
@@ -1,62 +1,149 @@
 package webserver;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 public class MyResponse implements HttpServletResponse {
 	private String encoding = "iso-8859-1";
+	private String contentType = "text/html";
+	private Locale locale = Locale.US;
 
 	/** Construct a Response */
 	public MyResponse() {
 	}
+	
 	/** Construct a Response to the given Socket */
 	public MyResponse(OutputStream out) {
 	}
 
-    public void addCookie(javax.servlet.http.Cookie cookie) { return; }
-    public void addDateHeader(String name, long val) { return; }
-    public void addHeader(String name, String val) { return; }
-    public void addIntHeader(String name, int val) { return; }
-    public boolean containsHeader(String hdrName) { return false; }
-    public String encodeRedirectURL(String url) { return null; }
-    public String encodeRedirectUrl(String url) { return null; }
-    public String encodeURL(String url) { return null; }
-    public String encodeUrl(String url) { return null; }
-    public void sendError(int code) throws java.io.IOException { return; }
-    public void sendError(int code, String val) throws java.io.IOException { return; }
-    public void sendRedirect(String url) throws java.io.IOException { return; }
-    public void setDateHeader(String val, long dval) {
-		setHeader("Date", new java.util.Date(dval).toString());
+    public void addCookie(Cookie cookie) {
+		return;
 	}
-    public void setHeader(String name, String val) { return; }
-    public void setIntHeader(String name, int ival) {
+
+	public void addDateHeader(String name, long val) {
+		return;
+	}
+
+	public void addHeader(String name, String val) {
+		return;
+	}
+
+	public void addIntHeader(String name, int val) {
+		return;
+	}
+
+	public boolean containsHeader(String hdrName) {
+		return false;
+	}
+
+	public String encodeRedirectURL(String url) {
+		return null;
+	}
+
+	public String encodeRedirectUrl(String url) {
+		return null;
+	}
+
+	public String encodeURL(String url) {
+		return null;
+	}
+
+	public String encodeUrl(String url) {
+		return null;
+	}
+
+	public void sendError(int code) throws java.io.IOException {
+		return;
+	}
+
+	public void sendError(int code, String val) throws IOException {
+		return;
+	}
+
+	public void sendRedirect(String url) throws IOException {
+		return;
+	}
+
+	public void setDateHeader(String val, long dval) {
+		setHeader(val, new Date(dval).toString());
+	}
+
+	public void setHeader(String name, String val) {
+		return;
+	}
+
+	public void setIntHeader(String name, int ival) {
 		setHeader(name, Integer.toString(ival));
 	}
-	public void setContentLength(int len) { }
-	public void setContentType(String type) { }
-    public void setStatus(int status) { return; }
-    public void setStatus(int status, String mesg) { return; }
-	public void setLocale(Locale loc) { }
-	public Locale getLocale() { return Locale.US; }
 
-	public void flushBuffer() { }
-	public void resetBuffer() { }
-	public int getBufferSize() { return 42; }
+	public void setContentLength(int len) {
+	}
+
+	public void setContentType(String type) {
+		this.contentType = type;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setStatus(int status) {
+		return;
+	}
+
+	public void setStatus(int status, String mesg) {
+		return;
+	}
+
+	public void setLocale(Locale loc) {
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void flushBuffer() {
+	}
+
+	public void resetBuffer() {
+	}
+
+	public int getBufferSize() {
+		return 42;
+	}
+
 	public String getCharacterEncoding() {
 		return encoding;
 	}
+
 	public void setCharacterEncoding(String enc) {
 		this.encoding = enc;
 	}
-	public javax.servlet.ServletOutputStream getOutputStream() { return null; }
-	public java.io.PrintWriter getWriter() { return null; }
-	public boolean isCommitted() { return true; }
-	public void reset() { }
-	public void setBufferSize(int size) { }
-	public String getContentType() {
+
+	public ServletOutputStream getOutputStream() {
 		return null;
 	}
+
+	public PrintWriter getWriter() {
+		return null;
+	}
+
+	public boolean isCommitted() {
+		return true;
+	}
+
+	public void reset() {
+	}
+
+	public void setBufferSize(int size) {
+	}
+
 
 }
