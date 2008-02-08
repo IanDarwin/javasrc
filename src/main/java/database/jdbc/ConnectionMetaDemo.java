75c088af096a1340c3c5af7dcccd7ec857a61759
Ian Darwin <ian@darwinsys.com>
2008-02-08 17:47:21 +0000

    Split off from Connect.java to make a class that shows Connection Metadata, similar to what DatabaseMetaDemo does.

--- src/main/java/database/jdbc/ConnectionMetaDemo.java
+++ src/main/java/database/jdbc/ConnectionMetaDemo.java
@@ -1,51 +1,34 @@
 package JDBC;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.sql.SQLWarning;
 
-/** Test of loading a driver and connecting to a database.
- * The URL assumes you have the M$ Example "Companies" database
- * configured as a System DSN (or user DSN for your user) in the
- * ODBC control panel.
+import com.darwinsys.sql.ConnectionUtil;
+
+/** Display JDBC ConnectionMetaData.
  */
-public class Connect {
+public class ConnectionMetaDemo {
 
-	public static void main(String[] av) {
-		String dbURL = "jdbc:odbc:Companies";
+	public static void main(String[] av) throws Throwable {
+		Connection conn = null;
 	    try {
-			// Load the jdbc-odbc bridge driver
-			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-
-			// Enable logging
-			DriverManager.setLogWriter(new PrintWriter((System.err)));
-
-			System.out.println("Getting Connection");
-			Connection conn = 
-				DriverManager.getConnection(dbURL, "ian", "");	// user, passwd
-
-			// If a SQLWarning object is available, print its
-			// warning(s).  There may be multiple warnings chained.
-
-			SQLWarning warn = conn.getWarnings();
-			while (warn != null) {
-				System.out.println("SQLState: " + warn.getSQLState());
-				System.out.println("Message:  " + warn.getMessage());
-				System.out.println("Vendor:   " + warn.getErrorCode());
-				System.out.println("");
-				warn = warn.getNextWarning();
-		    }
-
+			conn = ConnectionUtil.getConnection(av[0]);
+			
 			// Do something with the connection here...
+			final DatabaseMetaData metaData = conn.getMetaData();
+			System.out.printf("Database name %s%n",
+					metaData.getDatabaseProductName());
+			System.out.printf("Database version %d.%d%n",
+					metaData.getDatabaseMajorVersion(),
+					metaData.getDatabaseMinorVersion());
 
-			conn.close();	// All done with that DB connection
-
-	    } catch (ClassNotFoundException e) {
-			System.out.println("Can't load driver " + e);
 	    } catch (SQLException e) {
-			System.out.println("Database access failed " + e);
+			System.out.println("Database problem " + e);
+			e.printStackTrace();
+	    } finally {
+	    	if (conn != null)
+	    		conn.close();	// Done with that DB connection
 	    }
 	}
 }
