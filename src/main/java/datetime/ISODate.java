16ddcf4ab4ad570c156f26827e0be4903bad85b3
Ian Darwin <ian@darwinsys.com>
2008-02-12 21:01:05 +0000

    Demonstrate a couple of ways to format an ISO 8601 date

---
+++ src/main/java/datetime/ISODate.java
@@ -0,0 +1,13 @@
+package datetime;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+/** Demonstrate a couple of ways to format an ISO 8601 date */
+public class ISODate {
+	public static void main(String[] args) {
+		Date d =  new Date();
+		System.out.printf("%1$tY-%1$tm-%1$td%n", d);
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(d));
+	}
+}
