e6a26f436667df00d3aa08a5472d37356760fc6c
Ian Darwin <ian@darwinsys.com>
2008-03-06 23:14:37 +0000

    Sync

--- src/main/java/javacomm/JMModel.java
+++ src/main/java/javacomm/JMModel.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -257,7 +258,7 @@ public class JMModel {
 		}
 		if (xferProg == null) {
 			xferProg = new TModem(serialInput, serialOutput, 
-				System.out);
+				new PrintWriter(System.err));
 		}
 		String fileName = theGUI.getXferFileName();
 		if (fileName.length() == 0) {
