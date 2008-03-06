d86eb66fc411e825372b4b72a38b1a9b312f4128
Ian Darwin <ian@darwinsys.com>
2008-03-06 22:42:37 +0000

    Better name.

--- src/main/java/oo/ColorDemo.java
+++ src/main/java/oo/ColorDemo.java
@@ -2,7 +2,7 @@ package oo;
 
 import java.awt.Color;
 
-public class ColorTest {
+public class ColorDemo {
 	public static void main(String[] args) {
 		Color c = new Color(130, 130, 130, 130);
 		System.out.println(c + "; " + c.hashCode());
