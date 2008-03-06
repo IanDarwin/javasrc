743ae2fcf339783b963fc5508b0c11c5e249464c
Ian Darwin <ian@darwinsys.com>
2008-03-06 22:50:24 +0000

    SerialVersionUID

--- src/main/java/oo/ChessMoveException.java
+++ src/main/java/oo/ChessMoveException.java
@@ -2,6 +2,9 @@ package oo;
 
 /** A ChessMoveException is thrown  when the user makes an illegal move. */
 public class ChessMoveException extends Exception {
+
+	private static final long serialVersionUID = 802911736988179079L;
+
 	public ChessMoveException () {
 		super();
 	}
