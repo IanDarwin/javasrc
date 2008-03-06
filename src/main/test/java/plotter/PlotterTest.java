c6c4e73eaf1b488b60aa2bcff86df5a33d78b1c6
Ian Darwin <ian@darwinsys.com>
2008-03-06 23:04:17 +0000

    Simple test of Plotter, using PlotterDummy for starters.

---
+++ src/main/test/java/plotter/PlotterTest.java
@@ -0,0 +1,35 @@
+package Plotter;
+
+
+import java.awt.Point;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlotterTest {
+	Plotter p;
+	
+	@Before
+	public void setUp() throws Exception {
+		p = new PlotterDummy();
+	}
+	
+	@Test
+	public void testInitial() {
+		assertEquals("initial", new Point(0,0), p.getLocation());
+	}
+	
+	@Test
+	public void testMoveTo() {
+		p.moveTo(123, 456);
+		assertEquals("abs move", new Point(123, 456), p.getLocation());
+	}
+
+	@Test
+	public void testRMoveTo() {
+		p.moveTo(100, 100);
+		p.rmoveTo(123, 456);
+		assertEquals("abs move", new Point(223, 556), p.getLocation());
+	}
+}
