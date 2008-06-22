4062e9eb25b94ebaf644c9018ebe16d0d52cb548
Ian Darwin <ian@darwinsys.com>
2008-06-22 00:04:04 +0000

    Remove toy main, which referred to defunct test runner class.

--- src/main/test/java/strings/AllTests.java
+++ src/main/test/java/strings/AllTests.java
@@ -8,10 +8,6 @@ import junit.framework.TestSuite;
  */
 public class AllTests {
 
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(AllTests.class);
-	}
-
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for default package");
 		//$JUnit-BEGIN$
