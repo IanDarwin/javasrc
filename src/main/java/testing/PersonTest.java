7d009218e1a7938e06ada362059e4fbdd514233d
Ian Darwin <ian@darwinsys.com>
2008-06-24 21:57:53 +0000

    Remove optional constructor.

--- src/main/java/testing/PersonTest.java
+++ src/main/java/testing/PersonTest.java
@@ -5,11 +5,6 @@ import junit.framework.*;
 /** A simple test case for Person */
 public class PersonTest extends TestCase {
 
-	/** JUnit test classes require this constructor */
-	public PersonTest(String name) {
-		super(name);
-	}
-
 	public void testNameConcat() {
 		Person p = new Person("Ian", "Darwin");
 		String f = p.getFullName();
