73009ea57a17bd3d2d15a2b151b7a16a606a4f11
Ian Darwin <ian@darwinsys.com>
2008-06-24 22:33:23 +0000

    Fourify; @Ignore("Missing XML data file contents!!")

--- src/main/test/java/database/ResultsDecoratorSQLTest.java
+++ src/main/test/java/database/ResultsDecoratorSQLTest.java
@@ -1,44 +1,44 @@
 package JDBC.regress;
 
-import java.io.FileReader;
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 
 import javax.sql.rowset.WebRowSet;
 
-import com.sun.rowset.WebRowSetImpl;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
-import junit.framework.TestCase;
+import com.sun.rowset.WebRowSetImpl;
 
 /**
  * ResultsDecoratorSQLTest
  * @version $Id$
  */
-public class ResultsDecoratorSQLTest extends TestCase {
+public class ResultsDecoratorSQLTest {
 	protected static final String TEST_FILE = "rowsetdata.xml";
 	
 	protected WebRowSet testData;
 	
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 
 		testData= new WebRowSetImpl();
-		Reader r = new FileReader(TEST_FILE);
+		final InputStream dataStream = getClass().getResourceAsStream(TEST_FILE);
+		if (dataStream == null) {
+			throw new RuntimeException("Could not load " + TEST_FILE);
+		}
+		Reader r = new InputStreamReader(dataStream);
 		testData.readXml(r);
-
 	}
 	
+	@Ignore("Missing XML data file contents!!") @Test
 	public  void test1() throws Exception {
 		testData.absolute(1);
 		assertEquals("get data", "Ian", testData.getString("first_name"));
 	}
 	
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		// TODO Auto-generated method stub
-		super.tearDown();
-	}
 }
