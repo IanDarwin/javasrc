846f5c5ffc6111fa66f31e110d980bc0bd68b01c
Ian Darwin <ian@darwinsys.com>
2008-08-15 15:43:00 +0000

    Comments-only change.

--- src/main/java/lang/InheritAnnotations.java
+++ src/main/java/lang/InheritAnnotations.java
@@ -5,10 +5,12 @@ import java.lang.annotation.RetentionPolicy;
 
 @MyAnnotation
 class C0 {
+	// empty
 }
 
 /* No annotation here - is it inherited? */
 class C1 extends C0 {
+	// empty
 }
 
 /** If you run this you will find out
@@ -17,13 +19,14 @@ class C1 extends C0 {
 public class InheritAnnotations {
 	public static void main(String[] args) {
 		for (Class<C0> c : new Class[]{ C0.class, C1.class} ) {
-			System.out.printf("%s: Annotation is %s present%n",
+			System.out.printf("Class %s: Annotation is %s present%n",
 					c.getName(),
 					c.isAnnotationPresent(MyAnnotation.class) ? "" : "not");
 		}
 	}
 }
 
+/** Here's the annotation itself... */
 @Retention(RetentionPolicy.RUNTIME)
 @interface MyAnnotation {
 	// nothing needed here
