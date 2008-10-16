9b0f4816a1cd4088d0bd7f32170f1633cca6dbe3
Ian Darwin <ian@darwinsys.com>
2008-10-16 15:39:48 +0000

    Introduce a C-language version of helloworld.

---
+++ src/main/java/sidebyside/c/helloworld.c
@@ -0,0 +1,10 @@
+/**
+ * Basic, minimal, Hello World type program in C.
+ */
+ 
+#include <stdio.h>	/* included on all C development systems */
+ 
+/*ARGSUSED*/
+int main(int argc, char* argv[]) {
+		printf("Hello, World\n");
+}
