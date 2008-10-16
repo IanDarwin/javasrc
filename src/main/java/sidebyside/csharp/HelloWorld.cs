973970973b5c02078bac03c982e1bd7036aeb8ee
Ian Darwin <ian@darwinsys.com>
2008-10-16 15:26:25 +0000

    Add a C# example, mostly cloned from the Java example as that's how the C# language came to be. Requires either the open-source "Mono" or that proprietary expansive wall opening / door-crasher from Microsoft.

---
+++ src/main/java/sidebyside/csharp/HelloWorld.cs
@@ -0,0 +1,16 @@
+/*
+ * Basic, minimal Hello World type program in Microsoft C#.
+ * Note how most of C# was copied from Java, changing the
+ * API names to violate M$' licensing agreement with Sun.
+ */
+
+using System;	// like import
+
+namespace SideBySide.CSharp {	// like package (and optional)
+
+	public class MyClass {
+		public static void Main() {
+			Console.WriteLine("Hello, World");
+		}
+	} 
+}
