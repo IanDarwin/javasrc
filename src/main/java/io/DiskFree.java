16d91e4ff41b8c7810cac8fe93537ba68180d4b3
Ian Darwin <ian@darwinsys.com>
2009-02-21 20:27:55 +0000

    Disk free space (df)

---
+++ src/main/java/io/DiskFree.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.io.File;
+import com.darwinsys.text.ScaledNumberFormat;
+
+public class DiskFree {
+
+	public static void main(String[] args) {
+		File[] roots = File.listRoots();
+		ScaledNumberFormat fmt= new ScaledNumberFormat();
+		for (File f : roots) {
+			System.out.println(
+				fmt.format(f.getFreeSpace()) + " " + 	
+				fmt.format(f.getTotalSpace()));
+		}
+	}
+
+}
