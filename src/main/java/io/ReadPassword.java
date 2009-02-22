e7de0dede6fc92ceac07c1cb3462e6821a404f2a
Ian Darwin <ian@darwinsys.com>
2009-02-22 03:51:30 +0000

    Read a password from a user

---
+++ src/main/java/io/ReadPassword.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.io.*;
+
+/**
+ * Read a password from the user.
+ * REQUIRES JAVA 6 or later.
+ */
+public class ReadPassword {
+	public static void main(String[] args) {
+		Console cons;
+		if ((cons = System.console()) != null) {
+			char[] passwd = null;
+			try {
+				passwd = cons.readPassword("Password:");
+				// In real life you would send the password into authentication code
+				System.out.println("Your password was: " + new String(passwd));
+			} finally {
+				// Shred this in-memory copy for security reasons
+				if (passwd != null) {
+					java.util.Arrays.fill(passwd, ' ');
+				}
+			}
+		}
+	}
+}
