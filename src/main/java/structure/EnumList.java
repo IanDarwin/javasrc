3b0b78cca09ed95ac3073cac2de85956c1fb0cfd
Ian Darwin <ian@darwinsys.com>
2009-02-23 17:32:35 +0000

    Remove bogus semicolon.

--- src/main/java/structure/EnumList.java
+++ src/main/java/structure/EnumList.java
@@ -2,7 +2,9 @@ package structure;
 
 /** Simple demo to print all the types of an enum. */
 public class EnumList {
-	enum State { ON, OFF, UNKNOWN };
+	enum State { 
+		ON, OFF, UNKNOWN 
+	}
 	public static void main(String[] args) {
 		for (State i : State.values()) {
 			System.out.println(i);
