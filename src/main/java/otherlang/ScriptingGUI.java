dd22311140e1d99f5a72bf7e080ceffb78c79707
Ian Darwin <ian@darwinsys.com>
2009-03-20 21:45:32 +0000

    Add the action listeners so it does something. Display the drop-down language choice in a user-friendly way.

--- src/main/java/otherlang/ScriptingGUI.java
+++ src/main/java/otherlang/ScriptingGUI.java
@@ -1,16 +1,19 @@
 package otherlang;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Vector;
 
+import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -18,6 +21,7 @@ import javax.swing.JTextArea;
  * Based on a version of ScriptEnginesDemo that
  * I did for a customer, but not using any of the
  * code that I wrote for them.
+ * If running under Eclipse, add lib/*engine.jar to classpath!
  * @author ian
  */
 public class ScriptingGUI extends JFrame implements Serializable {
@@ -30,6 +34,19 @@ public class ScriptingGUI extends JFrame implements Serializable {
 
 	private ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
 
+	private class EngineChoice {
+		String name;
+		ScriptEngineFactory factory;
+		public EngineChoice(String languageName, ScriptEngineFactory factory) {
+			this.name = languageName;
+			this.factory = factory;
+		}
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+	
 	/** Set up the scripting engine list
 	 * and the GUI.
 	 */
@@ -39,19 +56,51 @@ public class ScriptingGUI extends JFrame implements Serializable {
 		// Get list of supported languages
 		List<ScriptEngineFactory> engineFactories = 
 			scriptEngineManager.getEngineFactories();
-		JComboBox box = new JComboBox(new Vector<ScriptEngineFactory>(engineFactories));
+		final JComboBox engineChoiceBox = new JComboBox();
+		for (ScriptEngineFactory f : engineFactories) {
+			EngineChoice ec = new EngineChoice(f.getLanguageName(), f);
+			engineChoiceBox.addItem(ec);
+		}
 		
 		setLayout(new BorderLayout());
 		JPanel top = new JPanel();
-		top.add(box);
+		top.add(engineChoiceBox);
 		JPanel bot = new JPanel();
-		bot.add(new JButton("Run"));
+		JButton runButton = new JButton("Run");
+		bot.add(runButton);
+		
+		final JTextArea area = new JTextArea(5,50);
+		area.setBorder(BorderFactory.createTitledBorder("Enter Script:"));
+		
+		runButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				try {
+					EngineChoice choice = (EngineChoice)engineChoiceBox.getSelectedItem();
+					ScriptEngineFactory engineFactory = choice.factory;
+					ScriptEngine engine = (engineFactory).getScriptEngine();	
+					String script = area.getText();
+					System.out.println(engineFactory.getLanguageName());
+					engine.eval(script);
+				} catch (Throwable ex) {
+					JOptionPane.showMessageDialog(ScriptingGUI.this, "Script failed, see log: " + ex);
+					ex.printStackTrace();
+				}
+			}			
+		});
+		
+		JButton quitButton = new JButton("Exit");
+		bot.add(quitButton);
+		quitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);		
+			}			
+		});
 		
 		add(top, BorderLayout.NORTH);
 		
-		JTextArea area = new JTextArea(5,50);
-		area.setBorder(BorderFactory.createTitledBorder("Enter Script:"));
 		add(area, BorderLayout.CENTER);
+
 		add(bot, BorderLayout.SOUTH);
 
 		pack();
