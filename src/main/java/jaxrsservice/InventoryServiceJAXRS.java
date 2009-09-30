5b6ba875fe5c799a0455493f7e650b94eba2d11d
IanDarwin <>
2009-09-30 21:11:43 +0000

    JAXRS allows development of this, but this is not yet developed.

--- src/main/java/jaxrsservice/InventoryServiceJAXRS.java
+++ src/main/java/jaxrsservice/InventoryServiceJAXRS.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * A raw-servlet implementation of the RESTful
+ * A start at a JAX-RS implementation of the RESTful
  * Inventory Control application from Chapter 7
  * of Learning Tree Course 577, Java XML Web Services
  * <ul>
@@ -19,46 +19,27 @@ import javax.servlet.http.HttpServletResponse;
  * <li>HTTP PUT will insert new; URL is /3012, qty is in posted XML
  * <li>HTTP DELETE /3012 will delete record by id.
  * </ul>
- * @author Ian Darwin, "inspired" by code in a previous
- * version of the course note (re-created from memory).
+ * @author Ian Darwin, "inspired" by code in the course notes (re-created from memory).
  */
-public class InventoryControlServlet extends HttpServlet {
+public class InventoryServletJAXRS extends HttpServlet {
 
 	private static final long serialVersionUID = 5349596315091650623L;
 
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		final String method = request.getMethod();
-		if ("GET".equals(method)) {
-			response.setContentType("text/xml");
-			PrintWriter out = response.getWriter();
-			String command = getLastPath(request);
-			if ("categories".equals(command)) {
-				out.println("<categories>");
-				for (String cat : getCategories()) {
-					out.println("<category>" + cat + "</category>");
-				}
-				out.println("</categories>");
-			} else if ("products".equals(command)) {
-				// same deal but for all proeucts
-			} else {
-				throw new IllegalArgumentException(command);
-			}
-		} else if ("POST".equals(method)) {
-			// do POST stuff
-		} else if ("PUT".equals(method)) {
-			// do PUT stuff
-		} else if ("DELETE".equals(method)) {
-			// do DELETE stuff
-		} else throw new IllegalArgumentException("Method " + method + " not supported");
+	protected String getCategories() {
+		String x = "<categories>";
+		for (String cat : DAOgetCategories()) {
+			x += "<category>" + cat + "</category>";
+		}
+		x += "</categories>";
+		return x;
+	}
+	
+	protected String deleteRecord() {
+		return "<OK>Record Deleted</OK>";
 	}
 	
 	// Dummy methods to make the above appear to work
-	private String[] getCategories() {
+	private String[] DAOgetCategories() {
 		return new String[]{ "Jazz", "Classical"};
-	}
-	
-	private String getLastPath(HttpServletRequest req) {
-		return req.getRequestURI().replaceAll(".*/", "");
 	}
 }
