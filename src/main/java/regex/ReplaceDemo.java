cc7451010044a2853c91a6135da195f581197df0
Ian Darwin <ian@darwinsys.com>
2009-11-27 18:32:51 +0000

    Did you notice that Polite and Political are from the same root?

--- src/main/java/regex/ReplaceDemo.java
+++ src/main/java/regex/ReplaceDemo.java
@@ -3,8 +3,8 @@ package regex;
 import java.util.regex.*;
 
 /**
- * Quick demo of RE substitution: correct "demon" and other
- * spelling variants to the correct, non-satanic "daemon".
+ * Quick demo of RE substitution: correct U.S. 'favor'
+ * to Canadian/British 'favour', but not in "favorite"
  * @author Ian F. Darwin, http://www.darwinsys.com/
  * @version $Id$
  */
@@ -12,26 +12,27 @@ public class ReplaceDemo {
 	public static void main(String[] argv) {
 
 		// Make an RE pattern to match almost any form (deamon, demon, etc.).
-		String patt = "d[ae]{1,2}mon";	// i.e., 1 or 2 'a' or 'e' any combo
+		String patt = "\\bfavor\\b";
 
 		// A test input.
-		String input = "Unix hath demons and deamons in it!";
+		String input = "Do me a favor? Fetch my favorite.";
 		System.out.println("Input: " + input);
 
 		// Run it from a RE instance and see that it works
 		Pattern r = Pattern.compile(patt);
 		Matcher m = r.matcher(input);
-		System.out.println("ReplaceAll: " + m.replaceAll("daemon"));
+		System.out.println("ReplaceAll: " + m.replaceAll("favour"));
 
 		// Show the appendReplacement method
 		m.reset();
 		StringBuffer sb = new StringBuffer();
 		System.out.print("Append methods: ");
 		while (m.find()) {
-			m.appendReplacement(sb, "daemon");	// Copy to before first match,
-												// plus the word "daemon"
+			// Copy to before first match,
+			// plus the word "favor"
+			m.appendReplacement(sb, "favour");
 		}
-		m.appendTail(sb);						// copy remainder
+		m.appendTail(sb);		// copy remainder
 		System.out.println(sb.toString());
 	}
 }
