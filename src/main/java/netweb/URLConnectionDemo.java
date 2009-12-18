6a494169c951da06e9db25d81c56f94984150435
Ian Darwin <ian@darwinsys.com>
2009-12-18 18:33:52 +0000

    Add output, but it still doesn't work on current Java!?

--- src/main/java/netweb/URLConnectionDemo.java
+++ src/main/java/netweb/URLConnectionDemo.java
@@ -1,7 +1,10 @@
 package netweb;
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Demonstrate reading a URL's data from a URLConnection
@@ -14,17 +17,29 @@ public class URLConnectionDemo {
 	        URL url = new URL("http://" + host + ":8080/MySSOAuth");
 	        URLConnection conn = url.openConnection();
 	        conn.setDoInput(true);
+	        conn.setDoOutput(true);
+	        conn.setAllowUserInteraction(true);
 	        System.out.println("Adding cookie");
 	        conn.addRequestProperty("Cookie", "JSESSIONID=551014ECE1008B90A66F2EF73A95A751");
 	        conn.addRequestProperty("Cookie", "test=no");
+	        
 	        conn.connect();
+		
+	        System.out.println("Sending POST body");
+	        PrintWriter out = new PrintWriter(conn.getOutputStream());
+	        out.println("<cat:findCatalogItem>");
+	        out.println("	<productId>3012</productId>");
+	        out.println("</cat:findCatalogItem>");
+	        out.close();
+
+	        System.out.println("Reading response");
 	        BufferedReader in = new BufferedReader(
 	        		new InputStreamReader(conn.getInputStream()));
-	        System.out.println("Reading response");
 	        String inputLine;
 
-	        while ((inputLine = in.readLine()) != null)
+	        while ((inputLine = in.readLine()) != null) {
 	            System.out.println(inputLine);
+	        }
 
 	        in.close();
 	        System.out.println("Done");
