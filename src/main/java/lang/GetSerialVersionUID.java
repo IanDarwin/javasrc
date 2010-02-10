4d6ba1a7d988ed8a4c27c24e08d26a6df575530e
Ian Darwin <ian@darwinsys.com>
2010-02-10 01:21:54 +0000

    Better expository style, error checking, &c.

--- src/main/java/lang/GetSerialVersionUID.java
+++ src/main/java/lang/GetSerialVersionUID.java
@@ -1,31 +1,34 @@
 package lang;
 
-import java.io.*;
+import java.io.Serializable;
+import java.io.ObjectStreamClass;
 
 /**
- * This program shows how to use getSerialVersionUID for a class that
+ * This program shows how to use getSerialVersionUID for a class. Because it
+ * uses Class.forName(), it will even operate upon a class that
  * isn't in a package and thus not findable to the normal "serialver" tool.
  *
  * @author Ian Darwin, http://www.darwinsys.com/
  */
-public class GetSerVersUID {
+public class GetSerVersUID implements Serializable {
+
 	static final String ident = "$Id$";
 	public static void main(String[] av) throws Exception {
 
-		// First we construct a Class object for the given class
-		Class<?> cl = Class.forName("Candidate");
+		// First construct a Class object for the given class
+		Class<?> cl = Class.forName(av.length == 0 ? "lang.GetSerVersUID" : av[0]);
 
 		// Then an ObjectStreamClass for the given class
-		ObjectStreamClass ocl = ObjectStreamClass.lookup(cl);
+		ObjectStreamClass ocl = ObjectStreamClass.lookupAny(cl);
 
-		// And use the ObjectStreamClass to get the Class'
-		// unique SerialVersionUID
-		System.out.println("For class " + cl);
-		System.out.println("static final long serialVersionUID = " +
-			ocl.getSerialVersionUID() + "L;"); // must be long
-
-		// And just for reference...
-		System.out.println("(Must range from " + Long.MIN_VALUE +
-			" to " + Long.MAX_VALUE + ".)");
+		// Use the ObjectStreamClass to get the Class' unique SerialVersionUID
+		long uid = ocl.getSerialVersionUID();
+		if (uid == 0L) {
+			System.out.println("Class " + cl + " is not Serializable!");
+		} else {
+			System.out.println("// For class " + cl);
+			System.out.println(
+				"static final long serialVersionUID = " + uid + "L;");
+		}
 	}
 }
