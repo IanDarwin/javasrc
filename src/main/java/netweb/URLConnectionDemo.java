c85e6885fd5d0112bafa34e9fa59827d492820b7
Ian Darwin <ian@darwinsys.com>
2010-03-04 16:33:08 +0000

    Split into main program and converse() method, with GET/POST option.

--- src/main/java/netweb/URLConnectionDemo.java
+++ src/main/java/netweb/URLConnectionDemo.java
@@ -1,51 +1,52 @@
 package netweb;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
 /**
- * Demonstrate reading a URL's data from a URLConnection
+ * Read a URL's data from a URLConnection
  */
 public class URLConnectionDemo {
 	
-	    public static void main(String[] args) throws Exception {
-	    	String host = null;
-	    	int port = -1;
-	      	host = "127.0.0.1";
-	      	host = "www.darwinsys.com";
-	      	port = 8080;
-	      	port = 80;
-	        URL url = new URL("http", host, port, "/java");
-	        URLConnection conn = url.openConnection();
-	        conn.setDoInput(true);
-	        conn.setDoOutput(true);
-	        conn.setAllowUserInteraction(true);
-	        System.out.println("Adding cookie");
-	        conn.addRequestProperty("Cookie", "JSESSIONID=551014ECE1008B90A66F2EF73A95A751");
-	        conn.addRequestProperty("Cookie", "test=no");
-	        
-	        conn.connect();
-		
-	        System.out.println("Sending POST body");
-	        PrintWriter out = new PrintWriter(conn.getOutputStream());
-	        out.println("name=Ian+Darwin");
-	        out.close();
-
-	        System.out.println("Reading response");
-	        BufferedReader in = new BufferedReader(
-	        		new InputStreamReader(conn.getInputStream()));
-	        String inputLine;
-
-	        while ((inputLine = in.readLine()) != null) {
-	            System.out.println(inputLine);
-	        }
-
-	        in.close();
-	        System.out.println("Done");
-	    }
+    public static void main(String[] args) throws Exception {
+    	String host = "192.168.1.42";
+    	String path = "/andcook/seam/resource/rest/recipe/list";
+    	String postBody = null;
+    	String ret = converse(host, 80, path, postBody);
+    	System.out.println(ret);
+    }
+    
+    public static String converse(String host, int port, String path, String postBody) throws IOException {
+		URL url = new URL("http", host, 80, path);
+        URLConnection conn = url.openConnection();
+        boolean post = postBody != null;
+        if (post)
+        	conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setAllowUserInteraction(true);
+        
+        conn.connect();
 	
+        if (post) {
+        	PrintWriter out = new PrintWriter(conn.getOutputStream());
+        	out.println(postBody);
+        	out.close();
+        }
 
+        StringBuilder sb = new StringBuilder();
+        BufferedReader in = new BufferedReader(
+        		new InputStreamReader(conn.getInputStream()));
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            sb.append(line);
+        }
+
+        in.close();
+        return sb.toString();
+    }
 }
