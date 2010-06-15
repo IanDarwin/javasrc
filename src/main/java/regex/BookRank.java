84b11be5bccadbdd7c38832d2acfc1df34a26bfd
Ian Darwin <ian@darwinsys.com>
2010-06-15 22:56:45 +0000

    Split out main functionality so it can be re-used. Need more careful merging.

--- src/main/java/regex/BookRank.java
+++ src/main/java/regex/BookRank.java
@@ -1,7 +1,10 @@
 package regex;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
@@ -11,12 +14,10 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.darwinsys.io.FileIO;
-import com.darwinsys.util.FileProperties;
-
 /** Graph of a book's sales rank on a given bookshop site.
  * @author Ian F. Darwin, http://www.darwinsys.com/, Java Cookbook author,
- *	originally translated fairly literally from Perl into Java.
+ *	originally translated fairly literally from Perl into Java,
+ *	and rewritten several times since.
  * @author Patrick Killelea <p@patrick.net>: original Perl version,
  *	from the 2nd edition of his book "Web Performance Tuning".
  * @version $Id$
@@ -24,12 +25,17 @@ import com.darwinsys.util.FileProperties;
 public class BookRank {
 	public final static String DATA_FILE = "book.sales";
 	public final static String GRAPH_FILE = "book.png";
+	public final static String PLOTTER_PROG = "/usr/local/bin/gnuplot";
 
+	final static String isbn = "0596007019"; 
+	final static String title = "Java Cookbook";
+	
 	/** Grab the sales rank off the web page and log it. */
 	public static void main(String[] args) throws Exception {
 
-		Properties p = new FileProperties(
-			args.length == 0 ? "bookrank.properties" : args[1]);
+		Properties p = new Properties();
+		p.load(new FileInputStream(
+			args.length == 0 ? "bookrank.properties" : args[1]));
 		String title = p.getProperty("title", "NO TITLE IN PROPERTIES");
 		// The url must have the "isbn=" at the very end, or otherwise
 		// be amenable to being string-catted to, like the default.
@@ -39,42 +45,21 @@ public class BookRank {
 		// The RE pattern (MUST have ONE capture group for the number)
 		String pattern = p.getProperty("pattern", "Rank: (\\d+)");
 
-		// Looking for something like this in the input:
-		//	 <b>QuickBookShop.web Sales Rank: </b>
-		//	 26,252
-		//	 </font><br>
+		int rank = getBookRank(isbn);
 
-		Pattern r = Pattern.compile(pattern);
+		System.out.println("Rank is " + rank);
 
-		// Open the URL and get a Reader from it.
-		BufferedReader is = new BufferedReader(new InputStreamReader(
-			new URL(url + isbn).openStream()));
-		// Read the URL looking for the rank information, as
-		// a single long string, so can match RE across multi-lines.
-		String input = FileIO.readerToString(is);
-		// System.out.println(input);
-
-		// If found, append to sales data file.
-		Matcher m = r.matcher(input);
-		if (m.find()) {
-			PrintWriter pw = new PrintWriter(
-				new FileWriter(DATA_FILE, true));
-			String date = // `date +'%m %d %H %M %S %Y'`;
-				new SimpleDateFormat("MM dd hh mm ss yyyy ").
-				format(new Date());
-			// Paren 1 is the digits (and maybe ','s) that matched; remove comma
-			Matcher noComma = Pattern.compile(",").matcher(m.group(1));
-			pw.println(date + noComma.replaceAll(""));
-			pw.close();
-		} else {
-			System.err.println("WARNING: pattern `" + pattern +
-				"' did not match in `" + url + isbn + "'!");
-		}
-
-		// Whether current data found or not, draw the graph, using 
-		// external plotting program against all historical data.
+		// Now try to draw the graph, using external
+		// plotting program against all historical data.
 		// Could use gnuplot, R, any other math/graph program.
 		// Better yet: use one of the Java plotting APIs.
+
+		PrintWriter pw = new PrintWriter(
+			new FileWriter(DATA_FILE, true));
+		String date = new SimpleDateFormat("MM dd hh mm ss yyyy ").
+			format(new Date());
+		pw.println(date + " " + rank);
+		pw.close();
 
 		String gnuplot_cmd = 
 			"set term png\n" + 
@@ -89,9 +74,59 @@ public class BookRank {
 				"\" using 1:7 title \"" + title + "\" with lines\n" 
 		;
 
-		Process proc = Runtime.getRuntime().exec("/usr/local/bin/gnuplot");
+		if (!new File(PLOTTER_PROG).exists()) {
+			System.out.println("Plotting software not installed");
+			return;
+		}
+		Process proc = Runtime.getRuntime().exec(PLOTTER_PROG);
 		PrintWriter gp = new PrintWriter(proc.getOutputStream());
 		gp.print(gnuplot_cmd);
 		gp.close();
+	}
+
+	/**
+	 * Look for something like this in the HTML input:
+	 *	 <b>Sales Rank:</b> 
+	 *	 #26,252
+	 * 	 </font><br>
+	 * @throws IOException 
+	 * @throws IOException 
+	 */
+	public static int getBookRank(String isbn) throws IOException {
+
+		// The RE pattern - digits and commas allowed
+		final String pattern = "Rank:</b> #([\\d,]+)";
+		final Pattern r = Pattern.compile(pattern);
+
+		// The url -- must have the "isbn=" at the very end, or otherwise
+		// be amenable to being appended to.
+		final String url = "http://www.amazon.com/exec/obidos/ASIN/" + isbn;
+
+		// Open the URL and get a Reader from it.
+		final BufferedReader is = new BufferedReader(new InputStreamReader(
+			new URL(url).openStream()));
+
+		// Read the URL looking for the rank information, as
+		// a single long string, so can match RE across multi-lines.
+		final String input = readerToString(is);
+
+		// If found, append to sales data file.
+		Matcher m = r.matcher(input);
+		if (m.find()) {
+			// Paren 1 is the digits (and maybe ','s) that matched; remove comma
+			return Integer.parseInt(m.group(1).replace(",",""));
+		} else {
+			throw new RuntimeException(
+				"Pattern not matched in `" + url + "'!");
+		}
+	}
+
+	private static String readerToString(BufferedReader is) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = is.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();
 	}
 }
