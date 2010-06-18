0a726a7b98c8d7a5f0d0fc535c07d43d2ec18281
Ian Darwin <ian@darwinsys.com>
2010-06-18 18:17:17 +0000

    Make helper class an inner class

--- src/main/java/dir_file/FNFilter.java
+++ src/main/java/dir_file/FNFilter.java
@@ -15,16 +15,16 @@ public class FNFilter {
 		for (int i=0; i<dir.length; i++)
 			System.out.println(dir[i]);	// Print the list
 	}
-}
 
-/** This class implements the FilenameFilter interface.
- * The Accept method returns true for .java, .class and .jar files.
- */
-class OnlyJava implements FilenameFilter {
-	public boolean accept(File dir, String s) {
-		if (s.endsWith(".java") || s.endsWith(".class") || s.endsWith(".jar"))
-			return true;
-		// others: projects, ... ?
-		return false;
+	/** This class implements the FilenameFilter interface.
+	 * The Accept method returns true for .java, .class and .jar files.
+	 */
+	class OnlyJava implements FilenameFilter {
+		public boolean accept(File dir, String s) {
+			if (s.endsWith(".java") || s.endsWith(".class") || s.endsWith(".jar"))
+				return true;
+			// others: projects, ... ?
+			return false;
+		}
 	}
 }
