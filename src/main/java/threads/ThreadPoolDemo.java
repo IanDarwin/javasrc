9dc6a46a2e9818cfd39ea7a2d11a61a0ef08cc2b
Ian Darwin <ian@darwinsys.com>
2010-07-16 15:55:27 +0000

    New.

---
+++ src/main/java/threads/ThreadPoolDemo.java
@@ -0,0 +1,47 @@
+package threads;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * Simple demo of the Java5 ThreadPool; run multiple threads.
+ */
+public class ThreadPoolDemo {
+
+	private static boolean done = false;
+
+	public static void main(String[] args) throws Throwable {
+		final ExecutorService pool = Executors.newFixedThreadPool(5);
+		List<Future> futures = new ArrayList<Future>(5);
+		for (int i = 0; i < 5; i++) {
+			Future<Integer> f = pool.submit(new DemoRunnable(i));
+			futures.add(f);
+		}
+		Thread.sleep(3 * 1000);
+		done = true;
+		for (Future f : futures) {
+			System.out.println("Result " + f.get());
+		}
+		pool.shutdown();
+	}
+
+	static class DemoRunnable implements Callable<Integer> {
+		int time, numRuns;
+		DemoRunnable(int t) { time = t; }
+
+		// @Override
+		public Integer call() {
+			while (!done) {
+				try {
+					Thread.sleep(500);
+					System.out.println("Running " + this);
+					++numRuns;
+				} catch (InterruptedException e) {
+					// nothing to do
+				}
+			}
+			System.out.println("Stopping " + this);
+			return numRuns;
+		}
+	}
+}
