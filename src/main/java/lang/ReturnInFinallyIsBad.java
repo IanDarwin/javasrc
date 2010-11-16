f0cdbc8c76f1234716488de3b95f6ff1ef2e92db
Ian Darwin <ian@darwinsys.com>
2010-11-16 19:51:26 +0000

    Show why return in finally is a bad idea.

---
+++ src/main/java/lang/ReturnInFinallyIsBad.java
@@ -0,0 +1,29 @@
+package lang;
+
+/** This shows why a "return" in a "finally" is bad.
+ * @author ian darwin
+ * @version $Id$
+ */
+public class ReturnInFinallyIsBad {
+	private static String getDestinationType() {
+		String ret = "Foo";
+		try {
+			ret = "Bar";
+			throw new RuntimeException("This is a drill");
+		} finally {
+			// This warns about:
+			// finally block does not complete normally
+			// because this return bypasses handling of the
+			// RuntimeException (or any other thrown Throwable)
+			return ret;
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(getDestinationType());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+}
