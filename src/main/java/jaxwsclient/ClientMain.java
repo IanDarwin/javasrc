7279684f48fb2f21a8a7a4e87646fcebfd5eb93c
IanDarwin <>
2011-02-08 22:08:51 +0000

    A real client.

--- src/main/java/jaxwsclient/ClientMain.java
+++ src/main/java/jaxwsclient/ClientMain.java
@@ -1,9 +1,57 @@
 package jaxwsclient;
 
+import java.io.Console;
+import java.util.StringTokenizer;
+
 
 public class ClientMain {
 	public static void main(String[] args) {
+		Console con = System.console();
+		if (con==null) {
+			throw new IllegalStateException("Could not get Console");
+		}
 		Calc client = new CalcService().getCalcPort();
-		System.out.println("add(34,66) = " + client.add(34,66));
+		String line = null;
+		while ((line = con.readLine()) != null) {
+			if (line.length() == 0 || line.startsWith("#")) {
+				continue;
+			}
+			StringTokenizer st = new StringTokenizer(line);
+			if (st.countTokens() != 3) {
+				System.err.println("Don't like this line: " + line);
+				continue;
+			}
+			int arg1 = 0; 
+			try {
+				arg1 = Integer.parseInt(st.nextToken());
+			} catch (NumberFormatException e) {
+				System.err.println("First number invalid");
+				continue;
+			}
+			char op = st.nextToken().charAt(0);
+			int arg2 = 0; 
+			try {
+				arg2 = Integer.parseInt(st.nextToken());
+			} catch (NumberFormatException e) {
+				System.err.println("Second number invalid");
+				continue;
+			}
+			int ret = 0;
+			switch(op) {
+			case '+':
+				ret = client.add(arg1, arg2);
+				break;
+			case '-':
+				ret = client.subtract(arg1, arg2);
+				break;
+			case '*':
+				ret = client.multiply(arg1, arg2);
+				break;
+			case '/':
+				ret = client.divide(arg1, arg2);
+				break;
+			}
+			System.out.printf("%s = %d%n", line, ret);
+		}
 	}
 }
