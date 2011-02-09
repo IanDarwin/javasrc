9feaef9f5c699ad7f2586c8ce23a892aa8a8725c
IanDarwin <>
2011-02-09 14:04:06 +0000

    Shorten.

--- src/main/java/jaxwsservice/ServiceMain.java
+++ src/main/java/jaxwsservice/ServiceMain.java
@@ -10,13 +10,10 @@ import javax.xml.ws.Endpoint;
 public class ServiceMain {
 	
 	public static void main(String[] args) {
-		// Create what used to be called the "service stub"
+		// Create the "service stub"
 		Calc impl = new Calc();
 		// Start the service running
-		Endpoint endPoint = 
-			Endpoint.publish("http://localhost:9090/calc", impl);
-		 
-		// Endpoint.publish("http://localhost:9090/calc2", impl);
-		System.out.println("Listening on " + endPoint);
+		Endpoint.publish("http://localhost:9090/calc", impl);
+
 	}
 }
