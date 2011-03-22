c278a0f9f8a275ddbb4ba6208ff0bbf3da939ab0
Ian Darwin <ian@darwinsys.com>
2011-03-22 02:21:26 +0000

    Package, organize imports.

--- src/main/java/unfinished/ThreadGroupDemo.java
+++ src/main/java/unfinished/ThreadGroupDemo.java
@@ -1,8 +1,10 @@
 package unfinished;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /** Show how to catch exceptions from action handlers' threads.
  * DOES NOT WORK -- DO NOT USE
