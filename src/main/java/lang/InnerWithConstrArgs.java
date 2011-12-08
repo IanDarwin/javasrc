92ae01cd0c502fd45ee95d44cb335136b3e3943a
Ian Darwin <ian@darwinsys.com>
2011-12-08 23:08:56 +0000

    Using anonymous inner class with constructor arguments

---
+++ src/main/java/lang/InnerWithConstrArgs.java
@@ -0,0 +1,21 @@
+package play;
+
+public class InnerWithConstrArgs {
+	public static abstract class A {
+		String s;
+		public abstract String getTitle();
+		public A(String s) {
+			this.s = s;
+		}
+	}
+	
+	public static void main(String[] args) {
+		A anA = new A("Hello") {
+			@Override
+			public String getTitle() {
+				return s;
+			}			
+		};
+		System.out.println(anA.getTitle());
+	}
+}
