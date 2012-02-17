e5fef76847d28e32f379233e2d3fcd76efc65783
Ian Darwin <ian@darwinsys.com>
2012-02-17 21:58:33 +0000

    New: Exercise the simple HTTP server built into Java SE 6+

---
+++ src/main/java/netweb/WebServerSimpleSE.java
@@ -0,0 +1,39 @@
+package netweb;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+/** Exercise the simple HTTP server built into Java SE 6+ */
+public class SimpleServerSE {
+
+	private static final int PORT = 8800;
+
+	public static void main(String[] args) throws IOException {
+		
+		HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 5);
+		server.createContext("/", new HttpHandler() {			
+			@Override
+			public void handle(HttpExchange rAndR) throws IOException {
+				System.out.println("Server...handle()");
+				
+				InputStream is = rAndR.getRequestBody();
+		        int j;
+				do {
+		        	j = is.read();
+		        } while (j != -1);
+		           
+				String message = "<html><p>Hello!</p></html>";
+				rAndR.sendResponseHeaders(200, message.length());
+				rAndR.getResponseBody().write(message.getBytes());
+				rAndR.getResponseBody().close();
+			}
+		});
+		server.setExecutor(null); // "We don't need no ex-e-cute-or, ..."
+		server.start();
+		System.out.println("Listening on " + server.getAddress());
+	}
+}
