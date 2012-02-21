0add85c18e1e928daab840b590eb936fae68d737
Ian Darwin <ian@darwinsys.com>
2012-02-21 17:09:23 +0000

    Build out a bit.

--- src/main/java/netweb/WebServerSimpleSE.java
+++ src/main/java/netweb/WebServerSimpleSE.java
@@ -2,6 +2,7 @@ package netweb;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
 import java.util.Date;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -18,11 +19,13 @@ public class SimpleServerSE {
 		HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 5);
 		server.createContext("/", new HttpHandler() {			
 			@Override
-			public void handle(HttpExchange rAndR) throws IOException {
-				System.out.println("Server...handle()");
+			public void handle(HttpExchange rr) throws IOException {
+				final String method = rr.getRequestMethod();
+				final URI uri = rr.getRequestURI();
+				System.out.printf("Server...handle(%s %s)%n", method, uri);
 				
 				// Clients are expected to readfully the inputstream.
-				InputStream is = rAndR.getRequestBody();
+				InputStream is = rr.getRequestBody();
 		        int j;
 				do {
 		        	j = is.read();
@@ -32,9 +35,9 @@ public class SimpleServerSE {
 				final String template = "<html><p>Hello at %s!</p></html>";
 				String message = String.format(template, new Date());
 				
-				rAndR.sendResponseHeaders(200, message.length());
-				rAndR.getResponseBody().write(message.getBytes());
-				rAndR.getResponseBody().close();
+				rr.sendResponseHeaders(200, message.length());
+				rr.getResponseBody().write(message.getBytes());
+				rr.getResponseBody().close();
 			}
 		});
 		server.setExecutor(null); // "We don't need no ex-e-cute-or, ..."
