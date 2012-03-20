e55664f9a73a44b819636e6312e358d105ccb0f3
Ian Darwin <ian@darwinsys.com>
2012-03-20 17:37:36 +0000

    Include full version.

--- src/main/java/datetime/ISODate.java
+++ src/main/java/datetime/ISODate.java
@@ -1,6 +1,7 @@
 package datetime;
 
 import java.util.Date;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 /** Demonstrate a couple of ways to format an ISO 8601 date */
@@ -9,5 +10,7 @@ public class ISODate {
 		Date d =  new Date();
 		System.out.printf("%1$tY-%1$tm-%1$td%n", d);
 		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(d));
+		DateFormat fullIsoDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'kk:mm:ss'Z'");
+		System.out.println(fullIsoDateFormat.format(d));
 	}
 }
