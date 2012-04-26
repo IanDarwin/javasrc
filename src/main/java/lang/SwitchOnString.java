fa48dd0b807268646f649e3dea0209f26475c12e
Ian Darwin <ian@darwinsys.com>
2012-04-26 14:35:07 +0000

    Document Java 7-ness.

--- src/main/java/lang/SwitchOnString.java
+++ src/main/java/lang/SwitchOnString.java
@@ -1,6 +1,11 @@
 package lang;
 
-/** Show the 'switch on string' feature of Java 7 */-
+/** Show the 'switch on string' feature of Java 7.
+ * 
+ * P L E A S E   R E A D   B E F O R E   C O M P L A I N I N G
+ * This class absolutely requires Java SE 7+, so just add an exclusion rule
+ * (Build Path -> Exclude) if you are living with a legacy version of Java SE.
+ */
 public class SwitchOnString {
 	public static void main(String[] args) {
 		String input = "red";
--- src/main/java/nio/FileWatchServiceDemo.java
+++ src/main/java/nio/FileWatchServiceDemo.java
@@ -11,7 +11,12 @@ import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
-/** Demonstrate the JavaSE 7+ NIO WatchService */
+/** Demonstrate the JavaSE 7+ NIO WatchService.
+ * 
+ * P L E A S E   R E A D   B E F O R E   C O M P L A I N I N G
+ * This class absolutely requires Java SE 7+, so just add an exclusion rule
+ * (Build Path -> Exclude) if you are living with a legacy version of Java SE.
+ */
 public class FileWatchServiceDemo {
 	public static void main(String[] args) throws Throwable {
 		String tempDirPath = "/tmp";
