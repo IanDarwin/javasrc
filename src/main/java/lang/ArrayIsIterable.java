235411a87e64ecb29c14b259854ab2b934c46ec4
Ian Darwin <ian@darwinsys.com>
2012-04-26 16:59:59 +0000

    Document the sucker.

--- src/main/java/lang/ArrayIsIterable.java
+++ src/main/java/lang/ArrayIsIterable.java
@@ -4,7 +4,9 @@ package lang;
  */
 public class ArrayIsIterable {
 	public static void main(String[] args) {
+		// Cannot just use (args instanceof Iterable) b/c compiler thinks you want args[i] there.
 		Class<? extends String[]> argsClass = args.getClass();
 		System.out.println(Iterable.class.isAssignableFrom(argsClass));
+		// Answer: No, at least on JDK 1.5 and 1.6.
 	}
 }
