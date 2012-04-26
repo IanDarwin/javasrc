04bb4c38204504af4d3b9f46266ebbd5d0fc3dae
Ian Darwin <ian@darwinsys.com>
2012-04-26 17:03:16 +0000

    Print what it does implement.

--- src/main/java/lang/ArrayIsIterable.java
+++ src/main/java/lang/ArrayIsIterable.java
@@ -6,7 +6,11 @@ public class ArrayIsIterable {
 	public static void main(String[] args) {
 		// Cannot just use (args instanceof Iterable) b/c compiler thinks you want args[i] there.
 		Class<? extends String[]> argsClass = args.getClass();
-		System.out.println(Iterable.class.isAssignableFrom(argsClass));
+		System.out.println("Arrays implement Iterable is " +
+				Iterable.class.isAssignableFrom(argsClass));
 		// Answer: No, at least on JDK 1.5 and 1.6.
+		for (Class<?> i : argsClass.getInterfaces()) {
+			System.out.println("But it does implement " + i.getName());
+		}
 	}
 }
