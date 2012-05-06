8b15d1aa7fddccd37a2413688bd4cdcaff1a5ab7
Ian Darwin <ian@darwinsys.com>
2012-05-06 16:58:52 +0000

    JavaDoc police.

--- src/main/java/structure/MediaFancy.java
+++ src/main/java/structure/MediaFancy.java
@@ -1,11 +1,21 @@
 package structure;
 
+/** An example of an enum with method overriding */
 public enum MediaFancy {
+	/** The enum constant for a book, with a method override */
 	book {
 		public String toString() { return "Book"; }
 	},
-	music_cd, music_vinyl, movie_vhs, movie_dvd;
+	/** The enum constant for a Music CD */
+	music_cd,
+	/** ... */
+	music_vinyl,
+	movie_vhs,
+	movie_dvd;
 
+	/** It is generally disparaged to have a main() in an enum;
+	 * please forgive this tiny demo class for doing so.
+	 */
 	public static void main(String[] args) {
 		MediaFancy[] data =  { book, movie_dvd, music_vinyl };
 		for (MediaFancy mf : data) {
