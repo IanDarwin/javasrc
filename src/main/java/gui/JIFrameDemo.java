965e601b67756d89f4bb1a1be9b1f26832d52519
Ian Darwin <ian@darwinsys.com>
2012-05-06 17:38:58 +0000

    Use setDefaultCloseOperation instead of WindowListener. Move setSize() to end so not tempted to add pack(). Tart up on-screen layout.

--- src/main/java/gui/JIFrameDemo.java
+++ src/main/java/gui/JIFrameDemo.java
@@ -4,8 +4,6 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -25,12 +23,6 @@ public class JIFrameDemo {
 	public static void main(String[] a) {
 		final JFrame jf = new JFrame("JIFrameDemo Main Window");
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		screenSize.width -= 42;
-		screenSize.height -= 42;
-		jf.setSize(screenSize);
-		jf.setLocation(20, 20);
-
 		JMenuBar mb = new JMenuBar();
 		jf.setJMenuBar(mb);
 		JMenu fm = new JMenu("File");
@@ -44,12 +36,11 @@ public class JIFrameDemo {
 		});
 
 		JDesktopPane dtp = new JDesktopPane();
-		//dtp.setBackground(Color.GREEN);
 		jf.setContentPane(dtp);
 
 		JInternalFrame mboxFrame = 
 			new JInternalFrame("Mail Reader", true, true, true, true);
-		JLabel reader = new JLabel("Mail Reader Would Be Here");
+		JLabel reader = new JLabel("Mail Reader Would Be Here",  JLabel.CENTER);
 		mboxFrame.setContentPane(reader);
 		mboxFrame.setSize(400, 300);
 		mboxFrame.setLocation(50, 50);
@@ -58,29 +49,27 @@ public class JIFrameDemo {
 
 		JInternalFrame compFrame = 
 			new JInternalFrame("Compose Mail", true, true, true, true);
-		JLabel composer = new JLabel("Mail Compose Would Be Here");
+		JLabel composer = new JLabel("Mail Compose Would Be Here",  JLabel.CENTER);
 		compFrame.setContentPane(composer);
 		compFrame.setSize(300, 200);
-		compFrame.setLocation(200, 200);
+		compFrame.setLocation(450, 200);
 		compFrame.setVisible(true);
 		dtp.add(compFrame);
 
 		JInternalFrame listFrame = 
 			new JInternalFrame("Users", true, true, true, true);
-		JLabel list = new JLabel("List of Users Would Be Here");
+		JLabel list = new JLabel("List of Users Would Be Here",  JLabel.CENTER);
 		listFrame.setContentPane(list);
-		listFrame.setLocation(400, 400);
+		listFrame.setLocation(450, 400);
 		listFrame.setSize(500, 200);
 		listFrame.setVisible(true);
 		dtp.add(listFrame);
 
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		jf.setSize(screenSize);
+		jf.setLocation(20, 20);
+
 		jf.setVisible(true);
-        jf.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-			jf.setVisible(false);
-			jf.dispose();
-			System.exit(0);
-			}
-		});
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
