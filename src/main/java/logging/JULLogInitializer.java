31a86173aacc5cfad2e70c066bfc071e715aa75b
Ian Darwin <ian@darwinsys.com>
2012-09-25 15:49:29 +0000

    Improve comments.

--- src/main/java/logging/JULLogInitializer.java
+++ src/main/java/logging/JULLogInitializer.java
@@ -8,10 +8,14 @@ import java.util.logging.Logger;
 /**
  * Alternate, contankerous way to specify our own java.util.logging config file.
  * ************  DO NOT USE AS AN EXAMPLE **************
- * This is NOT how you should specify logging, because it
+ * This is NOT how you should specify logging in a standalone app, because it
  * must be invoked by the user application!
  * You should instead use -Djava.util.logging.config.file=fullPathToLogConfig
  * -or- set this in a System Property as Log14Demo2 does - best way!
+ * However, feel free to use this when the simpler method, 
+ * System.setProperty("java.util.logging.config.file", realPath),
+ * fails, e.g., because the web container has already  intitialized the java.util.logging package
+ * and hence the system property is ignored. 
  * @author Ian Darwin
  */
 public class JULLogInitializer {
