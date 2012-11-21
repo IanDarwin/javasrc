02ff38388375298fb80b8c448c7155f223b00555
Ian Darwin <ian@darwinsys.com>
2012-11-21 22:41:46 +0000

    Tweak up.

--- src/main/java/structure/HashableCollectionWithoutEqualsHashCode.java
+++ src/main/java/structure/HashableCollectionWithoutEqualsHashCode.java
@@ -40,35 +40,36 @@ public class HashableCollectionWithoutEqualsHashCode {
 		Key sandy = new Key(NAME_SANDY);
 		String putOk = map.put(sandy, TYPE_HURRICANE);
 		// map.put returns the previous value
-		assertBoolean(null == putOk, "On first add, the previous value should have been null");
+		assertTrue(null == putOk, "On first add, the previous value should have been null");
 		putOk = map.put(sandy, TYPE_HURRICANE);
-		assertBoolean(putOk == TYPE_HURRICANE, "Although only in map once, it should now be the previous value");
-		assertBoolean(map.size() == 1, "There should obviously be only one entry in the map here.");
+		assertTrue(putOk == TYPE_HURRICANE, "Although only in map once, it should now be the previous value");
+		assertTrue(map.size() == 1, "There should obviously be only one entry in the map here.");
 
 		// Two value objects with the default equals and hashCode
 		Key mandy = new Key(NAME_SANDY);
 		// "sandy" and "mandy" are now distinct instances but with the same state; with a
 		// proper equals method, sandy.equals(mandy) would be true.
-		assertBoolean(!sandy.equals(mandy), "but with default equals method, they differ");
+		assertTrue(!sandy.equals(mandy), "but with default equals method, they differ");
 
 		map.put(mandy, TYPE_HURRICANE);
-		assertBoolean(map.size() == 2, "We think there should be two copies in the map now");
+		assertTrue(map.size() == 2, "We think there should be two copies in the map now");
 
-		assertBoolean(map.containsKey(mandy), "map should contain Key mandy");
-		assertBoolean(map.containsKey(sandy), "map should contain Key sandy");
+		assertTrue(map.containsKey(mandy), "map should contain Key mandy");
+		assertTrue(map.containsKey(sandy), "map should contain Key sandy");
 		
 		// What are we left with?
 		for (Key key : map.keySet()) {
 			System.out.println(key + " -> " + map.get(key));
 		}
 		
+		// Even though the values are Strings, which implement equals/hashCode correctly...
 		for (String s : map.values()) {
 			System.out.println("Value " + s + " is a value");
 		}
 	}
 	
 	/** Tiny subset of JUnit, baked in to avoid dependency */
-	void assertBoolean(boolean cond, String mesg) {
+	void assertTrue(boolean cond, String mesg) {
 		if (!cond) {
 			throw new AssertionError(mesg);
 		}
