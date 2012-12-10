04e0a1988f8f509e8a43a2bfff11198e8752b59e
Ian Darwin <ian@darwinsys.com>
2012-12-10 17:37:09 +0000

    Testbed for "who catches the interruptedexception?"

---
+++ src/main/java/threads/IntrWhoCatches.java
@@ -0,0 +1,46 @@
+package threads;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+/** 
+ * Testbed for answering the question:
+ * Who gets the InterruptedException, anyway?
+ * under differing circumstances (comment out parts to vary it).
+ */
+public class IntrWhoCatches {
+
+	
+	static Runnable r = new Runnable() {
+		public void run() {
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println("Interrupt caught in Runnable, on " + Thread.currentThread());
+			}
+			System.out.println("Lived long enough to interrupt myself!");
+			Thread.currentThread().interrupt();
+		}
+	};
+	
+	public static void main(String[] args) {
+		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+			
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				System.out.printf("Uncaught Throwable %s from Thread %s, on thread %s%n", e, t, Thread.currentThread());
+			}
+		});
+		
+		Thread t1 = new Thread(r, "BG Thread");
+		t1.setDaemon(false);
+		t1.start();
+		System.out.printf("Main thread %s, run thread %s%n", Thread.currentThread(), t1);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.out.println("Interrupt caught in main, on " + Thread.currentThread());
+		}
+		t1.interrupt();
+	}
+}
