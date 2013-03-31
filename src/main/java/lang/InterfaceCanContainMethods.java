005cb17eb81a94cc08faa77f92c3d52ea1b13624
Ian Darwin <ian@darwinsys.com>
2013-03-31 22:38:44 +0000

    Comment

--- src/main/java/lang/InterfaceCanContainMethods.java
+++ src/main/java/lang/InterfaceCanContainMethods.java
@@ -1,5 +1,8 @@
 package lang;
 
+/**
+ * Show that interfaces could contain nested classes which contained methods.
+ */
 public interface InterfaceCanContainMethods {
 	public class X {
 		public void setFoo() {}
