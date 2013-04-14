162df72140264bd8e75463be89e5d0112a193d41
Ian Darwin <ian@darwinsys.com>
2013-04-14 20:09:43 +0000

    Use printf(), not kludge around w/ DecimalFormat.

--- src/main/java/numbers/TempConverter2.java
+++ src/main/java/numbers/TempConverter2.java
@@ -1,13 +1,11 @@
 package numbers;
 
-import java.text.*;
 
 /* Print a table of fahrenheit and celsius temperatures, a bit more neatly.
  * @author Ian F. Darwin, http://www.darwinsys.com/
  * @version $Id$
  */
 public class TempConverter2 extends TempConverter {
-	protected DecimalFormat df;
 
 	public static void main(String[] args) {
 		TempConverter t = new TempConverter2();
@@ -16,17 +14,12 @@ public class TempConverter2 extends TempConverter {
 		t.end();
 	}
 
-	// Constructor
-	public TempConverter2() {
-		df = new DecimalFormat("#0.00");
-	}
-
 	protected void print(float f, float c) {
-		System.out.println(df.format(f) + " " + df.format(c));
+		System.out.printf("%6.2f %6.2f%n", f, c);
 	}
 
 	protected void start() {
-		System.out.println("Fahr    Centigrade.");
+		System.out.println("Fahr    Centigrade");
 	}
 
 	protected void end() {
