f5f25a3e2e305c79a157c1e69fc3b66d86b5b737
Ian Darwin <ian@darwinsys.com>
2013-04-19 18:21:58 +0000

    @SuppressWarnings.

--- src/main/java/gui/ArrayListTableSimple.java
+++ src/main/java/gui/ArrayListTableSimple.java
@@ -87,6 +87,7 @@ public class ArrayListTableSimple extends JFrame {
 
 		// Make the Model and View
 		Model m = new Model(data);
+		@SuppressWarnings("unchecked")		
 		JPanel v = new ArrayListTablePanel(ArrayListTableDatum.class, data, m);
 
 		// Make it show up in this JFrame
