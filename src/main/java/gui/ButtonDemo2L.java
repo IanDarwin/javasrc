cc8035056deae825394abb4f31bc45a295ea6532
IanDarwin <>
2013-07-08 01:12:28 +0000

    And so it begins anew.

---
+++ src/main/java/gui/ButtonDemo2L.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+/** Demonstrate a JButton with Lambda Action Listeners */
+public class ButtonDemo2l extends JFrame {
+
+	public ButtonDemo2L() {
+		super("ButtonDemo Lambda");
+
+		JButton	b;
+		add(b = new JButton("A button"));
+		b.addActionListener(e -> JOptionPane.showMessageDialog(this,
+			"Thanks for pushing my first button!"));
+
+		add(b = new JButton("Another button"));
+		b.addActionListener(e -> JOptionPane.showMessageDialog(this,
+			"Thanks for pushing my second button!");
+
+		pack();
+		setVisible(true);
+	}
+}
