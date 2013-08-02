18c1aaf2ddb128f1e150900bdddc5d5f647651d7
Ian Darwin <ian@darwinsys.com>
2013-08-02 17:07:29 +0000

    Get the month names out of Calendar.

---
+++ src/main/java/datetime/GetMonthNames.java
@@ -0,0 +1,17 @@
+package datetime;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+/** Get the months of the year in the current Locale; only works for Gregorian calendar! */
+public class GetMonthNames {
+
+	public static void main(String[] args) {
+		final Calendar cal = Calendar.getInstance();
+		for (int i = 0; i < 12; i++) {
+			cal.set(Calendar.MONTH, i);
+			System.out.println(cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()));
+		}
+	}
+
+}
