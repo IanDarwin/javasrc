47bfe0b1ee00d10a11ebabe975db0e999ce04275
Ian Darwin <ian@darwinsys.com>
2013-10-11 11:46:10 -0400

    This is 2013. JUnit 4 is here.

--- src/test/java/oo/EqualsDemoTest.java
+++ src/test/java/oo/EqualsDemoTest.java
@@ -1,13 +1,16 @@
 package oo;
 
-import junit.framework.*;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /** Some JUnit test cases for EqualsDemo.
  * Writing a full set is left as "an exercise for the reader".
  * Run as: $ java junit.textui.TestRunner EqualsDemoTest
  * @version $Id$
  */
-public class EqualsDemoTest extends TestCase {
+public class EqualsDemoTest {
 
 	/** an object being tested */
 	EqualsDemo d1;
@@ -15,24 +18,23 @@ public class EqualsDemoTest extends TestCase {
 	EqualsDemo d2;
 
 	/** init() method */
+	@Before
 	public void setUp() {
 		d1 = new EqualsDemo();
 		d2 = new EqualsDemo();
 	}
 
-	/** constructor plumbing for junit */
-	public EqualsDemoTest(String name) {
-		super(name);
-	}
-
+	@Test
 	public void testSymmetry() { 
 		assertTrue(d1.equals(d1));
 	}
 
+	@Test
 	public void testSymmetric() {
 		assertTrue(d1.equals(d2) && d2.equals(d1));
 	}
 
+	@Test
 	public void testCaution() {
 		assertTrue(!d1.equals(null));
 	}
