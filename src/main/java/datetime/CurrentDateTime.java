f5e387dce55f6f84ed988af7dfafd91bd6522932
IanDarwin <>
2013-10-29 02:30:13 +0000

    Show Local{Date,Time,DateTime} basics.

---
+++ src/main/java/datetime/CurrentDateTime.java
@@ -0,0 +1,16 @@
+package datetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class CurrentDateTime {
+	public static void main(String[] args) {
+		LocalDate dNow = LocalDate.now();
+		System.out.println(dNow);
+		LocalTime tNow = LocalTime.now();
+		System.out.println(tNow);
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
+	}
+}
