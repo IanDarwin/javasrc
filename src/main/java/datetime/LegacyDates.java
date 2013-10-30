22ddba156065a5157b8f240d80c1df3afc005810
IanDarwin <>
2013-10-30 01:42:47 +0000

    Convert from new dates API to old, various ways.

---
+++ src/main/java/datetime/LegacyDates.java
@@ -0,0 +1,29 @@
+package datetime;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class LegacyDates {
+	public static void main(String[] args) {
+
+		Date legacyDate = new Date();
+		System.out.println(legacyDate);
+
+		ZoneOffset zoneOffset = ZoneOffset.of("+0400");
+
+		// using the long integer-based methods
+		long longTime = legacyDate.getTime();
+		LocalDateTime convertedDate1 = LocalDateTime.ofEpochSecond(
+				longTime / 1000, (int) ((longTime % 1000) * 1000), zoneOffset);
+		System.out.println(convertedDate1);
+
+		// Using individual values
+		LocalDateTime convertedDate2 = LocalDateTime.of(
+				legacyDate.getYear() + 1900,
+				legacyDate.getMonth() + 1, legacyDate.getDate(),
+				legacyDate.getHours(), legacyDate.getMinutes(),
+				legacyDate.getSeconds());
+		System.out.println(convertedDate2);
+	}
+}
