5d511cfb70f715574537bc00c49096b0306213aa
IanDarwin <>
2013-11-09 23:56:01 +0000

    Better ways, using methods of legacy classes.

--- src/main/java/datetime/LegacyDates.java
+++ src/main/java/datetime/LegacyDates.java
@@ -1,29 +1,25 @@
 package datetime;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 public class LegacyDates {
 	public static void main(String[] args) {
 
+		// There and back again, via Date
 		Date legacyDate = new Date();
 		System.out.println(legacyDate);
-
-		ZoneOffset zoneOffset1 = ZoneOffset.of("-0400");
-
-		// using the long integer-based methods
-		long longTime = legacyDate.getTime();
-		LocalDateTime convertedDate1 = LocalDateTime.ofEpochSecond(
-				longTime / 1000, (int) ((longTime % 1000) * 1000), zoneOffset1);
-		System.out.println(convertedDate1);
-
-		// Using individual values
-		LocalDateTime convertedDate2 = LocalDateTime.of(
-				legacyDate.getYear() + 1900,
-				legacyDate.getMonth() + 1, legacyDate.getDate(),
-				legacyDate.getHours(), legacyDate.getMinutes(),
-				legacyDate.getSeconds());
-		System.out.println(convertedDate2);
+		
+		LocalDateTime newDate = LocalDateTime.ofInstant(legacyDate.toInstant(), ZoneId.of("EST"));
+		System.out.println(newDate);
+		
+		// newDate.t
+		
+		Calendar c = Calendar.getInstance();
+		System.out.println(c);
+		LocalDateTime newCal = LocalDateTime.ofInstant(c.toInstant(), ZoneId.of("EST"));
+		System.out.println(newCal);
 	}
 }
