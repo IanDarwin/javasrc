3c3cf1b62c99c6bceeb61269fd3273eecbe476a8
IanDarwin <>
2013-11-30 22:48:17 +0000

    Document it.

--- src/main/java/functional/ProcessIntsIntUnaryOperator.java
+++ src/main/java/functional/ProcessIntsIntUnaryOperator.java
@@ -9,6 +9,9 @@ import java.util.function.Function;
 public class ProcessIntsUsingFunctional {
 	static int[] integers = { 0, 1, 2, 3, 4, 5 };
 	
+	/** Function to be called with an int and a Function;
+	 * just apply the function to the int and return the result
+	 */
 	static int doTheMath(int n, Function<Integer,Integer> func) {
 		return func.apply(n);
 	}
@@ -17,6 +20,7 @@ public class ProcessIntsUsingFunctional {
 
 		int total = 0;
 		for (int i : integers) {
+			// Call doTheMath with 'i' and a Lambda for n^2 +1
 			total += doTheMath(i, k -> k * k + 1 );
 		}
 		System.out.println(total);
