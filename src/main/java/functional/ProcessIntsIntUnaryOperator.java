bdb22614f99cf7dbd051b65159ce7b968c5de811
IanDarwin <>
2013-11-30 22:46:31 +0000

    Is now working, but a very silly demo of using a Functional Interface

--- src/main/java/functional/ProcessIntsIntUnaryOperator.java
+++ src/main/java/functional/ProcessIntsIntUnaryOperator.java
@@ -1,12 +1,23 @@
 package functional;
 
+import java.util.function.Function;
+
+/**
+ * A very silly demo of using a Functional Interface
+ * @author Ian Darwin
+ */
 public class ProcessIntsUsingFunctional {
 	static int[] integers = { 0, 1, 2, 3, 4, 5 };
 	
+	static int doTheMath(int n, Function<Integer,Integer> func) {
+		return func.apply(n);
+	}
+	
 	public static void main(String[] args) {
+
 		int total = 0;
 		for (int i : integers) {
-			total += -> compute(x) { x * x + 1 };
+			total += doTheMath(i, k -> k * k + 1 );
 		}
 		System.out.println(total);
 	}
