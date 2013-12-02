b560614edc788ee81cd24ce68967a0a48d8dddca
Ian Darwin <foss@darwinsys.com>
2013-12-01 19:11:01 -0500

    Go Generic; make the getBean method work like Spring 3: 2nd arg is Class.

--- src/main/java/patterns/creation/Factory.java
+++ src/main/java/patterns/creation/Factory.java
@@ -1,34 +1,53 @@
 package patterns.creation;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
  * A really really simple example of a configurable Factory
+ * Reads from a Properties file on classpath of the form
+ * beanName=fullClassName
+ * e.g.:
+ * renderer=patterns.creation.MyRenderer
  */
 public class Factory {
 
+	private static final String FACTORY_CONFIG_RESOURCE_NAME = 
+				"/patterns/creation/factory.config";
+
 	public static void main(String[] args) throws Exception {
-		MessageRenderer p =
-			(MessageRenderer)Factory.getBean("renderer");
-		System.out.println(p.getClass());
+		MessageRenderer p =Factory.getBean("renderer", MessageRenderer.class);
+		System.out.println("Renderer is of " + p.getClass());
 	}
 
 	static Properties props = new Properties();
 
 	static {
 		try {
-			props.load(new FileInputStream("patterns/creation/factory.config"));
+			InputStream stream = 
+			Factory.class.getResourceAsStream(FACTORY_CONFIG_RESOURCE_NAME);
+			if (stream == null) {
+				throw new ExceptionInInitializerError("Can't load properties file from classpath: " + FACTORY_CONFIG_RESOURCE_NAME);
+			}
+			props.load(stream);
 		} catch (IOException e) {
 			throw new ExceptionInInitializerError(e);
 		}
 	}
 
-	public static Object getBean(String name) throws Exception {
-		final String clazz = props.getProperty(name);
-		final Class<? extends Object> c = Class.forName(clazz);
-		final Object o = c.newInstance();
+	/** Construct and return a bean whose declared class is clasz
+	 * and whose implementing class is the value of "bean" in a props file.
+	 * @param name - The name the Bean should have in the props/config
+	 * @param clazz - the declared (often an interface) class
+	 * @return The instantiated bean.
+	 * @throws Exception
+	 */
+	public static <T> T getBean(String name, Class<T> clazz) throws Exception {
+		final String clazzName = props.getProperty(name);
+		@SuppressWarnings("unchecked")
+		final Class<T> c = (Class<T>) Class.forName(clazzName);
+		final T o = c.newInstance();
 		return o;
 	}
 
