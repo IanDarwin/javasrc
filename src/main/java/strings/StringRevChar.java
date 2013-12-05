5e84505c0a8e8e8c6ff3f5dc8ec9c308c80ad87c
Ian Darwin <foss@darwinsys.com>
2013-12-05 17:19:07 -0500

    String reverse, two ways, with test.

--- src/main/java/strings/StringRevChar.java
+++ src/main/java/strings/StringRevChar.java
@@ -9,7 +9,7 @@ public class StringRevChar {
 	public static void main(String[] argv) {
 		//+
 		String sh = "FCGDAEB";
-		System.out.println(sh + " -> " + new StringBuffer(sh).reverse());
+		System.out.println(sh + " -> " + new StringBuilder(sh).reverse());
 		//-
 	}
 }
