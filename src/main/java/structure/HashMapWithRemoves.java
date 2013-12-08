f87c7744a6212edd8c28a34f8204bfd924fda271
Ian Darwin <foss@darwinsys.com>
2013-12-08 18:56:28 -0500

    Split out a bit of code for BEGIN processing test

---
+++ src/main/java/structure/HashMapWithRemoves.java
@@ -0,0 +1,43 @@
+package structure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Demonstrate the HashMap class, and an Iterator, with concurrent remove
+ */
+public class HashMapWithRemoves {
+
+	public static void main(String[] argv) {
+
+		// Construct and load the hash. This simulates loading a
+		// database or reading from a file, or wherever the data is.
+
+		Map<String,String> map = new HashMap<String,String>();
+
+		// The hash maps from company name to address.
+		// In real life this might map to an Address object...
+		map.put("Adobe", "Mountain View, CA");
+		map.put("IBM", "White Plains, NY");
+		map.put("Learning Tree", "Los Angeles, CA");
+		map.put("Microsoft", "Redmond, WA");
+		map.put("Netscape", "Mountain View, CA");
+		map.put("O'Reilly", "Sebastopol, CA");
+		map.put("Sun", "Mountain View, CA");
+
+		// BEGIN SafeRemoval
+		// Version 2: get ALL the keys and values 
+		// with concurrent modification
+		Iterator it = h.keySet( ).iterator( );
+		while (it.hasNext( )) {
+			String key = (String) it.next( );
+			if (key.equals("Sun")) {
+				it.remove();
+				continue;
+			}
+			System.out.println("Company " + key + "; " +
+				"Address " + h.get(key));
+		}
+		// END
+	}
+}
