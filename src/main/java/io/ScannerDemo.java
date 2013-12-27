3108d331ffdb64d0646f2be54bf54e7f2562c534
Ian Darwin <foss@darwinsys.com>
2013-12-26 20:53:44 -0500

    Undead: no source found, did not compile when first typed in...

---
+++ src/main/java/io/ScannerDemo.java
@@ -0,0 +1,17 @@
+package io;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		String sampleDate = "25 Dec 1988";
+
+		try (Scanner sDate = new Scanner(sampleDate)) {
+			int dayOfMonth = sDate.nextInt();
+			String month = sDate.next();
+			int year = sDate.nextInt();
+			System.out.printf("%d-%s-%02d%n", year, month, dayOfMonth);
+		}
+	}
+}
