0095799a2d0ffb75abe4142af555853698e7e006
Ian Darwin <foss@darwinsys.com>
2013-12-27 22:48:00 -0500

    tiny formatting change

--- src/main/java/starting/Buggy.java
+++ src/main/java/starting/Buggy.java
@@ -1,13 +1,12 @@
 package starting;
 
-/** This program exhibits some bugs, so we can use a debugger */
 // BEGIN main
+/** This program exhibits some bugs, so we can use a debugger */
 public class Buggy {
 	static String name;
 
 	public static void main(String[] args) {
 		int n = name.length();	// bug # 1
-
 		System.out.println(n);
 
 		name += "; The end.";	// bug #2
