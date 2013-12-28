9930a8f83c1a104f196d4440fa082aa723eb5204
Ian Darwin <ian@dalai.darwinsys.com>
2013-12-28 13:37:45 -0500

    New: simple Hibernate demo.

---
+++ src/main/java/database/hibernate/HibernateSimple.java
@@ -0,0 +1,56 @@
+package database.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+import database.Person;
+
+// BEGIN main
+public class HibernateSimple {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+
+		System.out.println("HibernateSimple.main()");
+
+		Configuration cf = new AnnotationConfiguration();
+		cf.configure();
+		SessionFactory sf = null;
+		Session session = null;
+		try {
+			sf = cf.buildSessionFactory();
+			session = sf.openSession();
+
+			Transaction tx = session.beginTransaction();
+
+			// Create an entity in the database.
+			Person np = new Person("Tom", "Boots");
+			System.out.println(np);
+			session.save(np);
+			tx.commit();
+
+			int id = np.getId();
+			System.out.println("Created Person with Id " + id);
+
+			tx = session.beginTransaction();
+
+			Query query = session.createQuery("select p from Person p order by p.lastName");
+
+			List<Person> list = query.list();
+			System.out.println("There are " + list.size() + " persons:");
+			for (Person p : list) {
+				System.out.println(
+						p.getFirstName() + ' ' + p.getLastName());
+			}
+			System.out.println();
+		} finally {
+			session.close();
+		}
+	}
+}
+// END main
