0814fe78dedad59335ba46aead2e57ca4040df1a
Ian Darwin <foss@darwinsys.com>
2013-12-28 15:18:15 -0500

    Import another simple entity from the jpademo project.

---
+++ src/main/java/database/Address.java
@@ -0,0 +1,49 @@
+package database;
+
+// BEGIN main
+import javax.persistence.*;
+
+@Entity
+public class Address {
+
+    private int id;
+
+    private String streetAddress;
+    private String city;
+    private String country;
+
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+    // Other accessors and methods omitted for brevity
+    // END main
+    
+    public void setId(int id) {
+    		this.id = id;
+    }
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
