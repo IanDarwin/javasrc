43e360898b0e3ac99a5301ab0c4c6292a70c2276
Ian Darwin <foss@darwinsys.com>
2013-12-28 14:15:14 -0500

    Clean up duplication of database/jdbc from JDBC.

--- src/main/java/database/jdbc/LoadDriver.java
+++ src/main/java/database/jdbc/LoadDriver.java
@@ -6,15 +6,14 @@ package database.jdbc;
  */
 // BEGIN main
 public class LoadDriver {
-	@SuppressWarnings("unchecked")
 	public static void main(String[] av) {
 		try {
 			// Try to load the jdbc-odbc bridge driver
 			// Should be present on Sun JDK implementations.
-			Class c = Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Class<?> c = Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 			System.out.println("Loaded " + c.getName());
 			// Try to load an Oracle driver.
-			Class d = Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class<?> d = Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("Loaded " + d.getName());
 		} catch (ClassNotFoundException ex) {
 			System.err.println(ex);
