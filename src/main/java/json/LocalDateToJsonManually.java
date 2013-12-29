ea41a932ecee8d545ee6f936417d00c81f8a1d92
IanDarwin <>
2013-12-29 01:14:15 +0000

    Name tells all.

---
+++ src/main/java/json/LocalDateToJsonManually.java
@@ -0,0 +1,27 @@
+package json;
+
+import java.time.LocalDate;
+
+public class LocalDateToJsonManually {
+
+	private static final String OPEN = "{";
+	private static final String CLOSE = "}";
+	
+	public static void main(String[] args) {
+		LocalDate dNow = LocalDate.now();
+		System.out.println(toJson(dNow));
+	}
+	public static String toJson(LocalDate dNow) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(OPEN).append("\n");
+		sb.append(jsonize("year", dNow.getYear()));
+		sb.append(jsonize("month", dNow.getMonth()));
+		sb.append(jsonize("day", dNow.getDayOfMonth()));
+		sb.append(CLOSE).append("\n");
+		return sb.toString();
+	}
+	
+	public static String jsonize(String key, Object value) {
+		return String.format("\"%s\": \"%s\",\n", key, value);
+	}
+}
