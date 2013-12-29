79d3342fbe3bf19d96bc34000ba12a8794e0c590
Ian Darwin <foss@darwinsys.com>
2013-12-29 16:54:11 -0500

    Add another "expect compile error" example, showing how @Override protects you
    ... against accidentally compiling an overload when you meant an override.

---
+++ src/main/java/lang/AnnotationOverrideDemo.java
@@ -0,0 +1,26 @@
+package lang;
+
+// BEGIN main
+/**
+ * AnnotationOverrideDemo - Simple demonstation of Metadata being used to verify
+ * that a method does in fact override (not overload) a method
+ * from the parent class. This class provides the method.
+ */
+abstract class Top {
+    public abstract void myMethod(Object o);
+}
+
+/** Simple demonstation of Metadata being used to verify
+ * that a method does in fact override (not overload) a method
+ * from the parent class. This class is supposed to do the overriding,
+ * but deliberately introduces an error to show how the 1.5 compiler
+ * behaves 
+ */
+class Bottom {
+
+    @Override
+    public void myMethod(String s) {    // EXPECT COMPILE ERROR
+        // Do something here...
+    }
+}
+// END main
