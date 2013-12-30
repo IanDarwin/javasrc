2795c668131f77bf5a5e1a47298a43390d00a1d7
Ian Darwin <foss@darwinsys.com>
2013-12-30 18:57:20 -0500

    Toss in org.json api and examples of it. Tweak others.

--- src/main/java/json/SoftwareParseJackson.java
+++ src/main/java/json/SoftwareParseJackson.java
@@ -6,14 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 // BEGIN main
 public class SoftwareParseJackson {
+	final static String FILE_NAME = "/json/softwareinfo.json";
 
 	public static void main(String[] args) throws Exception {
 		ObjectMapper mapper = new ObjectMapper(); // <1>
 
-		String fileName = "/json/softwareinfo.json";
-		InputStream jsonInput = SoftwareParseJackson.class.getResourceAsStream(fileName);
+		InputStream jsonInput = SoftwareParseJackson.class.getResourceAsStream(FILE_NAME);
 		if (jsonInput == null) {
-			throw new NullPointerException("can't find" + fileName);
+			throw new NullPointerException("can't find" + FILE_NAME);
 		}
 		SoftwareInfo sware = mapper.readValue(jsonInput, SoftwareInfo.class);
 		System.out.println(sware);
---
+++ src/main/java/json/WriteOrgJson.java
@@ -0,0 +1,16 @@
+package json;
+
+import org.json.JSONObject;
+
+// BEGIN main
+public class WriteOrgJson {
+	public static void main(String[] args) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("Name", "robinParse").		// <1>
+			put("Version", "1.2.3").
+			put("Class", "RobinParse");
+		String printable = jsonObject.toString();	// <2>
+		System.out.println(printable);
+	}
+}
+// END mains
