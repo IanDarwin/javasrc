2ffe79483a349b67510f0c475280773a6aaacd32
Ian Darwin <foss@darwinsys.com>
2014-01-01 14:29:22 -0500

    Tweak

--- src/main/java/printjdk14printservice/PrintServiceDemo.java
+++ src/main/java/printjdk14printservice/PrintServiceDemo.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
 
 import javax.print.Doc;
 import javax.print.DocFlavor;
@@ -13,10 +12,10 @@ import javax.print.DocPrintJob;
 import javax.print.PrintException;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
-import javax.print.attribute.DocAttributeSet;
+import javax.print.SimpleDoc;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
-import javax.print.attribute.standard.MediaSizeName;
+import javax.print.attribute.standard.MediaSize;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -82,12 +81,13 @@ public class PrintServiceDemo extends JFrame {
 		System.out.println("PrintServiceDemo.print(): Printing " + fileName);
 		DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
 		PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
-		aset.add(MediaSizeName.NA_LETTER);
+		aset.add(MediaSize.NA.LETTER);
 		PrintService[] pservices = 
 			PrintServiceLookup.lookupPrintServices(flavor, aset);
 		int i;
 		switch(pservices.length) {
 		case 0:
+			System.err.println(0);
 			JOptionPane.showMessageDialog(PrintServiceDemo.this,
 					"Error: No PrintService Found", "Error", JOptionPane.ERROR_MESSAGE);
 			return;
@@ -102,43 +102,10 @@ public class PrintServiceDemo extends JFrame {
 			break;
 		}
 		DocPrintJob pj = pservices[i].createPrintJob();
-		Doc doc = new MyDemoDoc(INPUT_FILE_NAME, flavor);
+		InputStream is = getClass().getResourceAsStream(INPUT_FILE_NAME);
+		Doc doc = new SimpleDoc(is, flavor, null);
 
 		pj.print(doc, aset);
-	}
-	
-	/**
-	 * Simple "Doc" implementation: just holder for filename and document flavor.
-	 */
-	final class MyDemoDoc implements Doc {
-		
-		private DocFlavor flavor;
-		private String fileName;
-
-		public MyDemoDoc(String fileName, DocFlavor flavor) {
-			this.fileName = fileName;
-			this.flavor = flavor;
-		}
-
-		public DocFlavor getDocFlavor() {
-			return flavor;
-		}
-
-		public Object getPrintData() throws IOException {
-			return null;
-		}
-
-		public DocAttributeSet getAttributes() {
-			return null;
-		}
-
-		public Reader getReaderForText() throws IOException {
-			return null;
-		}
-
-		public InputStream getStreamForBytes() throws IOException {
-			return getClass().getResourceAsStream(fileName);
-		}
 	}
 }
 // END main
