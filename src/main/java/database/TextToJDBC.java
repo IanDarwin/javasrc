e9900700c3013d6405755a589e547302817754c1
Ian Darwin <foss@darwinsys.com>
2014-01-02 19:47:37 -0500

    Imports police.

--- src/main/java/database/TextToJDBC.java
+++ src/main/java/database/TextToJDBC.java
@@ -2,11 +2,9 @@ package database;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.StringTokenizer;
 
