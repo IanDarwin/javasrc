6ee51b339349d5df4b4b399374fbe705f6c29cce
Ian Darwin <foss@darwinsys.com>
2014-01-05 18:04:24 -0500

    Demonstrate Paths, Path and Files.

---
+++ src/main/java/dir_file/PathsFilesDemo.java
@@ -0,0 +1,23 @@
+package dir_file;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathsFilesDemo {
+	public static void main(String[] args) throws Exception {
+		// BEGIN main
+		Path p = Paths.get("my junk file");                        // <1>
+		boolean deleted = Files.deleteIfExists(p);             // <2>
+		InputStream is =                                                  // <3>
+				PathsFilesDemo.class.getResourceAsStream("/demo.txt");
+		long newFileSize = Files.copy(is, p);                     // <4>
+		System.out.println(newFileSize);                           // <5>
+		final Path realPath = p.toRealPath();                     // <6>
+		System.out.println(realPath);
+		realPath.forEach(pc-> System.out.println(pc));     // <7>
+		Files.delete(p);                                                     // <8>
+		// END main
+	}
+}
