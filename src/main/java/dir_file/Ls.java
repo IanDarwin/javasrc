885e358dbec9ddec3af310767604f63ae996fe85
Ian Darwin <ian@darwinsys.com>
2014-01-10 22:25:49 -0500

    Missing import

--- src/main/java/dir_file/Ls.java
+++ src/main/java/dir_file/Ls.java
@@ -1,5 +1,7 @@
 package dir_file;
 
+import java.util.Arrays;
+
 /** Simple directory lister.
  * @author Ian Darwin, http://www.darwinsys.com/
  */
@@ -7,9 +9,9 @@ package dir_file;
 public class Ls {
 	public static void main(String args[]) {
 		String[] dirs = new java.io.File(".").list(); // Get list of names
-		java.util.Arrays.sort(dirs);		// Sort it (see <<javacook-structure-SECT-8>>)
+		Arrays.sort(dirs);		// Sort it (see <<javacook-structure-SECT-8>>)
 		for (String dir : dirs) {
-            System.out.println(dir);    	// Print the list
+			System.out.println(dir);    	// Print the list
 		}
 	}
 }
