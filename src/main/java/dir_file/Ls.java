0d1c09933a4cf25fb03a7377e841bf742836c1eb
Ian Darwin <foss@darwinsys.com>
2014-01-10 20:38:48 -0500

    Markers, and modernizer.

--- src/main/java/dir_file/Ls.java
+++ src/main/java/dir_file/Ls.java
@@ -3,11 +3,14 @@ package dir_file;
 /** Simple directory lister.
  * @author Ian Darwin, http://www.darwinsys.com/
  */
+// BEGIN main
 public class Ls {
-	public static void main(String argh_my_aching_fingers[]) {
-		String[] dir = new java.io.File(".").list(); // Get list of names
-		java.util.Arrays.sort(dir);		// Sort it (Data Structuring chapter))
-		for (int i=0; i<dir.length; i++)
-			System.out.println(dir[i]);	// Print the list
+	public static void main(String args[]) {
+		String[] dirs = new java.io.File(".").list(); // Get list of names
+		java.util.Arrays.sort(dirs);		// Sort it (see <<javacook-structure-SECT-8>>)
+		for (String dir : dirs) {
+            System.out.println(dir);    	// Print the list
+		}
 	}
 }
+// END main
