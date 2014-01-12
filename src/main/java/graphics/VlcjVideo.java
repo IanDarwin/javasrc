d480acdadce4e5f6099b370a0f3ad14e334eaaed
Ian Darwin <foss@darwinsys.com>
2014-01-11 20:45:39 -0500

    JCB markers.

--- src/main/java/graphics/VlcjVideo.java
+++ src/main/java/graphics/VlcjVideo.java
@@ -1,19 +1,30 @@
 package graphics;
 
-import uk.co.caprica.vlcj.component.*;
+import java.awt.BorderLayout;
+import java.io.File;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+import com.sun.jna.NativeLibrary;
+
+// BEGIN main
 public class VlcjVideo extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
-		new VideoPlayer(
-			"http://174.132.240.162:8000/;stream.nsv");
+		new VlcjVideo(args[0]);
 	}
 
 	public VlcjVideo(String url) {
 		setTitle("VLCJ Video");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(800, 600);
-		JPanel player = new MyPlayerPanel();
+		JPanel player = new MyVideoPanel();
 		add(player, BorderLayout.CENTER);
 		pack();
 		setVisible(true);
@@ -21,6 +32,7 @@ public class VlcjVideo extends JFrame {
 	}
 
 	class MyVideoPanel extends JPanel {
+		private static final long serialVersionUID = 1L;
 		private File vlcWhere = new File("/usr/local/lib");
 		private EmbeddedMediaPlayer player;
 
@@ -40,3 +52,4 @@ public class VlcjVideo extends JFrame {
 		}
 	}
 }
+// END main
