0ceb637192ff67f5342b8b736b0720e6878d468a
IanDarwin <>
2014-01-12 02:38:23 +0000

    Sample JavaFX video play.

---
+++ src/main/java/javafx/JfxVideo.java
@@ -0,0 +1,40 @@
+package javafx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+// BEGIN
+public class JfxVideo extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("JavaFX Video");
+		Media media = new Media(
+				"http://www.mediacollege.com/video-gallery/testclips/20051210-w50s.flv");
+		MediaPlayer player = new MediaPlayer(media);
+		player.play();
+
+		MediaView view = new MediaView(player);
+		Group root = new Group();
+		root.getChildren().add(view);
+		Scene scene = SceneBuilder.create().
+				width(360).height(288).
+				root(root).
+				fill(Color.WHITE).
+				build();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
+// END main
