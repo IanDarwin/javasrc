b75eddbd3634cd33fa4d4620a7dadb9095baf7ab
Ian Darwin <ian@darwinsys.com>
2014-01-25 13:35:47 -0500

    Another example pasted-out from an incomplete example in the book.

---
+++ src/main/java/environ/GetOptParseArgs.java
@@ -0,0 +1,24 @@
+package environ;
+
+import com.darwinsys.bleah.GetOpt;
+
+public class GetOptParseArgsDemo {
+public static void main(String[] args) {
+// BEGIN main
+GetOptDesc[] options = {
+    new GetOptDesc('n', "numeric", false),
+    new GetOptDesc('o', "output-file", true),
+};
+Map optionsFound = new GetOpt(options).parseArguments(argv);
+if (optionsFound.get("n") != null) {
+    System.out.println("sortType = NUMERIC;")
+}
+String outputFile = null;
+if ((outputFile = optionsFound.get("o") != null) {
+    System.out.println("output file specified as " + outputFile)
+} else {
+	System.out.println("Output to System.out");
+}
+// END main
+}
+}
