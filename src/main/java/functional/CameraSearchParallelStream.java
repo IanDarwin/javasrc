3e6db2a5283f9e5b2b032ca0b50fecaf0bdbf7d9
IanDarwin <>
2014-01-26 17:23:49 +0000

    One more for the mix.

---
+++ src/main/java/functional/CameraSearchParallelStream.java
@@ -0,0 +1,18 @@
+package functional;
+
+import java.util.List;
+
+public class CameraSearchParallelStream {
+	
+	static List<Camera> privateListOfCameras = CameraUtils.getList();
+	
+	// BEGIN search
+	public static void main(String[] args) {
+		for (Object camera : privateListOfCameras.parallelStream(). // <1>
+				filter(c -> c.isIlc() && c.getPrice() < 500).                     // <2>
+				toArray()) {                                                                   // <3>
+			System.out.println(camera);                                             // <4>
+		}
+	}
+	// END search
+}
