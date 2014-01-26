2727670e105d47b9c67b48161d3e8e2e5b39483b
IanDarwin <>
2014-01-26 15:55:11 +0000

    New.

---
+++ src/main/java/functional/ProcessIntsUsingFunctional.java
@@ -0,0 +1,15 @@
+package functional;
+
+interface ThisIsFunctional {
+    int compute(int x);
+}
+
+public class ProcessIntsUsingFunctional {
+	static int[] integers = {1, 2, 3};
+
+	public static void main(String[] args) {
+		for (int i : integers) 
+			total += x -> compute(x) { x * x + 1 };
+	}
+}
+
