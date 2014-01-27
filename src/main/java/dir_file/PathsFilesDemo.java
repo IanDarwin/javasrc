30f74c9fdac3c16a696c069c28250ee548edd497
Ian Darwin <foss@darwinsys.com>
2014-01-26 19:21:12 -0500

    Align the callouts.

--- src/main/java/dir_file/PathsFilesDemo.java
+++ src/main/java/dir_file/PathsFilesDemo.java
@@ -8,16 +8,16 @@ import java.nio.file.Paths;
 public class PathsFilesDemo {
 	public static void main(String[] args) throws Exception {
 		// BEGIN main
-		Path p = Paths.get("my junk file");                        // <1>
+		Path p = Paths.get("my_junk_file");                    // <1>
 		boolean deleted = Files.deleteIfExists(p);             // <2>
-		InputStream is =                                                  // <3>
+		InputStream is =                                       // <3>
 				PathsFilesDemo.class.getResourceAsStream("/demo.txt");
-		long newFileSize = Files.copy(is, p);                     // <4>
-		System.out.println(newFileSize);                           // <5>
-		final Path realPath = p.toRealPath();                     // <6>
+		long newFileSize = Files.copy(is, p);                  // <4>
+		System.out.println(newFileSize);                       // <5>
+		final Path realPath = p.toRealPath();                  // <6>
 		System.out.println(realPath);
-		realPath.forEach(pc-> System.out.println(pc));     // <7>
-		Files.delete(p);                                                     // <8>
+		realPath.forEach(pc-> System.out.println(pc));         // <7>
+		Files.delete(p);                                       // <8>
 		// END main
 	}
 }
