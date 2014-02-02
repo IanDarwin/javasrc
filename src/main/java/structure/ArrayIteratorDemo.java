7052146c983e713da0fd2e71e77c007a35278162
Ian Darwin <foss@darwinsys.com>
2014-02-02 11:46:54 -0500

    Move to darwinsys so we can use Java 8.

---
+++ src/main/java/structure/ArrayIteratorDemo.java
@@ -0,0 +1,22 @@
+// BEGIN main
+package structure;
+
+import com.darwinsys.util.ArrayIterator;
+
+public class ArrayIteratorDemo {
+	
+	private final static String[] names = {
+		"rose", "petunia", "tulip"
+	};
+	
+	public static void main(String[] args) {
+		ArrayIterator<String> arrayIterator = new ArrayIterator<>(names);
+		for (String s : arrayIterator) {
+			System.out.println(s);
+		}
+		
+		// Java 8 way
+		arrayIterator.forEach(s->System.out.println(s));
+	}
+}
+// END main
