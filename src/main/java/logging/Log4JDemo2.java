ff052124b24e96f3632e9d6039dbd5f35c0ffbc1
Ian Darwin <ian@darwinsys.com>
2014-02-09 13:03:31 -0500

    Better logging

--- src/main/java/logging/Log4JDemo2.java
+++ src/main/java/logging/Log4JDemo2.java
@@ -6,16 +6,16 @@ import org.apache.log4j.Logger;
 public class Log4JDemo2 {
 	public static void main(String[] args) {
 
-		Logger myLogger = Logger.getLogger("com.darwinsys");
+		Logger theLogger = Logger.getLogger("com.darwinsys");
 
 		try {
 			Object o = new Object();
 			if (o != null) {	// bogus, just to show logging
 				throw new IllegalArgumentException("Just testing");
 			}
-			myLogger.info("I created an object: " + o);
+			theLogger.info("I created an object: " + o);
 		} catch (Exception ex) {
-			myLogger.error("Caught Exception: " + ex);
+			theLogger.error("Caught Exception: " + ex, ex);
 		}
 	}
 }
