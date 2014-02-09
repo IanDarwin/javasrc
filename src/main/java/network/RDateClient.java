e7dda48312c0fdb612bf95194d5de3423884e198
Ian Darwin <foss@darwinsys.com>
2014-02-08 20:05:25 -0500

    Verify with a LocalDateTime, instead of running external date(1).

--- src/main/java/network/RDateClient.java
+++ src/main/java/network/RDateClient.java
@@ -1,8 +1,11 @@
 package network;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * DaytimeBinary - connect to the standard Time (binary) service.
@@ -19,8 +22,10 @@ public class RDateClient {
 	 */
 	protected static final long BASE_DAYS = 
 		(long)((1970-1900)*365 + (1970-1900-1)/4);
+
 	/* Seconds since 1970 */
 	public static final long BASE_DIFF = (BASE_DAYS * 24 * 60 * 60);
+
 	/** Convert from seconds to milliseconds */
 	public static final int MSEC = 1000;
 
@@ -35,7 +40,7 @@ public class RDateClient {
 			Socket sock = new Socket(hostName, TIME_PORT);
 			DataInputStream is = new DataInputStream(new 
 				BufferedInputStream(sock.getInputStream()));
-			// Need to read 4 bytes from the network, unsigned.
+			// Read 4 bytes from the network, unsigned.
 			// Do it yourself; there is no readUnsignedInt().
 			// Long is 8 bytes on Java, but we are using the
 			// existing time protocol, which uses 4-byte ints.
@@ -49,6 +54,7 @@ public class RDateClient {
 			System.out.println("Time diff == " + (remoteTime - BASE_DIFF));
 			Date d = new Date((remoteTime - BASE_DIFF) * MSEC);
 			System.out.println("Time on " + hostName + " is " + d.toString());
+			System.out.println("Local date/time = " + LocalDateTime.now());
 		} catch (IOException e) {
 			System.err.println(e);
 		}
