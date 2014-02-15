75b74eb138270280e3c41ddd1d485b6c3d6dd48c
Ian Darwin <foss@darwinsys.com>
2014-02-15 12:37:24 -0500

    Simplify: avoid large array, isTabStop() computes on fly.
    Still passes all tests. Based on an idea from Alex Stangl.

--- src/main/java/strings/Tabs.java
+++ src/main/java/strings/Tabs.java
@@ -1,6 +1,5 @@
 package strings;
 
-import com.darwinsys.util.Debug;
 
 /** Basic tab-character handling stuff.
  * <p>
@@ -15,30 +14,18 @@ public class Tabs {
 	protected int tabSpace = DEFTABSPACE;
 	/** The longest line that we initially set tabs for. */
 	public final static int MAXLINE  = 255;
-	/** the current tab stops */
-	protected boolean[] tabstops;
 
 	/** Construct a Tabs object with a given tab stop settings */
 	public Tabs(int n) {
 		if (n <= 0) {
 			n = 1;
 		}
-		tabstops = new boolean[MAXLINE];
 		tabSpace = n;
-		settabs();
 	}
 
 	/** Construct a Tabs object with a default tab stop settings */
 	public Tabs() {
 		this(DEFTABSPACE);
-	}
-
-	/** settabs - set initial tab stops */
-	private void settabs() {
-		for (int i = 0; i < tabstops.length; i++) {
-			tabstops[i] = ((i+1) % tabSpace) == 0;
-			Debug.println("tabs", "Tabs[" + i + "]=" + tabstops[i]);
-		}
 	}
 	
 	/**
@@ -52,11 +39,9 @@ public class Tabs {
 	 * @param col - the current column number
 	 */
 	public boolean isTabStop(int col) {
-		if (col > tabstops.length - 1) {
-			tabstops = new boolean[tabstops.length * 2];
-			settabs();
-		}
-		return tabstops[col];
+		if (col <= 0)
+			return false;
+		return (col+1) % tabSpace == 0;
 	}
 }
 // END main
