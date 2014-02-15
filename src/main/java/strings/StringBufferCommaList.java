65f9997af9a8fec6053a0720073f5fba37ee3138
Ian Darwin <ian@darwinsys.com>
2014-02-15 10:31:07 -0500

    Show two ways: original, and using builder.length from Alex Stangl

--- src/main/java/strings/StringBufferCommaList.java
+++ src/main/java/strings/StringBufferCommaList.java
@@ -3,15 +3,30 @@ package strings;
 import java.util.StringTokenizer;
 
 public class StringBufferCommaList {
+	public static final String SAMPLE_STRING = "Alpha Bravo Charlie";
 	public static void main(String[] args) {
-		StringTokenizer st = new StringTokenizer("Alpha Bravo Charlie");
-		StringBuffer sb = new StringBuffer();
+
+		// BEGIN main
+		// Method using regexp split 
+		StringBuilder sb1 = new StringBuilder();
+		for (String word : SAMPLE_STRING.split(" ")) {
+			if (sb1.length() > 0) {
+				sb1.append(", ");
+			} 
+			sb1.append(word);
+		}
+		System.out.println(sb1);
+
+		// Method using a StringTokenizer
+		StringTokenizer st = new StringTokenizer(SAMPLE_STRING);
+		StringBuilder sb2 = new StringBuilder();
 		while (st.hasMoreElements()) {
-			sb.append(st.nextToken());
+			sb2.append(st.nextToken());
 			if (st.hasMoreElements()) {
-				sb.append(", ");
+				sb2.append(", ");
 			}
 		}
-		System.out.println(sb);
+		System.out.println(sb2);
+		// END main
 	}
 }
