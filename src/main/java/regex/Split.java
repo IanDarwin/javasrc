356df464a7827832c98ecef4d97afed43a858bc2
Ian Darwin <foss@darwinsys.com>
2014-02-15 11:25:26 -0500

    sync

--- src/main/java/regex/Split.java
+++ src/main/java/regex/Split.java
@@ -5,12 +5,16 @@ import java.util.regex.*;
 /** Split a String into a Java Array of Strings divided by an RE
  */
 public class Split {
+	private static final String SAMPLE_STRING = "the darwinian devonian explodian chicken";
+
 	public static void main(String[] args) {
 		String[] x = 
-			Pattern.compile("ian").split(
-				"the darwinian devonian explodian chicken");
+			Pattern.compile("ian").split(SAMPLE_STRING);
 		for (int i=0; i<x.length; i++) {
 			System.out.println(i + " \"" + x[i] + "\"");
+		}
+		for (String word : SAMPLE_STRING.split(" ")) {
+			System.out.println(word);
 		}
 	}
 }
