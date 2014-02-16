a50f9bf19b9a872c33eea19ebe7be0156f477c11
Ian Darwin <foss@darwinsys.com>
2014-02-16 17:59:03 -0500

    Clean up a bit.

--- src/main/java/numbers/RandomInt.java
+++ src/main/java/numbers/RandomInt.java
@@ -13,7 +13,7 @@ public class RandomInt {
 		Random r = new Random();
 		for (int i=0; i<1000; i++)
 			// nextInt(10) goes from 0-9; add 1 for 1-10;
-			System.out.println(1+Math.round(r.nextInt(10)));
+			System.out.println(1+r.nextInt(10));
 	}
 }
 // END main
