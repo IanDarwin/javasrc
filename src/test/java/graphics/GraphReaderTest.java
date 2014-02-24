551285d3204bc69da309b79f280b4409c9a58852
Ian Darwin <foss@darwinsys.com>
2014-02-23 20:13:20 -0500

    Convert to double.parseDouble; add test case.

--- src/test/java/graphics/GraphReaderTest.java
+++ src/test/java/graphics/GraphReaderTest.java
@@ -19,6 +19,6 @@ public class GraphReaderTest {
 		List<Point2D> actual = GraphReader.read(reader, "unit test input");
 		assertEquals(2, actual.size());
 		assertEquals(new Point2D.Double(12.34, 56.78), actual.get(0));
-		assertEquals(new Point2D.Double(3.14, 3.14), actual.get(0));
+		assertEquals(new Point2D.Double(3.14, 3.14), actual.get(1));
 	}
 }
