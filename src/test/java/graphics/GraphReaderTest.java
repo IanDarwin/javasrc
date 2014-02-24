b324a817e9ad5b4d519ea0b36c30dfd4dec3d74d
Ian Darwin <foss@darwinsys.com>
2014-02-23 19:57:22 -0500

    Finish this up.

--- src/test/java/graphics/GraphReaderTest.java
+++ src/test/java/graphics/GraphReaderTest.java
@@ -1,19 +1,24 @@
 package graphics;
 
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import java.awt.geom.Point2D;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+
+import org.junit.Test;
 
 public class GraphReaderTest {
 
 	@Test
-	public void readListOK() {
+	public void readListOK() throws Exception {
 		String input = "12.34 56.78\n" +
 			"3.14 3.14\n";
 		Reader reader = new StringReader(input);
-		List<Point2D> actual = GraphReader.read(reader);
-		assertEqual(2, actual.size();
-		assertEqual(new Point2D(12.34, 56.78), actual.get(0));
-		assertEqual(new Point2D(3.14, 3.14), actual.get(0));
+		List<Point2D> actual = GraphReader.read(reader, "unit test input");
+		assertEquals(2, actual.size());
+		assertEquals(new Point2D.Double(12.34, 56.78), actual.get(0));
+		assertEquals(new Point2D.Double(3.14, 3.14), actual.get(0));
 	}
 }
