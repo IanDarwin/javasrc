87db3bd826925aaa429e8b233501046dc266d90a
Ian Darwin <foss@darwinsys.com>
2014-03-09 10:55:48 -0400

    Method Refs += "an Instance Method of an Arbitrary Object of a Particular Type"

---
+++ src/main/java/functional/Referencesdemo4.java
@@ -0,0 +1,52 @@
+package functional;
+
+// BEGIN main
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Referencesdemo4 {
+
+	static final String[] unsortedNames = { 
+		"Gosling", "de Raadt", "Torvalds", "Ritchie", "Hopper"
+	};
+	
+	public static void main(String[] args) {
+		String[] names;
+		
+		// Sort using a reference to an instance method
+		names = unsortedNames.clone();
+		Arrays.sort(names, String::compareToIgnoreCase);
+		dump(names);
+
+		// Equivalent Lambda:
+		names = unsortedNames.clone();
+		Arrays.sort(names, (str1, str2) -> str1.compareToIgnoreCase(str2));
+		dump(names);
+		
+		// Equivalent old way:
+		names = unsortedNames.clone();
+		Arrays.sort(names, new Comparator<String>() {
+			@Override
+			public int compare(String str1, String str2) {
+				return str1.compareToIgnoreCase(str2);
+			}
+		});
+		dump(names);
+		
+		// Simpest way, using existing comparator
+		names = unsortedNames.clone();
+		Arrays.sort(names, String.CASE_INSENSITIVE_ORDER);
+		dump(names);
+	}
+	// END main
+
+	/** Simple dumper just to show the order of the names in the array */
+	private static void dump(String[] names) {
+		for (String s : names) {
+			System.out.print(s);
+			System.out.print(' ');
+		}
+		System.out.println();
+	}
+
+}
