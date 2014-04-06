b1c1d07e4cbc507576030634dfbca363b49d57aa
Ian Darwin <ian@darwinsys.com>
2014-04-06 15:50:54 -0400

    detab, repair alignment

--- src/main/java/structure/Array2.java
+++ src/main/java/structure/Array2.java
@@ -7,31 +7,31 @@ import java.util.Date;
  */
 // BEGIN main
 public class Array2  {
-	public final static int INITIAL = 10, 	// <1>
-		GROW_FACTOR = 2;					// <2>
+    public final static int INITIAL = 10,   // <1>
+        GROW_FACTOR = 2;                    // <2>
 
-	public static void main(String[] argv) {
-		int nDates = 0;
-		Date[] dates = new Date[INITIAL];
-		StructureDemo source = new StructureDemo(21);
-		Date c;
-		while ((c=(Date)(source.getDate())) != null) {
+    public static void main(String[] argv) {
+        int nDates = 0;
+        Date[] dates = new Date[INITIAL];
+        StructureDemo source = new StructureDemo(21);
+        Date c;
+        while ((c=(Date)(source.getDate())) != null) {
 
-			// if (nDates >= dates.length) {
-			// 	System.err.println("Too Many Dates! Simplify your life!!");
-			// 	System.exit(1);  // wimp out
-			// }
+            // if (nDates >= dates.length) {
+            //     System.err.println("Too Many Dates! Simplify your life!!");
+            //     System.exit(1);  // wimp out
+            // }
 
-			// better: reallocate, making data structure dynamic
-			if (nDates >= dates.length) {
-				Date[] tmp = new Date[dates.length * GROW_FACTOR];
-				System.arraycopy(dates, 0, tmp, 0, dates.length);
-				dates = tmp;    // copies the array reference
-				// old array will be garbage collected soon...
-			}
-			dates[nDates++] = c;
-		}
-		System.out.println("Final array size = " + dates.length);
-	}
+            // better: reallocate, making data structure dynamic
+            if (nDates >= dates.length) {
+                Date[] tmp = new Date[dates.length * GROW_FACTOR];
+                System.arraycopy(dates, 0, tmp, 0, dates.length);
+                dates = tmp;    // copies the array reference
+                // old array will be garbage collected soon...
+            }
+            dates[nDates++] = c;
+        }
+        System.out.println("Final array size = " + dates.length);
+    }
 }
 // END main
