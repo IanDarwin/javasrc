18bb3336158e57788cc8e8ebce2bddf81fd1a5e7
Ian Darwin <ian@darwinsys.com>
2014-04-13 16:38:20 -0400

    detab for book to make callouts line up

--- src/main/java/javafx/HelloFx.java
+++ src/main/java/javafx/HelloFx.java
@@ -1,7 +1,7 @@
 package javafx;
 
 // BEGIN main
-import javafx.application.Application; // <1>
+import javafx.application.Application;           // <1>
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -9,28 +9,28 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class HelloFx extends Application { // <2>
+public class HelloFx extends Application {       // <2>
 
-	@Override
-	public void start(Stage stage) {			// <3>
-		stage.setTitle("JavaFX Hello!");
-		Button btn = new Button();
-		btn.setText("Run Greeting");
-		btn.setOnAction(new EventHandler<ActionEvent>() { // <4>
-			@Override
-			public void handle(ActionEvent e) {
-				System.out.println("Hello from JavaFX");
-			}
-		});
+    @Override
+    public void start(Stage stage) {             // <3>
+        stage.setTitle("JavaFX Hello!");
+        Button btn = new Button();
+        btn.setText("Run Greeting");
+        btn.setOnAction(new EventHandler<ActionEvent>() { // <4>
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("Hello from JavaFX");
+            }
+        });
 
-		StackPane rootPane = new StackPane();	// <5>
-		rootPane.getChildren().add(btn);
-		stage.setScene(new Scene(rootPane, 300, 200));
-		stage.show();
-	}
-	
-	public static void main(String[] args) {		// <6>
-		launch(args);
-	}
+        StackPane rootPane = new StackPane();    // <5>
+        rootPane.getChildren().add(btn);
+        stage.setScene(new Scene(rootPane, 300, 200));
+        stage.show();
+    }
+    
+    public static void main(String[] args) {     // <6>
+        launch(args);
+    }
 }
 // END main
