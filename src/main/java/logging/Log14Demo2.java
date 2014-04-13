03c2e591d46008e6b66c703e87b72497729b3ded
Ian Darwin <ian@darwinsys.com>
2014-04-13 18:36:05 -0400

    linewrap

--- src/main/java/logging/Log14Demo2.java
+++ src/main/java/logging/Log14Demo2.java
@@ -7,7 +7,8 @@ import java.util.logging.Logger;
 public class Log14Demo2 {
 	public static void main(String[] args) {
 
-		System.setProperty("java.util.logging.config.file", "logging/logging.properties");
+		System.setProperty("java.util.logging.config.file", 
+			"logging/logging.properties");
 
 		Logger logger = Logger.getLogger("com.darwinsys");
 
