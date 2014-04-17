9f58818d1e6703c65c8ac1c5930819ed26068b31
IanDarwin <ian@darwinsys.com>
2014-04-17 18:19:02 -0400

    Add package stmt; accept arg[0] as int; catch StackOverflow.

--- src/main/java/numbers/Fibonacci.java
+++ src/main/java/numbers/Fibonacci.java
@@ -1,13 +1,25 @@
+package numbers;
+
 /**
  * Fibonacci numbers
  */
 public class Fibonacci {
+
+	private final static boolean debug = false;
+
 	public static void main(String[] args) {
-		System.out.println(fib(5));
+		int n = args.length > 0 ? Integer.parseInt(args[0]) : 5;
+		try {
+			System.out.println(fib(n));
+		} catch (StackOverflowError e) {
+			System.err.println(
+				"Stack overflow; bump mem or reduce number: " + n);
+		}
 	}
 
 	public static int fib(int n) {
-		System.out.println( n );
+		if (debug) 
+			System.out.println( n );
 		if (n == 0) 
 			return 0;
 		if (n == 1)
