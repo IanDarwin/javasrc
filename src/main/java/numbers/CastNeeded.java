47de0edbe85d10c6fa2012b26af814f8e601aefd
Ian Darwin <ian@darwinsys.com>
2014-05-15 19:16:22 -0400

    Detab, and align comments.

--- src/main/java/numbers/CastNeeded.java
+++ src/main/java/numbers/CastNeeded.java
@@ -2,17 +2,17 @@ package numbers;
 
 /** Casting Demo. */
 public class CastNeeded {
-	// BEGIN main
-	public static void main(String[] argv) {
-		int i;
-		double j = 2.75;
-		i = j;			// EXPECT COMPILE ERROR
-		i = (int)j;		// with cast; i gets 2
-		System.out.println("i =" + i);
-		byte b;
-		b = i;			// EXPECT COMPILE ERROR
-		b = (byte)i;	// with cast, i gets 2
-		System.out.println("b =" + b);
-	}
-	// END main
+    // BEGIN main
+    public static void main(String[] argv) {
+        int i;
+        double j = 2.75;
+        i = j;            // EXPECT COMPILE ERROR
+        i = (int)j;       // with cast; i gets 2
+        System.out.println("i =" + i);
+        byte b;
+        b = i;            // EXPECT COMPILE ERROR
+        b = (byte)i;      // with cast, i gets 2
+        System.out.println("b =" + b);
+    }
+    // END main
 }
