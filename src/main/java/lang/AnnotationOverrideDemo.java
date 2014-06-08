67245279564d7c6bb40fbcf2667f8ae4278d37f6
Ian Darwin <ian@darwinsys.com>
2014-06-08 17:15:26 -0400

    Comments-only changes

--- src/main/java/lang/AnnotationOverrideDemo.java
+++ src/main/java/lang/AnnotationOverrideDemo.java
@@ -2,8 +2,8 @@ package lang;
 
 // BEGIN main
 /**
- * AnnotationOverrideDemo - Simple demonstation of Metadata being used to verify
- * that a method does in fact override (not overload) a method
+ * AnnotationOverrideDemo - Simple demonstation of Metadata being used to
+ * verify that a method does in fact override (not overload) a method
  * from the parent class. This class provides the method.
  */
 abstract class Top {
@@ -13,7 +13,7 @@ abstract class Top {
 /** Simple demonstation of Metadata being used to verify
  * that a method does in fact override (not overload) a method
  * from the parent class. This class is supposed to do the overriding,
- * but deliberately introduces an error to show how the 1.5 compiler
+ * but deliberately introduces an error to show how the modern compiler
  * behaves 
  */
 class Bottom {
