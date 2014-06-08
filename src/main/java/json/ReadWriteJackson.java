11d8902e9e43a9614c0d0b372eaae383b2809da4
Ian Darwin <ian@darwinsys.com>
2014-06-08 16:56:44 -0400

    Detab, then align comment markers

--- src/main/java/json/ReadWriteJackson.java
+++ src/main/java/json/ReadWriteJackson.java
@@ -9,17 +9,17 @@ import domain.Person;
 // BEGIN main
 public class ReadWriteJackson {
 
-	public static void main(String[] args) throws IOException {
-		ObjectMapper mapper = new ObjectMapper();		// <1>
-		
-		String jsonInput = 													// <2>
-				"{\"id\":0,\"firstName\":\"Robin\",\"lastName\":\"Wilson\"}";
-		Person q = mapper.readValue(jsonInput, Person.class);
-		System.out.println("Read and parsed Person from JSON: " + q);
-		
-		Person p = new Person("Roger", "Rabbit");				// <3>
-		System.out.print("Person object " + p +" as JSON = ");
-		mapper.writeValue(System.out, p);
-	}
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();                // <1>
+        
+        String jsonInput =                                       // <2>
+                "{\"id\":0,\"firstName\":\"Robin\",\"lastName\":\"Wilson\"}";
+        Person q = mapper.readValue(jsonInput, Person.class);
+        System.out.println("Read and parsed Person from JSON: " + q);
+        
+        Person p = new Person("Roger", "Rabbit");                // <3>
+        System.out.print("Person object " + p +" as JSON = ");
+        mapper.writeValue(System.out, p);
+    }
 }
 // END main
