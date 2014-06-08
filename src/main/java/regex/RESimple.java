f257346d48edf74e4e16fa761d68fda686dc9cf7
Ian Darwin <ian@darwinsys.com>
2014-06-07 20:27:55 -0400

    Two patterns now, one matches, one doesn't.

--- src/main/java/regex/RESimple.java
+++ src/main/java/regex/RESimple.java
@@ -10,14 +10,19 @@ import java.util.regex.*;
 public class RESimple {
 	public static void main(String[] argv) {
 		String pattern = "^Q[^u]\\d+\\.";
-		String input = "QA777. is the next flight. It is on time.";
+		String[] input = {
+			"QA777. is the next flight. It is on time.",
+			"Quack, Quack, Quack!"
+		};
 
 		Pattern p = Pattern.compile(pattern);
 
-		boolean found = p.matcher(input).lookingAt();
+		for (String in : input) {
+			boolean found = p.matcher(in).lookingAt();
 
-		System.out.println("'" + pattern + "'" +
-			(found ? " matches '" : " doesn't match '") + input + "'");
+			System.out.println("'" + pattern + "'" +
+			(found ? " matches '" : " doesn't match '") + in + "'");
+		}
 	}
 }
 // END main
