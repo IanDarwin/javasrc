0224ec1e72a72689442472f8a03ef99e0c8028b1
Ian Darwin <ian@darwinsys.com>
2014-08-24 13:05:28 -0400

    Add a method to ListFormat for parsing, and a method for doing lists.
    It's not totally solid in the face of certain inputs: don't use strings with embedded commas,
    and don't have other than String or numbers in our lists.

--- src/main/java/structure/ListFormat.java
+++ src/main/java/structure/ListFormat.java
@@ -1,7 +1,10 @@
 package structure;
 
-/** Output a list with { } around it, and commas between elements,
- * so that the array "one" "two" would print as { one, two }
+import java.util.*;
+
+/** Output a list with [ ] around it, and commas between elements,
+ * so that the array "one" "two" would print as [ "one", "two" ]
+ * Note that for simle cases this is valid JSON
  */
 public class ListFormat {
 
@@ -9,12 +12,34 @@ public class ListFormat {
 		StringBuffer sb = new StringBuffer();
 		for (Object d : data) {
 			if (sb.length() == 0)
-				sb.append("{");
+				sb.append("[");
 			else
 				sb.append(", ");
-			sb.append(d);
+			if (d instanceof String)
+				sb.append('"').append(d).append('"');
+			else
+				sb.append(d);
 		}
-		sb.append("}");
+		sb.append("]");
 		return sb.toString();
+	}
+
+	public static String format(List<?> data) {
+		return format(data.toArray());
+	}
+
+	public static List<String> parse(String input) {
+		List<String> ret = new ArrayList<>();
+		input = input.substring(1, input.length() - 1);	// get rid of [ and ]
+		StringTokenizer st = new StringTokenizer(input, ",");
+		while (st.hasMoreTokens()) {
+			String s = st.nextToken().trim();
+			if (s.charAt(0) == '"') // string
+				ret.add(s.substring(1, s.length() - 1)); // trim quotes
+			else {					// number?
+				ret.add(s);
+			}
+		}
+		return ret;
 	}
 }
--- src/test/java/structure/ListFormatTest.java
+++ src/test/java/structure/ListFormatTest.java
@@ -1,10 +1,26 @@
 package structure;
 
-import junit.framework.TestCase;
+import java.util.*;
 
-public class ListFormatTest extends TestCase {
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListFormatTest {
+
+	final static String[] UNFORMATTED = { "one", "two", "three" };
+	final static String FORMATTED = "[\"one\", \"two\", \"three\"]";
+
+	@Test
 	public void testFormat() {
-		String[] data = { "one", "two", "three" };
-		assertEquals("{one, two, three}", ListFormat.format(data));
+		assertEquals(FORMATTED, ListFormat.format(UNFORMATTED));
+	}
+
+	@Test
+	public void tesetParse() {
+		List<String> ret = ListFormat.parse(FORMATTED);
+		assertEquals(3, ret.size());
+		assertEquals(UNFORMATTED[0], ret.get(0));
+		assertEquals(UNFORMATTED[2], ret.get(2));
 	}
 }
