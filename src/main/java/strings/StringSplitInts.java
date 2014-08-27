1f61089a2d31ac3718c4622439172a9b44a30d77
IanDarwin <ian@darwinsys.com>
2014-08-27 12:53:35 -0400

    Squelch noise.  Document alternatives.

--- src/main/java/strings/StringSplitInts.java
+++ src/main/java/strings/StringSplitInts.java
@@ -8,7 +8,10 @@ package strings;
 public class StringSplitInts {
 	public static void main(String[] args) {
 		String in = "1  4   5   6   7  908  1231  23  53a";
+		// Using \\D for the pattern will make '53' pass.
 		for (String next : in.split("\\s")) {
+			if (next.trim().length() == 0)
+				continue;
 			try {
 				int n = Integer.parseInt(next);
 				System.out.println(next + " --> " + n);
