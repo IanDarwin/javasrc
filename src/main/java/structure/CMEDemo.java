b80d2266a5178e320f8d1d792aa66e78673dea50
IanDarwin <ian@darwinsys.com>
2014-10-08 11:59:18 -0400

    Expand (needed for a demo)

--- src/main/java/structure/CMEDemo.java
+++ src/main/java/structure/CMEDemo.java
@@ -1,6 +1,7 @@
 package structure;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /** Shows how easy it is to get a CME (ConcurrentModificationException);
@@ -16,12 +17,26 @@ public class CMEDemo {
 		strings.add("one");
 		strings.add("two");
 		strings.add("three");
-		
-		for (String s : strings) {
-			if (s.equals("one")) {
-				strings.remove(s);
+		strings.add("four");
+
+		System.out.println("Right way:");
+		Iterator<String> it = strings.iterator();
+		while (it.hasNext()) {
+			String s = it.next();
+			if (s.equals("two")) {
+				it.remove();	// removes current element
+			} else {
+				System.out.println(s);
 			}
-			System.out.println(s);
+		}
+		
+		System.out.println("Wrong way:");
+		for (String s : strings) {
+			if (s.equals("three")) {
+				strings.remove(s);	// CME will happen here.
+			} else {
+				System.out.println(s);
+			}
 		}
 	}
 }
