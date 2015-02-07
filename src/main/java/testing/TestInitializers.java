fed845aa1b007386458d2fe5a9c8534d163c83f0
Ian Darwin <ian@darwinsys.com>
2015-02-06 20:30:12 -0500

    Example of @BeforeClass, @Before, @AfterClass in one file.

---
+++ src/main/java/testing/TestInitializers.java
@@ -0,0 +1,58 @@
+package testing;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestInitializers {
+
+	static MyFactory factory;
+	ClassUnderTest target;
+
+	@BeforeClass
+	public static void initMyStaticStuff() {
+		factory = new MyFactory();
+	}
+
+	@Before
+	public void setUpMore() {
+		target = factory.createObject();
+	}
+
+	@Test
+	public void testName() {
+		target.setName("Ian Darwin");
+		String f = target.getName();
+		assertTrue(f.contains("Ian"));
+	}
+	
+	@AfterClass
+	public void closeDown() {
+		factory.close();
+	}
+
+	// Dummy classes to make the above compile
+	/** World's cheesiest factory */
+	static class MyFactory {
+		ClassUnderTest createObject() {
+			return new ClassUnderTest();
+		}
+
+		public void close() {
+			// Empty, but might need to free some resources here...
+		}
+	}
+
+	/** Stand-in for an actual Class being tested */
+	static class ClassUnderTest extends Object {
+		String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+}
+// END main
