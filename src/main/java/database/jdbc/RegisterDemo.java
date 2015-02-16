dd9578dde5c56efd3e9bd11413e310081157b801
Ian Darwin <ian@darwinsys.com>
2015-02-16 15:06:44 -0500

    Example of how a JDBC Driver registers *itself* with DriverManager
    ... when you do Class.forName("driverClassName").

---
+++ src/main/java/database/jdbc/RegisterDemo.java
@@ -0,0 +1,64 @@
+package database.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/** 
+ * Demonstrate how a JDBC Driver registers itself with the Driver Manager
+ * Please don't try to use this as a real driver!
+ * @author Ian Darwin
+ */
+public class RegisterDemo implements Driver {
+
+	static {
+		try {
+			DriverManager.registerDriver(new RegisterDemo());
+		} catch (SQLException e) {
+			throw new ExceptionInInitializerError("Failed to register driver");
+		}
+	}
+
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		return url.startsWith("registerdemo" + ":");
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMajorVersion() {
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		return 1;
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		return false;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		 throw new SQLFeatureNotSupportedException();
+	}
+}
