624a6a1cb8df5ef50cb5b5ff002ce2065db4089a
Ian Darwin <ian@darwinsys.com>
2015-03-08 23:57:17 -0400

    Extricte one element, just to show how.

--- src/main/java/xml/DocReadWriteDom.java
+++ src/main/java/xml/DocReadWriteDom.java
@@ -12,6 +12,8 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /** Read and write an XML document, preserving comments, using DOM
  * @author Ian Darwin, http://www.darwinsys.com/
@@ -21,16 +23,22 @@ public class DocReadWriteDom {
 
 	public static void main(String[] av) throws Exception {
 		DocReadWriteDom dw = new DocReadWriteDom();
-		Document doc = dw.makeDoc();
+		Document doc = dw.readDoc();
 
+		// Get out a couple of elements to show how
+		NodeList nameElements = doc.getElementsByTagName("name");
+		final Node nameNode = nameElements.item(0);
+		String name = nameNode.getTextContent();
+		System.out.println("Read the name: " + name);
+		
 		System.out.println("Writing the tree now...");
 		Transformer tx = TransformerFactory.newInstance().newTransformer();
 		tx.setOutputProperty(OutputKeys.INDENT, "yes");
 		tx.transform(new DOMSource(doc), new StreamResult(System.out));
 	}
 
-	/** Generate the XML document */
-	protected Document makeDoc() throws Exception {
+	/** Read the XML document */
+	protected Document readDoc() throws Exception {
 			DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
 			fact.setIgnoringComments(false);
 			DocumentBuilder parser = fact.newDocumentBuilder();
