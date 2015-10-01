15392aaee17c55348e8d7506d7ba22fec60b30a8
Ian Darwin <ian@darwinsys.com>
2015-10-01 13:27:53 -0400

    Better listing

--- src/main/java/netweb/URLDemo.java
+++ src/main/java/netweb/URLDemo.java
@@ -6,10 +6,12 @@ import java.net.URL;
 public class URLDemo {
 	public static void main(String[] args) throws MalformedURLException {
 
+		System.out.println("This should work:");
 		URL u = new URL("http://www.darwinsys.com/java/../openbsd/../index.jsp");
-		System.out.println("Raw: " + u);
+		System.out.println("URL: " + u);
 
-		URL url = new URL("bean:WonderBean");
-		System.out.println(url);
+		System.out.println("This should fail:");
+		URL u2 = new URL("bean:WonderBean");
+		System.out.println("URL: " + u2);
 	}
 }
