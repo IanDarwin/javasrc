b522549fa7c0772d24d7ffb32fbc9b68340f8206
Ian <ian@darwinsys.com>
2016-01-16 10:08:02 -0500

    Junit 3.8->4

--- src/test/java/oo/Clone1Test.java
+++ src/test/java/oo/Clone1Test.java
@@ -1,22 +1,24 @@
 package oo;
 
-import junit.framework.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
-public class Clone1Test extends TestCase {
+public class Clone1Test {
 
 	Clone1 c;
 
+	@Before
 	public void setUp() { 
 		c = new Clone1();
 		c.x = 100;
 		c.y = 200;
 	}
 
-	Clone1 d;
 
 	/** Test that clone() method returns OK. */
+	@Test
 	public void testClone() {
-		d = (Clone1)c.clone();
+		Clone1 d = (Clone1)c.clone();
 		// If we get here it didn't throw an exception.
 		System.out.println("c=" + c);
 		System.out.println("d=" + d);
@@ -24,7 +26,4 @@ public class Clone1Test extends TestCase {
 		assertEquals(c.x, d.x);
 		assertEquals(c.y, d.y);
 	}
-
-	/** Constructor needed by junit */
-	public Clone1Test(String name) { super(name); }
 }
