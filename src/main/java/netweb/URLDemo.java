e4c948d8bd885a6f0bc20b6fef89accc52a47b02
Ian <ian@darwinsys.com>
2016-09-19 21:08:46 -0400

    print args if any

--- src/main/java/netweb/URLDemo.java
+++ src/main/java/netweb/URLDemo.java
@@ -6,6 +6,12 @@ import java.net.URL;
 public class URLDemo {
 	public static void main(String[] args) throws MalformedURLException {
 
+		for (String s : args) {
+			System.out.println("Trying your URL: " + s);
+			URL u = new URL(s);
+			System.out.println("Created " + u);
+		}
+
 		System.out.println("This should work:");
 		URL u = new URL("http://www.darwinsys.com/java/../openbsd/../index.jsp");
 		System.out.println("URL: " + u);
