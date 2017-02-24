532fbc423666c965945a7e68b161e1b200301255
Ian <ian@darwinsys.com>
2017-02-24 13:41:51 -0500

    jfx wants a full URL for input

--- src/main/java/javafx/AudioPlay.java
+++ src/main/java/javafx/AudioPlay.java
@@ -1,14 +1,17 @@
 package javafx;
 
+import java.net.URL;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 // BEGIN main
 /** Simple Audio Playing with JavaFX */
 public class AudioPlay {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String clipName = "demo.mp3";
-		Media clip = new Media(clipName);
+		URL url = new URL(String.format("file:///%s/%s", System.getProperty("user.dir"), clipName));
+		Media clip = new Media(url.toString());
 		MediaPlayer mediaPlayer = new MediaPlayer(clip);
 		mediaPlayer.play();
 	}
