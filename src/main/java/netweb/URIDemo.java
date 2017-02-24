cd3886dd9e6fb043aa0a52aac8970b2e1212ea0a
Ian <ian@darwinsys.com>
2017-02-24 12:46:22 -0500

    Remove 'www.' from url; repair comment

--- src/main/java/netweb/URIDemo.java
+++ src/main/java/netweb/URIDemo.java
@@ -10,18 +10,18 @@ public class URIDemo {
 	public static void main(String[] args)
 	throws URISyntaxException, MalformedURLException {
 
-		URI u = new URI("http://www.darwinsys.com/java/../openbsd/../index.jsp");
+		URI u = new URI("https://darwinsys.com/java/../openbsd/../index.jsp");
 		System.out.println("Raw: " + u);
 		URI normalized = u.normalize();
 		System.out.println("Normalized: " + normalized);
-		final URI BASE = new URI("http://www.darwinsys.com");
+		final URI BASE = new URI("https://darwinsys.com");
 		System.out.println("Relativized to " + BASE + ": " + BASE.relativize(u));
 
 		// A URL is a type of URI
 		URL url = new URL(normalized.toString());
 		System.out.println("URL: " + url);
 
-		// Junk
+		// Demo of non-URL but valid URI
 		URI uri = new URI("bean:WonderBean");
 		System.out.println(uri);
 	}
