f391f9698998bba866fb7c389bf5ef8bf8d59512
Ian <ian@darwinsys.com>
2017-02-24 13:57:07 -0500

    Jfx needs its own infrastructure

--- src/main/java/javafx/AudioPlay.java
+++ src/main/java/javafx/AudioPlay.java
@@ -1,17 +1,38 @@
 package javafx;
 
 import java.net.URL;
+import java.util.List;
 
+import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
 // BEGIN main
 /** Simple Audio Playing with JavaFX */
-public class AudioPlay {
+public class AudioPlay extends Application {
+
 	public static void main(String[] args) throws Exception {
-		String clipName = "demo.mp3";
-		URL url = new URL(String.format("file:///%s/%s", System.getProperty("user.dir"), clipName));
-		Media clip = new Media(url.toString());
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		List<String> args = getParameters().getRaw();
+		if (args.size() == 0 ) {
+			playClip(AudioPlay.class.getResource("/audio/test.wav"));
+		} else {
+			for (String arg : args) {
+				playClip(
+					new URL(String.format("file:///%s/%s", 
+						System.getProperty("user.dir"), arg)));
+			}
+		}
+	}
+
+	public void playClip(URL clipUrl) {
+		System.out.println("URL is " + clipUrl);
+		Media clip = new Media(clipUrl.toString());
 		MediaPlayer mediaPlayer = new MediaPlayer(clip);
 		mediaPlayer.play();
 	}
