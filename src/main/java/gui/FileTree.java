143965791313fd7a1e2aa171a9401a4fca65abed
Ian Darwin <ian@darwinsys.com>
2017-04-28 11:18:41 -0400

    Vector->ArrayList, also braces

--- src/main/java/gui/FileTree.java
+++ src/main/java/gui/FileTree.java
@@ -7,7 +7,7 @@ import java.awt.Dimension;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -54,25 +54,27 @@ public class FileTree extends JPanel
 		if (curTop != null) {	// should only be null at root
 			curTop.add(curDir);
 		}
-		List<String> ol = new Vector<String>();
+		List<String> ol = new ArrayList<String>();
 		String[] tmp = dir.list();
 		for (int i=0; i<tmp.length; i++)
 			ol.add(tmp[i]);
 		Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
 		File f;
-		List<String> files = new Vector<String>();
+		List<String> files = new ArrayList<String>();
 		// Make two passes, one for Dirs and one for Files. This is #1.
 		for (int i=0; i<ol.size(); i++) {
 			String thisObject = ol.get(i);
 			String newPath;
-			if (curPath.equals("."))
+			if (curPath.equals(".")) {
 				newPath = thisObject;
-			else
+			} else {
 				newPath = curPath + File.separator + thisObject;
-			if ((f = new File(newPath)).isDirectory())
+			}
+			if ((f = new File(newPath)).isDirectory()) {
 				addNodes(curDir,  f);
-			else
+			} else {
 				files.add(thisObject);
+			}
 		}
 		// Pass two: for files.
 		for (int fnum=0; fnum<files.size(); fnum++)
