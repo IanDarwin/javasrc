bce3068e52082f100d95d8760fa69814ee593c19
Ian Darwin <ian@darwinsys.com>
2017-07-22 16:00:35 -0400

    New: demo of something Vector's synchronization doesn't protect against.

---
+++ src/main/java/structure/VectorMisused.java
@@ -0,0 +1,48 @@
+package structure;
+
+import java.util.Vector;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.*;
+
+/**
+ * Poorly-named example showing that Vector being synchronized is not the only protection you need
+ * It is NOT Vector that is borked, but the assumption that using it would solve all your problems :-)
+ * Will usually print something like "BUSTED! Expected 200000 but actual 199997" on a low-end computer 
+ * that is current in 2016 or 2017. 
+ * Explain why it fails, and why it will eventually start to work correctly.
+ */
+public class VectorMisused {
+
+	public static final int NTHREAD = 20;
+	public static final int NLOOP = 10000;
+
+	static final Vector<MyClass> v = new Vector<>();
+	static final ExecutorService pool = Executors.newFixedThreadPool(NTHREAD);
+	static final AtomicInteger count = new AtomicInteger(0);
+
+	static class MyClass {
+		int val = 0;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		v.add(new MyClass());
+
+		for (int i = 0; i < NTHREAD; i++) {
+			pool.submit(()->{
+				for (int j = 0; j < NLOOP; j++) {
+					MyClass m = v.get(0);
+					m.val++;
+					count.incrementAndGet();
+				}
+			});
+		}
+		pool.shutdown();
+		pool.awaitTermination(100, TimeUnit.SECONDS);
+		int expected = count.get();
+		int actual = v.get(0).val;
+		System.out.printf((expected==actual?"Surprised!":"BUSTED!") + " Expected %d but actual %d\n", count.get(), v.get(0).val);
+	}
+}
