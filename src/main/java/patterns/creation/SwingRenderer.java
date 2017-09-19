e32988f9248f07e7904003d2e896a86d3c99d33e
Ian <ian@darwinsys.com>
2017-09-19 14:38:43 -0400

    Rename class.

--- src/main/java/patterns/creation/SwingRenderer.java
+++ src/main/java/patterns/creation/SwingRenderer.java
@@ -2,7 +2,7 @@ package patterns.creation;
 
 import javax.swing.JOptionPane;
 
-public class MyOtherRenderer implements MessageRenderer {
+public class SwingRenderer implements MessageRenderer {
 	public void renderMessage(String message) {
 		JOptionPane.showMessageDialog(null, message);
 	}
