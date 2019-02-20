13a131d448a7aee04e3abfe64f87449b10e4a7fc
Ian Darwin <ian@darwinsys.com>
2019-02-20 16:45:30 -0500

    Unbreak test, not 100% sure right

--- src/test/java/strings/EnTabTest.java
+++ src/test/java/strings/EnTabTest.java
@@ -1,48 +1,46 @@
 package strings;
 
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
- * @author ian
- * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
+ * Test the guts of EnTab
  */
-public class EnTabTest extends TestCase {
+public class EnTabTest {
 
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(EnTabTest.class);
-	}
 	EnTab t4, t8;
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
+	@Before
+	public void setUp() throws Exception {
 		t4 = new EnTab(4);
 		t8 = new EnTab();
-		// For com.darwinsys.util.Debug: uncomment only 
-		// "when you find yourself in times of trouble..."
-		//System.setProperty("debug.tab", "true");
-		//System.setProperty("debug.pad", "true");
-		//System.setProperty("debug.space", "true");
 	}
 
+	@Test
 	public void testGetTabSpacingDelegation() {
 		assertEquals(4, t4.getTabSpacing());
 		assertEquals(8, t8.getTabSpacing());
 	}
-	
+
+	@Test
 	public void testIdempotency() {
 		assertEquals("", t4.entabLine(""));
 		assertEquals("", t8.entabLine(""));
 		assertEquals(" ", t4.entabLine(" "));
+		assertEquals("   ", t4.entabLine("   "));
 		assertEquals("abcde", t4.entabLine("abcde"));
 	}
-	
+
+	@Test
 	public void testLeadingTabs() {
 		System.out.println(t4.entabLine("    A"));
 		assertEquals("\tA", t4.entabLine("    A"));
+	}
+
+	@Test
+	public void testEmbeddedTabs() {
+		final String entebbe = t4.entabLine("    A   B");
+		System.out.println("testEmbeddedTabs: result " + entebbe.replaceAll("\t", "<TAB>"));
+		assertEquals("\tA\t B", entebbe);
 	}
 }
