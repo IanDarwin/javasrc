28eb0afab78da9a21ff4f05b1e2ecac0db8905c0
Ian Darwin <ian@darwinsys.com>
2019-08-30 15:35:34 -0400

    Java 12, for CompactNumberFormat demo

---
+++ src/main/java/numbers/CompactFormatDemo.java
@@ -0,0 +1,31 @@
+package numbers;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+// BEGIN main
+public class CompactFormatDemo {
+
+	static final Number[] nums = {
+		0, 1, 1.25, 1234, 12345, 123456.78, 123456789012L
+	};
+	static final String[] strs = {
+		"1", "1.25", "1234", "12.345K", "1234556.78", "123456789012L"	
+	};
+	
+	public static void main(String[] args) throws ParseException {
+		NumberFormat cnf = NumberFormat.getCompactNumberInstance();
+		System.out.println("Formatting:");
+		for (Number n : nums) {
+			cnf.setParseIntegerOnly(false);
+			cnf.setMinimumFractionDigits(2);
+			System.out.println(n + ": " + cnf.format(n));
+		}
+		System.out.println("Parsing:");
+		for (String s : strs) {
+			System.out.println(s + ": " + cnf.parse(s));
+		}
+	}
+
+}
+// END main
