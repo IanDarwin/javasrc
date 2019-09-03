196286a38043a7d5418ca31db02d1ecd4d88c68e
Ian Darwin <ian@darwinsys.com>
2019-09-03 17:20:36 -0400

    add tag

--- src/main/java/functional/UnixPipesFiltersReplacement.java
+++ src/main/java/functional/UnixPipesFiltersReplacement.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 /**
  * A Unix replacement for:
  cat lines.txt | sort | uniq | wc -l
- * It even gives the same answer.
+ * It even gives the same answer :-)
  */
 public class UnixPipesFiltersReplacement {
 
 	public static void main(String[] args) throws IOException {
-
+		// tag::main[]
 		BufferedReader br = null;
 		long numberLines =
 			(br = new BufferedReader(new FileReader("lines.txt")))
@@ -22,5 +22,6 @@ public class UnixPipesFiltersReplacement {
 			.count();
 		br.close();
 		System.out.println("lines.txt" + " contains " + numberLines + " unique lines.");
+		// end::main[]
 	}
 }
