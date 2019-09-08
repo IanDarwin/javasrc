9370f870396bd7cbfb936ecd9000fd7e2cdd3895
Ian Darwin <ian@darwinsys.com>
2019-09-08 16:12:09 -0400

    Comments

--- src/main/java/datetime/EpochStuff.java
+++ src/main/java/datetime/EpochStuff.java
@@ -13,12 +13,13 @@ public class EpochStuff {
 	public static void main(String[] args) {
 		ZoneId zone = ZoneId.systemDefault();
 		Instant bot = Instant.ofEpochSecond(0L); // begin of time
-		System.out.println(bot);
+		System.out.println("Unix Epoch began " + bot);
 		Instant now = Instant.ofEpochMilli(System.currentTimeMillis());
-		System.out.println(LocalDateTime.ofInstant(now, zone));
+		System.out.println("Now it's " + LocalDateTime.ofInstant(now, zone));
+		
 		int year = LocalDate.now().getYear();
 		int epochDay = ((int)((year - 1970) * 365.25));
-		System.out.println(LocalDate.ofEpochDay(epochDay));
+		System.out.println("Start of this year = " + LocalDate.ofEpochDay(epochDay));
 	}
 
 }
