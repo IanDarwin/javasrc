87f79992b9ba7e8625178cbf32e10edb461542b1
Ian Darwin <ian@darwinsys.com>
2019-09-08 16:58:44 -0400

    Not all valid asciidoctor tags recognized by my script, adapt

--- src/main/java/oo/SingletonDemo.java
+++ src/main/java/oo/SingletonDemo.java
@@ -3,14 +3,14 @@ package oo;
 public class SingletonDemo {
 	public static void main(String[] args) {
 
-		// tag::main[enum-based]
+		// tag::enumBased[]
 		// Demonstrate the enum method:
 		EnumSingleton.INSTANCE.demoMethod();
-		// end::main[enum-based]
+		// end::enumBased[]
 
-		// tag::main[code-based]
-		// Demonstrate the code-based method:
+		// tag::codeBased[]
+		// Demonstrate the codeBased method:
 		Singleton.getInstance().demoMethod();
-		// end::main[code-based]
+		// end::codeBased[]
 	}
 }
