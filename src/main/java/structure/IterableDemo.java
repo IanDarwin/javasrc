3638a45ed035643685dbc604e3ccdf2fc228583c
Ian Darwin <ian@darwinsys.com>
2019-09-08 13:25:25 -0400

    Comments; warning police

--- src/main/java/structure/IterableDemo.java
+++ src/main/java/structure/IterableDemo.java
@@ -20,17 +20,30 @@ public class IterableDemo {
 		/** This is the Iterator that makes it all happen */
 		class DemoIterator implements Iterator<T> {
 			int i = 0;
+
+			/**
+			 * Tell if there are any more elements.
+			 * @return true if next() will succeed, false otherwise
+			 */
 			public boolean hasNext() {
 				return i < 3;
 			}
+
+			/** @return the next element from the data */
+			@SuppressWarnings("unchecked")
 			public T next() {
 				return (T)data[i++];
 			}
+
+			/** Remove the object that next() just returned.
+			 * An Iterator is not required to support this interface, and we don't.
+			 * @throws UnsupportedOperationException unconditionally
+			 */
 			public void remove() {
 				throw new UnsupportedOperationException("remove");
 			}
 		}
-		
+
 		/** Method by which the Demo class makes its iterator available */
 		public Iterator<T> iterator() {
 			return new DemoIterator();
