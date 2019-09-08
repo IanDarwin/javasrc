f60984de36bd7c708ed5bf846d8de69f252e029d
Ian Darwin <ian@darwinsys.com>
2019-09-08 16:49:33 -0400

    Unbreak tags

--- src/main/java/oo/SingletonDemo.java
+++ src/main/java/oo/SingletonDemo.java
@@ -3,14 +3,14 @@ package oo;
 public class SingletonDemo {
 	public static void main(String[] args) {
 
-		// tag name=enum-based[]
+		// tag::main[enum-based]
 		// Demonstrate the enum method:
 		EnumSingleton.INSTANCE.demoMethod();
-		// end name=enum-based[]
+		// end::main[enum-based]
 
-		// tag name=code-based[]
+		// tag::main[code-based]
 		// Demonstrate the code-based method:
 		Singleton.getInstance().demoMethod();
-		// end name=code-based[]
+		// end::main[code-based]
 	}
 }
