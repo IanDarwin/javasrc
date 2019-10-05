b0e1221c5bd0a3ab65d65c373693a32654255224
Ian Darwin <ian@darwinsys.com>
2019-10-05 15:51:03 -0400

    Warning police: kill more applet HTML, change Applets to JPanel

---
+++ src/main/java/datetimeold/CalPanel.java
@@ -0,0 +1,52 @@
+package datetimeold;
+
+import java.awt.BorderLayout;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+
+import javax.swing.JPanel;
+
+/** A JPanel to display a "Cal" calendar component */
+public class CalPanel extends JPanel {
+
+	private static final long serialVersionUID = 4524198484331822351L;
+	int yy, mm, dd;
+	Cal cal;
+	TextField yyText, mmText, ddText;
+
+	public CalPanel() {
+		setLayout(new BorderLayout());
+
+		LocalDate now = LocalDate.now();
+		yy = now.getYear();
+		mm = now.getMonthValue();
+		dd = now.getDayOfMonth();
+
+		add(cal = new Cal(yy, mm, dd), BorderLayout.NORTH);
+
+		// The action for changing yy or mm draws a new calendar
+		final ActionListener listener = e -> {
+			System.out.println("CalApplet::ActionPerformed");
+			yy = Integer.parseInt(yyText.getText());
+			mm = Integer.parseInt(mmText.getText());
+			dd = Integer.parseInt(ddText.getText());
+			
+			cal.setDate(yy, mm-1, dd);
+		};
+		add(yyText = new TextField(""+yy));
+		yyText.addActionListener(listener);
+
+		add(mmText = new TextField(""+(mm+1)));
+		mmText.addActionListener(listener);
+
+		// The action for changing the day just highlights that day.
+		add(ddText = new TextField(""+dd));
+		ddText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cal.setDayActive(Integer.parseInt(ddText.getText()));
+			}
+		});
+	}
+}
--- src/main/java/datetimeold/ClockPanel.java
+++ src/main/java/datetimeold/ClockPanel.java
@@ -1,24 +1,26 @@
 package datetimeold;
 
-import java.applet.Applet;
 import java.awt.Graphics;
-import java.text.DateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
 
-/** An Applet to display the current time */
-public class ClockApplet extends Applet implements Runnable {
+import javax.swing.JPanel;
+
+/** An JPanel to display the current time */
+public class ClockPanel extends JPanel {
 
 	private static final long serialVersionUID = 1321097174379241717L;
 	/** A Thread to run the timer */
 	protected Thread timerThread;
-	/** The date object */
-	Date date = new Date();
-	/** The date format */
-	protected final DateFormat format = DateFormat.getTimeInstance();
 
-	/* Applet Lifestyle Methods */
-	public void start() {
-		timerThread = new Thread(this, "Clock");
+	public ClockPanel() {
+		timerThread = new Thread(() -> {
+			while (timerThread != null) {
+				repaint();	// request a redraw
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e){ /* do nothing*/ }
+			}
+		});
 		timerThread.start();
 	}
 
@@ -40,7 +42,6 @@ public class ClockApplet extends Applet implements Runnable {
 
 	/** Display the time. */
 	public void paint(Graphics g) {
-		date.setTime(System.currentTimeMillis());
-		g.drawString(format.format(date), 2, 10);
+		g.drawString(LocalDateTime.now().toString(), 2, 10);
 	}
 }
