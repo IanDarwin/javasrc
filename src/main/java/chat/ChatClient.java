20d452e3a081b5166d62b8b5220f71e81f3e986b
Ian Darwin <ian@darwinsys.com>
2019-10-16 17:26:09 -0400

    Modernize code

--- src/main/java/chat/ChatClient.java
+++ src/main/java/chat/ChatClient.java
@@ -1,21 +1,20 @@
 package chat;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Font;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextArea;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /** 
  * <p>
@@ -54,13 +53,13 @@ public class ChatClient extends JFrame {
 	/** PrintWriter for sending lines on socket */
 	protected PrintWriter pw;
 	/** TextField for input */
-	protected TextField tf;
+	protected JTextField tf;
 	/** TextArea to display conversations */
-	protected TextArea ta;
-	/** The Login button */
-	protected Button loginButton;
+	protected JTextArea ta;
+	/** The Login Button */
+	protected JButton loginButton;
 	/** The LogOUT button */
-	protected Button logoutButton;
+	protected JButton logoutButton;
 	/** The TitleBar title */
 	final static String TITLE = "ChatClient: Ian Darwin's Toy Chat Room Client";
 
@@ -72,47 +71,41 @@ public class ChatClient extends JFrame {
 		port = PORTNUM;
 		
 		// The GUI
-		ta = new TextArea(14, 80);
+		ta = new JTextArea(14, 80);
 		ta.setEditable(false);		// readonly
 		ta.setFont(new Font("Monospaced", Font.PLAIN, 11));
 		cp.add(BorderLayout.NORTH, ta);
 
-		Panel p = new Panel();
+		JPanel p = new JPanel();
 
 		// The login button
-		p.add(loginButton = new Button("Login"));
+		p.add(loginButton = new JButton("Login"));
 		loginButton.setEnabled(true);
 		loginButton.requestFocus();
-		loginButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		loginButton.addActionListener(e -> {
 				login();
 				loginButton.setEnabled(false);
 				logoutButton.setEnabled(true);
 				tf.requestFocus();	// set keyboard focus in right place!
-			}
 		});
 
 		// The logout button
-		p.add(logoutButton = new Button("Logout"));
+		p.add(logoutButton = new JButton("Logout"));
 		logoutButton.setEnabled(false);
-		logoutButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		logoutButton.addActionListener(e -> {
 				logout();
 				loginButton.setEnabled(true);
 				logoutButton.setEnabled(false);
 				loginButton.requestFocus();
-			}
 		});
 
-		p.add(new Label("Message here:"));
-		tf = new TextField(40);
-		tf.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		p.add(new JLabel("Message here:"));
+		tf = new JTextField(40);
+		tf.addActionListener(e -> {
 				if (loggedIn) {
 					pw.println(ChatProtocol.CMD_BCAST+tf.getText());
 					tf.setText(""); 
 				}
-			}
 		});
 		p.add(tf);
 
@@ -141,9 +134,9 @@ public class ChatClient extends JFrame {
 			loggedIn = true;
 
 		} catch(IOException e) {
-			showStatus("Can't get socket to " + 
+			warn("Can't get socket to " + 
 				serverHost + "/" + port + ": " + e);
-			cp.add(new Label("Can't get socket: " + e));
+			cp.add(new JLabel("Can't get socket: " + e));
 			return;
 		}
 
@@ -178,6 +171,9 @@ public class ChatClient extends JFrame {
 
 	public void showStatus(String message) {
 		System.out.println(message);
+	}
+	private void warn(String message) {
+		JOptionPane.showMessageDialog(this, message);
 	}
 
 	/** A main method to allow the client to be run as an Application */
