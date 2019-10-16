4f638efa7586d144d3a44773157f0fad5a0add39
Ian Darwin <ian@darwinsys.com>
2019-10-16 19:22:27 -0400

    A separate REST demo project

---
+++ restdemo/src/main/java/rest/RestApplication.java
@@ -0,0 +1,27 @@
+package rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+
+	public RestApplication() {
+		System.out.println("RestApplication.RestApplication()");
+	}
+
+	/** 
+	 * Optional efficiency tweak - only scan for annotations in
+	 * classes in this Set.
+	 */
+	@Override
+	public Set<Class<?>> getClasses() {
+		System.out.println("RestApplication.getClasses()");
+		Set<Class<?>> cl = new HashSet<>();
+		cl.add(RestService.class);
+		return cl;
+	}
+}
---
+++ restdemo/src/main/java/rest/RestService.java
@@ -0,0 +1,49 @@
+package rest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Simple REST Service
+ */
+@Path("")
+@ApplicationScoped
+public class RestService {
+	
+	public RestService() {
+		System.out.println("RestService.init()");
+	}
+
+	@GET @Path("/timestamp")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getDate() {
+		return LocalDateTime.now().toString();
+	}
+
+	/** A Hello message method
+	 */
+	@GET @Path("/greeting/{userName}")
+	@Produces("text/html")
+	public String doGreeting(@PathParam("userName")String userName) {
+		System.out.println("RestService.greeting()");
+		if (userName == null || userName.trim().length() <= 3) {
+			return "Missing or too-short username";
+		}
+		return String.format("<h1>Welcome %s</h1><p>%s, We are glad to see you back!",
+			userName, userName);
+	}
+	
+	/** Used to download all items */
+	@GET @Path("/names")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> findTasksForUser() {
+		return List.of("Robin", "Jedunkat", "Lyn", "Glen");
+	}
+}
