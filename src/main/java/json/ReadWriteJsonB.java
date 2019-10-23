192c32ff5dc4dd01068f282e70dbcc538a7a0fbb
Ian Darwin <ian@darwinsys.com>
2019-10-23 14:48:45 -0400

    + Simple JSON-B example

---
+++ src/main/java/json/ReadWriteJsonB.java
@@ -0,0 +1,26 @@
+package json;
+
+import java.io.IOException;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import domain.Person;
+
+// tag::main[]
+public class ReadWriteJsonB {
+
+    public static void main(String[] args) throws IOException {
+
+		Jsonb jsonb = JsonbBuilder.create();			// <1>
+
+		// Read
+        String jsonInput =								// <2>
+        		"{\"id\":0,\"firstName\":\"Robin\",\"lastName\":\"Williams\"}";
+        Person rw = jsonb.fromJson(jsonInput, Person.class);
+		System.out.println(rw);
+
+		String result = jsonb.toJson(rw);				// <3>
+		System.out.println(result);
+    }
+}
