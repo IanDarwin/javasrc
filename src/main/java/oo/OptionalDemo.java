151c5ff1cc2f30dd30673a800e84f722dcf28081
Ian Darwin <ian@darwinsys.com>
2019-11-19 11:52:27 -0500

    Add Optional demos

---
+++ src/main/java/oo/OptionalDemo.java
@@ -0,0 +1,49 @@
+package oo;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.NoSuchElementException;
+
+public class OptionalDemo {
+
+	public static void main(String[] args) {
+		// tag::items[]
+		List.of(
+			new Item("Item 1", LocalDate.now().plusDays(7)),
+			new Item("Item 2")).
+				forEach(System.out::println);
+		// end::items[]
+
+		Optional<String> opt = Optional.of("What a day!");
+		// opt = Optional.empty();
+		if (opt.isPresent()) {
+			System.out.println("Value is " + opt.get());
+		} else {
+			System.out.println("Value is not present.");
+		}
+		// Shorter way:
+		System.out.println("Value is " + opt.orElse("not present"));
+	}
+
+	// tag::items[]
+	static class Item {
+		String name;
+		Optional<LocalDate> dueDate;
+		Item(String name) {
+			this(name, null);
+		}
+		Item(String name, LocalDate dueDate) {
+			this.name = name;
+			this.dueDate = Optional.ofNullable(dueDate);
+		}
+
+		public String toString() {
+			return String.format("%s %s", name,
+				dueDate.isPresent() ?
+					"Item is due on " + dueDate.get() :
+					"Sorry, do not know when item is due");
+		}
+	}
+	// end::items[]
+}
---
+++ src/main/java/oo/OptionalMethods.java
@@ -0,0 +1,37 @@
+package oo;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.NoSuchElementException;
+
+public class OptionalMethods {
+
+	/** Explicit demos of some of the methods */
+	public static void main(String[] args) {
+		final Optional<String> e = Optional.empty();
+		final Optional<String> f = Optional.of("Hello");
+
+		show("f.get()", f.get());
+		shouldThrow("e.get()", ()->e.get(), NoSuchElementException.class);
+	}
+
+	static void show(String mesg, String gotten) {
+		System.out.println(mesg + "==>" + gotten);
+	}
+
+	static void shouldThrow(String mesg, Runnable method,
+		Class<? extends Throwable> cl) {
+
+		try {
+			method.run();
+			System.out.printf("Eek: %s did not throw anything!\n", mesg);
+		} catch (Throwable t) {
+			if (t.getClass() == cl) {
+				System.out.println(mesg + " did throw " + cl.getSimpleName());
+			} else {
+				System.out.printf("Ugh! %s threw %s, not %s\n",
+					mesg, t.getClass().getName(), cl.getSimpleName());
+			}
+		}
+	}
+}
