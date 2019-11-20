49507a5e6cbd0fd0e3da58fc8b344e9f631d0f9d
Ian Darwin <ian@darwinsys.com>
2019-11-20 09:12:46 -0500

    Java->Python tweaks

--- src/main/java/otherlang/PythonFromJava.java
+++ src/main/java/otherlang/PythonFromJava.java
@@ -4,31 +4,42 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.script.Bindings;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 // tag::main[]
-/** Demonstrates calling from Java into Python using javax.script */
+/**
+ * Scripting demo using Python (jython) to get a Java variable, print, and change it.
+ * @author Ian Darwin
+ */
 public class PythonFromJava {
 	private static final String PY_SCRIPTNAME = "pythonfromjava.py";
 
 	public static void main(String[] args) throws Exception {
 		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
 		
-		scriptEngineManager.getEngineFactories().forEach(factory ->
-			System.out.println(factory.getLanguageName()));
-		
 		ScriptEngine engine = scriptEngineManager.getEngineByName("python");
 		if (engine == null) {
-			throw new IllegalStateException(
-			"Could not find 'python' engine; add jython-xxx.jar to CLASSPATH");	
+			final String message = "Could not find 'python' engine; add jython-standalone-xxx.jar to CLASSPATH";
+			System.out.println(message);
+			System.out.println("Available script engines are: ");
+			scriptEngineManager.getEngineFactories().forEach(factory ->
+				System.out.println(factory.getLanguageName()));
+			throw new IllegalStateException(message);	
 		}
-		InputStream is =
-			PythonFromJava.class.getResourceAsStream("/" + PY_SCRIPTNAME);
+
+		final Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+		bindings.put("meaning", 42);
+		
+		// Let's run a python script stored on disk (well, on classpath):
+		InputStream is = PythonFromJava.class.getResourceAsStream("/" + PY_SCRIPTNAME);
 		if (is == null) {
 			throw new IOException("Could not find file " + PY_SCRIPTNAME);
 		}
 		engine.eval(new InputStreamReader(is));
+		System.out.println("Java: Meaning is now " + bindings.get("meaning"));
 	}
 }
 // end::main[]
--- src/main/resources/pythonfromjava.py
+++ src/main/resources/pythonfromjava.py
@@ -1,1 +1,4 @@
 print('Hello from python')
+global meaning
+print('Python: Meaning is ' + str(meaning))
+meaning = 'Good morning'
