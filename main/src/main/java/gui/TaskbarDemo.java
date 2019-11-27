9d0da836cc0825c5a00c86dd506361c616c01985
Ian Darwin <ian@darwinsys.com>
2019-11-27 18:27:14 -0500

    Taskbar interface demo, may or may not work for you.

---
+++ main/src/main/java/gui/TaskbarDemo.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+/**
+ * Explorations with the Java 9+ AWT "Taskbar" interface.
+ */
+public class TaskbarDemo {
+	public static void main(String[] args) throws Exception {
+		if (Taskbar.isTaskbarSupported()) {
+			Taskbar t = Taskbar.getTaskbar();
+			if (t.isSupported(Taskbar.Feature.ICON_BADGE_TEXT)) {
+				t.setIconBadge("Howdy!");
+			} else {
+				System.out.println("Badge not supported");
+			}
+		} else {
+			System.out.println("Taskbar not supported");
+		}
+		System.out.println("Now for some shut-eye.");
+		Thread.sleep(5 * 1000);
+	}
+}
