8af5a5a2c1e6e0017c39c226246cc841280d784c
Ian Darwin <ian@darwinsys.com>
2019-12-03 15:43:34 -0500

    More string char-iteration

--- main/src/main/java/strings/ForEachChar.java
+++ main/src/main/java/strings/ForEachChar.java
@@ -3,13 +3,20 @@ package strings;
 // tag::main[]
 public class ForEachChar {
 	public static void main(String[] args) {
-		String s = "Hello world";
-		// for (char ch : s) {		 // Does not compile
+		String mesg = "Hello world";
+
+		// Does not compile, Strings are not iterable
+		// for (char ch : mesg) {
 		//		System.out.println(ch);
 		// 
-		for (char ch : s.toCharArray()) {
+
+		System.out.println("Using toCharArray:");
+		for (char ch : mesg.toCharArray()) {
 			System.out.println(ch);
 		}
+
+		System.out.println("Using Streams:");
+		mesg.chars().forEach(c -> System.out.println((char)c));
 	}
 }
 // end::main[]
