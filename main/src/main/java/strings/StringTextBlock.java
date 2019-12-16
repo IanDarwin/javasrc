77f0cfac3f8aaf9ac59edcf1f9de3321ed36e569
Ian Darwin <ian@darwinsys.com>
2019-12-16 07:42:37 -0500

    Add two Java 14 examples, with exclude comment for now

---
+++ main/src/main/java/strings/StringTextBlock.java
@@ -0,0 +1,9 @@
+public class StringTextBlock {
+	final static String longJohnStringer = """
+    This is a long
+    text String""";
+
+	public static void main(String[] args) {
+		System.out.println(longJohnStringer);
+	}
+}
