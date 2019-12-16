87c251b0c814fd9be1971e47b6f42ae27f8a9ad4
Ian Darwin <ian@darwinsys.com>
2019-12-16 14:03:06 -0500

    typo

--- main/src/main/java/strings/ForEachChar.java
+++ main/src/main/java/strings/ForEachChar.java
@@ -8,7 +8,7 @@ public class ForEachChar {
 		// Does not compile, Strings are not iterable
 		// for (char ch : mesg) {
 		//		System.out.println(ch);
-		// 
+		// }
 
 		System.out.println("Using toCharArray:");
 		for (char ch : mesg.toCharArray()) {
