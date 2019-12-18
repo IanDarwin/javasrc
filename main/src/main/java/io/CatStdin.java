6f6a49ee62d6f243a1ad3dad8a2b67c25888f5e6
Ian Darwin <ian@darwinsys.com>
2019-12-18 16:21:17 -0500

    Use try-with-resources

--- main/src/main/java/io/CatStdin.java
+++ main/src/main/java/io/CatStdin.java
@@ -9,15 +9,13 @@ import java.io.*;
 public class CatStdin {
 
     public static void main(String[] av) {
-        try {
-            BufferedReader is =
-            	new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader is =
+            	new BufferedReader(new InputStreamReader(System.in))) {
             String inputLine;
 
             while ((inputLine = is.readLine()) != null) {
                 System.out.println(inputLine);
             }
-            is.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e);
         }
