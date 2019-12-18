6fc53b84179c8836bec2d0ec5190ecc8bd7f423f
Ian Darwin <ian@darwinsys.com>
2019-12-18 09:50:11 -0500

    Add demo of Clock being settable, for now(Clock).

---
+++ main/src/main/java/datetime/TestableDateTime.java
@@ -0,0 +1,19 @@
+package datetime;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+// tag::main[]
+/**
+ * TestableDateTime allows test code to plug in a Fixed clock
+ */
+public class TestableDateTime {
+	private static Clock clock = Clock.systemDefaultZone();
+	public static void main(String[] args) {
+		System.out.println("It is now " + LocalDateTime.now(clock));
+	}
+	public static void setClock(Clock clock) {
+		TestableDateTime.clock = clock;
+	}
+}
+// end::main[]
