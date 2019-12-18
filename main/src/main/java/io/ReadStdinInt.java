f27bc3ba27926073071f58fdb747c7f3e932c2ea
Ian Darwin <ian@darwinsys.com>
2019-12-18 16:26:15 -0500

    try-with-resources and comments

--- main/src/main/java/io/ReadStdinInt.java
+++ main/src/main/java/io/ReadStdinInt.java
@@ -2,7 +2,8 @@ package io;
 
 import java.io.*;
 /**
- * Read an int from Standard Input
+ * Read an int in String format from Standard Input
+ * (to read binary data, use DataInputStream::readInt()).
  * @author	Ian F. Darwin, http://www.darwinsys.com/
  */
 // tag::main[]
@@ -10,9 +11,8 @@ public class ReadStdinInt {
 	public static void main(String[] ap) {
 		String line = null;
 		int val = 0;
-		try {
-			BufferedReader is = new BufferedReader(
-				new InputStreamReader(System.in));
+		try (BufferedReader is = 
+			new BufferedReader(new InputStreamReader(System.in))) {
 			line = is.readLine();
 			val = Integer.parseInt(line);
 			System.out.println("I read this number: " + val);
