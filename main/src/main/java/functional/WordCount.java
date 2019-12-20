1813960782f416b337df7feb3d79850dd31e0dfc
Ian Darwin <ian@darwinsys.com>
2019-12-20 11:08:35 -0500

    Fixup comments, formatting in WordCount

--- main/src/main/java/functional/WordCount.java
+++ main/src/main/java/functional/WordCount.java
@@ -5,13 +5,13 @@ import java.util.stream.*;
 
 /**
  * Implement Unix "wc(1)" aka word count, in two (long) statements
- * 1) Count words using Streams with a mutable reduction into Map<String,int>.
- * 2) Print results sorted numerically descending, limit 20
  */
 public class WordCount {
 	static String curWord="";
 	static int n=0;
 		public static void main(String[] args) throws IOException {
+
+ 		// 1) Collect words with a mutable reduction into Map<String,Integer>.
 
 		Map<String,Integer> map = Files.lines(Path.of(args[0]))
 			.map(String::toLowerCase)
@@ -24,12 +24,12 @@ public class WordCount {
 					m.put(s, m.getOrDefault(s,0)+1);n=0;
 				}, HashMap::putAll);
 
-		map
-			.keySet()
+		// 2) Print results sorted numerically descending, limit 20
+		map .keySet()
 			.stream()
 			.sorted((s1,s2) -> (map.get(s2).compareTo(map.get(s1))))
 			.map(s->String.format("%4d %s", map.get(s),s))
 			.limit(20)
 			.forEach(System.out::println);
-	}
+		}
 }
