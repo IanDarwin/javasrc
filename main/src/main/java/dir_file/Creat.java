387d5c1039067081666b68774d9328203541771c
Ian Darwin <ian@darwinsys.com>
2019-12-21 14:24:30 -0500

    File -> Files+Path

--- main/src/main/java/dir_file/Creat.java
+++ main/src/main/java/dir_file/Creat.java
@@ -2,13 +2,12 @@ package dir_file;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-/**
- * Create one or more files by name.
- * The final "e" is omitted in homage to the underlying UNIX system call.
- * @author Ian F. Darwin, http://www.darwinsys.com/
- */
 // tag::main[]
+/** Create one or more files by name.
+ ** The final "e" is omitted in homage to the underlying UNIX system call. */
 public class Creat {
 	public static void main(String[] argv) throws IOException {
 
@@ -18,10 +17,10 @@ public class Creat {
 			System.exit(1);
 		}
 
-		for (String a : argv) {
-			// Constructing a File object doesn't affect the disk, but
+		for (String arg : argv) {
+			// Constructing a Path object doesn't affect the disk, but
 			// the createNewFile() method does.
-			new File(a).createNewFile();
+			final Path created = Files.createFile(Path.of(arg));
 		}
 	}
 }
--- main/src/main/java/dir_file/FileStatus.java
+++ main/src/main/java/dir_file/FileStatus.java
@@ -1,6 +1,8 @@
 package dir_file;
 
 import java.io.File;
+import java.nio.file.*;
+import java.nio.file.attribute.FileTime;
 import java.io.IOException;
 import java.util.Date;
 
@@ -25,7 +27,54 @@ public class FileStatus {
 	public static void status(String fileName) throws IOException {
 		System.out.println("---" + fileName + "---");
 
-		// Construct a File object for the given file.
+		// Construct a Path object for the given file.
+		Path p = Path.of(fileName);
+
+		// See if it actually exists
+		if (!Files.exists(p)) {
+			System.out.println("file not found");
+			System.out.println();	// Blank line
+			return;
+		}
+		// Print full name
+		System.out.println("Canonical name " + p.normalize());
+		// Print parent directory if possible
+		Path parent = p.getParent();
+		if (parent != null) {
+			System.out.println("Parent directory: " + parent);
+		}
+		// Check if the file is readable
+		if (Files.isReadable(p)) {
+			System.out.println(fileName + " is readable.");
+		}
+		// Check if the file is writable
+		if (Files.isWritable(p)) {
+			System.out.println(fileName + " is writable.");
+		}
+
+		// See if file, directory, or other. If file, print size.
+		if (Files.isRegularFile(p)) {
+			// Report on the file's size and possibly its type
+			System.out.printf("File size is %d bytes, content type %s\n",
+					Files.size(p),
+					Files.probeContentType(p));
+		} else if (Files.isDirectory(p)) {
+			System.out.println("It's a directory");
+		} else {
+			System.out.println("I dunno! Neither a file nor a directory!");
+		}
+
+		// Report on the modification time.
+		final FileTime d = Files.getLastModifiedTime(p);
+		System.out.println("Last modified " + d);
+
+		System.out.println();	// blank line between entries
+	}
+	
+	public static void statusLegacy(String fileName) throws IOException {
+		System.out.println("---" + fileName + "---");
+
+		// Construct a Path object for the given file.
 		File f = new File(fileName);
 
 		// See if it actually exists
