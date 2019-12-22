a10508659d04ac6936c6a80c0b2d412a3ef80982
Ian Darwin <ian@darwinsys.com>
2019-12-22 16:09:00 -0500

    Egad: use Executor, not Thread.start()

--- main/src/main/java/nio/FileWatchServiceDemo.java
+++ main/src/main/java/nio/FileWatchServiceDemo.java
@@ -11,6 +11,8 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Demonstrate the JavaSE NIO WatchService.
@@ -21,8 +23,8 @@ public class FileWatchServiceDemo {
 	final static String TEMP_DIR_PATH = "/tmp";
 	static final String FILE_SEMA_FOR = "MyFileSema.for";
 	final static Path SEMAPHORE_PATH = Path.of(TEMP_DIR_PATH ,FILE_SEMA_FOR);
-	static Thread mainRunner;
 	static volatile boolean done = false;
+	final static ExecutorService threadPool = Executors.newSingleThreadExecutor();
 
 	public static void main(String[] args) throws Throwable {
 		String tempDirPath = "/tmp";
@@ -33,8 +35,7 @@ public class FileWatchServiceDemo {
 			FileSystems.getDefault().newWatchService();
 		Kind<?>[] watchKinds = { ENTRY_CREATE, ENTRY_MODIFY };
 		p.register(watcher, watchKinds);
-		mainRunner = Thread.currentThread();
-		new Thread(new DemoService()).start();
+		threadPool.submit(new DemoService());
 		while (!done) {
 			WatchKey key = watcher.take();
 			for (WatchEvent<?> e : key.pollEvents()) {
@@ -47,7 +48,7 @@ public class FileWatchServiceDemo {
 				}
 			}
 			if (!key.reset()) {
-				System.err.println("Key failed to reset!");
+				System.err.println("WatchKey failed to reset!");
 			}
 		}
 	}
@@ -56,17 +57,15 @@ public class FileWatchServiceDemo {
 	 * Nested class whose only job is to wait a while, create a file in
 	 * the monitored directory, and then go away.
 	 */
-	static class DemoService implements Runnable {
+	private final static class DemoService implements Runnable {
 		public void run() {
 			try {
 				Thread.sleep(1000);
-				System.out.println("Creating file");
+				System.out.println("DemoService: Creating file");
 				Files.deleteIfExists(SEMAPHORE_PATH); // clean up from previous run
 				Files.createFile(SEMAPHORE_PATH);
 				Thread.sleep(1000);
-				System.out.println("Stopping DemoService");
-				Thread.sleep(1500);
-				//mainRunner.interrupt();
+				System.out.println("DemoService: Shutting down");
 			} catch (Exception e) {
 				System.out.println("Caught UNEXPECTED " + e);
 			}
