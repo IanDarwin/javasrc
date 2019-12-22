4923ae5e37bd0b7880accb666de9edbbec01f0e5
Ian Darwin <ian@darwinsys.com>
2019-12-21 19:46:44 -0500

    Code cleanup, no functional change

--- main/src/main/java/nio/FileWatchServiceDemo.java
+++ main/src/main/java/nio/FileWatchServiceDemo.java
@@ -3,8 +3,8 @@ package nio;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
-import java.io.File;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
@@ -12,22 +12,22 @@ import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
-/** Demonstrate the JavaSE 7+ NIO WatchService.
- * 
- * P L E A S E   R E A D   B E F O R E   C O M P L A I N I N G
- * This class absolutely requires Java SE 7+, so just add an exclusion rule
- * (Build Path -> Exclude) if you are living with a legacy version of Java SE.
+/**
+ * Demonstrate the JavaSE NIO WatchService.
  */
 // tag::main[]
 public class FileWatchServiceDemo {
 
-	final static String tempDirPath = "/tmp";
+	final static String TEMP_DIR_PATH = "/tmp";
+	static final String FILE_SEMA_FOR = "MyFileSema.for";
+	final static Path SEMAPHORE_PATH = Path.of(TEMP_DIR_PATH ,FILE_SEMA_FOR);
 	static Thread mainRunner;
 	static volatile boolean done = false;
 
 	public static void main(String[] args) throws Throwable {
 		String tempDirPath = "/tmp";
 		System.out.println("Starting watcher for " + tempDirPath);
+		System.out.println("Semaphore file is " + SEMAPHORE_PATH);
 		Path p = Paths.get(tempDirPath);
 		WatchService watcher = 
 			FileSystems.getDefault().newWatchService();
@@ -41,7 +41,7 @@ public class FileWatchServiceDemo {
 				System.out.println(
 					"Saw event " + e.kind() + " on " + 
 					e.context());
-				if (e.context().toString().equals("MyFileSema.for")) {
+				if (e.context().toString().equals(FILE_SEMA_FOR)) {
 					System.out.println("Semaphore found, shutting down watcher");
 					done = true;
 				}
@@ -52,17 +52,21 @@ public class FileWatchServiceDemo {
 		}
 	}
 
+	/**
+	 * Nested class whose only job is to wait a while, create a file in
+	 * the monitored directory, and then go away.
+	 */
 	static class DemoService implements Runnable {
 		public void run() {
 			try {
 				Thread.sleep(1000);
 				System.out.println("Creating file");
-				new File(tempDirPath + "/MyFileSema.for").createNewFile();
+				Files.deleteIfExists(SEMAPHORE_PATH); // clean up from previous run
+				Files.createFile(SEMAPHORE_PATH);
 				Thread.sleep(1000);
-				System.out.println("Stopping WatcherServiceDemo");
-				done = true;
+				System.out.println("Stopping DemoService");
 				Thread.sleep(1500);
-				mainRunner.interrupt();
+				//mainRunner.interrupt();
 			} catch (Exception e) {
 				System.out.println("Caught UNEXPECTED " + e);
 			}
