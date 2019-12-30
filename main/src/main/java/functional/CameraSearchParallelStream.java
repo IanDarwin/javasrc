bbee648aab15c858997363b0020b8bad5b133851
Ian Darwin <ian@darwinsys.com>
2019-12-30 15:35:29 -0500

    Show a shorter, more functional approach

--- main/src/main/java/functional/CameraSearchParallelStream.java
+++ main/src/main/java/functional/CameraSearchParallelStream.java
@@ -8,11 +8,17 @@ public class CameraSearchParallelStream {
 	
 	// tag::search[]
 	public static void main(String[] args) {
+		System.out.println("Search Results using For Loop");
 		for (Object camera : privateListOfCameras.parallelStream(). // <1>
 				filter(c -> c.isIlc() && c.getPrice() < 500).       // <2>
 				toArray()) {                                        // <3>
 			System.out.println(camera);                             // <4>
 		}
+
+		System.out.println("Search Results from shorter, more functional approach");
+		privateListOfCameras.parallelStream().
+                filter(c -> c.isIlc() && c.getPrice() < 500).
+				forEach(System.out::println);
 	}
 	// end::search[]
 }
