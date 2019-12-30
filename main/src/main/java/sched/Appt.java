c0c330d9797da90dbaef305f6e162fbe2bb1af17
Ian Darwin <ian@darwinsys.com>
2019-12-30 13:32:07 -0500

    Seriously rework Comparable example

--- main/src/main/java/sched/Appt.java
+++ main/src/main/java/sched/Appt.java
@@ -1,8 +1,15 @@
 package sched;
 
-import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.StringTokenizer;
 
-/** Simple Appointment object, to represent a year-month-day-hour-minute.
+import javax.validation.constraints.NotNull;
+
+/** 
+ * Simple Appointment object, to represent a year-month-day-hour-minute.
+ * Classes are semi-immutable: main fields immutable, repetition settable.
  * @author Ian Darwin
  */
 public class Appt implements Comparable<Appt> {
@@ -11,22 +18,16 @@ public class Appt implements Comparable<Appt> {
     //    MAIN CLASS VARIABLES -- APPOINTMENT
     //-----------------------------------------------------------------
     /** What we have to do at this time. */
-    String text;    
-    /** The year (Gregorian calendar) when the appointment is */
-    int year;
-    /** The month (0-origin) */
-    int month;
-    /** The day */
-    int day;
-    /** The hour */
-    int hour;
-    /** The minute */
-    int minute;
+	@NotNull
+    final String text;    
+    /** The day when the appointment is */
+	@NotNull
+    final LocalDate date;
+    /** The time of day - optional for TODO-type items */
+    final LocalTime time;
     //-----------------------------------------------------------------
     //    CONSTANTS FOR REPETITIONS
     //-----------------------------------------------------------------
-    /** A constant for fields that do not take part in a comparison. */
-    public static final int N_A = -1;
     /** The repetition type for no repetition */
     public static final int NONE = 0;
     /** The repetition type for no repetition */
@@ -53,22 +54,27 @@ public class Appt implements Comparable<Appt> {
     protected int r_interval = NONE;
     /** The number of times to repeat this event */
     protected int r_count = NONE;
-    /** The Calendar object used for date calculations. */
-    protected static GregorianCalendar gc;
 
     //-----------------------------------------------------------------
     //    METHODS - CONSTRUCTOR(S)
     //-----------------------------------------------------------------
     /** Construct an Appointment. */
-    public Appt(String text, int y, int mo, int d, int h, int min) {
+    public Appt(int y, int mo, int d, int h, int min, String text) {
         this.text = text;
-        year = y;
-        month  = mo;
-        day  = d;
-        hour = h;
-        minute = min;
-        if (gc == null)
-            gc = new GregorianCalendar();
+        date = LocalDate.of(y, mo, d);
+        time = LocalTime.of(h, min);
+    }
+    
+    public Appt(LocalDateTime datetime, String text) {
+    	this.text = text;
+    	this.date = datetime.toLocalDate();
+    	this.time = datetime.toLocalTime();
+    }
+    
+    public Appt(LocalDate date, LocalTime time, String text) {
+    	this.text = text;
+    	this.date = date;
+    	this.time = time;
     }
 
     //-----------------------------------------------------------------
@@ -84,35 +90,22 @@ public class Appt implements Comparable<Appt> {
      */
     public boolean matches(int y, int m, int d) {
         // Do the simple case first!
-        if (year == y && month == m && day == d)
+		LocalDate dt = LocalDate.of(y, m, d);
+        if (date.equals(dt))
             return true;
         // If NOT today AND no repetition, not interesting.
         if (r_count == NONE)
             return false;
 
         // Else potentially interesting!
+		LocalDate newDay = date;
 
-        // System.out.println("ME:"+year+","+month+","+day);
-        // System.out.println("YE:"+y   +","+m    +","+d  );
-
-        // using our GregorianCalendar for calculations from here on... 
-        gc.set(Calendar.YEAR,  year);
-        gc.set(Calendar.MONTH, month-1);
-        gc.set(Calendar.DAY_OF_MONTH, day);
-        gc.set(Calendar.HOUR,  hour);
-        gc.set(Calendar.MINUTE, minute);
-
-        System.out.println(gc.getTime().toString());
-
-        for (int i=0; i<r_count && 
-            gc.get(Calendar.YEAR)<=year && 
-            gc.get(Calendar.MONTH)<=month && 
-            gc.get(Calendar.DAY_OF_MONTH)<=day; i++) {
+        for (int i=0; i<r_count && date.compareTo(dt) < 0; i++) {
             switch(r_type) {
             case HOURLY:
                 break;
             case DAILY:
-                gc.add(Calendar.DAY_OF_MONTH, r_interval);
+                newDay = date.plusDays(r_interval);
                 break;
             case WEEKLY:
                 break;
@@ -126,11 +119,9 @@ public class Appt implements Comparable<Appt> {
 
             // OK, we did the increment. Now see if it
             // matches the date we're looking for.
-            if (gc.get(Calendar.YEAR) == y &&
-                gc.get(Calendar.MONTH) == m &&
-                gc.get(Calendar.DAY_OF_MONTH) == d)
+            if (newDay.equals(date))
                 return true;
-        } 
+        }
 
         // We got out of the loop without finding a match, so...
         return false;
@@ -144,71 +135,82 @@ public class Appt implements Comparable<Appt> {
     //-----------------------------------------------------------------
     /** compareTo method, from Comparable interface.
      * Compare this Appointment against another, for purposes of sorting.
-     * <P>Only text, and date and time participate, not repetition!
+     * <P>Only date and time, then text, participate, not repetition!
      * (Repetition has to do with recurring events, e.g., 
 	 *  "Meeting every Tuesday at 9").
      * This methods is consistent with equals().
      * @return -1 if this<a2, +1 if this>a2, else 0.
      */
     @Override
-    public int compareTo(Appt a2) {
-        if (year < a2.year)
-            return -1;
-        if (year > a2.year)
-            return +1;
-        if (month < a2.month)
-            return -1;
-        if (month > a2.month)
-            return +1;
-        if (day < a2.day)
-            return -1;
-        if (day > a2.day)
-            return +1;
-        if (hour < a2.hour)
-            return -1;
-        if (hour > a2.hour)
-            return +1;
-        if (minute < a2.minute)
-            return -1;
-        if (minute > a2.minute)
-            return +1;
-        return text.compareTo(a2.text);
-    }
+	public int compareTo(Appt a2) {
+		// If dates not same, trigger on their comparison
+		int dateComp = date.compareTo(a2.date);
+		if (dateComp != 0)
+			return dateComp;
+		// Same date. If times not same, trigger on their comparison
+		if (time != null && a2.time != null) {
+			// Neither time is null
+			int timeComp = time.compareTo(a2.time);
+			if (timeComp != 0)
+				return timeComp;
+		} else /* At least one time is null */ {
+			if (time == null && a2.time != null) {
+				return -1; // All-day appts sort low to appear first
+			} else if (time != null && a2.time == null)
+				return +1;
+				// else both have no time set, so carry on,
+		}
+		// Same date & time, trigger on text
+		return text.compareTo(a2.text);
+	}
 
-    /** Compare this appointment against another, for equality.
-     * Consistent with compareTo(). For this reason, only
-     * text, date & time participate, not repetition.
-     * @returns true if the objects are equal, false if not.
-     */
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + ((time == null) ? 0 : time.hashCode());
+		return result;
+	}
+
     @Override
     public boolean equals(Object o2) {
+    	if (this == o2)
+    		return true;
+		if (o2.getClass() != Appt.class)
+			return false;
         Appt a2 = (Appt) o2;
-        if (year != a2.year ||
-            month != a2.month ||
-            day != a2.day ||
-            hour != a2.hour ||
-            minute != a2.minute)
+        if (!date.equals(a2.date))
+            return false;
+        if (time != null && !time.equals(a2.time))
             return false;
         return text.equals(a2.text);
     }
-    // end::main[]
 
     /** Return a String representation of this Appt.
      * Output is intended for debugging, not presentation!
      */
     @Override
     public String toString() {
-        return new StringBuilder().append(year).append(' ').
-            append(month).append(' ').append(day).append(' ').
-            append(hour).append(' ').append(minute).append(' ').
-            append(text).toString();
+        var sb = new StringBuilder();
+        sb.append(date).append(' ');
+        if (time != null) {
+            sb.append(time.getHour()).append(':').append(time.getMinute()).append(' ');
+        } else {
+        	sb.append("(All day)").append(' ');
+        }
+        sb.append(text).toString();
+        return sb.toString();
     }
 
     /** Factory: build a String representation into an Appt.
      * Cast as a static factory for minor efficiency gains - reconsider.
      */
     public static Appt fromString(String s) {
-        StringTokenizer st = new StringTokenizer(s);
+    	System.out.println("Appt.fromString(): " + s);
+    	// Tokenize, after stripping '-' and ':'.
+        StringTokenizer st = new StringTokenizer(s.replaceAll("[-:]", " "));
         if (st.countTokens() < 6) throw new
             IllegalArgumentException("Too few fields in " + s);
         int y = Integer.parseInt(st.nextToken());
@@ -222,23 +224,23 @@ public class Appt implements Comparable<Appt> {
             if (st./*still*/hasMoreElements())
                 sb.append(' ');
         }
-        return new Appt(sb.toString(), y, m, d, h, i);
+        return new Appt(y, m, d, h, i, sb.toString());
     }
 
     public int getDay() {
-        return day;
+        return date.getDayOfMonth();
     }
 
     public int getHour() {
-        return hour;
+        return time.getHour();
     }
 
     public int getMinute() {
-        return minute;
+        return time.getMinute();
     }
 
     public int getMonth() {
-        return month;
+        return date.getMonthValue();
     }
     
     public String getText() {
@@ -246,7 +248,7 @@ public class Appt implements Comparable<Appt> {
     }
     
     public int getYear() {
-        return year;
+        return date.getYear();
     }
     
 }
