1a96738445bd2362ea4ab60ee5b943093f0bc49a
Ian Darwin <ian@darwinsys.com>
2019-12-30 15:53:37 -0500

    array -> Collection, for Streams/forEach.

--- main/src/main/java/functional/ReferencesDemo4.java
+++ main/src/main/java/functional/ReferencesDemo4.java
@@ -1,32 +1,34 @@
 package functional;
 
-// tag::main[]
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class ReferencesDemo4 {
 
-	static final String[] unsortedNames = { 
-		"Gosling", "de Raadt", "Torvalds", "Ritchie", "Hopper"
-	};
+	static final List<String> unsortedNames = List.of(
+		"Gosling", "de Raadt", "Amdahl", "Turing", "Ritchie", "Hopper"
+	);
 	
 	public static void main(String[] args) {
-		String[] names;
+		List<String> names;
 		
 		// Sort using 
 		// "an Instance Method of an Arbitrary Object of a Particular Type"
-		names = unsortedNames.clone();
-		Arrays.sort(names, String::compareToIgnoreCase);                    // <1>
+		names = new ArrayList<>(unsortedNames);
+		Collections.sort(names, String::compareToIgnoreCase);   // <1>
 		dump(names);
 
 		// Equivalent Lambda:
-		names = unsortedNames.clone();
-		Arrays.sort(names, (str1, str2) -> str1.compareToIgnoreCase(str2)); // <2>
+		names = new ArrayList<>(unsortedNames);
+		Collections.sort(names, 
+			(str1, str2) -> str1.compareToIgnoreCase(str2));    // <2>
 		dump(names);
 		
 		// Equivalent old way:
-		names = unsortedNames.clone();
-		Arrays.sort(names, new Comparator<String>() {                       // <3>
+		names = new ArrayList<>(unsortedNames);
+		Collections.sort(names, new Comparator<String>() {      // <3>
 			@Override
 			public int compare(String str1, String str2) {
 				return str1.compareToIgnoreCase(str2);
@@ -35,18 +37,15 @@ public class ReferencesDemo4 {
 		dump(names);
 		
 		// Simpest way, using existing comparator
-		names = unsortedNames.clone();
-		Arrays.sort(names, String.CASE_INSENSITIVE_ORDER);                  // <4>
+		names = new ArrayList<>(unsortedNames);
+		Collections.sort(names, String.CASE_INSENSITIVE_ORDER); // <4>
 		dump(names);
 	}
 	// end::main[]
 
 	/** Simple dumper just to show the order of the names in the array */
-	private static void dump(String[] names) {
-		for (String s : names) {
-			System.out.print(s);
-			System.out.print(' ');
-		}
+	private static void dump(List<String> names) {
+		System.out.print(String.join(", ", names));
 		System.out.println();
 	}
 
