4467fee7a2254dc48c9e0fa3af4a46d7730fb888
Ian Darwin <ian@darwinsys.com>
2020-01-01 21:52:12 -0500

    Rename to fix typo in class name

--- main/src/main/java/io/FilesBooleanInfos.java
+++ main/src/main/java/io/FilesBooleanInfos.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class FileBooleanInfos {
+public class FilesBooleanInfos {
 	public static void main(String[] args) throws IOException {
 
 		// tag::main[]
