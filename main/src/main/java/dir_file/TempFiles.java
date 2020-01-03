8ddbddf2272f3e948f2eaf4ff36854c4a939e36f
Ian Darwin <ian@darwinsys.com>
2020-01-03 14:31:29 -0500

    File->Files. Void where prohibited by incompleteness.

--- main/src/main/java/dir_file/TempFiles.java
+++ main/src/main/java/dir_file/TempFiles.java
@@ -1,6 +1,9 @@
 package dir_file;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Work with temporary files in Java.
@@ -10,8 +13,7 @@ import java.io.*;
 public class TempFiles {
 	public static void main(String[] argv) throws IOException {
 
-		// 1. Make an existing file temporary
-
+		// 1. Making an existing file temporary
 		// Construct a File object for the backup created by editing
 		// this source file. The file probably already exists.
 		// My editor creates backups by putting ~ at the end of the name.
@@ -22,18 +24,19 @@ public class TempFiles {
 		// 2. Create a new temporary file.
 
 		// Make a file object for foo.tmp, in the default temp directory
-		File tmp = File.createTempFile("foo", "tmp");
+		Path tmp = Files.createTempFile("foo", "tmp");
 		// Report on the filename that it made up for us.
-		System.out.println("Your temp file is " + tmp.getCanonicalPath());
+		System.out.println("Your temp file is " + tmp.normalize());
 		// Arrange for it to be deleted at exit.
-		tmp.deleteOnExit();
+		tmp.toFile().deleteOnExit();
 		// Now do something with the temporary file, without having to
 		// worry about deleting it later.
-		writeDataInTemp(tmp.getCanonicalPath());
+		writeDataInTemp(tmp);
 	}
 
-	public static void writeDataInTemp(String tempnam) {
+	public static void writeDataInTemp(Path tempFile) throws IOException {
 		// This version is dummy. Use your imagination.
+		Files.writeString(tempFile, "This is a temp file");
 	}
 }
 // end::main[]
