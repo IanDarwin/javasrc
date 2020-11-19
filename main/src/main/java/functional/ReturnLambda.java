ea700f4336a6fb7b387a8bfd238b357e84ab301a
Ian Darwin <ian@darwinsys.com>
2020-11-19 16:59:02 -0500

    Display the Lambda propertly

--- main/src/main/java/functional/ReturnLambda.java
+++ main/src/main/java/functional/ReturnLambda.java
@@ -4,9 +4,8 @@ public class ReturnLambda {
 	public Runnable getRunnable() {
 		return () -> { System.out.println("Hello"); };
 	}
+	
 	public static void main(String[] args) {
-		Runnable runnable = new ReturnLambda().getRunnable();
-		System.out.println(runnable);
-		runnable.run();
+		System.out.println("ReturnLambda.main()");
 	}
 }
