68b568f86845045b2faf1a8023f5b3683a625e64
Ian Darwin <ian@darwinsys.com>
2020-12-07 16:52:00 -0500

    Implement logic (but not UI) for reverse mode - move time up each day, not down

--- main/src/main/java/datetime/ScheduleSlider.java
+++ main/src/main/java/datetime/ScheduleSlider.java
@@ -8,18 +8,22 @@ import java.time.*;
  * XXX Needs to read a config file to get all values (including labels)
  */
 public class ScheduleSlider {
-	private static boolean FORWARD = true;
 	public static void main(String[] args) {
 		LocalDate start = LocalDate.parse("2020-12-06"); 
 		LocalDate end = LocalDate.parse("2020-12-18");
+		if (start.equals(end)) {
+			throw new IllegalArgumentException("Your days will get you nowhere!");
+		}
+		boolean reverse = end.compareTo(start) < 0;
+		final int hoursIncrement = reverse ? -1 : +1;
+		final int daysIncrement = reverse ? -1 : +1;
 		LocalTime breakfast = LocalTime.of(07,00),
 			lunch = LocalTime.of(12, 00),
 			dinner = LocalTime.of(17, 00),
 			bed = LocalTime.of(21, 30);
 		String format = "%-10s  %-10s %s  %s  %s\n";
-		final int hoursIncrement = FORWARD ? +1 : -1;
 		System.out.printf(format, "Day", "Breakfast", "Lunch", "Dinner", "Bed");
-		for (LocalDate day = start; day.compareTo(end) <= 0; day = day.plusDays(1)) {
+		for (LocalDate day = start; day.compareTo(end) <= 0; day = day.plusDays(daysIncrement)) {
 			breakfast = breakfast.plusHours(hoursIncrement);
 			lunch = lunch.plusHours(hoursIncrement);
 			dinner = dinner.plusHours(hoursIncrement);
