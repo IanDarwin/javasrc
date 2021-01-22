523a87c8e5744b6b926453cc09f7729b3885f737
Ian Darwin <ian@darwinsys.com>
2021-01-22 11:51:06 -0500

    new

---
+++ main/src/main/java/lang/VarDecls.java
@@ -0,0 +1,10 @@
+package lang;
+
+public class VarLocalsDemo {
+
+	public static void main(String[] args) {
+		var x = 12, y = Math.PI / 2, z = "Zorro";
+
+		System.out.print("x,y,z = %d, %f, %s\n", x, y, z);
+	}
+}
