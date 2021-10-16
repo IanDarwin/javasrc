faa06305e9913b82c04639f6fe1a6155522706dd
Ian Darwin <ian@darwinsys.com>
2021-10-16 09:26:35 -0400

    Comment and variable name police

--- main/src/main/java/datetime/DateDiff.java
+++ main/src/main/java/datetime/DateDiff.java
@@ -10,19 +10,19 @@ import java.time.Period;
 public class DateDiff {
 
 	public static void main(String[] args) {
-		/** The date at the end of the last century */
-		LocalDate endof20thCentury = LocalDate.of(2000, 12, 31);
+		/* The date at the end of the last century */
+		LocalDate endOf20thCentury = LocalDate.of(2000, 12, 31);
 		LocalDate now = LocalDate.now();
 		if (now.getYear() > 2100) {
 			System.out.println("The 21st century is over!");
 			return;
 		}
 		
-		Period diff = Period.between(endof20thCentury, now);
+		Period diff = Period.between(endOf20thCentury, now);
 		
 		System.out.printf("The 21st century (up to %s) is %s old%n", now, diff);
 		System.out.printf(
-				"The 21st century is %d years, %d months and %d days old",
+				"The 21st century is %d years, %d months and %d days old%n",
 				diff.getYears(), diff.getMonths(), diff.getDays());
 	}
 }
