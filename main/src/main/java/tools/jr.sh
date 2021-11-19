5ec41be6eaee6ff137250f8f4b95c6a3badda12c
Ian Darwin <ian@darwinsys.com>
2021-11-19 15:04:39 -0500

    remove unused

--- main/src/main/java/tools/jr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-darwinsys_jar="${HOME}/classes/ext/com-darwinsys-all.jar"
-tools_jar="/usr/local/jdk1.3.1-linux/lib/tools.jar"
-
-java -classpath ".:${tools_jar}:${darwinsys_jar}" com.darwinsys.tools.jr $*
