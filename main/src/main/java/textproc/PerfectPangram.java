8be4a0b8606fef714bbff8cf4f3b7dc16911a1c5
Ian Darwin <ian@darwinsys.com>
2022-01-07 10:34:39 -0500

    Some corrections (incl tests). Add pangramSentence().

--- main/src/main/java/textproc/PerfectPangram.java
+++ main/src/main/java/textproc/PerfectPangram.java
@@ -2,22 +2,39 @@ package textproc;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.Arrays;
 
 /**
  * Find perfect pangrams of the given length.
  * A pangram is a word that uses all the letters of a given alphabet,
  * a term oft used in puzzles like Spelling Bee to refer to a word
- * that uses all (7) given letters.
+ * that uses all letters in the given alphabet.
  * A perfect pangram is a pangram that uses each letter exactly once.
  */
 public class PerfectPangram {
-	private boolean perfect = true;
+	private final boolean DEBUG = false;
+	public static final String DEF_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+	public static final boolean DEFAULT_SEEK_PERFECTION = false;
+	private boolean perfect = false;
 	final char[] alphabet;
-	private final int len;
+	private final int alphabetLen;
+
+	public PerfectPangram(String alphabet, boolean perfect) {
+		this.alphabet = alphabet.toCharArray();
+		this.alphabetLen = alphabet.length();
+		this.perfect = perfect;
+	}
 
 	public PerfectPangram(String alphabet) {
-		this.alphabet = alphabet.toCharArray();
-		this.len = alphabet.length();
+		this(alphabet, DEFAULT_SEEK_PERFECTION);
+	}
+
+	public PerfectPangram(boolean perfect) {
+		this(DEF_ALPHABET, perfect);
+	}
+
+	public PerfectPangram() {
+		this(DEF_ALPHABET, DEFAULT_SEEK_PERFECTION);
 	}
 
 	/** Default main: print picked (perfect?) pangrams purloined from /usr/dict/words */
@@ -35,26 +52,36 @@ public class PerfectPangram {
 	}
 
 	public boolean pangram(String cand) {
-		if (perfect && cand.length() != len)
+		if (perfect && cand.length() != alphabetLen)
 			return false;
 		var found = new boolean[26];
 		int matchedCount = 0;
 		for (char c : cand.toCharArray()) {
 			if (!inDict(c))
 				return false;
-			if (perfect && found[c-'a'])
+			if (perfect && found[c-'a']) {
 				// no dupes in perfect pangrams
 				return false;
+			}
+			if (!found[c-'a'])
+				matchedCount++;
 			found[c-'a'] = true;
-			matchedCount++;
 		}
 		if (perfect)
-			return matchedCount == len;	// all used?
-		int used = 0;
-		for (int i = 0; i < found.length; i++)
-			if (found[i])
-				++used;
-		return matchedCount == len;
+			return matchedCount == alphabetLen;	// all used?
+		if (DEBUG) {
+			System.out.printf("Input %s, alphabet %s, matchedCount %d\n",
+					cand, new String(alphabet), matchedCount);
+			for (char t : alphabet) {
+				System.out.print(t + ": " + (found[t - 'a'] ? 'T' : 'F') + ' ');
+			}
+			System.out.println();
+		}
+		return matchedCount == alphabetLen;
+	}
+
+	public boolean pangramSentence(String cand) {
+		return pangram(cand.toLowerCase().replaceAll(" ", ""));
 	}
 
 	private boolean inDict(char c) {
--- main/src/test/java/textproc/PerfectPangramTest.java
+++ main/src/test/java/textproc/PerfectPangramTest.java
@@ -3,6 +3,8 @@ package textproc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /** 
@@ -18,26 +20,39 @@ public class PerfectPangramTest {
 	}
 	
 	@Test
-	public void testPPGood() throws Exception {
+	public void testPPGood() {
 		assertTrue(tester.pangram("acronym"));
 		assertTrue(tester.pangram("camnory"));
+	}
+
+	@Test
+	public void testPPBad() {
 		assertFalse(tester.pangram("abcdefg"));
 		assertFalse(tester.pangram("harmony"));
 		assertFalse(tester.pangram("rodwalk"));
-	}
-
-	@Test
-	public void testPPBad() throws Exception {
 		assertFalse(tester.pangram("abecadarian"));	 // too long
 		assertFalse(tester.pangram("acrimony"));	 // 'i' not in alphabet'
-		assertFalse(tester.pangram("acronom"));	 // repeated letter
 		assertFalse(tester.pangram("akron"));	 // too short
+
 	}
 
 	@Test
-	public void testLonger() {
+	public void testPerfect() {
+		tester = new PerfectPangram("camnory", true);
+		assertFalse(tester.pangram("acronom"));	 // repeated letter
+	}
+
+	@Test
+	public void testWithLongerAlphabet() {
 		tester = new PerfectPangram("abcejklmru");
 		assertTrue (tester.pangram("lumberjack"));
 		assertFalse(tester.pangram("wolframite"));
+	}
+
+	@Test
+	public void testFullAlphabet() {
+		tester = new PerfectPangram(false);
+		assertTrue(tester.pangramSentence("The quick brown foxes jumped over the lazy dog"));
+		assertTrue(tester.pangramSentence("Few may back quirky judge who rips galvanized text"));
 	}
 }
