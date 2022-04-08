9bd9fdccb4cc53bd33aabf3764a3b821101850a2
Ian Darwin <ian@darwinsys.com>
2022-04-08 11:21:21 -0400

    Reorder code

--- main/src/main/java/threads/RunnableLambda.java
+++ main/src/main/java/threads/RunnableLambda.java
@@ -5,10 +5,11 @@ import java.util.concurrent.*;
 // tag::main[]
 public class RunnableLambda {
 
+	private static ExecutorService threadPool = Executors.newSingleThreadExecutor();
+
 	public static void main(String[] args) {
 		threadPool.submit(() -> System.out.println("Hello from a thread"));
 		// end::main[]
 		threadPool.shutdown();
 	}
-	private static ExecutorService threadPool = Executors.newSingleThreadExecutor();
 }
