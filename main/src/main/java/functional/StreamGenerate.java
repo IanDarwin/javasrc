da6edacaf3ee1f336462e2c5d5b7c45a835243b2
Ian Darwin <ian@darwinsys.com>
2022-05-30 14:22:50 -0400

    Example of generating a Stream yourself.

---
+++ main/src/main/java/functional/StreamGenerate.java
@@ -0,0 +1,20 @@
+package functional;
+
+import java.util.stream.Stream;
+
+/**
+ * Generate a Stream, from your own data
+ */
+public class StreamGenerate {
+	public static void main(String[] args) {
+		// Stream.generate with a Producer<T> is all you need to create your own Stream of data.
+		Stream.generate(StreamGenerate::produceValue).limit(10).forEach(System.out::println);
+	}
+
+	static String[] data = {"Fluffy", "Peter", "Roger"};
+	static int index;
+
+	static String produceValue() {
+		return data[index++ % data.length];
+	}
+}
