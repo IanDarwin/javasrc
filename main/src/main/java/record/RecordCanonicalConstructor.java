3217a991db218b8d31724ca70578356ac67e8d08
Ian Darwin <ian@darwinsys.com>
2022-06-02 16:06:07 -0400

    commentary

--- main/src/main/java/record/RecordCanonicalConstructor.java
+++ main/src/main/java/record/RecordCanonicalConstructor.java
@@ -1,12 +1,13 @@
 package record;
 
+/**
+ * The "Canonical Constructor" (which must be public) can be used
+ * to clamp values or do other value modifications/calculations
+ * on the parameters, and is run before the default constructor.
+ * Note the syntax: public CLAZZNAME { ... } (no parens!).
+ */
 public record RecordCanonicalConstructor(int rate, double amount) {
 
-	/**
-	 * The "Canonical Constructor" (which must be public) can be used
-	 * to clamp values or do other value modifications/calculations
-	 * on the parameters, and is run before the default constructor.
-	 */
 	public RecordCanonicalConstructor {
 		if (rate < 0) {
 			rate = 0;
