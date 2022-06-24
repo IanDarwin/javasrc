39569031fd22a60fcd4dbe8137dfecaf9b86fadf
Ian Darwin <ian@darwinsys.com>
2022-06-24 15:32:11 -0400

    Show doing it the hard way and the easy way

--- main/src/main/java/functional/SimpleCollectorsGroupingBy.java
+++ main/src/main/java/functional/SimpleCollectorsGroupingBy.java
@@ -5,11 +5,14 @@ import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.*;
 
+/** Convert a list of Purchases to a map from each Customer 
+ * who has a purchase, to a List of their purchases.
+ */
 public class SimpleCollectorsGroupingBy {
 	static Customer chip = new Customer("Chip"),
 		dale = new Customer("Dale");
 	
-	static List<Purchase> purchases = List.of(
+	static List<Purchase> purchaseList = List.of(
 		new Purchase(chip, LocalDate.now(), 2.34),
 		new Purchase(dale, LocalDate.now().minusDays(1), 3.21),
 		new Purchase(chip, LocalDate.now().minusDays(2), 6.50),
@@ -17,19 +20,33 @@ public class SimpleCollectorsGroupingBy {
 	);
 		
 	public static void main(String[] args) {
-		Map<Customer, List<Purchase>> result = 
-			// Consider writing this without Streams:
-			//	create Map of customers to purchases.
-			//  for each purchase, check if customer in map, and
-			//		add(customer, new List<Purchases>) if not 
-			//    then get list from map, and add purchase into it.
-			purchases
-			.stream()
-			.collect(
-				groupingBy(Purchase::customer));
+		process(purchaseList);
+	}
+
+	static void process(List<Purchase> purchaseList) {
+		Map<Customer, List<Purchase>> map = new HashMap<>();
+
+		// Consider writing this without Java 8 mechanisms
+		//	create Map of customers to purchases.
+		//	for each purchase, check if customer in map;
+		//	if not, map.add(customer, new List<Purchases>)
+		//	then add purchase into list
+		/*
+		for (Purchase p : purchaseList) {
+			List<Purchase> thisCustPurchases = map.get(p.customer());
+			if (thisCustPurchases == null) {
+				thisCustPurchases = new ArrayList<>();
+				map.put(p.customer(), thisCustPurchases);
+			}
+			thisCustPurchases.add(p);
+		}
+		*/
+
+		// A bit shorter with Streams
+		map = purchaseList.stream().collect(groupingBy(Purchase::customer));
 
 		// Now just print the map
-		result.forEach((k,v) -> System.out.printf("Customer %s, purchases %s\n", k.name(), v));
+		map.forEach((k,v) -> System.out.printf("Customer %s, purchases %s\n", k.name(), v));
 	}
 
 	record Customer(String name) {
