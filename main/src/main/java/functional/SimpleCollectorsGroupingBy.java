e526e50d1d70407aa7e3ff9caaf5b1fea5764ef4
Ian Darwin <ian@darwinsys.com>
2022-06-24 20:58:27 -0400

    Purchase -> Transaction

--- main/src/main/java/functional/SimpleCollectorsGroupingBy.java
+++ main/src/main/java/functional/SimpleCollectorsGroupingBy.java
@@ -5,57 +5,58 @@ import java.util.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.*;
 
-/** Convert a list of Purchases to a map from each Customer 
- * who has a purchase, to a List of their purchases.
+/** Convert a List<Transaction> to a Map from each Customer 
+ * who has a Transactions, to a List of their Transactions.
  */
 public class SimpleCollectorsGroupingBy {
+
 	static Customer chip = new Customer("Chip"),
 		dale = new Customer("Dale");
 	
-	static List<Purchase> purchaseList = List.of(
-		new Purchase(chip, LocalDate.now(), 2.34),
-		new Purchase(dale, LocalDate.now().minusDays(1), 3.21),
-		new Purchase(chip, LocalDate.now().minusDays(2), 6.50),
-		new Purchase(dale, LocalDate.now().minusDays(3), 0.50)
+	static List<Transaction> transactionList = List.of(
+		new Transaction(chip, LocalDate.now(), 2.34),
+		new Transaction(dale, LocalDate.now().minusDays(1), 3.21),
+		new Transaction(chip, LocalDate.now().minusDays(2), 6.50),
+		new Transaction(dale, LocalDate.now().minusDays(3), 0.50)
 	);
 		
 	public static void main(String[] args) {
-		process(purchaseList);
+		process(transactionList);
 	}
 
-	static void process(List<Purchase> purchaseList) {
-		Map<Customer, List<Purchase>> map = new HashMap<>();
+	static void process(List<Transaction> transactionList) {
+		Map<Customer, List<Transaction>> map = new HashMap<>();
 
 		// Consider writing this without Java 8 mechanisms
-		//	create Map of customers to purchases.
-		//	for each purchase, check if customer in map;
-		//	if not, map.add(customer, new List<Purchases>)
-		//	then add purchase into list
+		//	create Map of customers to Transaction.
+		//	for each Transaction, check if customer in map;
+		//	if not, map.add(customer, new List<Transaction>)
+		//	then add Transaction into list
 		/*
-		for (Purchase p : purchaseList) {
-			List<Purchase> thisCustPurchases = map.get(p.customer());
-			if (thisCustPurchases == null) {
-				thisCustPurchases = new ArrayList<>();
-				map.put(p.customer(), thisCustPurchases);
+		for (Transaction p : transactionList) {
+			List<Transaction> thisCustomerTransactions = map.get(p.customer());
+			if (thisCustomerTransactions == null) {
+				thisCustomerTransactions = new ArrayList<>();
+				map.put(p.customer(), thisCustomerTransactions);
 			}
-			thisCustPurchases.add(p);
+			thisCustomerTransactions.add(p);
 		}
 		*/
 
 		// A bit shorter with Streams
-		map = purchaseList.stream().collect(groupingBy(Purchase::customer));
+		map = transactionList.stream().collect(groupingBy(Transaction::customer));
 
 		// Now just print the map
-		map.forEach((k,v) -> System.out.printf("Customer %s, purchases %s\n", k.name(), v));
+		map.forEach((k,v) -> System.out.printf("Customer %s, Transaction %s\n", k.name(), v));
 	}
 
 	record Customer(String name) {
 		// Nothing to add!
 	}
 
-	record Purchase(Customer customer, LocalDate when, double amount) {
+	record Transaction(Customer customer, LocalDate when, double amount) {
 		public String toString() {
-			return String.format("Purchaser %s, Date %s, purchase %.2f", customer.name(), when, amount);
+			return String.format("Customer %s, Date %s, transaction amount %.2f", customer.name(), when, amount);
 		}
 	}
 
