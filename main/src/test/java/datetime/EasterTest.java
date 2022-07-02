7094934fa4e977cc0c076cfee9827f4c59892d72
Ian Darwin <ian@darwinsys.com>
2022-07-01 21:57:12 -0400

    Add test for Easter finder

---
+++ main/src/test/java/datetime/EasterTest.java
@@ -0,0 +1,24 @@
+package datetime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class EasterTest {
+
+	@ParameterizedTest
+	@CsvSource(textBlock = """
+		1900,'1900-04-15'
+		1951,'1951-03-25'
+		1977,'1977-04-10'
+		2000,'2000-04-23'
+		2001,'2001-04-15'
+		2020,'2020-04-12'
+		""")
+	public void testConvert(int year, String expected) {
+		assertEquals(expected, Easter.findHolyDay(year).toString());
+	}
+}
