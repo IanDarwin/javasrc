630e54c5ca8dce13897e0224709952516d72c2d4
Ian Darwin <ian@darwinsys.com>
2022-07-01 21:57:56 -0400

    add comments and no-arg constr

--- main/src/main/java/record/RecordCanonicalConstructor.java
+++ main/src/main/java/record/RecordCanonicalConstructor.java
@@ -1,6 +1,7 @@
 package record;
 
 /**
+ * The "Compact Constructor" has no parameter list.
  * The "Canonical Constructor" (which must be public) can be used
  * to clamp values or do other value modifications/calculations
  * on the parameters, and is run before the default constructor.
@@ -8,13 +9,21 @@ package record;
  */
 public record RecordCanonicalConstructor(int rate, double amount) {
 
+	/** In this case the Compact Constructor - the one with no params -
+	 * inherits the parameter list and becomes the Canonical Constructor.
+	 */
 	public RecordCanonicalConstructor {
 		if (rate < 0) {
 			rate = 0;
 		}
 	}
 
+	RecordCanonicalConstructor() {
+		this(0, 0);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(new RecordCanonicalConstructor(-1, 42.0));
+		System.out.println(new RecordCanonicalConstructor());
 	}
 }
