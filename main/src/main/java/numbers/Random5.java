821cc3f77b311bfebfc4068461433a1bb27fe314
Ian Darwin <ian@darwinsys.com>
2022-12-05 09:08:45 -0500

    Update Random5 to match Random4

--- main/src/main/java/numbers/Random5.java
+++ main/src/main/java/numbers/Random5.java
@@ -17,10 +17,18 @@ import com.darwinsys.io.FileIO;
 public class Random5 {
 	// tag::main[]
 	private static final String R_SCRIPT_FILE = "/randomnesshistograms.r";
-	private static final int N = 10000;
+	private static int N = 100_000;
 
 	public static void main(String[] argv) throws Exception {
-		// java.util.Random methods are non-static, do need to construct 
+		// java.util.Random methods are non-static; we need an instance
+		if (argv.length == 1) {
+			try {
+				N = Integer.parseInt(argv[0]);
+			} catch (NumberFormatException ex) {
+				System.out.printf("Number %s invalid, using %d\n", argv[0], N);
+			}
+		}
+		System.out.println("Printing " + N + " randoms to each text file");
 		Random r = new Random();
 		double[] us = new double[N], ns = new double[N];
 		for (int i=0; i<N; i++) {
@@ -36,6 +44,7 @@ public class Random5 {
 			ScriptEngine engine = manager.getEngineByName("Renjin");
 			engine.put("us", us);
 			engine.put("ns", ns);
+			engine.put("dontread", true);
 			engine.eval(FileIO.readerToString(new InputStreamReader(is)));
 		}
 	}
