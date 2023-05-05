28c832b5f38c0952445f20f978d58c5d8df0b4c0
Ian Darwin <ian@darwinsys.com>
2023-05-05 10:36:28 -0400

    Keep both two-phase and combined WordFreq; filter empty lines

--- main/src/main/java/functional/WordFreq2.java
+++ main/src/main/java/functional/WordFreq2.java
@@ -8,26 +8,25 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // tag::main[]
+
 /**
- * Implement word frequency count; shown in to main steps
- * (WordFreq2 shows the steps combined into one statement).
+ * Implement word frequency count
+ * WordFreq shows this done in two stages
  */
-public class WordFreq {
+public class WordFreq2 {
 	public static void main(String[] args) throws IOException {
 
 		String fileName = args.length == 0 ?  "README.adoc" : args[0];
 
 		// Collect words with a mutable reduction into
-		// a Map<String,Long>
-		Map<String,Long> map = Files.lines(Path.of(fileName))
+		// a Map<String,Long>; get entrySet as a stream,
+		// sort -r, count, print highest 20.
+		Files.lines(Path.of(fileName))
 			.filter(line->!line.isEmpty())
 			.flatMap(s -> Stream.of(s.split(" ")))
 			.collect(Collectors.groupingBy(
-				String::toLowerCase, Collectors.counting()));
-
-		// Get map's entrySet as a stream,
-		// sort -r, count, print highest 20.
-		map.entrySet().stream()
+				String::toLowerCase, Collectors.counting()))
+			.entrySet().stream()
 			.sorted(Map.Entry.<String,Long>comparingByValue() 
 			.reversed())
 			.limit(20)
