4a11377ab68cda6e48a0aa9771cb3a52b0822172
Ian Darwin <ian@darwinsys.com>
2023-05-25 10:14:53 -0400

    Remove old comment about needing Java 7(!).

--- main/src/main/java/lang/SwitchOnString.java
+++ main/src/main/java/lang/SwitchOnString.java
@@ -1,10 +1,7 @@
 package lang;
 
-/** Show the 'switch on string' feature of Java 7.
- * 
- * P L E A S E   R E A D   B E F O R E   C O M P L A I N I N G
- * This class absolutely requires Java SE 7+, so just add an exclusion rule
- * (Build Path -> Exclude) if you are living with a legacy version of Java SE.
+/**
+ * Show the 'switch on string' feature of Java 7.
  */
 public class SwitchOnString {
 	public static void main(String[] args) {
