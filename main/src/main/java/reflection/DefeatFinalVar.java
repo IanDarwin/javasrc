4ce262cfd5c635e8ecb2486cd6e160009ce7456f
Ian Darwin <ian@darwinsys.com>
2023-06-20 11:15:29 -0400

    Expand DefeatFinal to record.

--- main/src/main/java/reflection/DefeatFinalVar.java
+++ main/src/main/java/reflection/DefeatFinalVar.java
@@ -3,12 +3,13 @@ package reflection;
 import java.lang.reflect.Field;
 
 /**
- * You can modify the final fields of a class, but the results are not optimal.
+ * You can modify the final fields of a class, but not a record,
+ * and even for the class, the results are not optimal.
  */
 public class DefeatFinalVar {
 
 	public static void main(String[] args) throws Exception {
-		X x = new X();
+		ClassWithFinalField x = new ClassWithFinalField();
 		Class<?> c = x.getClass();
 		Field f = c.getDeclaredField("secret");
 		f.setAccessible(true); // gets past the "private" keyword
@@ -18,13 +19,32 @@ public class DefeatFinalVar {
 		System.out.println("X.secret from public getSecret() method: " + x.getSecret());
 		System.out.println("X.secret from field.get(): " + f.getInt(x));
 		System.out.println("X.secret from class-visible field access: " + x.secret);
-	}
-	
-	private static class X {
-		private final int secret = 24;
-		public int getSecret() {
-			return secret;
-		}
+		System.out.println();
+
+		RecordsAllFinalFields y = new RecordsAllFinalFields(24);
+		Class<?> c2 = x.getClass();
+		Field f2 = c2.getDeclaredField("secret");
+		f.setAccessible(true); // gets past the "private" keyword
+		System.out.println("Y.secret from field.get(): " + f2.getInt(x));
+		System.out.println("Trying to set Y.secret to " + 123 + " via reflection");
+		f.setInt(y, 123);
+		System.out.println("Y.secret from public getSecret() method: " + y.getSecret());
+		System.out.println("Y.secret from field.get(): " + f2.getInt(x));
+		System.out.println("Y.secret from class-visible field access: " + y.secret());
 	}
 
+}
+interface SecretKeeper { int getSecret(); }
+
+class ClassWithFinalField implements SecretKeeper {
+	final int secret = 24;
+	public int getSecret() {
+		return secret;
+	}
+}
+
+record RecordsAllFinalFields(int secret) implements SecretKeeper {
+	public int getSecret() {
+		return secret;
+	}
 }
