ed0e943b8b96fbe22fa3d04c462842c4fab44eb0
Ian Darwin <ian@darwinsys.com>
2023-06-28 06:28:36 -0400

    Simplify: interface not needed now.

--- main/src/main/java/reflection/DefeatFinalVar.java
+++ main/src/main/java/reflection/DefeatFinalVar.java
@@ -3,7 +3,7 @@ package reflection;
 import java.lang.reflect.Field;
 
 /**
- * You can modify the final fields of a class, but not a record,
+ * You can access the final fields of a class, but not a record,
  * and even for the class, the results are not optimal.
  */
 public class DefeatFinalVar {
@@ -12,7 +12,7 @@ public class DefeatFinalVar {
 		ClassWithFinalField x = new ClassWithFinalField();
 		Class<?> c = x.getClass();
 		Field f = c.getDeclaredField("secret");
-		f.setAccessible(true); // gets past the "private" keyword
+		f.setAccessible(true);
 		System.out.println("X.secret from field.get(): " + f.getInt(x));
 		System.out.println("Trying to set X.secret to " + 123 + " via reflection");
 		f.setInt(x, 123);
@@ -24,7 +24,7 @@ public class DefeatFinalVar {
 		RecordsAllFinalFields y = new RecordsAllFinalFields(24);
 		Class<?> c2 = x.getClass();
 		Field f2 = c2.getDeclaredField("secret");
-		f.setAccessible(true); // gets past the "private" keyword
+		f.setAccessible(true);
 		System.out.println("Y.secret from field.get(): " + f2.getInt(x));
 		System.out.println("Trying to set Y.secret to " + 123 + " via reflection");
 		f.setInt(y, 123);
@@ -34,16 +34,15 @@ public class DefeatFinalVar {
 	}
 
 }
-interface SecretKeeper { int getSecret(); }
 
-class ClassWithFinalField implements SecretKeeper {
+class ClassWithFinalField {
 	final int secret = 24;
 	public int getSecret() {
 		return secret;
 	}
 }
 
-record RecordsAllFinalFields(int secret) implements SecretKeeper {
+record RecordsAllFinalFields(int secret) {
 	public int getSecret() {
 		return secret;
 	}
