203b1d422c3dcd09bd6a26506aedf63e1bb9a880
Ian Darwin <ian@darwinsys.com>
2023-11-05 21:19:12 -0500

    Add Hamcrest Demo

---
+++ testing/src/test/java/testing/HamcrestDemo.java
@@ -0,0 +1,20 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.AssertAll.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HamcrestDemo {
+
+	@Test
+	public void showGroupAssertions() {
+		assertAll("Assert tests in a group",
+			() -> assertEquals(2,3), // Guaranteed to fail, first, see if others run.
+			() -> assertEquals(2,1+1),
+			() -> assertEquals("Hi", "Highlighter".substring(0,2)),
+			
+		);
+	}
+
+}
