ccc471688ea5fb275dc56dbd0e34874239b1c029
Ian Darwin <ian@darwinsys.com>
2023-12-06 10:07:26 -0500

    Add demo of Loom Virtual Threads.

---
+++ main/src/main/java/threads/VirtualThreadsDemo.java
@@ -0,0 +1,27 @@
+package threads;
+
+import java.util.concurrent.*;
+import java.util.stream.*;
+
+public class VirtualThreadsDemo {
+
+	final static int MAX = 5;
+
+	public static void main(String[] args) throws Exception {
+
+		Runnable r = () -> {
+			System.out.println("Hello from " + Thread.currentThread()); 
+		};
+
+		// Start vthreads with an ExecutorService
+		try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+			IntStream.range(0, MAX).forEach(i -> executor.submit(r));
+			executor.submit(r);
+		}
+
+		// Start vthreads directly
+		IntStream.range(0,MAX).forEach(i -> Thread.startVirtualThread(r));
+
+		Thread.sleep(100);
+	}
+}
