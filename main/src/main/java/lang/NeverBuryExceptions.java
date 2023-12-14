02c0d55a7de8747b17e74fd2917389d5aa7597df
Ian Darwin <ian@darwinsys.com>
2023-12-14 10:29:08 -0500

    Add 'Never Bury Exceptions' code

---
+++ main/src/main/java/lang/NeverBuryExceptions.java
@@ -0,0 +1,20 @@
+package com.learningtree.crs471;
+
+public class NeverBuryExceptions {
+	void exceptionTranslationIsGood() {
+		try {
+			int x = 42 / 0;
+			System.out.println(x);
+		} catch (RuntimeException e) {
+			throw new IllegalArgumentException("Numeric error", e);
+		}
+	}
+	
+	void buryExceptionIsBad() {
+		try {
+			int y = 57 / 0;
+			System.out.println(y);
+		// NEVER DO THS: EVER!
+		} catch (Exception e) {}
+	}
+}
