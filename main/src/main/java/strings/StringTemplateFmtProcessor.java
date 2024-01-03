c5d6964c9d71d0d9f7e6624aa0299085374f5768
Ian Darwin <ian@darwinsys.com>
2024-01-02 19:56:41 -0500

    Example of a custom StringTemplate.Processor implementation.

---
+++ main/src/main/java/strings/StringTemplateFmtProcessor.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+
+/**
+ * Demonstrate the creation and use of a fairly basic custom StringTemplate.Processor,
+ * in particular, the FMT processor described in https://bugs.openjdk.org/browse/JDK-8273943
+ * as "another standard processor" but never implemented for Java 21.
+ */
+void main() {
+var MYFMT = StringTemplate.Processor.of((StringTemplate st) -> {
+    StringBuilder sb = new StringBuilder();
+	// The constructed StringTemplate has two array fields,
+	// fragments (the parts not in \{...}) and values.
+    var fragments = st.fragments().iterator();
+    for (Object value : st.values()) {
+        sb.append(String.format(fragments.next(), value));
+    }
+	sb.append(fragments.next());	// any leftover portion.
+    return sb.toString();
+});
+
+var user = "Ian";
+
+System.out.println(MYFMT."Hello %s\{user}, Math.PI to 5 decimal places is %7.5f\{Math.PI}.");
+}
