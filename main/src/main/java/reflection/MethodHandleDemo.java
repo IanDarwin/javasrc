69818fd60f1104345e031eb8705715186e8d2874
Ian Darwin <ian@darwinsys.com>
2024-01-04 15:14:12 -0500

    Simple example of MethodHandles.

---
+++ main/src/main/java/reflection/MethodHandleDemo.java
@@ -0,0 +1,35 @@
+package reflection;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.time.LocalDate;
+
+public class MethodHandleDemo {
+    public static void main(String[] args) throws Throwable {
+
+        // Create a Lookup object
+        MethodHandles.Lookup lup = MethodHandles.lookup();
+
+        // First invoke LocalDate's static "of(year, month, day)" method
+
+        // Create a matcher for the arg list types we want to invoke
+        // First the return type, then the argument types
+        MethodType mt = MethodType.methodType(LocalDate.class, int.class, int.class, int.class);
+        // Use the Lookup object to find the static method "of" in Localdate
+        MethodHandle mh = lup.findStatic(LocalDate.class, "of", mt);
+        // At last, invoke the method!
+        LocalDate endDate = (LocalDate) mh.invokeExact(2014, 6, 10);
+        System.out.println(STR."LocalDate from 'of' = \{endDate}");
+
+        // Now invoke the LocalDate's instance method toString() explicitly
+        // "toString()" returns a String, takes no args
+        mt = MethodType.methodType(String.class);
+        // Find the instance method in the LocalDate class
+        mh = lup.findVirtual(LocalDate.class, "toString", mt);
+        // Invoke it in the context of the LocalDate object created earlier
+        String asString = (String) mh.invokeExact(endDate);
+        System.out.println(STR."LocalDate as String is \{asString}");
+
+    }
+}
