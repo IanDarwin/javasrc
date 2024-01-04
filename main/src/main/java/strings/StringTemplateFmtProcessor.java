b1d56b9edeea0be309a450a1a030163aae7924fd
Ian Darwin <ian@darwinsys.com>
2024-01-03 19:40:16 -0500

    linewrap

--- main/src/main/java/strings/StringTemplateFmtProcessor.java
+++ main/src/main/java/strings/StringTemplateFmtProcessor.java
@@ -1,5 +1,6 @@
 import java.util.Iterator;
 
+// tag::main[]
 /**
  * Demonstrate the creation and use of a fairly basic custom StringTemplate.Processor,
  * in particular, the FMT processor described in https://bugs.openjdk.org/browse/JDK-8273943
@@ -8,8 +9,8 @@ import java.util.Iterator;
 void main() {
 var MYFMT = StringTemplate.Processor.of((StringTemplate st) -> {
     StringBuilder sb = new StringBuilder();
-	// The constructed StringTemplate has two array fields,
-	// fragments (the parts not in \{...}) and values.
+	// The constructed StringTemplate has two List fields,
+	// List<String> fragments (the parts not in \{...}) and List<Object> values.
     var fragments = st.fragments().iterator();
     for (Object value : st.values()) {
         sb.append(String.format(fragments.next(), value));
@@ -20,5 +21,7 @@ var MYFMT = StringTemplate.Processor.of((StringTemplate st) -> {
 
 var user = "Ian";
 
-System.out.println(MYFMT."Hello %s\{user}, Math.PI to 5 decimal places is %7.5f\{Math.PI}.");
+System.out.println(
+    MYFMT."Hello %s\{user}, Math.PI to 5 decimal places is %7.5f\{Math.PI}.");
 }
+// end::main[]
