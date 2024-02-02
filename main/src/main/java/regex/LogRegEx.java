a5a1b23b0657a594b6a7752500e9f9f8cd88458d
Ian Darwin <ian@darwinsys.com>
2024-02-02 17:04:14 -0500

    Convert LogRexEx to multi-line (COMMENTS) form; +test file

--- main/src/main/java/regex/LogRegEx.java
+++ main/src/main/java/regex/LogRegEx.java
@@ -12,37 +12,54 @@ import java.util.regex.Pattern;
 // tag::main[]
 public class LogRegEx {
 
-	final static String LOG_ENTRY_PATTERN = 
-			"^([\\w\\d.-]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+-]\\d{4})\\] " +
-			"([a-zA-Z.]+ )?" +
-			"\"(.+?)\" (\\d{3}) (\\d+)( \"([^\"]+)\")?( \"([^\"]+)\")?";
+	public static final int MIN_FIELDS = 8;
+
+	// Sample line: 123.45.67.89 - - [27/Oct/2000:09:27:09 -0400] "GET /java/javaResources.html HTTP/1.0" 200 10450 "-" "Mozilla/4.6 [en] (X11; U; OpenBSD 2.8 i386; Navigator)"
+
+	/** Multi-line regex, but text block keeps leading tabs, so undent. */
+	final static String LOG_ENTRY_PATTERN = """
+^([\\w\\d.-]+)\\s+      # 1 - IP
+(\\S+)\\s+              # 2 - User, from identd (always "-")
+(\\S+)\\s+              # 3 - User, from https (often "-")
+\\[([\\w:/]+\\s[+-]\\d{4})\\]\\s+ # 4 - Date, time, space, timezone-offset
+([a-zA-Z.]+\\s+)?       # 5 - domainname, in some formats
+"(.+?)"\\s+             # 6 - Entire request line
+(\\d{3})\\s+            # 7 - Status code (200, 404, etc)
+(\\d+)\\s*              # 8 - Byte count
+("[^"]+"\\s*)?          # 9 - Referrer, or "-"
+("([^"]+)")?            # 10 - Browser advertising clause, free-form
+""";					
 
 	public static void main(String argv[]) throws IOException {
 
-		Pattern p = Pattern.compile(LOG_ENTRY_PATTERN);
-		System.out.println("RE Pattern: " + p);
+		Pattern p = Pattern.compile(LOG_ENTRY_PATTERN, Pattern.COMMENTS);
+		System.out.println("RE Pattern:");
+		System.out.println(p);
 
-		String fileName = argv[0];
+		for (String fileName : argv) {
 		Files.lines(Path.of(fileName)).forEach(logEntryLine -> {
 
 			System.out.println("Input line:" + logEntryLine);
 			Matcher matcher = p.matcher(logEntryLine);
-			if (!matcher.matches() || 
-				LogParseInfo.MIN_FIELDS > matcher.groupCount()) {
-				System.err.println("Bad log entry (or problem with regex):");
-				System.err.println(logEntryLine);
+			if (!matcher.find()) {
+				System.err.println("Failed to match (Bad log entry or problem with regex)");
+				return;
+			}
+			if (matcher.groupCount() < MIN_FIELDS) {
+				System.err.println("Matched, but has too few fields):");
 				return;
 			}
 			System.out.println("IP Address: " + matcher.group(1));
 			System.out.println("UserName: " + matcher.group(3));
 			System.out.println("Date/Time: " + matcher.group(4));
 			System.out.println("Request: " + matcher.group(5));
-			System.out.println("Response: " + matcher.group(6));
-			System.out.println("Bytes Sent: " + matcher.group(7));
-			if (!matcher.group(8).equals("-"))
-				System.out.println("Referer: " + matcher.group(8));
-			System.out.println("User-Agent: " + matcher.group(9));
+			System.out.println("Response: " + matcher.group(7));
+			System.out.println("Byte Count: " + matcher.group(8));
+			if (!matcher.group(9).equals("-"))
+				System.out.println("Referer: " + matcher.group(9));
+			System.out.println("User-Agent: " + matcher.group(10));
 		});
+	}
 	}
 }
 // end::main[]
