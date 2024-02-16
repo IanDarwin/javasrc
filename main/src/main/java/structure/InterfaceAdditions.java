38ee4e8a9cfbec89359bae6916dcc1ddf42ba768
Ian Darwin <ian@darwinsys.com>
2024-02-15 21:00:19 -0500

    Show off new stuff in interfaces

---
+++ main/src/main/java/structure/InterfaceAdditions.java
@@ -0,0 +1,26 @@
+package structure;
+
+public class InterfaceChanges implements Demo {
+	void main() { // Instance main in a named class
+		// Call public interface method in implementing class
+		System.out.println(getStatus());
+		// Call static interface method
+		System.out.println(Demo.getPresent());
+		// Following won't compile, private method
+		// var s = innerStatus(); 
+	}
+}
+
+interface Demo {
+
+	default String getStatus() { return innerStatus(); }
+
+	static String getPresent() { return "Freebie"; }
+
+	private String innerStatus() { return "OK"; }
+}
+
+class InterfaceChangesII implements Demo {
+	// Forllowing is a valid override, but must be public
+	public String getStatus() { return "Unknown"; }
+}
