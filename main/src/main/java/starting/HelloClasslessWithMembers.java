806d0107208b8cf1fce9927014ea146e07f8f09c
Ian Darwin <ian@darwinsys.com>
2024-02-16 13:09:08 -0500

    Move file to better location

---
+++ main/src/main/java/starting/HelloClasslessWithMembers.java
@@ -0,0 +1,11 @@
+	import java.lang.reflect.*;	// Unused, just to show import is allowed
+
+	int x;						// instance fields
+
+	void main(String[] args) {	// instance main
+		System.out.println("Hello, world");
+		process(1);
+	}
+	void process(int x) {		// instance method
+		System.out.println("Hello " +x);
+	}
