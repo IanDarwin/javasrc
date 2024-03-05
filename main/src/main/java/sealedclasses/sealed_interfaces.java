e0f6efe668b79894f65b24385b8409bfa4520f89
Ian Darwin <ian@darwinsys.com>
2024-03-05 18:36:44 -0500

    Add HelloClasslessWithMembers et al.

---
+++ main/src/main/java/sealedclasses/sealed_interfaces.java
@@ -0,0 +1,19 @@
+// package sealedclasses;
+
+void main() {
+	System.out.println("Nothing to see here, folks.");
+}
+
+sealed interface A1 permits A2, A3, A4 {
+	void process();
+}
+
+non-sealed interface A2 extends A1 {}
+
+interface B1 extends A2 {}		// Allowed because A2 not sealed
+
+non-sealed interface A3 extends A1 {}
+
+sealed interface A4 extends A1 permits B2 {}
+
+non-sealed interface B2 extends A4 {}
--- main/src/main/java/structure/ArraysEqualsCompareToString.java
+++ main/src/main/java/structure/ArraysEqualsCompareToString.java
@@ -2,10 +2,12 @@ package structure;
 
 import java.util.Arrays;
 
-public class ArraysCompareDemo {
+// tag::main[]
+
+public class ArraysEqualsCompareToString {
 
     void main() {
-
+// end::main[]
         System.out.println("Permute char value");
         char[] ca = { 'a', 'b', 'c'},
                 cb = { 'a', 'b', 'c'};
@@ -19,6 +21,7 @@ public class ArraysCompareDemo {
 
         System.out.println();
 
+        // tag::main[]
         System.out.println("Permute int value");
         int[] ia = { 1, 2, 3, 4, 5},
                 ib = { 1, 2, 3, 4, 5};
@@ -29,7 +32,7 @@ public class ArraysCompareDemo {
         System.out.println(STR."ia = \{Arrays.toString(ia)}, ib = \{Arrays.toString(ib)}");
         System.out.println(STR."By Arrays.equals(): \{Arrays.equals(ia, ib)}");
         System.out.println(STR."compare(ia,ib) returns \{Arrays.compare(ia, ib)}");
-
+        // end::main[]
 
         System.out.println();
 
