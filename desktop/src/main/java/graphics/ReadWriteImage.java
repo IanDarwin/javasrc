2f4065381225078214a68e80f1011c80641dee66
Ian Darwin <ian@darwinsys.com>
2024-03-05 18:35:36 -0500

    Add Rectangle mode to ReadWriteImage

--- desktop/src/main/java/graphics/ReadWriteImage.java
+++ desktop/src/main/java/graphics/ReadWriteImage.java
@@ -21,55 +21,79 @@ import javax.imageio.ImageIO;
  */
 public class ReadWriteImage {
 	/** The mode: IMAGE reads a file, CIRCLE creates a canvas with a gray circle */
-	enum Mode { IMAGE, CIRCLE }
+	enum Mode { IMAGE, CIRCLE, R_RECTANGLE }
 	/** The mode used in this run */
-	static Mode mode = Mode.IMAGE;
+	static Mode mode = Mode.R_RECTANGLE;
 	/** Java release to start with */
 	final static int START = 8;
 	/** Several beyond current Java release, room for expansion */
 	final static int END = 25;
+	/** Releases we still care about Preview/Incubation in these releases */
+	final static int START_PC = 20;
 	/** Font size to draw in */
-	final static int FONT_SIZE = 84;
+	final static int FONT_SIZE_BIG = 84, FONT_SIZE_NORMAL = 20;
 	/** Radius, only used if mode == CIRCLE */
 	final static int RADIUS = 160;
-	final static String DIRECTORY = "images" + File.separatorChar;
+	/** Width, height for Rounded Rectangle */
+	final static int WIDTH=60, HEIGHT=50;
+	final static String DIRECTORY = "versions" + File.separatorChar;
 
 	public static void main(String[] args) throws Exception {
 		String dir = DIRECTORY;
 		IntStream.rangeClosed(START, END).forEach(i -> process(DIRECTORY, Integer.toString(i)));
-		List.of("21P").forEach(s -> process(DIRECTORY, s));
+		IntStream.rangeClosed(START_PC, END).forEach(i -> process(DIRECTORY, i + "P"));
+		IntStream.rangeClosed(START_PC, END).forEach(i -> process(DIRECTORY, i + "C"));
 		System.exit(0);
 	}
 
 	static void process(String dir, String label) {
 		try {
 		BufferedImage image = null;
-		if (mode == Mode.IMAGE) {
-			image = ImageIO.read(new File(dir + "coffeecup.png"));
-		} else if (mode == Mode.CIRCLE) {
-			image = new BufferedImage(RADIUS, RADIUS, BufferedImage.TYPE_INT_RGB);
-		} else {
-			throw new IllegalStateException(STR."Invalid Mode \{mode}");
-		}
-		Graphics2D g = image.createGraphics();
-		if (mode == Mode.CIRCLE) {
-
+		Graphics2D g = null;
+		int fontSize = 0;
+		switch(mode) {
+		case Mode.R_RECTANGLE:
 			// Draw the filled Circle
+			image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+			g = image.createGraphics();
+			g.setColor(Color.BLACK);
+			g.fillRoundRect(0, 0, WIDTH, HEIGHT, 10, 10);
+			g.setColor(Color.WHITE);
+			fontSize = FONT_SIZE_NORMAL;
+			break;
+		case Mode.CIRCLE:
+			// Draw the filled Circle
+			image = new BufferedImage(RADIUS, RADIUS, BufferedImage.TYPE_INT_RGB);
+			g = image.createGraphics();
 			g.setColor(Color.GRAY);
 			g.fillOval(0, 0, RADIUS, RADIUS);
-		} else {
-			// Nothing, the image was already loaded
+			g.setColor(Color.WHITE);
+			fontSize = FONT_SIZE_BIG;
+			break;
+		case Mode.IMAGE:
+			image = ImageIO.read(new File(dir + "coffeecup.png"));
+			g = image.createGraphics();
+			g.setColor(Color.WHITE);
+			fontSize = FONT_SIZE_BIG;
+			break;
 		}
 
 		// Draw the number
-		Font textFont = new Font("SansSerif", Font.BOLD, FONT_SIZE);
+		Font textFont = new Font("SansSerif", Font.BOLD, fontSize);
 		g.setFont(textFont);
-		g.setColor(mode == Mode.CIRCLE ? Color.WHITE : Color.GRAY);
 		Rectangle2D lineMetrics = textFont.getStringBounds(label, g.getFontRenderContext());
-		// System.out.println(STR."Font measured size = \{lineMetrics}");
 		int x = (int)((image.getWidth() - lineMetrics.getWidth()) / 2);
-		int y = (int)((image.getHeight() - lineMetrics.getHeight()) / 2);
-		y += mode == Mode.CIRCLE ? 80 : 60;
+		int y = (int)((image.getHeight() + lineMetrics.getHeight()) / 2);
+		switch (mode) {
+			case Mode.CIRCLE:
+				y += 80; break;
+			case Mode.IMAGE: 
+				y += 60; break;
+			default:
+				// Nothing
+				break;
+		}
+		System.out.println(STR."Drawing in \{g.getColor()} at \{x},\{y}");
 		g.drawString(label, x, y);
 		ImageIO.write(image, "png", 
 			new File(String.format("%sjava%s.png",dir, label)));
