07a0f018424714511418cc23ed9b19814b5563ec
Ian Darwin <ian@darwinsys.com>
2024-03-05 18:36:19 -0500

    Add VectorOps example

--- incubation/src/main/java/numbers/VectorOps.java
+++ incubation/src/main/java/numbers/VectorOps.java
@@ -1,37 +1,33 @@
 package numbers;
 
+import java.util.Arrays;
+
 import jdk.incubator.vector.DoubleVector;
 import jdk.incubator.vector.VectorSpecies;
 
 /** Compute ax^2+2b on double arrays */
 public class VectorOps {
+
+    static final VectorSpecies<Double> SPECIES = DoubleVector.SPECIES_PREFERRED;
+
     public static void main(String[] args) {
-        System.out.println(STR."Using Vector API with SPECIES \{SPECIES}");
+        System.out.println(STR."Using Vector API with Species \{SPECIES}");
         double[] a = { 2, 4, 6, 8, 9, 10, 11, 12, 13, 14};
         double x = Math.PI;
         double[] b = { 1, 3, 5, 7, 9, 10, 11, 12, 13, 14};
         double[] sresults = new double[a.length], vresults = new double[a.length];
-        System.out.println(STR."Inputs: a=\{arrayToString(a)}, x = \{x}, b = \{arrayToString(b)}");
+        System.out.println(STR."Inputs: a=\{Arrays.toString(a)}, x = \{x}, b = \{Arrays.toString(b)}");
         scalarComputation(a, x, b, sresults);
-        System.out.println(STR."Results from scalar = \{arrayToString(sresults)}");
+        System.out.println(STR."Results from scalar = \{Arrays.toString(sresults)}");
         vectorComputation(a, x, b, vresults);
-        System.out.println(STR."Results from vector = \{arrayToString(vresults)}");
+        System.out.println(STR."Results from vector = \{Arrays.toString(vresults)}");
         for (int i = 0; i < sresults.length; i++) {
-            if (Math.abs(sresults[i] - vresults[i]) > 0.000001D) {
+            if (Math.abs(sresults[i] - vresults[i]) > 0.00000001D) {
                 throw new IllegalStateException(
                         STR."Values differ: \{sresults[i]} != \{vresults[i]}");
             }
         }
         System.out.println("Computed both ways: Close enough!");
-    }
-
-    static String arrayToString(double[] array) {
-        var sb = new StringBuilder("[");
-        for (double f : array) {
-            sb.append(f).append(",");
-        }
-        sb.append("]");
-        return sb.toString();
     }
 
     static void scalarComputation(double[] a, double x, double[] b, double[] c) {
@@ -40,18 +36,19 @@ public class VectorOps {
         }
     }
 
-    static final VectorSpecies<Double> SPECIES = DoubleVector.SPECIES_PREFERRED;
-
     static void vectorComputation(double[] a, double x, double[] b, double[] c) {
         int i = 0;
         int upperBound = SPECIES.loopBound(a.length);
         for (; i < upperBound; i += SPECIES.length()) {
             var va = DoubleVector.fromArray(SPECIES, a, i);
             var vb = DoubleVector.fromArray(SPECIES, b, i);
-            var vc = va.mul(x*x)
+			System.out.println(STR."va length = \{va.length()}");
+            var vc = va
+					.mul(x*x)
                     .add(vb.mul(2));
             vc.intoArray(c, i);
         }
+		// Handle any leftover
         for (; i < a.length; i++) {
             c[i] = (a[i] * x * x + b[i] * 2);
         }
