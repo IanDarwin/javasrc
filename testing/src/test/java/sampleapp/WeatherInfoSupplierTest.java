1a348b97964af8889fbc0804edd732c03d05afd7
Ian Darwin <ian@darwinsys.com>
2024-04-23 19:19:05 -0400

    Rebuild with Mockito demo

--- testing/src/test/java/sampleapp/WeatherInfoSupplierTest.java
+++ testing/src/test/java/sampleapp/WeatherInfoSupplierTest.java
@@ -1,23 +1,29 @@
 package sampleapp;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class WeatherInfoSupplierTest {
 
+	// The main class; what we are testing
 	WeatherReporter reporter = new WeatherReporter();
 
+	// Its dependency, a WeatherStation, which will
+	// be mocked up for the tests.
 	WeatherStation mockStation;
 
 	static int testCount;
 
+	/** A hand-coded mock WeatherStation */
 	class MyMockStation implements WeatherStation {
 
-		private Units units;
+		private Units units = Units.SI; // "Systeme Internationale"
 
 		@Override
 		public void setUnits(Units units) {
@@ -27,36 +33,37 @@ public class WeatherInfoSupplierTest {
 		@Override
 		public double getTemperature() {
 			switch (units) {
-				case SI: return 22.2;
+				case SI: return 19.0;
 				case US: return 68.0;
 				default: throw new RuntimeException();
 			}
 		}
 	}
 
-	@Before
-	public void setup() {
-		// First use hard-coded mock station, next time use Mockito
-		if (testCount++%2 == 0) {
-			mockStation = new MyMockStation();
-		} else {
-			mockStation = mock(WeatherStation.class);
-			when(mockStation.getTemperature()).thenReturn(68.0);
-		}
-		reporter.setStation(mockStation);
-	}
-
 	@Test
 	public void testGetTemperatureDegC() {
+		reporter.setStation(mockStation = new MyMockStation());
 		reporter.setUnits(Units.SI);
-		// 22.2 C, an OK temperature
-		assertEquals(22.2, reporter.getTemperature(), 0.01);
+		// 19.0 C, an OK temperature
+		assertEquals(19.0, reporter.getTemperature(), 0.01);
 	}
 
 	@Test
 	public void testGetTemperatureDegF() {
+		reporter.setStation(mockStation = new MyMockStation());
 		reporter.setUnits(Units.US);
 		// 68 F, an OK temperature
 		assertEquals(68.0, reporter.getTemperature(), 0.01);
+	}
+
+	@Test
+	public void showExpectations() {
+		reporter.setStation(mockStation = mock(WeatherStation.class));
+		when(mockStation.getTemperature()).thenReturn(19.0);
+		if (testCount == 0) { // hard-coded mock, no expectations
+			return;
+		}
+		reporter.getTemperature(); // Delegates to station::getTemperature
+		verify(mockStation, atLeastOnce()).getTemperature();
 	}
 }
