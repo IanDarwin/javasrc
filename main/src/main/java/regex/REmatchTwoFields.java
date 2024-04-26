7b555ba04633b44e4ed2e71e899a6d52acf7bc59
Ian Darwin <ian@darwinsys.com>
2024-04-26 19:18:34 -0400

    Style police

--- main/src/main/java/regex/REmatchTwoFields.java
+++ main/src/main/java/regex/REmatchTwoFields.java
@@ -14,11 +14,12 @@ import java.util.regex.Pattern;
 public class REmatchTwoFields {
 	public static void main(String[] args) {
 		String inputLine = "Adams, John Quincy";
-		// Construct an RE with parens to "grab" both field1 and field2
+		// Construct an RE with parens to "grab" field1 and field2
 		Pattern r = Pattern.compile("(.*), (.*)");
 		Matcher m = r.matcher(inputLine);
-		if (!m.matches())
+		if (!m.matches()) {
 			throw new IllegalArgumentException("Bad input");
+		}
 		System.out.println(m.group(2) + ' ' + m.group(1));
 	}
 }
