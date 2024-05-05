a152b9d5fb5921dc2f1abc4610bfe4b9583c0735
Ian Darwin <ian@darwinsys.com>
2024-05-05 09:54:51 -0400

    Use a value object for the values

--- main/src/main/java/structure/HashMapDemo.java
+++ main/src/main/java/structure/HashMapDemo.java
@@ -10,43 +10,48 @@ import java.util.Map;
 // tag::main[]
 public class HashMapDemo {
 
+	record Address(String city, String state){}
+
 	public static void main(String[] argv) {
 
 		// Construct and load the hash. This simulates loading a
 		// database or reading from a file, or wherever the data is.
 
-		Map<String,String> map = new HashMap<String,String>();
+		Map<String,Address> map = new HashMap<>();
 
-		// The hash maps from company name to address.
-		// In real life this might map to an Address object...
-		map.put("Adobe", "Mountain View, CA");
-		map.put("IBM", "White Plains, NY");
-		map.put("Learning Tree", "Los Angeles, CA");
-		map.put("Microsoft", "Redmond, WA");
-		map.put("Netscape", "Mountain View, CA");
-		map.put("O'Reilly", "Sebastopol, CA");
-		map.put("Sun", "Mountain View, CA");
+		// Populate it: the map maps from company name to Address.
+		map.put("Adobe", new Address("Mountain View", "CA"));
+		map.put("IBM", new Address("White Plains", "NY"));
+		map.put("Learning Tree", new Address("Los Angeles", "CA"));
+		map.put("Microsoft", new Address("Redmond", "WA"));
+		map.put("O'Reilly", new Address("Sebastopol", "CA"));
+		map.put("Rejminet Group", new Address("Toronto", "ON"));
 
-		// Two versions of the "retrieval" phase.
+		// Several versions of the "retrieval" phase.
 		// Version 1: get one pair's value given its key
 		// (presumably the key would really come from user input):
+		System.out.println("Version 1");
 		String queryString = "O'Reilly";
 		System.out.println("You asked about " + queryString + ".");
-		String resultString = map.get(queryString);
-		System.out.println("They are located in: " + resultString);
+		var result = map.get(queryString);
+		System.out.println("They are located in: " + result);
 		System.out.println();
 
 		// Version 2: get ALL the keys and values 
 		// (maybe to print a report, or to save to disk)
+		System.out.println("Version 2");
 		for( String key : map.keySet()) {
 			System.out.println("Key " + key + 
 				"; Value " + map.get(key));
 		}
+		System.out.println();
 		
 		// Version 3: Same but using a Map.Entry lambda
+		System.out.println("Version 3");
 		map.entrySet().forEach(mE -> 
-			System.out.println("Key + " + mE.getKey()+ 
+			System.out.println("Key " + mE.getKey()+ 
 				"; Value " +mE.getValue()));
+		System.out.println();
 	}
 }
 // end::main[]
