79ed9c9e9f00dd67850f3146bbc2df8379a28482
Ian Darwin <ian@darwinsys.com>
2024-05-08 18:04:33 -0400

    taggit

--- main/src/main/java/lang/PatternRecordDestruction.java
+++ main/src/main/java/lang/PatternRecordDestruction.java
@@ -6,22 +6,19 @@ import java.util.List;
  * Demo the "destruction" (really: destructuring) of a record
  */
 
+// tag::main[]
 record Graduate(String name, String degree, int year){}
 
-List<?> grads = List.of(
-	new Graduate("J. Smith", "M.B.A", 2019),
-	new Graduate("K. Longbow", "B.Sc", 2018)
-);
-
 void main() {
-	// explicit instanceof
+
+	// with instanceof:
 	for (Object o : grads) {
 		if (o instanceof Graduate(var nm, var deg, var year)) {
 			System.out.printf("%s graduated %d with a %s\n", nm, year, deg);
 		}
 	}
 
-	// pattern match in switch
+	// pattern match in switch:
 	for (Object o : grads) {
 		switch(o) {
 			case Graduate(var nm, var deg, var year) ->
@@ -30,3 +27,9 @@ void main() {
 		}
 	}
 }
+// end::main[]
+
+List<?> grads = List.of(
+	new Graduate("J. Smith", "M.B.A", 2019),
+	new Graduate("K. Longbow", "B.Sc", 2018)
+);
