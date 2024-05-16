1085cd60db8b898fbc20cd2cef63f3006902f722
Ian Darwin <ian@darwinsys.com>
2024-05-16 12:44:18 -0400

    Comment before <n> xref

--- main/src/main/java/classfileapi/CreateLoadAndRun.java
+++ main/src/main/java/classfileapi/CreateLoadAndRun.java
@@ -18,8 +18,8 @@ import static java.lang.constant.ConstantDescs.*;
 // tag::main[]
 public class CreateLoadAndRun extends ClassLoader {
 
-	final String fullClassName = "notapackage.Hello";		<1>
-	// A few ClassDescs that are not in ConstantDescs		<2>
+	final String fullClassName = "notapackage.Hello";		// <1>
+	// A few ClassDescs that are not in ConstantDescs		// <2>
 	final ClassDesc CD_fullClassName = ClassDesc.of(fullClassName);
 	final ClassDesc CD_System = ClassDesc.of(System.class.getName());
 	final ClassDesc CD_PrintStream =
@@ -33,29 +33,29 @@ public class CreateLoadAndRun extends ClassLoader {
 	void main() throws Exception {
 		byte[] classData = ClassFile
 			.of()
-			.build(CD_fullClassName, 						<3>
+			.build(CD_fullClassName, 						// <3>
 			clb -> clb.withFlags(ClassFile.ACC_PUBLIC)
 
-				// requires no-arg constructor to be valid	<4>
+				// requires no-arg constructor to be valid	// <4>
 				.withMethod(ConstantDescs.INIT_NAME, ConstantDescs.MTD_void,
 					ClassFile.ACC_PUBLIC,
 					mb -> mb.withCode(
-						cob -> cob.aload(0)					<5>
+						cob -> cob.aload(0)					// <5>
 							.invokespecial(ConstantDescs.CD_Object,
 							ConstantDescs.INIT_NAME, ConstantDescs.MTD_void)
 							.return_()))
 
 				// Standard "void main(String[])" main method
-				.withMethod("main", MTD_void_StringArray,	<6>
+				.withMethod("main", MTD_void_StringArray,	// <6>
 					ClassFile.ACC_PUBLIC + ClassFile.ACC_STATIC,
-					methodBuilder -> methodBuilder.withCode( <7>
+					methodBuilder -> methodBuilder.withCode( // <7>
 						cob -> cob.getstatic(CD_System, "out", CD_PrintStream)
 							.ldc("Hello World")
 							.invokevirtual(CD_PrintStream, "println", MTD_void_String)
 							.return_())));				
 
 		// Save the new class to disk, just so we can examine & verify
-		var dirPath = Path.of("/tmp/notapackage");			<8>
+		var dirPath = Path.of("/tmp/notapackage");			// <8>
 		if (!Files.exists(dirPath)) {
 			Files.createDirectory(dirPath);
 		}
@@ -63,7 +63,7 @@ public class CreateLoadAndRun extends ClassLoader {
 
 		// Define the class in current ClassLoader
 		// (e.g., this) and then call main()
-		Class<?> c = 										<9>
+		Class<?> c = 										// <9>
 			defineClass(fullClassName, classData, 0, classData.length);
 
 		Method m = c.getMethod("main", String[].class);
