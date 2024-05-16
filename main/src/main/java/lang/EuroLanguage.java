1bd4c6ba5e9b47702fa599195959e3db16dabfa3
Ian Darwin <ian@darwinsys.com>
2024-05-16 12:39:23 -0400

    Re-encode in UTF-8

--- main/src/main/java/lang/EuroLanguage.java
+++ main/src/main/java/lang/EuroLanguage.java
@@ -1,19 +1,20 @@
 package lang;
 
 /* Make sure the compiler accepts European languages everywhere.
- * May require a locale to be set before it will compile!
- * But if you rename the class to, say, Español, many runtimes will be confused.
+ * This file is encoded as UTF-8 and should work on modern Java.
+ * But: with older Java versions and/or on MS-Windows, it
+ * may require a locale to be set before it will compile!
  */
 public class EuroLanguage {
-	public static int número;
+	public static int nÃºmero;
 	public static void main(String[] args) {
-		System.out.println("Acción!");
-		acción(123);
-		System.out.println("Número = " + número);
+		System.out.println("AcciÃ³n!");
+		acciÃ³n(123);
+		System.out.println("NÃºmero = " + nÃºmero);
 	}
-	public static void acción(int costa) {
+	public static void acciÃ³n(int costa) {
 		System.out.println("Su tarjeta cuesta los " + costa);
-		número++;
+		nÃºmero++;
 	}
 }
 
