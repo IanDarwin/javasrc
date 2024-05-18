f73a04daaffbaaf09dab601070c376afbb5f4fae
Ian Darwin <ian@darwinsys.com>
2024-05-18 14:56:40 -0400

    Unbreak, but make pedantically verbose

--- main/src/main/java/functional/StreamGathererScanDemo.java
+++ main/src/main/java/functional/StreamGathererScanDemo.java
@@ -3,7 +3,11 @@ import java.util.stream.*;
 void main() {
 	List<Integer> plusOnes =
 		Stream.of(1,2,3,4,5)
-		.gather(Gatherers.scan(() -> 1, (init, val) -> init + val))
-		.toList()
+		.gather(Gatherers.scan(() -> 1, (prev, val) -> {
+				System.out.println(STR."prev \{prev}, val \{val}");
+				return prev + val;
+		}))
+		.toList();
+	plusOnes
 		.forEach(System.out::println);
 }
