a9ad906545b6d07167fdfbba4a991422bd10490d
Ian Darwin <ian@darwinsys.com>
2024-06-21 16:12:43 -0400

    Make a single file example for JCB 'Recurring Events' recipe

---
+++ main/src/main/java/datetime/MeetingDates.java
@@ -0,0 +1,45 @@
+package datetime;
+
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
+
+public class MeetingDates {
+
+/**
+ * Compute dates of recurring meeting
+ * This version has hard-coded to be 3rd Wednesday of each month.
+ */
+LocalDate now = LocalDate.now();
+	int weekOfMonth = 3;
+	DayOfWeek dayOfWeek = DayOfWeek.WEDNESDAY;
+
+	// tag::method[]
+	public LocalDate getNextMeeting(int meetingsAway) {
+		LocalDate thisMeeting = now.with(
+			TemporalAdjusters.dayOfWeekInMonth(weekOfMonth,dayOfWeek));
+		// Has the meeting already happened this month?
+		if (thisMeeting.isBefore(now)) {
+			// start from next month
+			meetingsAway++;
+		}
+		if (meetingsAway > 0) {
+			thisMeeting = thisMeeting.plusMonths(meetingsAway).
+				with(TemporalAdjusters.dayOfWeekInMonth(weekOfMonth,dayOfWeek));
+		}
+		return thisMeeting;
+	}
+	// end::method[]
+
+	// tag::main[]
+	public static void main(String[] args) {
+		MeetingDates mp = new MeetingDates();
+		DateTimeFormatter dfm = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+		for (int i = 0; i <= 2; i++) {
+			LocalDate dt = mp.getNextMeeting(i);
+			System.out.println(dt.format(dfm));
+		}
+	}
+	// end::main[]
+}
+
