4f6eea790a886535111cf58d9590d7f10cdefb80
Ian Darwin <ian@darwinsys.com>
2024-06-30 16:12:11 -0400

    Use HttpURLConnection explicitly; reformat code

--- main/src/main/java/netweb/RestClientURLDemo.java
+++ main/src/main/java/netweb/RestClientURLDemo.java
@@ -2,17 +2,20 @@ package netweb;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URLConnection;
 
 // tag::main[]
 public class RestClientURLDemo {
 	public static void main(String[] args) throws Exception {
-		URLConnection conn = URI.create(HttpClientDemo.urlString + HttpClientDemo.keyword).toURL()
-			.openConnection();
-		try (BufferedReader is = 
-			new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-
+		HttpURLConnection conn = (HttpURLConnection)
+			URI.create(HttpClientDemo.urlString +
+				HttpClientDemo.keyword).toURL()
+				.openConnection();
+		try (BufferedReader is =
+			new BufferedReader(
+				new InputStreamReader(conn.getInputStream()))) {
 			String line;
 			while ((line = is.readLine()) != null) {
 				System.out.println(line);
