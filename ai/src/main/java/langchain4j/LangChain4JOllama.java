dd2ce11557901c7d1aec00509f191c18b5b466b1
Ian Darwin <ian@darwinsys.com>
2024-07-04 19:21:08 -0400

    adoc tags

--- ai/src/main/java/langchain4j/LangChain4JOllama.java
+++ ai/src/main/java/langchain4j/LangChain4JOllama.java
@@ -1,23 +1,22 @@
 package langchain4j;
 
+// tag::main[]
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import ai.Constants;
 
 class LangChain4JOllama {
 
-    static String MODEL_NAME = "mistral";
-
     public static void main(String[] args) {
 
         ChatLanguageModel model = OllamaChatModel.builder()
                 .baseUrl("http://127.0.0.1:11434")
-                .modelName(MODEL_NAME)
+                .modelName("mistral")
                 .build();
 
         String answer = model.generate(Constants.TEXT_PROMPT);
 
         System.out.println(answer);
     }
-
 }
+// end::main[]
