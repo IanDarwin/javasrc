d1b1ffc98cf582665626906a7c2918b20a1d7c0b
Ian Darwin <ian@darwinsys.com>
2024-07-07 15:42:35 -0400

    Formatting, comments

--- main/src/main/java/numbers/NumFormat2.java
+++ main/src/main/java/numbers/NumFormat2.java
@@ -7,12 +7,11 @@ import java.text.NumberFormat;
  */
 // tag::main[]
 public class NumFormat2 {
-	/** A number to format */
+
 	public static final double data[] = {
 		0, 1, 22d/7, 100.2345678
 	};
 
-	/** The main (and only) method in this class. */
 	public static void main(String[] av) { 
 		// Get a format instance
 		NumberFormat form = NumberFormat.getInstance();
@@ -23,9 +22,9 @@ public class NumFormat2 {
 		form.setMaximumFractionDigits(4);
 
 		// Now print using it.
-		for (int i=0; i<data.length; i++)
-			System.out.println(data[i] + "\tformats as " +
-				form.format(data[i]));
+		for (double d : data) {
+			System.out.println(
+				d + "\tformats as " + form.format(d));
 	}
 }
 // end::main[]
