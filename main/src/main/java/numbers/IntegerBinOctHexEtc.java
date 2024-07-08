e3e0205b948336bf394dbfe5bce365f8bdf0cd0a
Ian Darwin <ian@darwinsys.com>
2024-07-08 17:09:00 -0400

    Use parseInt not valueOf

--- main/src/main/java/numbers/IntegerBinOctHexEtc.java
+++ main/src/main/java/numbers/IntegerBinOctHexEtc.java
@@ -6,14 +6,14 @@ package numbers;
  * @author Ian F. Darwin, https://darwinsys.com/
  */
 public class IntegerBinOctHexEtc {
-	public static void main(String[] argv) {
+	public static void main(String[] argv) { 
 		// tag::main[]
 		String input = "101010";
 		int i = 42;
 		System.out.println(input + " in default toString() is " + Integer.toString(i));
 		for (int radix : new int[] { 2, 8, 10, 16, 36 }) {
 			System.out.print(input + " in base " + radix + " is "
-					+ Integer.valueOf(input, radix) + "; ");
+					+ Integer.parseInt(input, radix) + "; ");
 			System.out.println(i + " formatted in base " + radix + " is "
 					+ Integer.toString(i, radix));
 		}
