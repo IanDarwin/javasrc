1da6cdafc6cbf87583dfe92b412c3d26b373b018
Ian Darwin <ian@darwinsys.com>
2024-07-11 12:13:33 -0400

    Formatting/tab-alignment

--- main/src/main/java/structure/ArraysEqualsCompareToString.java
+++ main/src/main/java/structure/ArraysEqualsCompareToString.java
@@ -7,15 +7,19 @@ import java.util.Arrays;
 public class ArraysEqualsCompareToString {
 
     void main() {
-// end::main[]
+        // end::main[]
         System.out.println("Permute char value");
         char[] ca = { 'a', 'b', 'c'},
                cb = { 'a', 'b', 'c'};
-        System.out.println("ca = " + Arrays.toString(ca) + ", cb = " + Arrays.toString(cb));
+        System.out.println(
+            "ca = " + Arrays.toString(ca) + ", +
+            "cb = " + Arrays.toString(cb));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ca, cb));
         System.out.println("compare(ca,cb) returns " + Arrays.compare(ca, cb));
         cb[2] = 'd';
-        System.out.println("ca = " + Arrays.toString(ca) + ", cb = " + Arrays.toString(cb));
+        System.out.println(
+            "ca = " + Arrays.toString(ca) + ", + 
+            "cb = " + Arrays.toString(cb));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ca, cb));
         System.out.println("compare(ca,cb) returns " + Arrays.compare(ca, cb));
 
@@ -26,14 +30,14 @@ public class ArraysEqualsCompareToString {
         int[] ia = { 1, 2, 3, 4, 5},
               ib = { 1, 2, 3, 4, 5};
         System.out.println(
-			"ia = " + Arrays.toString(ia) +
-			"ib = " + Arrays.toString(ib));
+            "ia = " + Arrays.toString(ia) +
+            "ib = " + Arrays.toString(ib));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ia, ib));
         System.out.println("compare(ia,ib) returns " + Arrays.compare(ia, ib));
         ia[4] = 6;
         System.out.println(
-			"ia = " + Arrays.toString(ia) +
-			"ib = " + Arrays.toString(ib));
+            "ia = " + Arrays.toString(ia) + ", "
+            "ib = " + Arrays.toString(ib));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ia, ib));
         System.out.println("compare(ia,ib) returns " + Arrays.compare(ia, ib));
         // end::main[]
@@ -43,13 +47,18 @@ public class ArraysEqualsCompareToString {
         System.out.println("Permute order");
         ia = new int[]{ 1, 2, 3, 4, 5};
         ib = new int[]{ 1, 2, 3, 4, 5};
-        System.out.println("ia = " + Arrays.toString(ia) + ", ib = " + Arrays.toString(ib));
+        System.out.println(
+            "ia = " + Arrays.toString(ia) + ", " +
+            "ib = " + Arrays.toString(ib));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ia, ib));
         System.out.println("compare(ia,ib) returns " + Arrays.compare(ia, ib));
         ia[4] = 4;
         ia[3] = 5;
-        System.out.println("ia = " + Arrays.toString(ia) + ", ib = " + Arrays.toString(ib));
+        System.out.println(
+            "ia = " + Arrays.toString(ia) +  ", "
+            "ib = " + Arrays.toString(ib));
         System.out.println("By Arrays.equals(): " + Arrays.equals(ia, ib));
         System.out.println("compare(ia,ib) returns " + Arrays.compare(ia, ib));
     }
+    // end::main[]
 }
