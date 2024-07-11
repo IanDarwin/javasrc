9123a0eb8d1ea908e88a107325f97a3dff095c36
Ian Darwin <ian@darwinsys.com>
2024-07-11 09:44:07 -0400

    Reformat record and println

--- main/src/main/java/structure/ArraysToString.java
+++ main/src/main/java/structure/ArraysToString.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
 void main() {
 // tag::main[]
-record Cat(String name){ public String toString() { return "Meower(" + name + ")";}}
+record Cat(String name){
+	public String toString() { return "Meower(" + name + ")";}
+}
 Cat[] pets = new Cat[]{new Cat("Tom"), new Cat("Slinky")};
-System.out.println("Pets are" + Arrays.toString(pets));
+System.out.println("Pets are " + Arrays.toString(pets));
 // end::main[]
 }
