5acb73244c7606412815929dd0e8144588d3dac5
Ian Darwin <ian@darwinsys.com>
2024-07-22 18:20:05 -0400

    Whitespace repair

--- json/src/main/java/json/ReadWriteJsonB.java
+++ json/src/main/java/json/ReadWriteJsonB.java
@@ -10,18 +10,18 @@ import domain.Person;
 // tag::main[]
 public class ReadWriteJsonB {
 
-    public static void main(String[] args) throws IOException {
+	 public static void main(String[] args) throws IOException {
 
 		Jsonb jsonb = JsonbBuilder.create();			// <1>
 
 		// Read
-        String jsonInput =								// <2>
-        		"{\"id\":0,\"firstName\":\"Robin\",\"lastName\":\"Williams\"}";
-        Person rw = jsonb.fromJson(jsonInput, Person.class);
+		String jsonInput =								// <2>
+			"{\"id\":0,\"firstName\":\"Robin\",\"lastName\":\"Williams\"}";
+		Person rw = jsonb.fromJson(jsonInput, Person.class);
 		System.out.println(rw);
 
 		String result = jsonb.toJson(rw);				// <3>
 		System.out.println(result);
-    }
+	 }
 }
 // end::main[]
