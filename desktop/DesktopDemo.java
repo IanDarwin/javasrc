348f78cd0a3c5ed2b88b4f1df3b6f375ca108408
Ian Darwin <ian@darwinsys.com>
2024-08-04 13:23:35 -0400

    Show a few more bits

--- desktop/DesktopDemo.java
+++ desktop/DesktopDemo.java
@@ -2,23 +2,36 @@ package desktop;
 
 import java.awt.Desktop;
 import java.net.URI;
+import java.io.File;
 
 public class DesktopDemo {
 
 	public static void main(String[] args) throws Exception {
 		Desktop desktop = null;
-        if (Desktop.isDesktopSupported()) {
-            desktop = Desktop.getDesktop();
-        } else {
-            System.err.println("no desktop support");
-            return;
-        }
+		if (Desktop.isDesktopSupported()) {
+			desktop = Desktop.getDesktop();
+		} else {
+			System.err.println("No desktop support");
+			return;
+		}
 
-        if (desktop.isSupported(Desktop.Action.BROWSE)) {
-            desktop.browse(new URI("https://darwinsys.com/"));
-        } else {
-        	System.err.println("no browser support");
-        }
+		if (desktop.isSupported(Desktop.Action.BROWSE)) {
+			desktop.browse(new URI("https://darwinsys.com/"));
+		} else {
+			System.err.println("No browser support");
+		}
+
+		if (desktop.isSupported(Desktop.Action.PRINT)) {
+			desktop.print(new File(System.getProperty("user.dir") + ".profile"));
+		} else {
+			System.err.println("No Print support");
+		}
+
+		if (desktop.isSupported(Desktop.Action.APP_HELP_VIEWER)) {
+			desktop.openHelpViewer();
+		} else {
+			System.err.println("No Help support");
+		}
 
 	}
 
