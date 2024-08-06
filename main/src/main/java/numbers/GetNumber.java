b5bf747f9d3930dfdd87b48299ccf3f6d8810309
Ian Darwin <ian@darwinsys.com>
2024-08-06 17:22:09 -0400

    Simplify to CLI program, provide demo args, print better

--- main/src/main/java/numbers/GetNumber.java
+++ main/src/main/java/numbers/GetNumber.java
@@ -1,69 +1,50 @@
 package numbers;
 
-import java.awt.BorderLayout;
-import java.awt.Frame;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /**
  * GetNumber - program to determine if a number is float or int.
  *
  * @author Ian Darwin, https://darwinsys.com/
  */
-public class GetNumber extends Frame {
-
-	private static final long serialVersionUID = 1L;
-	/** The input textField */
-	private TextField textField;
-	/** The results area */
-	private TextField statusLabel;
-
-	/** Constructor: set up the GUI */
-	public GetNumber() {
-		Panel p = new Panel();
-		p.add(new Label("Number:"));
-		p.add(textField = new TextField(10));
-		add(BorderLayout.NORTH, p);
-		textField.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ev) {
-				String s = textField.getText();
-				statusLabel.setText(process(s).toString());
-			}
-		});
-		add(BorderLayout.SOUTH, statusLabel = new TextField(10));
-		pack();
-	}
+public class GetNumber {
 
 	// tag::main[]
 	/*
 	 * Process one String, returning it as a Number subclass
 	 */
-	public static Number process(String s) {
+	public static void process(String s) {
 		if (s.matches("[+-]*\\d*\\.\\d+[dDeEfF]*")) {
 			try {
 				double dValue = Double.parseDouble(s);
-				System.out.println("It's a double: " + dValue);
-				return Double.valueOf(dValue);
+				System.out.println(s + ": is a double: " + dValue +
+					" by Double.parseDouble()");
 			} catch (NumberFormatException e) {
-				System.out.println("Invalid double: " + s);
-				return Double.NaN;
+				System.out.println(s + ": Invalid double: " + s);
+				return;
 			}
-		} else // did not contain . d e or f, so try as int.
-			try {
+		} else try {
+			// did not contain . d e or f, so try as int.
 				int iValue = Integer.parseInt(s);
-				System.out.println("It's an int: " + iValue);
-				return Integer.valueOf(iValue);
+				System.out.println(s + ": is an int: " + iValue +
+					" by Integer.parseInt()");
 			} catch (NumberFormatException e2) {
-				System.out.println("Not a number: " + s);
-				return Double.NaN;
+				System.out.println(s + ": Not a number: " + s);
+				return;
 			}
+		Double d = Double.valueOf(s);
+		System.out.println(s + ": is " + (d.intValue() == d.doubleValue() ?
+			"an integer" : "a floating-point number") +
+			" by Double.xxxValue() comparison");
 	}
 	// end::main[]
 
-	public static void main(String[] ap) {
-		new GetNumber().setVisible(true);
+	final static String[] demoArgs = { "156", "156.25", "23 skidoo" };
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			args = demoArgs;
+		}
+		for (String s : args) {
+			process(s);
+		}
 	}
 }
