7d8008273f19dd605d6ea8c090a035276792d61d
Ian Darwin <ian@darwinsys.com>
2024-08-06 16:55:46 -0400

    Add another data point

--- main/src/main/java/numbers/RealValues.java
+++ main/src/main/java/numbers/RealValues.java
@@ -8,5 +8,6 @@ public class RealValues {
 	public static void main(String[] argv) {
 		System.out.println("The real 0.3 is " + 0.3);
 		System.out.println("The real 0.3 times 3 is " + 0.3 * 3);
+		System.out.println("The real 3.0 times 3 is " + 3.0 * 3);
 	}
 }
