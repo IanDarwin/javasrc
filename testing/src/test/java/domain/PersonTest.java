ee2e5a7641953dda20523faa0effa32b58d1ba51
Ian Darwin <ian@darwinsys.com>
2024-08-06 11:44:24 -0400

    Move PersonTest to domain package, remove import

--- testing/src/test/java/domain/PersonTest.java
+++ testing/src/test/java/domain/PersonTest.java
@@ -1,11 +1,9 @@
-package testing;
+package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import domain.Person;
 
 /** A simple test case for Person */
 // tag::main[]
