f3588c0a8bd54829fd2c5bd7cd1b306b24bdbee6
Ian Darwin <ian@darwinsys.com>
2024-10-06 14:04:27 -0400

    break long line

--- main/src/main/java/di/cdi/MainAndController.java
+++ main/src/main/java/di/cdi/MainAndController.java
@@ -7,7 +7,8 @@ import org.jboss.weld.environment.se.Weld;
 // tag::main[]
 public class MainAndController {
     public static void main(String[] args) {
-        final Instance<Object> weldInstance = new Weld().initialize().instance();
+        final Instance<Object> weldInstance = 
+			new Weld().initialize().instance();
         weldInstance.select(ConsoleViewer.class).get().displayMessage();
     }
 }
