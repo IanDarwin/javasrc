1033564b1817fa4f597ecd16cd2e951e78dc4554
Ian Darwin <ian@darwinsys.com>
2024-10-09 13:54:15 -0400

    AutoSave 'extends Thread' -> 'implements Runnable'

--- main/src/main/java/threads/AutoSave.java
+++ main/src/main/java/threads/AutoSave.java
@@ -1,52 +1,13 @@
 package threads;
 
-/*
- * Copyright (c) Ian F. Darwin, https://darwinsys.com/, 1996-2002.
- * All rights reserved. Software written by Ian F. Darwin and others.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by Ian F. Darwin.
- * 4. Neither the name of the author nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * 
- * Java, the Duke mascot, and all variants of Sun's Java "steaming coffee
- * cup" logo are trademarks of Sun Microsystems. Sun's, and James Gosling's,
- * pioneering role in inventing and promulgating (and standardizing) the Java 
- * language and environment is gratefully acknowledged.
- * 
- * The pioneering role of Dennis Ritchie and Bjarne Stroustrup, of AT&T, for
- * inventing predecessor languages C and C++ is also gratefully acknowledged.
- */
-
 // import - none
 
-/** Demonstration of using a Thread to automatically save
+/*
+ * Demonstration of using a Runnable to automatically save
  * the user's work periodically.
  */
 // tag::main[]
-public class AutoSave extends Thread {
+public class AutoSave implements Runnable {
 	/** The FileSave interface is implemented by the main class. */
 	protected FileSaver model;
 	/** How long to sleep between tries */
@@ -54,7 +15,7 @@ public class AutoSave extends Thread {
 	private static final int SECONDS = MINUTES * 60;
 
 	public AutoSave(FileSaver m) {
-		super("AutoSave Thread");
+		Thread.currentThread().setName("AutoSave Thread");
 		setDaemon(true);		// so we don't keep the main app alive
 		model = m;
 	}
