a32f0f27f7f822e202b3de29ad7cccd38ffbaf05
Ian Darwin <ian@darwinsys.com>
2024-10-10 11:01:12 -0400

    Rounding explained

--- main/src/main/java/numbers/Rounding.java
+++ main/src/main/java/numbers/Rounding.java
@@ -3,8 +3,11 @@ void main() {
 	/** This shows a rounding error */
 	System.out.println(0.3 * 3);
 	System.out.println(3.0 * 3);
-	/** Printf %f hides the rounding error by rounding off(?) */
+	/** Printf %f hides the rounding error by rounding it away */
 	System.out.printf("%f\n", 0.3 * 3);
 	System.out.printf("%f\n", 3.0 * 3);
+	/** With enough digits, printf %f shows the rounding error */
+	System.out.printf("%22.20f\n", 0.3 * 3);
+	System.out.printf("%22.20f\n", 3.0 * 3);
 }
 
