a5189181cce345fb11ba9605864979b1a767d4c4
Ian Darwin <ian@darwinsys.com>
2024-10-13 19:10:41 -0400

    Finish the getResourceAsStream demo

--- main/src/main/java/io/GetResourceDemo.java
+++ main/src/main/java/io/GetResourceDemo.java
@@ -1,3 +1,4 @@
+// tag::main[]
 package io;
 
 import java.io.*;
@@ -7,18 +8,24 @@ public class GetResourceDemo {
 
 	public static void main(String[] args) throws Exception {
 		Class<?> c = GetResourceDemo.class;
-		System.out.println("Class is " + c.getName());
-		InputStream isOne = c.getResourceAsStream("/getresourcedemo1.txt");
-		System.out.println("InputStream One = " + isOne);
-		displayContents(isOne);
 
+		// By file name
+		System.out.println("Class is " + c.getName());
+		final String pathName1 = "/getresourcedemo1.txt";
+		System.out.println("pathName1 = " + pathName1);
+		InputStream isOne = c.getResourceAsStream(pathName1);
+		System.out.println("InputStream One = " + isOne);
+		displayContents(isOne); // simple convenience routing
+
+		// By package-relative name
 		final String name = c.getPackage().getName();
 		final String fullPathName = "/" + name.replace('.', '/') + "/" + MY_CONFIG_FILE;
-		System.out.println(fullPathName);
+		System.out.println("pathName2 = " + fullPathName);
 		InputStream isTwo = c.getResourceAsStream(fullPathName);
 		System.out.println("InputStream Two = " + isTwo);
 		displayContents(isTwo);
 	}
+	// end::main[]
 
 	static void displayContents(InputStream is) throws IOException {
 		if (is == null) {
