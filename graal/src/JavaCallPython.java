a9c353897e285dded8de34164920c403f684c9e0
Ian Darwin <ian@darwinsys.com>
2024-10-15 09:54:11 -0400

    Move adoc tag to include imports

--- graal/src/JavaCallPython.java
+++ graal/src/JavaCallPython.java
@@ -1,14 +1,15 @@
+// tag::main[]
 import java.io.*;
 import java.util.stream.*;
-import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 
 /**
  * GraalVM polyglot: calling Python from Java/
  */
-// tag::main[]
 public class JavaCallPython {
 
-	public static void main(String[] args) throws java.io.IOException {
+	public static void main(String[] args) throws IOException {
 
 		 try (Context context = Context.create("jython")) {
 			Value result = context.execute("2 + 2");
