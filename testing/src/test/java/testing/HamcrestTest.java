32b8955f495f206ecafbe7f569e443a756dc0b6a
Ian Darwin <ian@darwinsys.com>
2024-10-16 09:52:04 -0400

    Make demo runnable; comment assertAll operation.

--- testing/src/test/java/testing/HamcrestTest.java
+++ testing/src/test/java/testing/HamcrestTest.java
@@ -6,12 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // tag::main[]
-public class HamcrestDemo {
+/// This is really a Demo, not a Test...
+public class HamcrestTest {
 
 	@Test
 	public void showGroupAssertions() {
 		assertAll("Assert tests in a group",
-			() -> assertEquals(2,3), // Guaranteed to fail, first, see if others run.
+		//	() -> assertEquals(2,3), // Guaranteed to fail, first: see if others run.
+		//	() -> assertEquals(3,1+1), // Will also run if uncommented, and fail
 			() -> assertEquals(2,1+1),
 			() -> assertEquals("Hi", "Highlighter".substring(0,2))
 		);
