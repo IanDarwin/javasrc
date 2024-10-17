10cfe6d430d4f64c0067f9dd384b140a65ec58e1
Ian Darwin <ian@darwinsys.com>
2024-10-16 20:13:52 -0400

    Wrap long lines

--- main/src/main/java/dir_file/WalkTreeDemo.java
+++ main/src/main/java/dir_file/WalkTreeDemo.java
@@ -14,7 +14,7 @@ import java.text.MessageFormat;
  */
 // tag::main[]
 public class WalkTreeDemo {
-    
+
     public static void main(String[] args) throws IOException {
         String dir = args.length == 0 ? "." : args[0];
         Files.walkFileTree(Path.of(dir), myVisitor);
@@ -22,25 +22,29 @@ public class WalkTreeDemo {
 
     static final FileVisitor myVisitor = new FileVisitor() {
         @Override
-        public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
+        public FileVisitResult preVisitDirectory(Object dir,
+			BasicFileAttributes attrs) throws IOException {
             System.out.println("Starting Directory " + dir);
             return FileVisitResult.CONTINUE;
         }
 
         @Override
-        public FileVisitResult postVisitDirectory(Object dir, IOException exc) throws IOException {
+        public FileVisitResult postVisitDirectory(Object dir,
+			IOException exc) throws IOException {
             System.out.println("Finished Directory " + dir + " " + (exc != null? exc : ""));
             return FileVisitResult.CONTINUE;
         }
 
         @Override
-        public FileVisitResult visitFile(Object file, BasicFileAttributes attrs) throws IOException {
+        public FileVisitResult visitFile(Object file,
+			BasicFileAttributes attrs) throws IOException {
             System.out.println("Visiting File " + file);
             return FileVisitResult.CONTINUE;
         }
 
         @Override
-        public FileVisitResult visitFileFailed(Object file, IOException exc) throws IOException {
+        public FileVisitResult visitFileFailed(Object file,
+			IOException exc) throws IOException {
             System.out.println(MessageFormat.format("FAILURE visiting {0} ({1})", file, exc));
             return FileVisitResult.CONTINUE;
         }
